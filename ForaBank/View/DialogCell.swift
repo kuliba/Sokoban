3e5aded9b051cfe8b774ddd242e55733ae8553a1
IM <>
2018-09-28 18:08:46 +0400

    Add dialog cell

---
+++ ForaBank/View/DialogCell.swift
@@ -0,0 +1,16 @@
+//
+//  DialogCell.swift
+//  ForaBank
+//
+//  Created by Ilya Masalov (xmasalov@gmail.com) on 27/09/2018.
+//  Copyright © 2018 BraveRobin. All rights reserved.
+//
+
+import UIKit
+
+class DialogCell: UITableViewCell {
+
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userPicImageView: CircularImageView!
+    @IBOutlet weak var messageLabel: UILabel!
+}
