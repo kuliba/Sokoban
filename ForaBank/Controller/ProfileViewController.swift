eb4fcdc90affafec9aaee4a7a59baaafbabbba4e
IM <>
2018-09-28 18:13:08 +0400

    Add profile view controller

---
+++ ForaBank/Controller/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  ForaBank
+//
+//  Created by Ilya Masalov (xmasalov@gmail.com) on 26/09/2018.
+//  Copyright © 2018 BraveRobin. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+}
