a8272e2231786203651175ea7a6f56611ad0c2dc
IM <>
2018-09-28 18:12:18 +0400

    Add chat dialogs table view

---
+++ ForaBank/View/ChatDialogsTableView.swift
@@ -0,0 +1,31 @@
+//
+//  ChatDialogsTableView.swift
+//  ForaBank
+//
+//  Created by Ilya Masalov (xmasalov@gmail.com) on 28/09/2018.
+//  Copyright © 2018 BraveRobin. All rights reserved.
+//
+
+import UIKit
+
+class ChatDialogsTableView: UITableView {
+    
+    // MARK: - Lifecycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setContentInset()
+        removeExtraEmptyCells()
+    }
+}
+
+// MARK: - Private methods
+private extension ChatDialogsTableView {
+    
+    func setContentInset() {
+        contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+    }
+    
+    func removeExtraEmptyCells() {
+        tableFooterView = UIView()
+    }
+}
