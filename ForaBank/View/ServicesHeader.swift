aaace5976f19d428af8ad9cec7dcc2e69e9090dd
IM <>
2018-10-02 10:49:29 +0400

    Add services search, cell and header

---
+++ ForaBank/View/ServicesHeader.swift
@@ -0,0 +1,15 @@
+//
+//  ServicesHeader.swift
+//  ForaBank
+//
+//  Created by Ilya Masalov (xmasalov@gmail.com) on 01/10/2018.
+//  Copyright © 2018 BraveRobin. All rights reserved.
+//
+
+import UIKit
+
+class ServicesHeader: UIView {
+
+    // MARK: - Properties
+    @IBOutlet weak var titleLabel: UILabel!
+}
