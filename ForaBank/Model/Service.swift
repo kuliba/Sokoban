038e5f5a71208b84baae6d7430c9796e47d9d20d
IM <>
2018-10-02 10:50:22 +0400

    Add Service model

---
+++ ForaBank/Model/Service.swift
@@ -0,0 +1,14 @@
+//
+//  Service.swift
+//  ForaBank
+//
+//  Created by Ilya Masalov (xmasalov@gmail.com) on 01/10/2018.
+//  Copyright © 2018 BraveRobin. All rights reserved.
+//
+
+struct Service {
+    let name: String
+    let description: String
+    let iconName: String
+    let isPartner: Bool
+}
