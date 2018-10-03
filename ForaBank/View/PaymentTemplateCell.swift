3918f6f853faaed2b419fdb66be43151ff1cd634
IM <>
2018-10-03 10:35:02 +0400

    Add payment cells

---
+++ ForaBank/View/PaymentTemplateCell.swift
@@ -0,0 +1,19 @@
+//
+//  PaymentTemplateCell.swift
+//  ForaBank
+//
+//  Created by IM on 02/10/2018.
+//  Copyright © 2018 BraveRobin. All rights reserved.
+//
+
+import UIKit
+
+class PaymentTemplateCell: UITableViewCell {
+
+    // MARK: - Properties
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subTitleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var bottomSeparatorView: UIView!
+}
