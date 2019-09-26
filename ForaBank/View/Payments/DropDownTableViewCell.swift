ae1d3e9bbfc0b180dacfdecf4ee6b4981d5f62dc
Vladimir Boyko <Vboyko024@gmail.com>
2019-09-26 11:32:49 +0300

    DropDownCell created

---
+++ ForaBank/View/Payments/DropDownTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DropDownTableViewCell.swift
+//  ForaBank
+//
+//  Created by Бойко Владимир on 26/09/2019.
+//  Copyright © 2019 (C) 2017-2019 OОО "Бриг Инвест". All rights reserved.
+//
+
+import UIKit
+
+class DropDownTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
