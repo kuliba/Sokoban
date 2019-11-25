8696b5c6b51f34f264c6cc07cc89ed2a687b70e9
Dmitriy <da.savushkin4@gmail.com>
2019-11-25 16:12:23 +0300

    fix contactButton
    fix contactButton

--- ForaBank/PresentationLayer/View/Pager/PagerCells/TextFieldPagerViewCell.swift
+++ ForaBank/PresentationLayer/View/Pager/PagerCells/TextFieldPagerViewCell.swift
@@ -11,6 +11,7 @@ import FSPagerView
 
 class TextFieldPagerViewCell: FSPagerViewCell, IConfigurableCell, ContactsPickerDelegate {
 
+    @IBOutlet weak var buttonContactList: UIButton!
     @IBOutlet weak var leftButton: UIButton!
     @IBOutlet weak var textField: UITextField!
 
