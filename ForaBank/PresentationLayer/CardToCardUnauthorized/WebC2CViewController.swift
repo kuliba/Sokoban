2975b0549cd1d59c228b652c0dd624ce0c638473
Dmitriy <da.savushkin4@gmail.com>
2019-12-03 18:37:12 +0300

    С2СBest2Pay and MainScreen Index save
    С2СBest2Pay and MainScreen Index save

--- ForaBank/PresentationLayer/CardToCardUnauthorized/WebC2CViewController.swift
+++ ForaBank/PresentationLayer/CardToCardUnauthorized/WebC2CViewController.swift
@@ -9,10 +9,6 @@
 import UIKit
 import WebKit
 import Alamofire
-import Foundation
-import var CommonCrypto.CC_MD5_DIGEST_LENGTH
-import func CommonCrypto.CC_MD5
-import typealias CommonCrypto.CC_LONG
 
 class WebC2CViewController: UIViewController {
     @IBOutlet weak var backButton: UIButton!
@@ -21,62 +17,7 @@ class WebC2CViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-        
-        func MD5(string: String) -> Data {
-                let length = Int(CC_MD5_DIGEST_LENGTH)
-                let messageData = string.data(using:.utf8)!
-                var digestData = Data(count: length)
-
-                _ = digestData.withUnsafeMutableBytes { digestBytes -> UInt8 in
-                    messageData.withUnsafeBytes { messageBytes -> UInt8 in
-                        if let messageBytesBaseAddress = messageBytes.baseAddress, let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress {
-                            let messageLength = CC_LONG(messageData.count)
-                            CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
-                        }
-                        return 0
-                    }
-                }
-                return digestData
-            }
-
-        
-//        let sector =  "1009"
-//        let amount = "1000"
-//        let pan1 =  "4656260150230695"
-//        let pan2 =  "4809388889655340"
-//        let currency = "643"
-//        let cvc =  "314"
-//        let month =  "07"
-//        let year =  "2024"
-//        let fee =  "7000"
-//        let signature =  String()
-        
-        
-        let mdd5Plus = "1009" + "2200200111114591" + "05" + "2022" + "426" + "4809388889655340" + "1000" + "643"
-
-        let mdd5Plus2 = "1009" + "1000" + "643" + "test"
-        let MD5str = "1" + "100" + "643" + "test"
-        
-        
-        
-
-        //Test:
-        let md5Data = MD5(string: mdd5Plus2)
-
-        let md5Hex =  md5Data.map { String(format: "%02hhx", $0) }.joined()
-        print(md5Hex)
-        
-        let  md5Base64 = md5Data.base64EncodedString()
-        print("md5Base64: \(md5Base64)")
-        let normHex = md5Hex.base64Encoded()
-        print(normHex)
-        let purchase = "1009"+"565668"+"test"
-        let purchaseMD5 = MD5(string: purchase)
-       let md5Purchase =  purchaseMD5.map { String(format: "%02hhx", $0) }.joined()
-     let purchaseBase64 =  md5Purchase.base64Encoded()
-        print("PURCHASE \(purchaseBase64)")
+     
         let headers = [
                           "Content-Type": "application/x-www-form-urlencoded"
                       ]
@@ -106,7 +47,9 @@ class WebC2CViewController: UIViewController {
                           let data = response.request
                       }
         
-        let urlP2P = URL(string: "https://test.best2pay.net/webapi/P2PTransfer?sector=1009&signature=90d058fe1259aecf1f564c9d3a6a25c1&action=pay")
+        let url = URL(string: "https://www.google.com")
+        let request = URLRequest(url: url!)
+        let urlP2P = URL(string: "https://test.best2pay.net/webapi/P2PTransfer?sector=1009&apan2=4656260150230695")
          let requestP2P = URLRequest(url: urlP2P!)
             
             
@@ -132,15 +75,4 @@ class WebC2CViewController: UIViewController {
     }
     */
 
-}
-
-extension String {
-    func base64Encoded() -> String? {
-        return data(using: .utf8)?.base64EncodedString()
-    }
-
-    func base64Decoded() -> String? {
-        guard let data = Data(base64Encoded: self) else { return nil }
-        return String(data: data, encoding: .utf8)
-    }
 }
