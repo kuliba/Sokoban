9ca41764467c9852ddf1d4b5791cefa7b97bc2d6
Vladimir Boyko <Vboyko024@gmail.com>
2019-12-10 15:26:18 +0300

    Init created

--- ForaBank/PresentationLayer/Controllers/Passcode/PasscodeViewController.swift
+++ ForaBank/PresentationLayer/Controllers/Passcode/PasscodeViewController.swift
@@ -10,6 +10,17 @@ import Foundation
 import TOPasscodeViewController
 
 class PasscodeViewController: TOPasscodeViewController {
+
+    let rightTitle: String
+    init(rightTitle: String, style: TOPasscodeViewStyle, passcodeType: TOPasscodeType) {
+        self.rightTitle = rightTitle
+        super.init(style: style, passcodeType: passcodeType)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func keypadButtonTapped() {
         guard let count = self.passcodeView.passcode?.count else { return }
         let title = count > 0 ? "Delete" : "Cancel1"
@@ -26,10 +37,15 @@ class PasscodeViewController: TOPasscodeViewController {
 
     override func optionsCodeButtonTapped() {
         super.optionsCodeButtonTapped()
-        
-        if (!(self.rightAccessoryButton != nil) && !self.cancelButton) {
-            
-//            [self.cancelButton setTitle:NSLocalizedString(@"Cancel", @"Cancel") forState:UIControlStateNormal];
+
+        if (self.rightAccessoryButton == nil && self.cancelButton == nil) {
+            guard let count = self.passcodeView.passcode?.count else { return }
+            let title = count > 0 ? "Delete" : "Cancel1"
+
+            UIView.performWithoutAnimation {
+                self.cancelButton.setTitle(NSLocalizedString(title, comment: title), for: .normal)
+                self.cancelButton.sizeToFit()
+            }
         }
     }
 }
