81786e3c017562ce18bfe2a570bc0e3917397c6f
AlekseyK <ak@briginvest.ru>
2020-03-05 14:35:16 +0300

    30% DBSNEW-402

---
+++ ForaBank/BusinessLogicLayer/Network/API/MapService.swift
@@ -0,0 +1,9 @@
+//
+//  MapService.swift
+//  ForaBank
+//
+//  Created by  Карпежников Алексей  on 05.03.2020.
+//  Copyright © 2020 (C) 2017-2019 OОО "Бриг Инвест". All rights reserved.
+//
+
+import Foundation
