ccd686214cb33e59a2ba372c68f2f9aa70d16859
Михаил Колотилин <kolotilin@inn4b.ru>
2021-05-27 17:19:41 +0300

    start new Project

---
+++ ForaBank/ForaBank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForaBank
+//
+//  Created by Mikhail on 27.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
