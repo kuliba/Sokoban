6bc839b0597f7e00ab58d6227bceb51a0f7be247
Konstantin <ksavyalov@gmail.com>
2021-07-08 01:03:18 +0300

    Добавил окно выбора карт, если их количество больше 7

---
+++ ForaBank/ForaBank/Controllers/Views/CustomCardListView/CustomCardListCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomCardListCell.swift
+//  ForaBank
+//
+//  Created by Константин Савялов on 08.07.2021.
+//
+
+import UIKit
+
+class CustomCardListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
