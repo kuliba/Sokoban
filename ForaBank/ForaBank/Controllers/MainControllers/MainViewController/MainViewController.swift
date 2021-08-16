ba07c4e627f6f20b8c351e0045f4213b48982da4
Dmitriy <da.savushkin4@gmail.com>
2021-08-16 16:52:02 +0400

    mainScreen
    mainScreen

---
+++ ForaBank/ForaBank/Controllers/MainControllers/MainViewController/MainViewController.swift
@@ -0,0 +1,216 @@
+//
+//  MainViewController.swift
+//  ForaBank
+//
+//  Created by Дмитрий on 11.08.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+        
+        var payments = [PaymentsModel]() {
+            didSet {
+                DispatchQueue.main.async {
+                    self.reloadData(with: nil)
+                }
+            }
+        }
+        var productList = [GetProductListDatum]()
+        var transfers = [PaymentsModel]()
+        var pay = [PaymentsModel]()
+        var offer = [PaymentsModel]()
+    
+        enum Section: Int, CaseIterable {
+            case payments, transfers, pay, offer
+            func description() -> String {
+                switch self {
+                case .payments:
+                    return "Мои продукты"
+                case .transfers:
+                    return "Быстрые операции"
+                case .offer:
+                    return ""
+                case .pay:
+                    return "Обмен валют"
+                }
+            }
+        }
+        
+        var collectionView: UICollectionView!
+        var dataSource: UICollectionViewDiffableDataSource<Section, PaymentsModel>?
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            view.backgroundColor = .white
+            setupData()
+            setupSearchBar()
+            setupCollectionView()
+            createDataSource()
+            reloadData(with: nil)
+            loadLastPhonePayments()
+            loadLastPayments()
+        }
+    
+        func getCardList(completion: @escaping (_ cardList: [GetProductListDatum]?,_ error: String?)->()) {
+        
+        let param = ["isCard": "true", "isAccount": "true", "isDeposit": "false", "isLoan": "false"]
+        
+        NetworkManager<GetProductListDecodableModel>.addRequest(.getProductListByFilter, param, [:]) { model, error in
+            if error != nil {
+                completion(nil, error)
+            }
+            guard let model = model else { return }
+            if model.statusCode == 0 {
+                guard let cardList = model.data else { return }
+                completion(cardList, nil)
+            } else {
+                guard let error = model.errorMessage else { return }
+                completion(nil, error)
+            }
+        }
+        
+        }
+        
+        func setupData() {
+            payments = MockItems.returnPayments()
+            getCardList { data, errorMessage in
+                self.transfers = [PaymentsModel(productList: (data?[0])!)]
+            }
+            offer = MockItems.returnPay()
+            transfers = MockItems.returnTransfers()
+            pay = MockItems.returnPay()
+        }
+        
+        private func setupSearchBar() {
+            navigationController?.navigationBar.barTintColor = .white
+            navigationController?.navigationBar.backgroundColor = .white
+            navigationController?.navigationBar.shadowImage = UIImage()
+            let searchController = UISearchController(searchResultsController: nil)
+            navigationItem.searchController = searchController
+            navigationItem.hidesSearchBarWhenScrolling = false
+            searchController.hidesNavigationBarDuringPresentation = true
+            searchController.obscuresBackgroundDuringPresentation = false
+            searchController.automaticallyShowsCancelButton = false
+            searchController.searchBar.delegate = self
+            searchController.searchBar.placeholder = "Название категории, ИНН"
+            searchController.searchBar.showsBookmarkButton = true
+            searchController.searchBar.setImage(UIImage(named: "scanCard")?.withTintColor(.black), for: .bookmark, state: .normal)
+            searchController.searchBar.backgroundColor = .white
+            
+        }
+        
+        private func setupCollectionView() {
+            collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createCompositionLayout())
+            collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            collectionView.backgroundColor = .white
+            view.backgroundColor = .white
+            view.addSubview(collectionView)
+            
+            collectionView.register(SectionHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SectionHeader.reuseId)
+            
+            collectionView.register(OrderProductsCollectionViewCell.self, forCellWithReuseIdentifier: OrderProductsCollectionViewCell.reuseId)
+            collectionView.register(CurrencyExchangeCollectionViewCell.self, forCellWithReuseIdentifier: CurrencyExchangeCollectionViewCell.reuseId)
+            collectionView.register(PaymentsCell.self, forCellWithReuseIdentifier: PaymentsCell.reuseId)
+            collectionView.register(ProductCell.self, forCellWithReuseIdentifier: ProductCell.reuseId)
+            collectionView.register(PayCell.self, forCellWithReuseIdentifier: PayCell.reuseId)
+            
+            
+            collectionView.isScrollEnabled = false
+            collectionView.delegate = self
+            
+        }
+        
+        func reloadData(with searchText: String?) {
+            var snapshot = NSDiffableDataSourceSnapshot<Section, PaymentsModel>()
+            
+            snapshot.appendSections([.payments, .transfers,.offer, .pay])
+            snapshot.appendItems(payments, toSection: .payments)
+            snapshot.appendItems(transfers, toSection: .transfers)
+            snapshot.appendItems(offer, toSection: .offer)
+            snapshot.appendItems(pay, toSection: .pay)
+            
+            dataSource?.apply(snapshot, animatingDifferences: true)
+            collectionView.reloadData()
+            
+        }
+        
+
+    }
+
+    //MARK: - API
+    extension MainViewController {
+        
+        func loadLastPayments() {
+            NetworkManager<GetPaymentCountriesDecodableModel>.addRequest(.getPaymentCountries, [:], [:]) { model, error in
+                if error != nil {
+                    print("DEBUG: error", error!)
+                } else {
+                    guard let model = model else { return }
+                    guard let lastPaymentsList = model.data else { return }
+                    if lastPaymentsList.count > 3 {
+                        let payArr = lastPaymentsList.prefix(3)
+                        payArr.forEach { lastPayment in
+                            let mod = ChooseCountryHeaderViewModel(model: lastPayment)
+                            let payment = PaymentsModel(lastCountryPayment: mod)
+                            self.payments.append(payment)
+                        }
+                    } else {
+                        lastPaymentsList.forEach { lastPayment in
+                            let mod = ChooseCountryHeaderViewModel(model: lastPayment)
+                            let payment = PaymentsModel(lastCountryPayment: mod)
+                            self.payments.append(payment)
+                        }
+                    }
+                }
+            }
+        }
+        
+        func loadLastPhonePayments() {
+            NetworkManager<GetLatestPaymentsDecodableModel>.addRequest(.getLatestPayments, [:], [:]) { model, error in
+                if error != nil {
+                    print("DEBUG: Error: ", error ?? "")
+                }
+                guard let model = model else { return }
+                print("DEBUG: LatestPayment: ", model)
+                if model.statusCode == 0 {
+                    guard let lastPaymentsList  = model.data else { return }
+                    
+                    if lastPaymentsList.count > 3 {
+                        let payArr = lastPaymentsList.prefix(3)
+                        payArr.forEach { lastPayment in
+                            let payment = PaymentsModel(lastPhonePayment: lastPayment)
+                            self.payments.append(payment)
+                        }
+                    } else {
+                        lastPaymentsList.forEach { lastPayment in
+                            let payment = PaymentsModel(lastPhonePayment: lastPayment)
+                            self.payments.append(payment)
+                        }
+                    }
+                } else {
+                    print("DEBUG: Error: ", model.errorMessage ?? "")
+                }
+            }
+        }
+        
+        
+        func getFastPaymentContractList(_ completion: @escaping (_ model: [FastPaymentContractFindListDatum]? ,_ error: String?) -> Void) {
+            NetworkManager<FastPaymentContractFindListDecodableModel>.addRequest(.fastPaymentContractFindList, [:], [:]) { model, error in
+                if error != nil {
+                    print("DEBUG: Error: ", error ?? "")
+                    completion(nil, error)
+                }
+                guard let model = model else { return }
+                print("DEBUG: fastPaymentContractFindList", model)
+                if model.statusCode == 0 {
+                    completion(model.data,nil)
+                } else {
+                    print("DEBUG: Error: ", model.errorMessage ?? "")
+                    completion(nil, model.errorMessage)
+                }
+            }
+        }
+    }
+
+
---
+++ ForaBank/ForaBank/Controllers/MainControllers/MainViewController/MainViewControllerSearchBarDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewControllerSearchBarDelegate.swift
+//  ForaBank
+//
+//  Created by Дмитрий on 11.08.2021.
+//
+
+import Foundation
+import UIKit
+
+extension MainViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        reloadData(with: searchText)
+        
+    }
+    
+    func searchBarBookmarkButtonClicked(_ searchBar: UISearchBar) {
+        print(#function)
+        
+    }
+}
