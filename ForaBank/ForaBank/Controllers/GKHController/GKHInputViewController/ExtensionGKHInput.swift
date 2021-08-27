6830313b32495630c572cd5b0acc963f03fdfe16
Konstantin <ksavyalov@gmail.com>
2021-08-27 18:46:37 +0300

    test

--- ForaBank/ForaBank/Controllers/GKHController/GKHInputViewController/ExtensionGKHInput.swift
+++ ForaBank/ForaBank/Controllers/GKHController/GKHInputViewController/ExtensionGKHInput.swift
@@ -18,26 +18,30 @@ extension GKHInputViewController: UITableViewDataSource, TableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == 3 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: GKHCardCell.reuseId, for: indexPath) as! GKHCardCell
-            return cell
-        } else {
+//        if indexPath.row == 3 {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: GKHCardCell.reuseId, for: indexPath) as! GKHCardCell
+//            return cell
+//        } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: GKHInputCell.reuseId, for: indexPath) as! GKHInputCell
             guard operatorData?.parameterList.count != 0 else { return cell }
             
             cell.setupUI((operatorData?.parameterList[indexPath.row])!)
             cell.tableViewDelegate = (self as TableViewDelegate)
             return cell
-        }
+//        }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        var hight: CGFloat = 80.0
-        if indexPath.row == 3 {
-            hight = 200
-        }
+        let hight: CGFloat = 80.0
         return hight
     }
+    
+//    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+//        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 40))
+//        footerView.backgroundColor = UIColor.red
+//
+//        return footerView
+//      }
 }
 
 extension GKHInputViewController {
