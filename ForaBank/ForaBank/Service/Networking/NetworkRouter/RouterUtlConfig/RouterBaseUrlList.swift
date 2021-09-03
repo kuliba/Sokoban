8af1c7ed004a188a4e1938cb9f7ea768bb495fed
Mikhail <kolotilinma@gmail.com>
2021-09-03 11:54:21 +0300

    fix

--- ForaBank/ForaBank/Service/Networking/NetworkRouter/RouterUtlConfig/RouterBaseUrlList.swift
+++ ForaBank/ForaBank/Service/Networking/NetworkRouter/RouterUtlConfig/RouterBaseUrlList.swift
@@ -91,14 +91,14 @@ enum RouterBaseUrlList: String {
     // MARK: - В не авторизованную зону.
     ///Обновление по serial
     #if DEBUG
-    case getAnywayOperatorsList    = "/getAnywayOperatorsList"
+    case getAnywayOperatorsList    = "/dict/getAnywayOperatorsList"
     case getFullBankInfoList       = "/dict/getFullBankInfoList"
     case getCurrencyList           = "/dict/getCurrencyList"
     case getBanks                  = "/dict/getBanks"
     case getPaymentSystemList      = "/dict/getPaymentSystemList"
     case getCountries              = "/dict/getCountries"
 
-    #elseif RELEASE
+    #else
     case getAnywayOperatorsList    = "/rest/getAnywayOperatorsList"
     case getFullBankInfoList       = "/rest/dict/getFullBankInfoList"
     case getCurrencyList           = "/rest/dict/getCurrencyList"
