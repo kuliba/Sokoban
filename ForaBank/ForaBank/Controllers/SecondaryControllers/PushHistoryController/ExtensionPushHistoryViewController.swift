599183ae03ab1805a30e785482676536c282bd87
Konstantin <ksavyalov@gmail.com>
2021-11-12 01:47:34 +0300

    Добавил окно истории пашей

--- ForaBank/ForaBank/Controllers/SecondaryControllers/PushHistoryController/ExtensionPushHistoryViewController.swift
+++ ForaBank/ForaBank/Controllers/SecondaryControllers/PushHistoryController/ExtensionPushHistoryViewController.swift
@@ -11,11 +11,14 @@ extension PushHistoryViewController: UITableViewDelegate, UITableViewDataSource 
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
     }
     
     
