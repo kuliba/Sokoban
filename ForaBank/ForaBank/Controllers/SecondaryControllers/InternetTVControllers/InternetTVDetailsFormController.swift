b2f6097b7377de70b66b161fa69ba5fab8b819d5
romanvorobev <romhin25@gmail.com>
2021-11-22 21:16:45 +0300

    Internet TV

--- ForaBank/ForaBank/Controllers/SecondaryControllers/InternetTVControllers/InternetTVDetailsFormController.swift
+++ ForaBank/ForaBank/Controllers/SecondaryControllers/InternetTVControllers/InternetTVDetailsFormController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import RealmSwift
 
-class InternetTVDetailsFormController: BottomPopUpViewAdapter, UITableViewDataSource, TableViewDelegate {
+class InternetTVDetailsFormController: BottomPopUpViewAdapter, UITableViewDataSource, InternetTableViewDelegate {
 
     var bodyValue = [String : String]()
     var bodyArray = [[String : String]]()
@@ -37,7 +37,7 @@ class InternetTVDetailsFormController: BottomPopUpViewAdapter, UITableViewDataSo
 //        goButton.backgroundColor = .lightGray
         goButton.add_CornerRadius(5)
         puref = operatorData?.puref ?? ""
-        tableView.register(UINib(nibName: "GKHInputCell", bundle: nil), forCellReuseIdentifier: GKHInputCell.reuseId)
+        tableView.register(UINib(nibName: "InternetInputCell", bundle: nil), forCellReuseIdentifier: InternetTVInputCell.reuseId)
 //        tableView.register(GKHInputFooterView.self, forHeaderFooterViewReuseIdentifier: "sectionFooter")
         
         // Изменения символа валюты
@@ -304,11 +304,11 @@ class InternetTVDetailsFormController: BottomPopUpViewAdapter, UITableViewDataSo
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: GKHInputCell.reuseId, for: indexPath) as! GKHInputCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: InternetTVInputCell.reuseId, for: indexPath) as! InternetTVInputCell
         guard operatorData?.parameterList.count != 0 else { return cell }
 
         cell.setupUI(indexPath.row, (operatorData?.parameterList[indexPath.row])!, qrData)
-        cell.tableViewDelegate = (self as TableViewDelegate)
+        cell.tableViewDelegate = (self as InternetTableViewDelegate)
 
         cell.showInfoView = { value in
             let infoView = GKHInfoView()
@@ -323,10 +323,191 @@ class InternetTVDetailsFormController: BottomPopUpViewAdapter, UITableViewDataSo
         return hight
     }
 
-    func afterClickingReturnInTextField(cell: GKHInputCell) {
+    func afterClickingReturnInTextField(cell: InternetTVInputCell) { //GKHInputCell
         bodyValue.removeAll()
         bodyValue = cell.body
         bodyArray.append(bodyValue)
     }
 }
+
+@objc protocol InternetTableViewDelegate: NSObjectProtocol{
+    func afterClickingReturnInTextField(cell: InternetTVInputCell)
+}
+
+class InternetTVInputCell: UITableViewCell, UITextFieldDelegate {
+
+    var info = ""
+
+    var showInfoView: ((String) -> ())? = nil
+    var showGoButton: ((Bool) -> ())? = nil
+
+    weak var tableViewDelegate: InternetTableViewDelegate?
+
+    static let reuseId = "InternetTVInputCell"
+
+    var fieldid = ""
+    var fieldname = ""
+    var fieldvalue = ""
+    var body = [String: String]()
+    var perAcc = ""
+    var isSelect = true
+    @IBOutlet weak var infoButon: UIButton!
+    @IBOutlet weak var operatorsIcon: UIImageView!
+    @IBOutlet weak var showFioButton: UIButton!
+    @IBOutlet weak var placeholderLable: UILabel!
+    @IBOutlet weak var errorLable: UILabel!
+    @IBOutlet weak var showFIOButton: UIButton!
+    @IBOutlet weak var payTypeButton: UIButton!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var lineView: UIView!
+
+    var placeholder = ""
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        showFIOButton.isHidden = true
+        textField.delegate = self
+        textField.clearButtonMode = .whileEditing
+    }
+
+    // UITextField Defaults delegates
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        fieldvalue = textField.text ?? ""
+    }
+    // DataSetup
+//    func setupUI (_ index: Int, _ dataModel: [String: String]) {
+//        if emptyCell() == false {
+//            infoButon.isHidden = true
+//            self.fieldid = String(index + 1)
+//            fieldname = dataModel["id"] ?? ""
+//            let q = GKHDataSorted.a(dataModel["title"] ?? "")
+//
+//            DispatchQueue.main.async {
+//                self.operatorsIcon.image = UIImage(named: q.1)
+//            }
+//
+//            textField.placeholder = q.0
+//            placeholder = q.0
+//
+//            if q.0 == "Лицевой счет" {
+//                let h = dataModel["Лицевой счет"]
+//                perAcc = h ?? ""
+//                if h != "" {
+//                    textField.text = h
+//                }
+//            }
+//
+//            if q.0 == "" {
+//                textField.placeholder = dataModel["title"] ?? ""
+//            }
+//            if q.0 == "ФИО" {
+//                showFioButton.isHidden = false
+//            } else {
+//                showFioButton.isHidden = true
+//            }
+//
+//            if dataModel["subTitle"] != nil {
+//                info = dataModel["subTitle"] ?? ""
+//                infoButon.isHidden = false
+//            }
+//            if dataModel["viewType"] != "INPUT" {
+//                self.textField.isEnabled = false
+//                isSelect = false
+//            }
+//        }
+//    }
+
+    func setupUI (_ index: Int, _ dataModel: Parameters, _ qrData: [String: String]) {
+
+        infoButon.isHidden = true
+        self.fieldid = String(index + 1)
+        fieldname = dataModel.id ?? ""
+        let q = GKHDataSorted.a(dataModel.title ?? "")
+
+        DispatchQueue.main.async {
+            self.operatorsIcon.image = UIImage(named: q.1)
+        }
+
+        textField.placeholder = q.0
+        placeholder = q.0
+
+        if q.0 == "Лицевой счет" {
+            let h = qrData.filter { $0.key == "Лицевой счет"}
+            if h.first?.value != "" {
+                textField.text = h.values.first
+            }
+        }
+
+        if q.0 == "" {
+            textField.placeholder = dataModel.title
+        }
+        if q.0 == "ФИО" {
+            showFioButton.isHidden = false
+        } else {
+            showFioButton.isHidden = true
+        }
+
+        if dataModel.subTitle != nil {
+            info = dataModel.subTitle ?? ""
+            infoButon.isHidden = false
+        }
+    }
+
+    @IBAction func textField(_ sender: UITextField) {
+        fieldvalue = textField.text ?? ""
+        body.updateValue(fieldid, forKey: "fieldid")
+        body.updateValue(fieldname, forKey: "fieldname")
+        body.updateValue(textField.text ?? "" , forKey: "fieldvalue")
+        self.perAcc = body["Лицевой счет"] ?? ""
+        haveEmptyCell()
+        tableViewDelegate?.responds(to: #selector(InternetTableViewDelegate.afterClickingReturnInTextField(cell:)))
+        tableViewDelegate?.afterClickingReturnInTextField(cell: self)
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+        //        let previousText:NSString = textField.text! as NSString
+        //        let updatedText = previousText.replacingCharacters(in: range, with: string)
+        //        print("updatedText > ", updatedText)
+        return true
+    }
+
+    @IBAction func payTapButton(_ sender: UIButton) {
+    }
+
+    @IBAction func showFIOButton(_ sender: UIButton) {
+    }
+
+    @IBAction func showInfo(_ sender: UIButton) {
+        showInfoView?(info)
+    }
+
+    final func haveEmptyCell() {
+
+        if ( fieldvalue != "" && isSelect == true) {
+            showGoButton?(true)
+        } else if ( fieldvalue == "" && isSelect == false) {
+            showGoButton?(true)
+        }
+        if ( fieldvalue == "" && isSelect == true) {
+            showGoButton?(false)
+        }
+    }
+
+    final func emptyCell() -> Bool {
+
+        var result = false
+        if ( fieldid != "" || fieldname != "" || fieldvalue != "" ) {
+            result = true
+        }
+        return result
+    }
+
+}
+
 
