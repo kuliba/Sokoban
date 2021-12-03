361628c48e5235f823d178f14a01e6c1903cb426
Max Gribov <gribov.max@gmail.com>
2021-12-04 00:24:25 +0300

    Diff calculation while caching uploaded from server cards

---
+++ ForaBank/ForaBank/Resours/Extensions/RealmResults+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  RealmResults+Extension.swift
+//  RealmPlayground
+//
+//  Created by Max Gribov on 03.12.2021.
+//
+
+import Foundation
+import RealmSwift
+
+extension Results {
+    
+    func toArray<T>(ofType: T.Type) -> [T] {
+        
+        var array = [T]()
+        
+        for i in 0 ..< count {
+            
+            if let result = self[i] as? T {
+                
+                array.append(result)
+            }
+        }
+
+        return array
+    }
+}
--- ForaBank/ForaBank/Resours/Helpers/REALMModels/AuthZoneRequest/GetAllCards/AddAllUserCardtList.swift
+++ ForaBank/ForaBank/Resours/Helpers/REALMModels/AuthZoneRequest/GetAllCards/AddAllUserCardtList.swift
@@ -11,84 +11,49 @@ import RealmSwift
 // MARK: - Save REALM
 struct AddAllUserCardtList {
     
-    static func add(_ completion: @escaping () -> ()) {
+    static func add(_ completion: @escaping () -> Void) {
         
-        /// Общая информация об поставщике услуг
-        var cardsArray     = [UserAllCardsModel]()
-        var tempcardsArray = [UserAllCardsModel]()
-        var tempCard = UserAllCardsModel()
-        let param = ["isCard": "true", "isAccount": "true", "isDeposit": "true", "isLoan": "false"]
+        let params = ["isCard": "true", "isAccount": "true", "isDeposit": "true", "isLoan": "false"]
         
-        NetworkManager<GetProductListDecodableModel>.addRequest(.getProductListByFilter, param, [:]) { model, error in
+        NetworkManager<GetProductListDecodableModel>.addRequest(.getProductListByFilter, params, [:]) { model, error in
             
-            if error != nil {
-                print("DEBUG: error", error!)
-            } else {
-                guard let model = model else { return }
-                guard let cardList = model.data else { return }
+            if let error = error {
                 
-                cardList.forEach { card in
+                print("DEBUG: error", error)
+                return
+            }
+            
+            guard let model = model,
+                  let receivedData = model.data else {
+                      
+                      return
+                  }
+            
+            let receivedCards = receivedData.map{ UserAllCardsModel(with: $0) }
+            
+            do {
+                
+                let realm = try Realm()
+                let existingCards = realm.objects(UserAllCardsModel.self).sorted(byKeyPath: "id", ascending: true).toArray(ofType: UserAllCardsModel.self)
+                
+                guard let changes = existingCards.update(with: receivedCards) else { return }
+                
+                try realm.write({
                     
-                    let cards      = UserAllCardsModel()
-                    cards.number             = card.number
-                    cards.numberMasked       = card.numberMasked
-                    cards.balance            = card.balance ?? 0.0
-                    cards.currency           = card.currency
-                    cards.productType        = card.productType
-                    cards.productName        = card.productName
-                    cards.ownerID            = card.ownerID ?? 0
-                    cards.accountNumber      = card.accountNumber
-                    cards.allowDebit         = card.allowDebit ?? false
-                    cards.allowCredit        = card.allowCredit ?? false
-                    cards.customName         = card.customName
-                    cards.cardID             = card.cardID ?? 0
-                    cards.name               = card.name
-                    cards.validThru          = card.validThru ?? 0
-                    cards.status             = card.status
-                    cards.holderName         = card.holderName
-                    cards.product            = card.product
-                    cards.branch             = card.branch
-                    cards.mainField          = card.mainField
-                    cards.additionalField    = card.additionalField
-                    cards.smallDesign        = card.smallDesign
-                    cards.mediumDesign       = card.mediumDesign
-                    cards.largeDesign        = card.largeDesign
-                    cards.paymentSystemName  = card.paymentSystemName
-                    cards.paymentSystemImage = card.paymentSystemImage
-                    cards.fontDesignColor    = card.fontDesignColor
-                    cards.id                 = card.id ?? 0
-                    cards.openDate           = card.openDate ?? 0
-                    cards.branchId           = card.branchId ?? 0
-                    cards.accountID          = card.accountID ?? 0
-                    cards.expireDate         = card.expireDate
-                    cards.XLDesign           = card.XLDesign
-                    cards.statusPC           = card.statusPC
-                    
-                    card.background.forEach { color in
-                        let colors = UserAllCardsbackgroundModel()
-                        colors.color = color
-                        cards.background.append(colors)
-                    }
-                    tempCard = cards
-                    tempcardsArray.append(tempCard)
-                }
-                tempcardsArray.forEach { value in
-                    cardsArray.append(value)
-                }
-//                tempcardsArray.removeAll()
-                /// Сохраняем в REALM
-                let realm = try? Realm()
-                do {
-                    let operators = realm?.objects(UserAllCardsModel.self)
-                    realm?.beginWrite()
-                    realm?.delete(operators!)
-                    realm?.add(cardsArray)
-                    try realm?.commitWrite()
-                    completion()
-                } catch {
-                    print(error.localizedDescription)
-                }
+                    realm.delete(changes.removals)
+                    realm.add(changes.updates, update: .modified)
+                    realm.add(changes.additions)
+                })
+                
+                completion()
+                
+            } catch  {
+                
+                print(error.localizedDescription)
             }
         }
     }
 }
+
+
+
