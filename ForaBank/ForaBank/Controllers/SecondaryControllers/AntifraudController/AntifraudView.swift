1c827b4e36ecebd2455bd85a1ecb38c12b156cfd
Dmitriy <da.savushkin4@gmail.com>
2021-12-08 15:17:39 +0400

    create antifraudView

---
+++ ForaBank/ForaBank/Controllers/SecondaryControllers/AntifraudController/AntifraudView.swift
@@ -0,0 +1,21 @@
+//
+//  AntifraudView.swift
+//  ForaBank
+//
+//  Created by Дмитрий on 08.12.2021.
+//
+
+import SwiftUI
+
+struct AntifraudView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+    }
+}
+
+struct AntifraudView_Previews: PreviewProvider {
+    static var previews: some View {
+        AntifraudView()
+    }
+}
