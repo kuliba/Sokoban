6c76858279df974fd3e44684fe3442ccd7a13154
Max Gribov <gribov.max@gmail.com>
2021-12-09 12:34:57 +0300

    GetNotificationsModelSaved updated

--- ForaBank/ForaBank/Resours/Helpers/REALMModels/AuthZoneRequest/GetNotifications/GetNotificationsModelSaved.swift
+++ ForaBank/ForaBank/Resours/Helpers/REALMModels/AuthZoneRequest/GetNotifications/GetNotificationsModelSaved.swift
@@ -10,51 +10,46 @@ import RealmSwift
 
 struct GetNotificationsModelSaved {
     
-    static func add(_ param: [String : String], _ body: [String: AnyObject], complition: @escaping () -> Void) {
+    static func add(_ param: [String : String], _ body: [String: AnyObject], completion: @escaping () -> Void) {
         
         NetworkManager<GetNotificationsDecodableModel>.addRequest(.getNotifications, param, body) { model, error in
-            if error != nil {
-                print("DEBUG: error", error!)
-            } else {
-                guard let statusCode = model?.statusCode else { return }
-                if statusCode == 0 {
+            
+            if let error = error {
+                print("DEBUG: error", error)
+                completion()
+                return
+            }
+            
+            guard let model = model, let notificationsData = model.data else {
+                completion()
+                return
+            }
+
+            let updatedNotifications = notificationsData.map{ GetNotificationsSectionModel(with: $0) }
+            
+            do {
+                
+                let realm = try Realm()
+                let existingNotifications = realm.objects(GetNotificationsSectionModel.self)
+                
+                // fitst transaction: delete items to inform subscribers in UI
+                try realm.write {
                     
-                    guard let model = model else { return }
-                    guard let m = model.data else { return }
-                    
-                    var tempArray = [GetNotificationsSectionModel]()
-                    
-                    m.forEach { b in
-                        let c = GetNotificationsSectionModel()
-                        let a = GetNotificationsCellModel()
-                        a.date = b.date
-                        a.title = b.title
-                        a.text = b.text
-                        a.type = b.type
-                        a.state = b.state
-                        
-                        c.date = b.date
-                        c.getNotificationsCell.append(a)
-                        tempArray.append(c)
-                        
-                    }
-                    
-                    /// Сохраняем в REALM
-                    let realm = try? Realm()
-                    do {
-//                        let b = realm?.objects(GetNotificationsSectionModel.self)
-//                        let a = realm?.objects(GetNotificationsCellModel.self)
-                        realm?.beginWrite()
-//                        realm?.delete(b!)
-//                        realm?.delete(a!)
-                        realm?.add(tempArray)
-                        try realm?.commitWrite()
-                        print("REALM", realm?.configuration.fileURL?.absoluteString ?? "")
-                        complition()
-                    } catch {
-                        print(error.localizedDescription)
-                    }
+                    realm.delete(existingNotifications)
                 }
+                
+                // second transaction: add fresh data from server
+                try realm.write {
+                    
+                    realm.add(updatedNotifications)
+                }
+                
+                completion()
+                
+            } catch {
+                
+                print(error.localizedDescription)
+                completion()
             }
         }
     }
--- ForaBank/ForaBank/Resours/Helpers/REALMModels/AuthZoneRequest/GetNotifications/GetNotificationsSectionModel.swift
+++ ForaBank/ForaBank/Resours/Helpers/REALMModels/AuthZoneRequest/GetNotifications/GetNotificationsSectionModel.swift
@@ -22,3 +22,21 @@ class GetNotificationsCellModel: Object {
     @objc dynamic var type: String?
     @objc dynamic var state: String?
 }
+
+extension GetNotificationsSectionModel {
+    
+    convenience init(with data: GetNotifications) {
+        
+        self.init()
+
+        let cellModel = GetNotificationsCellModel()
+        cellModel.date = data.date
+        cellModel.title = data.title
+        cellModel.text = data.text
+        cellModel.type = data.type
+        cellModel.state = data.state
+        
+        date = data.date
+        getNotificationsCell.append(cellModel)
+    }
+}
