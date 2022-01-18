f8e1ef7ab8b6c9e187ae7986b7687b3e7df469a3
Dmitriy <da.savushkin4@gmail.com>
2022-01-18 14:07:47 +0400

    server commands Registration

---
+++ ForaBank/ForaBank/Services/Server/ServerCommands+Registration.swift
@@ -0,0 +1,170 @@
+//
+//  ServerComands+Registration.swift
+//  ForaBank
+//
+//  Created by Дмитрий on 18.01.2022.
+//
+
+import Foundation
+
+extension ServerCommands{
+    
+    enum RegistrationContoller {
+        
+        /*
+         https://git.briginvest.ru/dbo/api/v3/swagger-ui/index.html#/registration/checkClient
+         */
+        struct CheckClient: ServerCommand {
+            
+            let endpoint = "/registration/checkClient"
+            let method: ServerCommandMethod = .post
+            let payload: Payload?
+            let token: String?
+            
+            struct Payload: Encodable {
+                
+                let cardNumber: String
+                let cryptoVersion: String
+            }
+            
+            struct Response: ServerResponse {
+                
+                let statusCode: ServerStatusCode
+                let errorMessage: String?
+                let data: EmptyData
+            }
+            
+            internal init(token: String, payload: Payload) {
+                
+                self.token = token
+                self.payload = payload
+            }
+        }
+        /*
+         https://git.briginvest.ru/dbo/api/v3/swagger-ui/index.html#/registration/doRegistration
+         */
+        struct DoRegistration: ServerCommand {
+            
+            let endpoint = "/registration/doRegistration"
+            let method: ServerCommandMethod = .post
+            let payload: Payload?
+            let token: String?
+            
+            struct Payload: Encodable {
+                
+                let cryptoVersion: String
+                let model: String
+                let operationSystem: String
+                let pushDeviceId: String
+                let pushFcmToken: String
+                
+            }
+            
+            struct Response: ServerResponse {
+                
+                let statusCode: ServerStatusCode
+                let errorMessage: String?
+                let data: EmptyData
+            }
+            
+            internal init(token: String, payload: Payload) {
+                
+                self.token = token
+                self.payload = payload
+            }
+        }
+        
+        /*
+         https://git.briginvest.ru/dbo/api/v3/swagger-ui/index.html#/registration/getCode
+         */
+        struct GetCode: ServerCommand {
+            
+            let endpoint = "/registration/getCode"
+            let method: ServerCommandMethod = .post
+            let payload: Payload? = nil
+            let token: String?
+            
+            struct Payload: Encodable {}
+
+            
+            struct Response: ServerResponse {
+                
+                let statusCode: ServerStatusCode
+                let errorMessage: String?
+                let data: EmptyData
+            }
+            
+            internal init(token: String, payload: Payload) {
+                
+                self.token = token
+            }
+        }
+        /*
+         https://git.briginvest.ru/dbo/api/v3/swagger-ui/index.html#/registration/setDeviceSettings
+         */
+        struct SetDeviceSettings: ServerCommand {
+            
+            let endpoint = "/registration/setDeviceSettings"
+            let method: ServerCommandMethod = .post
+            let payload: Payload?
+            let token: String?
+            
+            struct Payload: Encodable {
+                let cryptoVersion: String
+                let pushDeviceId: String
+                let pushFcmToken: String
+                let serverDeviceGUID: String
+                let settings: [Settings]
+            }
+            
+            struct Settings: Encodable {
+                let isActive: Bool
+                let type: String
+                let value: String
+            }
+
+            
+            struct Response: ServerResponse {
+                
+                let statusCode: ServerStatusCode
+                let errorMessage: String?
+                let data: EmptyData
+            }
+            
+            internal init(token: String, payload: Payload) {
+                
+                self.token = token
+                self.payload = payload
+            }
+        }
+        /*
+         https://git.briginvest.ru/dbo/api/v3/swagger-ui/index.html#/registration/verifyCode
+         */
+        struct VerifyCode: ServerCommand {
+            
+            let endpoint = "/registration/verifyCode"
+            let method: ServerCommandMethod = .post
+            let payload: Payload?
+            let token: String?
+            
+            struct Payload: Encodable {
+                let cryptoVersion: String
+                let verificationCode: String
+            }
+
+            
+            struct Response: ServerResponse {
+                
+                let statusCode: ServerStatusCode
+                let errorMessage: String?
+                let data: EmptyData
+            }
+            
+            internal init(token: String, payload: Payload) {
+                
+                self.payload = payload
+                self.token = token
+            }
+        }
+    }
+}
