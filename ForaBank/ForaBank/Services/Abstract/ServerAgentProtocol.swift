e67f6b58f48f092ad2ce9465b1418db44e93e040
Fora iOS Dev <project15_bot@example.com>
2022-02-02 17:11:54 +0000

    DBSNEW-3425: ServerDownloadCommand

--- ForaBank/ForaBank/Services/Abstract/ServerAgentProtocol.swift
+++ ForaBank/ForaBank/Services/Abstract/ServerAgentProtocol.swift
@@ -11,20 +11,40 @@ import SwiftUI
 protocol ServerAgentProtocol {
     
     func executeCommand<Command: ServerCommand>(command: Command, completion: @escaping (Result<Command.Response, ServerAgentError>) -> Void)
+    func executeDownloadCommand<Command: ServerDownloadCommand>(command: Command, completion: @escaping (Result<Command.Response, ServerAgentError>) -> Void)
 }
 
+/// Regular server request
 protocol ServerCommand {
     
     associatedtype Payload: Encodable
     associatedtype Response: ServerResponse
+    
+    var token: String { get } //FIXME: optional
+    var endpoint: String { get }
+    var method: ServerCommandMethod { get }
+    var parameters: [ServerCommandParameter]? { get }
+    var payload: Payload? { get }
+    //TODO: add timeoutInterval
+//    var timeoutInterval: TimeInterval? { get }
+}
+
+/// Multipart download server request
+protocol ServerDownloadCommand {
+    
+    associatedtype Payload: Encodable
+    typealias Response = Data
     
     var token: String { get }
     var endpoint: String { get }
     var method: ServerCommandMethod { get }
     var parameters: [ServerCommandParameter]? { get }
     var payload: Payload? { get }
+    //TODO: add timeoutInterval
+//    var timeoutInterval: TimeInterval? { get }
 }
 
+/// Regular server request response
 protocol ServerResponse: Decodable, Equatable {
     
     associatedtype Payload: Decodable
@@ -34,12 +54,7 @@ protocol ServerResponse: Decodable, Equatable {
     var data: Payload { get }
 }
 
-enum ServerCommandAuth {
-    
-    case none
-    case token(String)
-}
-
+/// ServerAgent's error
 enum ServerAgentError: Error {
     
     case requestCreationError(Error)
