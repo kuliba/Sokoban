9ef311105a5cce5de8e62123c69d637bf2ffd727
Fora iOS Dev <project15_bot@example.com>
2022-02-03 16:55:57 +0000

    DBSNEW-3438

---
+++ ForaBank/ForaBank/Domain/Dictionary/FSSPData.swift
@@ -0,0 +1,16 @@
+//
+//  FSSPData.swift
+//  ForaBank
+//
+//  Created by Max Gribov on 03.02.2022.
+//
+
+import Foundation
+
+struct FSSPData: Codable, Equatable {
+    
+    let md5hash: String
+    let svgImage: SVGImageData
+    let text: String
+    let value: String
+}
