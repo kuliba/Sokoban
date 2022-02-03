0e2907ef0d462ed4a1fe9eba1f7ecd306cac97d7
Савушкин Дмитрий <da.savushkin4@gmail.com>
2022-02-03 14:56:20 +0000

    DaData server commands and tests

---
+++ ForaBank/ForaBank/Services/Server/ServerCommands+DaData.swift
@@ -0,0 +1,44 @@
+//
+//  ServerCommands+DaData.swift
+//  ForaBank
+//
+//  Created by Дмитрий on 03.02.2022.
+//
+
+import Foundation
+
+extension ServerCommands {
+    
+    enum DaDataController {
+        
+        /*
+         https://git.briginvest.ru/dbo/api/v3/swagger-ui/index.html#/rest/getPhoneInfo
+         */
+        struct GetPhoneInfo: ServerCommand {
+
+            let token: String
+            let endpoint = "/rest/getPhoneInfo"
+            let method: ServerCommandMethod = .post
+            let parameters: [ServerCommandParameter]? = nil
+            let payload: Payload?
+            
+            struct Payload: Encodable {
+                
+                let phoneNumbersList: [String]
+            }
+            
+            struct Response: ServerResponse {
+
+                let statusCode: ServerStatusCode
+                let errorMessage: String?
+                let data: [DaDataPhoneData]?
+            }
+            
+            internal init(token: String, payload: Payload) {
+
+                self.token = token
+                self.payload = payload
+            }
+        }
+    }
+}
