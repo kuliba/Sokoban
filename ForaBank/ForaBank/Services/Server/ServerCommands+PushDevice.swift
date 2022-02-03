d15d5487c2b5d4c078ab6d349c0a52dc093aae00
Fora iOS Dev <project15_bot@example.com>
2022-02-03 16:17:22 +0000

    DBSNEW-3242: PushDevice

---
+++ ForaBank/ForaBank/Services/Server/ServerCommands+PushDevice.swift
@@ -0,0 +1,105 @@
+//
+//  ServerCommands+PushDevice.swift
+//  ForaBank
+//
+//  Created by Max Gribov on 03.02.2022.
+//
+
+import Foundation
+
+extension ServerCommands {
+    
+    enum PushDeviceController {
+        
+        /*
+         https://git.briginvest.ru/dbo/api/v3/swagger-ui/index.html#/PushDeviceController/registerPushDeviceForUserUsingPOST
+         */
+        struct RegisterPushDeviceForUser: ServerCommand {
+
+            let token: String
+            let endpoint = "/push_device_user/registerPushDeviceForUser"
+            let method: ServerCommandMethod = .post
+            let parameters: [ServerCommandParameter]? = nil
+            let payload: BasePayload?
+  
+            struct Response: ServerResponse {
+
+                let statusCode: ServerStatusCode
+                let errorMessage: String?
+                let data: EmptyData?
+            }
+            
+            internal init(token: String, payload: BasePayload) {
+                
+                self.token = token
+                self.payload = payload
+            }
+        }
+        
+        /*
+         https://git.briginvest.ru/dbo/api/v3/swagger-ui/index.html#/PushDeviceController/installPushDeviceUsingPOST
+         */
+        struct InstallPushDevice: ServerCommand {
+
+            let token: String
+            let endpoint = "/push_device/installPushDevice"
+            let method: ServerCommandMethod = .post
+            let parameters: [ServerCommandParameter]? = nil
+            let payload: Payload?
+            
+            struct Payload: Encodable {
+                
+                let cryptoVersion: String
+                let model: String
+                let operationSystem: String = "IOS"
+                let pushDeviceId: String
+                let pushFcmToken: String
+            }
+            
+            struct Response: ServerResponse {
+
+                let statusCode: ServerStatusCode
+                let errorMessage: String?
+                let data: EmptyData?
+            }
+            
+            internal init(token: String, payload: Payload) {
+                
+                self.token = token
+                self.payload = payload
+            }
+        }
+        
+        /*
+         https://git.briginvest.ru/dbo/api/v3/swagger-ui/index.html#/PushDeviceController/uninstallPushDeviceUsingPOST
+         */
+        struct UninstallPushDevice: ServerCommand {
+
+            let token: String
+            let endpoint = "/push_device/uninstallPushDevice"
+            let method: ServerCommandMethod = .post
+            let parameters: [ServerCommandParameter]? = nil
+            let payload: BasePayload?
+            
+            struct Response: ServerResponse {
+
+                let statusCode: ServerStatusCode
+                let errorMessage: String?
+                let data: EmptyData?
+            }
+            
+            internal init(token: String, payload: BasePayload) {
+                
+                self.token = token
+                self.payload = payload
+            }
+        }
+        
+        struct BasePayload: Encodable {
+            
+            let cryptoVersion: String
+            let pushDeviceId: String
+            let pushFcmToken: String
+        }
+    }
+}
