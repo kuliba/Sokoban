3d9608e2cd7b0775ac81b065a97e99ba97a6a69c
Max Gribox <gribov.max@gmail.com>
2022-02-21 12:28:57 +0300

    Initial structure

---
+++ ForaBank/ForaBank/Components/Main/MainBlockProductsViewComponent.swift
@@ -0,0 +1,44 @@
+//
+//  MainBlockProductsViewComponent.swift
+//  ForaBank
+//
+//  Created by Max Gribov on 21.02.2022.
+//
+
+import Foundation
+import SwiftUI
+
+//MARK: - ViewModel
+
+extension MainBlockProductsView {
+    
+    struct ViewModel {
+        
+    }
+}
+
+//MARK: - View
+
+struct MainBlockProductsView: View {
+    
+    var body: some View {
+        
+        Text("")
+    }
+}
+
+//MARK: - Preview
+
+struct MainBlockProductsView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        
+        MainBlockProductsView()
+    }
+}
+
+//MARK: - Preview Content
+
+extension MainBlockProductsView {
+    
+}
