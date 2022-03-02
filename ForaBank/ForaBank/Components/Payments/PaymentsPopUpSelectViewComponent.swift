80473dcbbfa20ee0f5d78c997ccc20fe6f604684
Fora iOS Dev <project15_bot@example.com>
2022-03-02 20:03:10 +0000

    DBSNEW-3688

---
+++ ForaBank/ForaBank/Components/Payments/PaymentsPopUpSelectViewComponent.swift
@@ -0,0 +1,161 @@
+//
+//  PaymentsPopUpSelectViewComponent.swift
+//  ForaBank
+//
+//  Created by Константин Савялов on 22.02.2022.
+//
+
+import SwiftUI
+import Combine
+
+//MARK: - ViewModel
+
+extension PaymentsPopUpSelectView {
+    
+    class ViewModel: PaymentsParameterViewModel {
+        
+        let action: PassthroughSubject<Action, Never> = .init()
+        
+        var title: String
+        let description: String?
+        var items: [ItemViewModel]
+        
+        @Published var selected: Payments.ParameterSelectSimple.Option.ID?
+        private var bindings: Set<AnyCancellable> = []
+        
+        internal init(title: String, description: String, items: [ItemViewModel], parameter: Payments.Parameter = .init(id: UUID().uuidString, value: "")) {
+            
+            self.title = title
+            self.description = description
+            self.items = items
+            super.init(parameter: parameter)
+            self.bind()
+        }
+        
+        init(with parameterSelect: Payments.ParameterSelectSimple, selectedID: Payments.ParameterSelectSimple.Option.ID? = nil) {
+            
+            self.title = parameterSelect.title
+            self.description = parameterSelect.description
+            self.selected = selectedID
+            self.items = []
+            super.init(parameter: parameterSelect)
+            self.items = parameterSelect.options.map { ItemViewModel(id: $0.id, name: $0.name, isSelected: false, action: {[weak self] itemId in
+                self?.selected = itemId
+            }) }
+        }
+        
+        func bind() {
+            
+            $selected
+                .receive(on: DispatchQueue.main)
+                .sink { [unowned self] selected in
+                    
+                    for item in items {
+                        
+                        item.isSelected = item.id == id ? true : false
+                    }
+                    
+                }.store(in: &bindings)
+        }
+        
+        class ItemViewModel: Identifiable, ObservableObject {
+            
+            let id: Payments.ParameterSelectSimple.Option.ID
+            let name: String
+            let action: (Payments.ParameterSelectSimple.Option.ID?) -> Void
+            @Published var isSelected: Bool
+            
+            init(id: Payments.ParameterSelectSimple.Option.ID, name: String, isSelected: Bool, action: @escaping (Payments.ParameterSelectSimple.Option.ID?) -> Void) {
+                
+                self.id = id
+                self.name = name
+                self.isSelected = isSelected
+                self.action = action
+            }
+        }
+    }
+}
+
+//MARK: - View
+
+struct PaymentsPopUpSelectView: View {
+    
+    var viewModel: PaymentsPopUpSelectView.ViewModel
+    
+    var body: some View {
+        
+        VStack(alignment: .leading, spacing: 20) {
+            
+            Text(viewModel.title)
+                .foregroundColor(Color(hex: "#1C1C1C"))
+                .font(Font.custom("Inter-SemiBold", size: 18))
+            
+            if let description = viewModel.description {
+                Text(description)
+                    .foregroundColor(Color(hex: "#999999"))
+                    .font(Font.custom("Inter-Regular", size: 14))
+            }
+            ScrollView {
+                
+                VStack(spacing: 30) {
+                    
+                    ForEach(viewModel.items) { item in
+                        ItemView(viewModel: item)
+                    }
+                }
+            }
+        } .padding()
+    }
+    
+    struct ItemView: View {
+        
+        let viewModel: ViewModel.ItemViewModel
+        
+        var body: some View {
+            
+            HStack(alignment: .center, spacing: 20) {
+                
+                Image(viewModel.isSelected ? "info-1" : "info_2")
+                    .frame(width: 26, height: 26)
+                    .padding(.bottom, 10)
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    
+                    Text(viewModel.name)
+                        .foregroundColor(Color(hex: "#1C1C1C"))
+                        .font(Font.custom("Inter-Regular", size: 14))
+                    Divider()
+                        .frame(height: 1)
+                        .foregroundColor(Color(hex: "#EAEBEB"))
+                }
+            } .onTapGesture {
+                viewModel.action(viewModel.id)
+            }
+        }
+    }
+    
+}
+
+//MARK: - Preview
+
+struct PaymentsPopUpSelectView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        PaymentsPopUpSelectView(viewModel: sample)
+            .previewLayout(.fixed(width: 375, height: 500))
+    }
+}
+
+//MARK: - Preview Content
+
+extension PaymentsPopUpSelectView_Previews {
+    
+    static let sample = PaymentsPopUpSelectView.ViewModel(
+        title: "Выберите значение",
+        description: "Государственная пошлина за выдачу паспорта удостоверяющего личность гражданина РФ за пределами территории РФ гражданину РФ",
+        items: [.init(id: "0", name: "В возрасте от 14 лет", isSelected: true, action: {_ in }),
+                .init(id: "1", name: "В возрасте до 14 лет", isSelected: false, action: {_ in }),
+                .init(id: "2", name: "В возрасте до 14 лет (новый образец)", isSelected: false, action: {_ in }),
+                .init(id: "3", name: "Содержащего электронный носитель информации (паспорта нового поколения)", isSelected: false, action: {_ in }),
+                .init(id: "4", name: "За внесение изменений в паспорт", isSelected: false, action: {_ in })])
+}
