f8c6166f4f9aef0f017d4a3ab007d33bb940f5d6
Dmitriy <da.savushkin4@gmail.com>
2022-03-02 11:45:10 +0400

    Button Section Component

---
+++ ForaBank/ForaBank/Components/Profile/HistoryViewComponent.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryViewComponent.swift
+//  ForaBank
+//
+//  Created by Дмитрий on 02.03.2022.
+//
+
+import SwiftUI
+
+struct HistoryViewComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HistoryViewComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryViewComponent()
+    }
+}
---
+++ ForaBank/ForaBank/Components/Profile/SpendingViewComponent.swift
@@ -0,0 +1,20 @@
+//
+//  SpendingViewComponent.swift
+//  ForaBank
+//
+//  Created by Дмитрий on 02.03.2022.
+//
+
+import SwiftUI
+
+struct SpendingViewComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpendingViewComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        SpendingViewComponent()
+    }
+}
