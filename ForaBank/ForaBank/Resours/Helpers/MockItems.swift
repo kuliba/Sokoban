4d609c4ef3468a9050cf6ad5aa9f01b8ce3c996a
Mikhail <kolotilinma@gmail.com>
2022-03-03 02:28:49 +0300

    update deposits

--- ForaBank/ForaBank/Resours/Helpers/MockItems.swift
+++ ForaBank/ForaBank/Resours/Helpers/MockItems.swift
@@ -104,7 +104,7 @@ class MockItems {
             PaymentsModel(id: 98, name: "Вклад",
                           iconName: "openDeposit",
                           controllerName: "https://www.forabank.ru/private/deposits/",
-                          description: "9,25% годовых"),
+                          description: "21,30% годовых"),
             //
             //            PaymentsModel(id: 97, name: "Кредит",
             //                          iconName: "loanIcon",
