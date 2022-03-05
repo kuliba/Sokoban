5665205c85df209c3c6b7de37bc3b677895d9f7f
Max Gribox <gribov.max@gmail.com>
2022-03-05 17:21:29 +0300

    MainView

---
+++ ForaBank/ForaBank/View/Main/MainView.swift
@@ -0,0 +1,58 @@
+//
+//  MainView.swift
+//  ForaBank
+//
+//  Created by Max Gribov on 05.03.2022.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    @ObservedObject var viewModel: MainViewModel
+    
+    var body: some View {
+        
+        ScrollView(showsIndicators: false) {
+            
+            VStack(spacing: 20) {
+                
+                ForEach(viewModel.sections) { section in
+                    
+                    switch section {
+                    case let productsSectionViewModel as MainSectionProductsView.ViewModel:
+                        MainSectionProductsView(viewModel: productsSectionViewModel)
+                        
+                    case let fastOperationViewModel as MainSectionFastOperationView.ViewModel:
+                        MainSectionFastOperationView(viewModel: fastOperationViewModel)
+                        
+                    case let promoViewModel  as MainSectionPromoView.ViewModel:
+                        MainSectionPromoView(viewModel: promoViewModel)
+                        
+                    case let currencyViewModel as MainSectionCurrencyView.ViewModel:
+                        MainSectionCurrencyView(viewModel: currencyViewModel)
+                        
+                    case let openProductViewModel as MainSectionOpenProductView.ViewModel:
+                        MainSectionOpenProductView(viewModel: openProductViewModel)
+                        
+                    default:
+                        EmptyView()
+                    }
+                }
+            }
+        }
+        .padding(20)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        MainView(viewModel: .sample)
+    }
+}
+
+extension MainViewModel {
+    
+    static let sample = MainViewModel(sections: [MainSectionProductsView.ViewModel.sample, MainSectionFastOperationView.ViewModel.sample, MainSectionPromoView.ViewModel.sample, MainSectionCurrencyView.ViewModel.sample, MainSectionOpenProductView.ViewModel.sample])
+}
