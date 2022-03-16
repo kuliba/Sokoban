135e7bee07e0101825ce27e01377c8c0a5e4689a
Konstantin <ksavyalov@gmail.com>
2022-03-16 03:53:39 +0300

    Первый коммит

---
+++ ForaBank/ForaBank/Controllers/SecondaryControllers/ContactsPicker/EPContactSelfDataView.swift
@@ -0,0 +1,20 @@
+//
+//  EPContactSelfDataView.swift
+//  ForaBank
+//
+//  Created by Константин Савялов on 15.03.2022.
+//
+
+import UIKit
+
+class EPContactSelfDataView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
