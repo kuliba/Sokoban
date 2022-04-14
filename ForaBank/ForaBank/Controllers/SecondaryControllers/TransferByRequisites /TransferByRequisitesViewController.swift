7a512921e7ccd62284c8ef13bb9727fa10408156
Konstantin <ksavyalov@gmail.com>
2022-04-14 21:26:34 +0300

    Поправил вылет при оплате юр лицам

--- ForaBank/ForaBank/Controllers/SecondaryControllers/TransferByRequisites /TransferByRequisitesViewController.swift
+++ ForaBank/ForaBank/Controllers/SecondaryControllers/TransferByRequisites /TransferByRequisitesViewController.swift
@@ -835,8 +835,10 @@ class TransferByRequisitesViewController: UIViewController, UITextFieldDelegate,
             kppField.textField.text = ""
             commentField.textField.text = ""
         }
+        
+        let queryParameter = innField.textField.text ?? ""
         let body = [
-            "query": innField.textField.text
+            "query": queryParameter
         ] as [String: AnyObject]
         
         NetworkManager<SuggestCompanyDecodableModel>.addRequest(.suggestCompany, [:], body) { model, error in
