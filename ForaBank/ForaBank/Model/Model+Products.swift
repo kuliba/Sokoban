e3bd734aee05115bf577c005815cb8440163e072
Грибов Максим Валентинович <gribov@inn4b.ru>
2022-04-15 08:59:06 +0000

    DBSNEW-4271

--- ForaBank/ForaBank/Model/Model+Products.swift
+++ ForaBank/ForaBank/Model/Model+Products.swift
@@ -31,6 +31,100 @@ extension Model {
         
     }
     
+    func handleProductsUpdateCustomName(_ payload: ModelAction.Products.UpdateCustomName.Request) {
+        
+        guard let token = token else {
+            handledUnauthorizedCommandAttempt()
+            return
+        }
+        
+        let id = payload.productId
+        let name = payload.name
+        let defaultErrorMessage = "Возникла техническая ошибка. Свяжитесь с технической поддержкой банка для уточнения."
+    
+        switch payload.productType {
+        case .card:
+            let command = ServerCommands.CardController.SaveCardName(token: token, payload: .init(cardNumber: nil, endDate: nil, id: id, name: name, startDate: nil, statementFormat: nil))
+            serverAgent.executeCommand(command: command) { result in
+                
+                switch result {
+                case .success(let response):
+                    switch response.statusCode {
+                    case .ok:
+                        self.action.send(ModelAction.Products.UpdateCustomName.Response.complete(name: name))
+                        
+                    default:
+                        self.handleServerCommandStatus(command: command, serverStatusCode: response.statusCode, errorMessage: response.errorMessage)
+                        self.action.send(ModelAction.Products.UpdateCustomName.Response.failed(message: response.errorMessage ?? defaultErrorMessage))
+                    }
+                case .failure(let error):
+                    self.action.send(ModelAction.PaymentTemplate.Save.Failed(error: error))
+                    self.action.send(ModelAction.Products.UpdateCustomName.Response.failed(message: defaultErrorMessage))
+                }
+            }
+            
+        case .account:
+            let command = ServerCommands.AccountController.SaveAccountName(token: token, payload: .init(accountNumber: nil, endDate: nil, id: id, name: name, startDate: nil, statementFormat: nil))
+            serverAgent.executeCommand(command: command) { result in
+                
+                switch result {
+                case .success(let response):
+                    switch response.statusCode {
+                    case .ok:
+                        self.action.send(ModelAction.Products.UpdateCustomName.Response.complete(name: name))
+                        
+                    default:
+                        self.handleServerCommandStatus(command: command, serverStatusCode: response.statusCode, errorMessage: response.errorMessage)
+                        self.action.send(ModelAction.Products.UpdateCustomName.Response.failed(message: response.errorMessage ?? defaultErrorMessage))
+                    }
+                case .failure(let error):
+                    self.action.send(ModelAction.PaymentTemplate.Save.Failed(error: error))
+                    self.action.send(ModelAction.Products.UpdateCustomName.Response.failed(message: defaultErrorMessage))
+                }
+            }
+            
+        case .deposit:
+            let command = ServerCommands.DepositController.SaveDepositName(token: token, payload: .init(endDate: nil, id: id, name: name, startDate: nil, statementFormat: nil))
+            serverAgent.executeCommand(command: command) { result in
+                
+                switch result {
+                case .success(let response):
+                    switch response.statusCode {
+                    case .ok:
+                        self.action.send(ModelAction.Products.UpdateCustomName.Response.complete(name: name))
+                        
+                    default:
+                        self.handleServerCommandStatus(command: command, serverStatusCode: response.statusCode, errorMessage: response.errorMessage)
+                        self.action.send(ModelAction.Products.UpdateCustomName.Response.failed(message: response.errorMessage ?? defaultErrorMessage))
+                    }
+                case .failure(let error):
+                    self.action.send(ModelAction.PaymentTemplate.Save.Failed(error: error))
+                    self.action.send(ModelAction.Products.UpdateCustomName.Response.failed(message: defaultErrorMessage))
+                }
+            }
+            
+        case .loan:
+            let command = ServerCommands.LoanController.SaveLoanName(token: token, payload: .init(endDate: nil, id: id, name: name, startDate: nil, statementFormat: nil))
+            serverAgent.executeCommand(command: command) { result in
+                
+                switch result {
+                case .success(let response):
+                    switch response.statusCode {
+                    case .ok:
+                        self.action.send(ModelAction.Products.UpdateCustomName.Response.complete(name: name))
+                        
+                    default:
+                        self.handleServerCommandStatus(command: command, serverStatusCode: response.statusCode, errorMessage: response.errorMessage)
+                        self.action.send(ModelAction.Products.UpdateCustomName.Response.failed(message: response.errorMessage ?? defaultErrorMessage))
+                    }
+                case .failure(let error):
+                    self.action.send(ModelAction.PaymentTemplate.Save.Failed(error: error))
+                    self.action.send(ModelAction.Products.UpdateCustomName.Response.failed(message:  defaultErrorMessage))
+                }
+            }
+        }
+    }
+    
     func reduce(products: [ProductType: [ProductData]], with productsData: [ProductData]) -> [ProductType: [ProductData]] {
         
         var productsUpdated = products
@@ -125,6 +219,22 @@ extension ModelAction {
                         let result: Result<[ProductData], Error>
                     }
                 }
+            }
+        }
+        
+        enum UpdateCustomName {
+        
+            struct Request: Action {
+                
+                let productId: ProductData.ID
+                let productType: ProductType
+                let name: String
+            }
+            
+            enum Response: Action {
+                
+                case complete(name: String)
+                case failed(message: String)
             }
         }
     }
--- ForaBank/ForaBank/Services/Server/ServerCommands+Loan.swift
+++ ForaBank/ForaBank/Services/Server/ServerCommands+Loan.swift
@@ -43,5 +43,40 @@ extension ServerCommands {
                 self.payload = payload
             }
         }
+        
+        
+        //TODO: tests
+        /*
+         http://192.168.50.113:8080/swagger-ui/index.html#/LoanController/saveLoanCustomName
+         */
+        struct SaveLoanName: ServerCommand {
+            
+            let token: String?
+            let endpoint = "/rest/saveLoanName"
+            let method: ServerCommandMethod = .post
+            let payload: BasePayload?
+            
+            struct Response: ServerResponse {
+                
+                let statusCode: ServerStatusCode
+                let errorMessage: String?
+                let data: EmptyData?
+            }
+            
+            internal init(token: String, payload: BasePayload) {
+                
+                self.token = token
+                self.payload = payload
+            }
+        }
+        
+        struct BasePayload: Encodable {
+
+            let endDate: String?
+            let id: Int
+            let name: String?
+            let startDate: String?
+            let statementFormat: StatementFormat?
+        }
     }
 }
