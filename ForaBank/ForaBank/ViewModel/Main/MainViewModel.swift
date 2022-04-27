d10d74164c4a168c0b9eb631b2bcbe15a178d8b4
Max Gribox <gribov.max@gmail.com>
2022-04-27 10:41:05 +0300

    main screen navigation buttons

--- ForaBank/ForaBank/ViewModel/Main/MainViewModel.swift
+++ ForaBank/ForaBank/ViewModel/Main/MainViewModel.swift
@@ -7,16 +7,58 @@
 
 import Foundation
 import Combine
+import SwiftUI
 
 class MainViewModel: ObservableObject {
     
+    let action: PassthroughSubject<Action, Never> = .init()
+
+    lazy var userAccountButton: UserAccountButtonViewModel = UserAccountButtonViewModel(logo: .ic12LogoForaColor, avatar: nil, name: "Александр", action: { [weak self] in self?.action.send(MainViewModelAction.ButtonTapped.UserAccount())})
+    @Published var navButtonsRight: [NavBarButtonViewModel]
     @Published var sections: [MainSectionViewModel]
     @Published var isRefreshing: Bool
     
-    init(sections: [MainSectionViewModel], isRefreshing: Bool) {
+    private var bindings = Set<AnyCancellable>()
+    
+    init(navButtonsRight: [NavBarButtonViewModel], sections: [MainSectionViewModel], isRefreshing: Bool) {
         
+        self.navButtonsRight = navButtonsRight
         self.sections = sections
         self.isRefreshing = isRefreshing
+    }
+}
+
+extension MainViewModel {
+    
+    class UserAccountButtonViewModel: ObservableObject {
+        
+        @Published var logo: Image
+        @Published var avatar: Image?
+        @Published var name: String
+        let action: () -> Void
+        
+        init(logo: Image, avatar: Image?, name: String, action: @escaping () -> Void) {
+            
+            self.logo = logo
+            self.avatar = avatar
+            self.name = name
+            self.action = action
+        }
+    }
+    
+    struct NavBarButtonViewModel: Identifiable {
+        
+        let id: UUID = UUID()
+        let icon: Image
+        let action: () -> Void
+    }
+}
+
+enum MainViewModelAction {
+
+    enum ButtonTapped {
+        
+        struct UserAccount: Action {}
     }
 }
 
