4776da934d2e68dd138def7cb5f6f3f0ecb4b0fa
Dmitriy <da.savushkin4@gmail.com>
2022-05-27 10:53:50 +0400

    product profile init

--- ForaBank/ForaBank/Components/ProductProfile/ProductProfileCardViewComponent.swift
+++ ForaBank/ForaBank/Components/ProductProfile/ProductProfileCardViewComponent.swift
@@ -39,13 +39,17 @@ extension ProductProfileCardView {
 
             let products = model.products.value.values.flatMap{ $0 }
             let productData = products.first(where: ({$0.id == productId}))
-    
-            if let balance = productData?.displayBalance, let fontColor = productData?.fontDesignColor.color, let name = productData?.displayName, let backgroundColor = productData?.background.first?.color {
+            
+            if let productData = products.first(where: ({$0.id == productId})), let balance = productData.displayBalance, let backgroundColor = productData.background.first?.color {
                 
-                self.product = .init(id: 12, header: .init(logo: nil, number: productData?.displayNumber, period: nil), name: name, footer: .init(balance: balance, paymentSystem: nil), statusAction: .init(status: .activation, style: .profile, action: {}), appearance: .init(textColor: fontColor, background: .init(color: backgroundColor, image: nil), size: .normal), isUpdating: false, productType: .init(rawValue: productData?.productType.rawValue ?? "card") ?? .card, action: {})
+                let fontColor = productData.fontDesignColor.color
+                let name = productData.displayName
+                
+                self.product = .init(id: productData.id, header: .init(logo: nil, number: productData.displayNumber, period: nil), name: name, footer: .init(balance: balance, paymentSystem: nil), statusAction: .init(status: .activation, style: .profile, action: {}), appearance: .init(textColor: fontColor, background: .init(color: backgroundColor, image: nil), size: .normal), isUpdating: false, productType: productData.productType, action: {})
                 
             } else {
                 
+                //FIXME: derty hack remove!
                 self.product = .init(id: 13, header: .init(logo: nil, number: productData?.number, period: nil), name: productData?.mainField ?? "", footer: .init(balance: productData?.displayBalance ?? "", paymentSystem: nil), statusAction: nil, appearance: .init(textColor: productData?.fontDesignColor.color ?? .init(""), background: .init(color: productData?.background.first?.color ?? .init(""), image: productData?.extraLargeDesign.image!), size: .normal), isUpdating: true, productType: ProductType(rawValue: productData?.productType.rawValue ?? "CARD") ?? .card, action: {})
             }
             
