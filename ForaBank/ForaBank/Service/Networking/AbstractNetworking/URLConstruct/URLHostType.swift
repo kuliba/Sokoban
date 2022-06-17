11a9113ca5223d04d1a61de72159aae993e78f3f
Грибов Максим Валентинович <gribov@inn4b.ru>
2022-06-17 07:44:24 +0000

    DBSNEW-5124

--- ForaBank/ForaBank/Service/Networking/AbstractNetworking/URLConstruct/URLHostType.swift
+++ ForaBank/ForaBank/Service/Networking/AbstractNetworking/URLConstruct/URLHostType.swift
@@ -13,8 +13,7 @@ class URLHost {
     func getHost() -> String {
         var host = "bg.forabank.ru/dbo/api/v4/f437e29a3a094bcfa73cea12366de95b"
         #if DEBUG
-        host = "test.inn4b.ru/dbo/api/v3"
-//        host = "git.briginvest.ru/dbo/api/v3"
+        host = "pl.forabank.ru/dbo/api/v3"
         #endif
         return host
     }
@@ -25,6 +24,6 @@ class URLHost {
         case release  = "bg.forabank.ru/dbo/api/v4/f437e29a3a094bcfa73cea12366de95b"
         
         /// Для релизных запросов используется этот хост
-        case qa = "test.inn4b.ru/dbo/api/v3"
+        case qa = "pl.forabank.ru/dbo/api/v3"
     }
 }
