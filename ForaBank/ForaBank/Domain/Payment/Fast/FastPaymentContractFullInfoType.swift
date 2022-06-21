34bce3c86c4ac02849e62fd5bc31a743c0e4f84c
Dmitry Martynov <mdymailru@gmail.com>
2022-06-21 20:08:32 +0300

    add FastPaymentContractFullInfoType+Legacy

---
+++ ForaBank/ForaBank/Domain/Payment/Fast/FastPaymentContractFullInfoType.swift
@@ -0,0 +1,15 @@
+//
+//  FastPaymentContractFullInfoType.swift
+//  ForaBank
+//
+//  Created by Dmitry Martynov on 21.06.2022.
+//
+
+import Foundation
+
+struct FastPaymentContractFullInfoType: Decodable, Equatable {
+    
+    let fastPaymentContractAccountAttributeList: [FastPaymentContractAccountAttributeTypeData]?
+    let fastPaymentContractAttributeList: [FastPaymentContractAttributeTypeData]?
+    let fastPaymentContractClAttributeList: [FastPaymentContractClAttributeTypeData]?
+}
