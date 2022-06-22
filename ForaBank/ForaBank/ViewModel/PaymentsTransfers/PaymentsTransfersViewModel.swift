5427dec8425a0de6d9692fcf0d86b89c901df764
Dmitry Martynov <mdymailru@gmail.com>
2022-06-22 20:26:20 +0300

    add FullBankInfoList

--- ForaBank/ForaBank/ViewModel/PaymentsTransfers/PaymentsTransfersViewModel.swift
+++ ForaBank/ForaBank/ViewModel/PaymentsTransfers/PaymentsTransfersViewModel.swift
@@ -109,11 +109,12 @@ class PaymentsTransfersViewModel: ObservableObject {
                     //Payments Section
                     case let payload as PTSectionPaymentsViewAction.ButtonTapped.Payment:
                         switch payload.type {
-                        case .qrPayment: //link = .exampleDetail(payload.type.rawValue) //TODO:
+                        case .qrPayment:
                             let viewModel = MeToMeSettingView.ViewModel
                                                 .init(model: model.fastPaymentContractFullInfo.value?
-                                                    .map { $0.getFastPaymentContractFindListDatum() },
-                                                closeAction: { [weak self] in self?.link = nil })
+                                                        .map { $0.getFastPaymentContractFindListDatum() },
+                                                      newModel: model,
+                                                      closeAction: { [weak self] in self?.link = nil })
                             link = .fastPaymentsSettings(viewModel)
                         case .mobile: sheet = .init(type: .exampleDetail(payload.type.rawValue)) //TODO:
                         case .service: sheet = .init(type: .exampleDetail(payload.type.rawValue)) //TODO:
