d44120021f3a54f7952f02ef261ba6316e2b04ea
Савушкин Дмитрий <da.savushkin4@gmail.com>
2022-07-29 17:53:50 +0000

    Update assets

--- ForaBank/ForaBank/Extensions/Assets/Font+Assets.swift
+++ ForaBank/ForaBank/Extensions/Assets/Font+Assets.swift
@@ -11,9 +11,9 @@
 import SwiftUI
 
 public extension Font {
-    
+
     static func buttonExtraSmallR12140() -> Font {
-        Font.custom("Inter-Regular", size: 12.0)
+        Font.custom("Inter", size: 12.0)
     }
     static func buttonLargeSB16180() -> Font {
         Font.custom("Inter-SemiBold", size: 16.0)
@@ -25,10 +25,10 @@ public extension Font {
         Font.custom("Inter-Medium", size: 14.0)
     }
     static func printFormBodyMR9120() -> Font {
-        Font.custom("Roboto", size: 9.0)
+        Font.custom("Roboto-Regular", size: 9.0)
     }
     static func printFormBodySR8100() -> Font {
-        Font.custom("Roboto", size: 8.0)
+        Font.custom("Roboto-Regular", size: 8.0)
     }
     static func printFormBodyXSBl780() -> Font {
         Font.custom("Roboto-Black", size: 7.0)
@@ -37,19 +37,22 @@ public extension Font {
         Font.custom("Roboto-Medium", size: 7.0)
     }
     static func printFormBodyXSR780() -> Font {
-        Font.custom("Roboto", size: 7.0)
+        Font.custom("Roboto-Regular", size: 7.0)
     }
     static func printFormH0B12160() -> Font {
         Font.custom("Roboto-Bold", size: 12.0)
     }
     static func printFormH1R10140() -> Font {
-        Font.custom("Roboto", size: 10.0)
+        Font.custom("Roboto-Regular", size: 10.0)
     }
     static func textBodyMM14200() -> Font {
         Font.custom("Inter-Medium", size: 14.0)
     }
+    static func textBodyMR14180() -> Font {
+        Font.custom("Inter", size: 14.0)
+    }
     static func textBodyMR14200() -> Font {
-        Font.custom("Inter-Regular", size: 14.0)
+        Font.custom("Inter", size: 14.0)
     }
     static func textBodyMSB14200() -> Font {
         Font.custom("Inter-SemiBold", size: 14.0)
@@ -58,7 +61,7 @@ public extension Font {
         Font.custom("Inter-Medium", size: 12.0)
     }
     static func textBodySR12160() -> Font {
-        Font.custom("Inter-Regular", size: 12.0)
+        Font.custom("Inter", size: 12.0)
     }
     static func textBodySSB12160() -> Font {
         Font.custom("Inter-SemiBold", size: 12.0)
@@ -67,10 +70,16 @@ public extension Font {
         Font.custom("Inter-Medium", size: 11.0)
     }
     static func textBodyXSR11140() -> Font {
-        Font.custom("Inter-Regular", size: 11.0)
+        Font.custom("Inter", size: 11.0)
     }
     static func textBodyXSSB11140() -> Font {
         Font.custom("Inter-SemiBold", size: 11.0)
+    }
+    static func textBodyXSUnderlineM11140() -> Font {
+        Font.custom("Inter-Medium", size: 11.0)
+    }
+    static func textBodyXSUnderlineR11140() -> Font {
+        Font.custom("Inter", size: 11.0)
     }
     static func textH0B32402() -> Font {
         Font.custom("Inter-Bold", size: 32.0)
@@ -85,27 +94,54 @@ public extension Font {
         Font.custom("Inter-Medium", size: 20.0)
     }
     static func textH2R20282() -> Font {
-        Font.custom("Inter-Regular", size: 20.0)
+        Font.custom("Inter", size: 20.0)
     }
     static func textH2SB20282() -> Font {
+        Font.custom("Inter-SemiBold", size: 20.0)
+    }
+    static func textH2UnderlineM20282() -> Font {
+        Font.custom("Inter-Medium", size: 20.0)
+    }
+    static func textH2UnderlineR20282() -> Font {
+        Font.custom("Inter", size: 20.0)
+    }
+    static func textH2UnderlineSB20282() -> Font {
         Font.custom("Inter-SemiBold", size: 20.0)
     }
     static func textH3M18240() -> Font {
         Font.custom("Inter-Medium", size: 18.0)
     }
     static func textH3R18240() -> Font {
-        Font.custom("Inter-Regular", size: 18.0)
+        Font.custom("Inter", size: 18.0)
     }
     static func textH3SB18240() -> Font {
+        Font.custom("Inter-SemiBold", size: 18.0)
+    }
+    static func textH3UnderlineM18240() -> Font {
+        Font.custom("Inter-Medium", size: 18.0)
+    }
+    static func textH3UnderlineR18240() -> Font {
+        Font.custom("Inter", size: 18.0)
+    }
+    static func textH3UnderlineSB18240() -> Font {
         Font.custom("Inter-SemiBold", size: 18.0)
     }
     static func textH4M16240() -> Font {
         Font.custom("Inter-Medium", size: 16.0)
     }
     static func textH4R16240() -> Font {
-        Font.custom("Inter-Regular", size: 16.0)
+        Font.custom("Inter", size: 16.0)
     }
     static func textH4SB16240() -> Font {
         Font.custom("Inter-SemiBold", size: 16.0)
+    }
+    static func textUnderlineH0B32402() -> Font {
+        Font.custom("Inter-Bold", size: 32.0)
+    }
+    static func textUnderlineH1R24322() -> Font {
+        Font.custom("Inter-Regular", size: 24.0)
+    }
+    static func textUnderlineH1SB24322() -> Font {
+        Font.custom("Inter-SemiBold", size: 24.0)
     }
 }
