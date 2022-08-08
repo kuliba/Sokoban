a8376e8ea32bd54794bb678458825fbe155a10ba
Pavel Samsonov <psamsonov@inn4b.ru>
2022-08-08 13:52:11 +0300

    Make course change

--- ForaBank/ForaBank/Components/CurrencyWallet/CurrencyExchangeConfirmationComponent.swift
+++ ForaBank/ForaBank/Components/CurrencyWallet/CurrencyExchangeConfirmationComponent.swift
@@ -23,6 +23,8 @@ extension CurrencyExchangeConfirmationView {
         let commissionLabel = "Комиссия"
         let currencySumLabel = "Сумма зачисления в валюте"
         
+        var courseChangeViewModel: CourseChangeViewModel?
+        
         init(sum: String, commission: String, currencySum: String) {
             
             self.sum = sum
@@ -36,7 +38,9 @@ extension CurrencyExchangeConfirmationView {
                   let fee = response.fee,
                   let creditAmount = response.creditAmount,
                   let currencyPayerCode = response.currencyPayer?.description,
-                  let currencyPayeeCode = response.currencyPayee?.description else {
+                  let currencyPayeeCode = response.currencyPayee?.description,
+                  let currencyPayee = response.currencyPayee,
+                  let currencyRate = response.currencyRate else {
                 return nil
             }
             
@@ -51,6 +55,43 @@ extension CurrencyExchangeConfirmationView {
             currencySum = model.amountFormatted(amount: creditAmount,
                                                 currencyCode: currencyPayeeCode,
                                                 style: .fraction) ?? String(creditAmount)
+            
+            courseChangeViewModel = makeCourseChange(currency: currencyPayee, currencyRate: currencyRate)
+        }
+    }
+}
+
+extension CurrencyExchangeConfirmationView.ViewModel {
+    
+    private func makeCourseChange(currency: Currency, currencyRate: Double) -> CurrencyExchangeConfirmationView.CourseChangeViewModel? {
+        
+        guard currencyRate > 0 else {
+            return nil
+        }
+        
+        return .init(currency: currency, currencyRate: currencyRate)
+    }
+}
+
+extension CurrencyExchangeConfirmationView {
+    
+    // MARK: - CourseChange
+    
+    class CourseChangeViewModel: ObservableObject {
+        
+        let currency: Currency
+        let currencyRate: Double
+        let icon: Image
+        
+        var title: String {
+            "Курс изменился 1\(currency.description) = \(currencyRate), мы пересчитали сумму операции"
+        }
+        
+        init(currency: Currency, currencyRate: Double, icon: Image = .init("Warning Course")) {
+            
+            self.currency = currency
+            self.currencyRate = currencyRate
+            self.icon = icon
         }
     }
 }
@@ -68,33 +109,59 @@ struct CurrencyExchangeConfirmationView: View {
             Color.mainColorsGrayLightest
                 .cornerRadius(12)
             
-            HStack {
+            VStack(spacing: 16) {
                 
-                VStack(alignment: .leading, spacing: 12) {
+                HStack {
                     
-                    Text(viewModel.sumLabel)
-                    Text(viewModel.commissionLabel)
-                    Text(viewModel.currencySumLabel)
-                }
-                .font(.textBodyMR14200())
-                .foregroundColor(.textPlaceholder)
-                
-                Spacer()
-                
-                VStack(alignment: .trailing, spacing: 12) {
+                    VStack(alignment: .leading, spacing: 12) {
+                        
+                        Text(viewModel.sumLabel)
+                        Text(viewModel.commissionLabel)
+                        Text(viewModel.currencySumLabel)
+                    }
+                    .font(.textBodyMR14200())
+                    .foregroundColor(.textPlaceholder)
                     
-                    Text(viewModel.sum)
-                    Text(viewModel.commission)
-                    Text(viewModel.currencySum)
-                }
-                .font(.textBodyMM14200())
-                .foregroundColor(.textSecondary)
-                
-            }.padding(.horizontal, 20)
-            
+                    Spacer()
+                    
+                    VStack(alignment: .trailing, spacing: 12) {
+                        
+                        Text(viewModel.sum)
+                        Text(viewModel.commission)
+                        Text(viewModel.currencySum)
+                    }
+                    .font(.textBodyMM14200())
+                    .foregroundColor(.textSecondary)
+                    
+                }.padding(.horizontal, 20)
+            }
         }
         .frame(height: 124)
         .padding(.horizontal, 20)
+    }
+}
+
+extension CurrencyExchangeConfirmationView {
+    
+    // MARK: - CourseChange
+    
+    struct CourseChangeView: View {
+        
+        @ObservedObject var viewModel: CourseChangeViewModel
+        
+        var body: some View {
+            
+            HStack {
+                
+                viewModel.icon
+                    .resizable()
+                    .frame(width: 16, height: 16)
+                
+                Text(viewModel.title)
+                    .font(.textBodyMR14200())
+                    .foregroundColor(.systemColorWarning)
+            }
+        }
     }
 }
 
