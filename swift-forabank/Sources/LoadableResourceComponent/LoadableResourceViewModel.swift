ec7115be94e728104521ecb6cf9c054a9b66d19a
Fora iOS Dev <project15_bot@example.com>
2023-06-23 21:16:10 +0000

    LoadableResourceComponent module scaffolding

---
+++ swift-forabank/Sources/LoadableResourceComponent/LoadableResourceViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoadableResourceViewModel.swift
+//  
+//
+//  Created by Igor Malyarov on 23.06.2023.
+//
+
+import Foundation
+
+public final class LoadableResourceViewModel: ObservableObject {
+    
+    public init() {}
+}
---
+++ swift-forabank/Tests/LoadableResourceComponentTests/LoadableResourceViewModelTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoadableResourceViewModelTests.swift
+//  
+//
+//  Created by Igor Malyarov on 23.06.2023.
+//
+
+import LoadableResourceComponent
+import XCTest
+
+final class LoadableResourceViewModelTests: XCTestCase {
+    
+    func test() {
+ 
+        let sut = LoadableResourceViewModel()
+    }
+}
