a1d5a01c5c3725f9ad2ad79883a5e9b7a71c67ca
Савушкин Дмитрий <da.savushkin4@gmail.com>
2023-09-08 06:13:54 +0000

    Resolve DBSNEW-9912 "Fix/ cursorposition"

--- swift-forabank/Sources/TextFieldDomain/Extensions/String+Ext.swift
+++ swift-forabank/Sources/TextFieldDomain/Extensions/String+Ext.swift
@@ -14,13 +14,13 @@ extension String {
         with replacementText: String
     ) -> String {
         
-        let valid = (0...count)
+        let valid = (0...utf16.count)
         
         guard
             valid ~= range.location,
             valid ~= range.location + range.length,
-            let rangeStart = index(startIndex, offsetBy: range.lowerBound, limitedBy: endIndex),
-            let rangeEnd = index(startIndex, offsetBy: range.upperBound, limitedBy: endIndex)
+            let rangeStart = self.utf16.index(startIndex, offsetBy: range.lowerBound, limitedBy: endIndex),
+            let rangeEnd = self.utf16.index(startIndex, offsetBy: range.upperBound, limitedBy: endIndex)
         else {
             return replacementText
         }
