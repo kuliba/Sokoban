d8ea0bd5e44eb9d21878a22fefc2d2392bdc8dbc
Dmitriy Savushkin <da.savushkin4@gmail.com>
2023-09-26 10:54:00 +0400

    add `PaymentStickerView`

--- ForaBank/ForaBank/Components/General/PaymentStickerVIewComponent.swift
+++ ForaBank/ForaBank/Components/General/PaymentStickerVIewComponent.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+struct PaymentStickerView: View {
+     
+    let viewModel: PaymentStickerView.ViewModel
+    
+    var body: some View {
+        
+        ZStack {
+
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundColor(.mainColorsGrayLightest)
+
+            VStack(alignment: .leading) {
+
+                OpenAccountItemView.HeaderView(viewModel: viewModel.header)
+
+                HStack(alignment: .top, spacing: 20) {
+
+                    OpenAccountCardView(viewModel: .sample)
+
+                    VStack {
+
+                        ForEach(viewModel.options) { option in
+                            OpenAccountItemView.OptionView(viewModel: option)
+                        }
+                    }
+                }.padding(20)
+
+                Spacer()
+            }
+        }
+        .frame(width: UIScreen.main.bounds.width - 40, height: 200)
+    }
+}
+
