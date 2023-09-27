a4d2ee646ad98c12a3a5d19d7b0f9afc612f3a5c
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-09-27 22:09:39 +0300

    extract `KeyExchangeCryptographer+live` and `SecKeySwaddleCryptographer+live`

---
+++ ForaBank/ForaBank/Services/Services/BindPublicKey/KeyExchangeCryptographer+live.swift
@@ -0,0 +1,53 @@
+//
+//  KeyExchangeCryptographer+live.swift
+//  ForaBank
+//
+//  Created by Igor Malyarov on 27.09.2023.
+//
+
+import ForaCrypto
+import Foundation
+
+extension KeyExchangeCryptographer {
+    
+    static let live: Self = .init(
+        generateP384KeyPair: generateP384KeyPair,
+        publicKeyData: publicKeyData,
+        transportEncrypt: transportEncrypt,
+        sharedSecret: sharedSecret
+    )
+    
+    private static func generateP384KeyPair(
+    ) -> P384KeyAgreementDomain.KeyPair {
+        
+        ForaCrypto.Crypto.generateP384KeyPair()
+    }
+    
+    private static func publicKeyData(
+        publicKey: PublicKey
+    ) throws -> Data {
+        
+        publicKey.derRepresentation
+    }
+    
+    private static func transportEncrypt(
+        _ data: Data
+    ) throws -> Data {
+        
+        try ForaCrypto.Crypto.transportEncrypt(data, padding: .PKCS1)
+    }
+    
+    private static func sharedSecret(
+        string: String,
+        privateKey: PrivateKey
+    ) throws -> Data {
+        
+        let serverPublicKey = try ForaCrypto.Crypto.transportDecryptP384PublicKey(from: string)
+        let sharedSecret = try ForaCrypto.Crypto.sharedSecret(
+            privateKey: privateKey,
+            publicKey: serverPublicKey
+        )
+        
+        return sharedSecret
+    }
+}
