05e2ea4d81b842b48f6975b21db6f44c50a89b71
Andrew Kurdin <exehomework@gmail.com>
2023-10-20 15:38:44 +0300

    show sticker tests and min fix

--- ForaBank/ForaBank/ViewModel/Auth/LoginViewModel/AuthLoginViewModel.swift
+++ ForaBank/ForaBank/ViewModel/Auth/LoginViewModel/AuthLoginViewModel.swift
@@ -80,7 +80,6 @@ class AuthLoginViewModel: ObservableObject {
     
     func showProducts() {
         handleLandingAction(.orderCard)
-        //handleLandingAction(.sticker) // for tests
     }
 }
 
