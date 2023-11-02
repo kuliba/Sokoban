c56564130c90e4aa5a072245abdb9422b3e4dda3
Andrew Kurdin <exehomework@gmail.com>
2023-11-02 11:06:07 +0300

    tests, read description
    я понял что выносить в helpers/dsl, и как тестить поток, только реализацию кода с stickerVM считаю не рабочей, и не вижу смысла писать тесты в этой реализации

--- ForaBank/ForaBankTests/ViewModel/Main/MainViewModelTests.swift
+++ ForaBank/ForaBankTests/ViewModel/Main/MainViewModelTests.swift
@@ -67,8 +67,48 @@ final class MainViewModelTests: XCTestCase {
         )
     }
     
-    // MARK: - Helpers
+    typealias mainSectionVMAction = MainSectionViewModelAction.Products
     
+    func test_productCarouselStickerDidTapped()  {
+        
+        let (sut, _) = makeSUTWithMainSectionProcutsViewVM()
+        _ = XCTWaiter().wait(for: [.init()], timeout: 0.5)
+        
+        let sutActionSpy = ValueSpy(
+            sut.productCarouselViewModel.action.compactMap { $0 as? ProductCarouselViewModelAction.Products.StickerDidTapped })
+        _ = XCTWaiter().wait(for: [.init()], timeout: 0.5)
+        
+        XCTAssertNoDiff(sutActionSpy.values.count, 0)
+        
+        sut.productCarouselViewModel.action.send(ProductCarouselViewModelAction.Products.StickerDidTapped())
+        
+        _ = XCTWaiter().wait(for: [.init()], timeout: 0.5)
+        
+        XCTAssertNoDiff(sutActionSpy.values.count, 1)
+       
+        XCTAssertNotNil(sutActionSpy.values.first)
+    }
+ 
+    func test_orderSticker_showsAlert_whenNoCards() {
+        
+        let localAgentDataStubClear: [String: Data] = [
+            "Array<OperatorGroupData>": .init(),
+        ]
+        let localAgent = LocalAgentStub(stub: localAgentDataStubClear)
+        let model = Model.mockWithEmptyExcept(
+          localAgent: localAgent
+        )
+        
+      let sut = MainViewModel(model)
+     
+      sut.orderSticker()
+        _ = XCTWaiter().wait(for: [.init()], timeout: 0.05)
+        
+      XCTAssertNotNil(sut.alert)
+    }
+    
+    // MARK: - Helpers
+  
     private func makeSUT(
         file: StaticString = #file,
         line: UInt = #line
@@ -82,6 +122,24 @@ final class MainViewModelTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         // TODO: restore memory leaks tracking after Model fix
         // trackForMemoryLeaks(model, file: file, line: line)
+        
+        return (sut, model)
+    }
+    
+    typealias MainSectionViewVM = MainSectionProductsView.ViewModel
+    typealias StickerViewModel = ProductCarouselView.StickerViewModel
+    
+    private func makeSUTWithMainSectionProcutsViewVM(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (
+        sut: MainSectionViewVM,
+        model: Model
+    ) {
+        let model: Model = .mockWithEmptyExcept()
+        let sut = MainSectionViewVM(model, stickerViewModel: nil)
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
         
         return (sut, model)
     }
@@ -180,6 +238,26 @@ private extension MainViewModel {
             return nil
         }
     }
+    
+    var openProductSection: MainSectionOpenProductView.ViewModel? {
+        
+        sections.compactMap {
+            
+            $0 as? MainSectionOpenProductView.ViewModel
+        }
+        .first
+    }
+    
+    var authProductsViewModel: AuthProductsViewModel? {
+        
+        switch link {
+        case let .openCard(authProductsViewModel):
+            return authProductsViewModel
+            
+        default:
+            return nil
+        }
+    }
 }
 
 private extension MainViewModel.Link {
@@ -228,4 +306,62 @@ private extension MainViewModel {
         
         _ = XCTWaiter().wait(for: [.init()], timeout: timeout)
     }
+}
+
+private extension MainViewModel {
+    
+    func closeAndWait(timeout: TimeInterval = 0.05) {
+        
+        action.send(MainViewModelAction.Close.Link())
+        
+        _ = XCTWaiter().wait(for: [.init()], timeout: timeout)
+    }
+}
+
+private extension MainSectionOpenProductView.ViewModel {
+    
+    func tapStickerAndWait(timeout: TimeInterval = 0.05) {
+        let stickerAction = MainSectionViewModelAction.Products.StickerDidTapped()
+        action.send(stickerAction)
+        
+        _ = XCTWaiter().wait(for: [.init()], timeout: timeout)
+    }
+}
+
+private extension MainViewModel {
+    
+    func showStickerAndWait(timeout: TimeInterval = 0.05) {
+        
+        let stickerAction = MainSectionViewModelAction.Products.StickerDidTapped()
+        action.send(stickerAction)
+        
+        _ = XCTWaiter().wait(for: [.init()], timeout: timeout)
+    }
+}
+
+private extension ProductCardData {
+    
+    convenience init(id: Int, currency: Currency, ownerId: Int = 0, allowCredit: Bool = true, allowDebit: Bool = true, status: ProductData.Status = .active, loanBaseParam: ProductCardData.LoanBaseParamInfoData? = nil, statusPc: ProductData.StatusPC = .active, isMain: Bool = true) {
+        
+        self.init(id: id, productType: .card, number: nil, numberMasked: nil, accountNumber: nil, balance: nil, balanceRub: nil, currency: currency.description, mainField: "", additionalField: nil, customName: nil, productName: "", openDate: nil, ownerId: ownerId, branchId: nil, allowCredit: allowCredit, allowDebit: allowDebit, extraLargeDesign: .init(description: ""), largeDesign: .init(description: ""), mediumDesign: .init(description: ""), smallDesign: .init(description: ""), fontDesignColor: .init(description: ""), background: [], accountId: nil, cardId: 0, name: "", validThru: Date(), status: status, expireDate: nil, holderName: nil, product: nil, branch: "", miniStatement: nil, paymentSystemName: nil, paymentSystemImage: nil, loanBaseParam: loanBaseParam, statusPc: statusPc, isMain: isMain, externalId: nil, order: 0, visibility: true, smallDesignMd5hash: "", smallBackgroundDesignHash: "")
+    }
+}
+
+private extension MainViewModelTests {
+    
+    static let cardActiveMainDebitOnlyMain = ProductCardData(id: 11, currency: .rub, allowCredit: false)
+    static let cardBlockedMainRub = ProductCardData(id: 12, currency: .rub, status: .blocked, statusPc: .blockedByBank)
+    
+    static let cardActiveAddUsdIsMainFalse = ProductCardData(id: 22, currency: .usd, isMain: false)
+    static let cardActiveAddUsdIsMainTrue = ProductCardData(id: 21, currency: .usd)
+}
+
+private extension MainViewModelTests {
+    
+    static let cardProducts = [cardActiveAddUsdIsMainFalse,
+                               cardActiveAddUsdIsMainTrue]
+}
+
+extension MainSectionViewModelAction.Products.StickerDidTapped: Equatable {
+    public static func == (lhs: MainSectionViewModelAction.Products.StickerDidTapped, rhs: MainSectionViewModelAction.Products.StickerDidTapped) -> Bool { return true }
 }
