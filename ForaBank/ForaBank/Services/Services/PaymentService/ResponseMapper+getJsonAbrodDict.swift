57db153d525e92fbd920c1e355b2e33569ac2e21
Dmitriy Savushkin <da.savushkin4@gmail.com>
2023-11-02 12:25:02 +0400

    update decoder for `StickerDictionary`

--- ForaBank/ForaBank/Services/Services/PaymentService/ResponseMapper+getJsonAbrodDict.swift
+++ ForaBank/ForaBank/Services/Services/PaymentService/ResponseMapper+getJsonAbrodDict.swift
@@ -53,7 +53,7 @@ extension ResponseMapper {
         }
     }
     
-    enum StickerDictionaryError: Error {
+    enum StickerDictionaryError: Error , Equatable{
         
         case error(
             statusCode: Int,
@@ -62,7 +62,7 @@ extension ResponseMapper {
         case invalidData(statusCode: Int, data: Data)
     }
     
-    enum StickerDictionary: Decodable {
+    enum StickerDictionary: Decodable, Equatable {
         
         case orderForm(StickerOrderForm)
         case deliveryOffice(DeliveryOffice)
@@ -94,21 +94,21 @@ extension ResponseMapper {
         }
     }
     
-    struct StickerOrderForm: Decodable {
+    struct StickerOrderForm: Decodable, Equatable {
         
         let header: [Header]
         let main: [Main]
         let footer: [Footer]
         let serial: String
         
-        struct Footer: Decodable {}
+        struct Footer: Decodable, Equatable {}
         
-        struct Header: Decodable {
+        struct Header: Decodable, Equatable {
             
             let type: ComponentType
             let data: Data
             
-            struct Data: Decodable {
+            struct Data: Decodable, Equatable {
                 
                 let title: String
                 let isFixed: Bool
@@ -116,101 +116,118 @@ extension ResponseMapper {
         }
     }
     
-    struct DeliveryOffice: Decodable {
+    struct DeliveryOffice: Decodable, Equatable {
         
         let main: [Main]
         let serial: String
     }
     
-    struct DeliveryCourier: Decodable {
+    struct DeliveryCourier: Decodable, Equatable {
         
         let main: [Main]
         let serial: String
     }
     
-    struct ServerError: Decodable {
+    struct ServerError: Decodable, Equatable {
         
         let statusCode: Int
         let errorMessage: String
     }
     
-    struct Main: Decodable {
+    struct Main: Decodable, Equatable {
         
         let type: ComponentType
         let data: DataType
         
-        enum DataType: Decodable {
+        private enum CodingKeys: CodingKey {
+            
+            case type
+            case data
+        }
+        
+        init(
+            type: ComponentType,
+            data: DataType
+        ) {
+            self.type = type
+            self.data = data
+        }
+        
+        init(from decoder: Decoder) throws {
+            
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            type = try container.decode(ComponentType.self, forKey: .type)
+            
+            switch type {
+            case .citySelector:
+                let citySelector = try container.decode(CitySelector.self, forKey: .data)
+                data = .citySelector(citySelector)
+                
+            case .endSeparator:
+                let endSeparator = try container.decode(Separator.self, forKey: .data)
+                data = .separator(endSeparator)
+                
+            case .officeSelector:
+                let officeSelector = try container.decode(OfficeSelector.self, forKey: .data)
+                data = .officeSelector(officeSelector)
+                
+            case .pageTitle:
+                let pageTitle = try container.decode(StickerOrderForm.Header.Data.self, forKey: .data)
+                data = .pageTitle(pageTitle)
+                
+            case .productInfo:
+                let productInfo = try container.decode(Banner.self, forKey: .data)
+                data = .banner(productInfo)
+                
+            case .productSelect:
+                let productSelect = try container.decode(Product.self, forKey: .data)
+                data = .product(productSelect)
+                
+            case .selector:
+                let selector = try container.decode(Selector.self, forKey: .data)
+                data = .selector(selector)
+                
+            case .separator:
+                let separator = try container.decode(Separator.self, forKey: .data)
+                data = .separator(separator)
+                
+            case .separatorSubGroup:
+                data = .separatorGroup
+                
+            case .textsWithIconHorizontal:
+                let textsWithIconHorizontal = try container.decode(Hint.self, forKey: .data)
+                data = .hint(textsWithIconHorizontal)
+            }
+        }
+        
+        enum DataType: Decodable, Equatable {
             
             case hint(Hint)
             case banner(Banner)
             case product(Product)
             case separator(Separator)
+            case separatorGroup
             case selector(Selector)
             case citySelector(CitySelector)
             case officeSelector(OfficeSelector)
             case pageTitle(StickerOrderForm.Header.Data)
             case noValid(String)
-            
-            init(from decoder: Decoder) throws {
-                
-                let container = try decoder.container(keyedBy: CodingKeys.self)
-                let type = try? container.decode(ComponentType.self, forKey: .type)
-                
-                switch type {
-                case .citySelector:
-                    let data = try container.decode(CitySelector.self, forKey: .data)
-                    self = .citySelector(data)
-                    
-                case .endSeparator:
-                    let data = try container.decode(Separator.self, forKey: .data)
-                    self = .separator(data)
-                    
-                case .officeSelector:
-                    let data = try container.decode(OfficeSelector.self, forKey: .data)
-                    self = .officeSelector(data)
-                    
-                case .pageTitle:
-                    let data = try container.decode(StickerOrderForm.Header.Data.self, forKey: .data)
-                    self = .pageTitle(data)
-                    
-                case .productInfo:
-                    let data = try container.decode(Banner.self, forKey: .data)
-                    self = .banner(data)
-                    
-                case .productSelect:
-                    let data = try container.decode(Product.self, forKey: .data)
-                    self = .product(data)
-                    
-                case .selector:
-                    let data = try container.decode(Selector.self, forKey: .data)
-                    self = .selector(data)
-                    
-                case .separator:
-                    let data = try container.decode(Separator.self, forKey: .data)
-                    self = .separator(data)
-                    
-                case .separatorSubGroup:
-                    let data = try container.decode(Separator.self, forKey: .data)
-                    self = .separator(data)
-                    
-                case .textsWithIconHorizontal:
-                    let data = try container.decode(Hint.self, forKey: .data)
-                    self = .hint(data)
-                    
-                case .none:
-                    self = .noValid("could not decode data")
-                }
-            }
         }
         
-        struct Selector: Decodable {
+        struct Selector: Decodable, Equatable {
             
             let title: String
             let subtitle: String
             let md5hash: String
             let list: [Option]
             
-            struct Option: Decodable {
+            private enum CodingKeys: String, CodingKey {
+                
+                case title, md5hash, list
+                case subtitle = "subTitle"
+            }
+            
+            struct Option: Decodable, Equatable {
                 
                 let type: OptionType
                 let md5hash: String
@@ -218,7 +235,7 @@ extension ResponseMapper {
                 let backgroundColor: String
                 let value: Double
                 
-                enum OptionType: String, Decodable {
+                enum OptionType: String, Decodable, Equatable {
                     
                     case typeDeliveryOffice
                     case typeDeliveryCourier
@@ -226,17 +243,17 @@ extension ResponseMapper {
             }
         }
         
-        struct Product: Decodable {
+        struct Product: Decodable, Equatable {
             
             let withoutPadding: Bool
         }
         
-        struct Separator: Decodable {
+        struct Separator: Decodable, Equatable {
             
             let color: String
         }
         
-        struct Banner: Decodable {
+        struct Banner: Decodable, Equatable {
             
             let title: String
             let subtitle: String
@@ -245,7 +262,13 @@ extension ResponseMapper {
             let md5hash: String
             let txtConditionList: [Condition]
             
-            struct Condition: Decodable {
+            private enum CodingKeys: String, CodingKey {
+                
+                case title, currencyCode, currency, md5hash, txtConditionList
+                case subtitle = "subTitle"
+            }
+            
+            struct Condition: Decodable, Equatable {
                 
                 let name: String
                 let description: String
@@ -253,29 +276,77 @@ extension ResponseMapper {
             }
         }
         
-        struct Hint: Decodable {
+        struct Hint: Decodable, Equatable {
             
-            let md5hash: String
             let title: String
+            let md5hash: String
             let contentCenterAndPull: Bool
         }
         
-        struct CitySelector: Decodable {
+        struct CitySelector: Decodable, Equatable {
             
             let title: String
             let subtitle: String
             let isCityList: Bool
             let md5hash: String
-            let list: [String?]
+            
+            private enum CodingKeys: String, CodingKey {
+                
+                case title, isCityList, md5hash
+                case subtitle = "subTitle"
+            }
+            
+            init(
+                title: String,
+                subtitle: String,
+                isCityList: Bool,
+                md5hash: String
+            ) {
+                self.title = title
+                self.subtitle = subtitle
+                self.isCityList = isCityList
+                self.md5hash = md5hash
+            }
+            
+            init(from decoder: Decoder) throws {
+                
+                let container = try decoder.container(keyedBy: CodingKeys.self)
+                title = try container.decode(String.self, forKey: .title)
+                subtitle = try container.decode(String.self, forKey: .subtitle)
+                isCityList = try container.decode(Bool.self, forKey: .isCityList)
+                md5hash = try container.decode(String.self, forKey: .md5hash)
+            }
         }
         
-        struct OfficeSelector: Decodable {
+        struct OfficeSelector: Decodable, Equatable {
             
             let title: String
             let subtitle: String
-            let isCityList: Bool
             let md5hash: String
-            let list: [String?]
+            
+            internal init(
+                title: String,
+                subtitle: String,
+                md5hash: String
+            ) {
+                self.title = title
+                self.subtitle = subtitle
+                self.md5hash = md5hash
+            }
+            
+            private enum CodingKeys: String, CodingKey {
+                
+                case title, md5hash
+                case subtitle = "subTitle"
+            }
+            
+            init(from decoder: Decoder) throws {
+                
+                let container = try decoder.container(keyedBy: CodingKeys.self)
+                title = try container.decode(String.self, forKey: .title)
+                subtitle = try container.decode(String.self, forKey: .subtitle)
+                md5hash = try container.decode(String.self, forKey: .md5hash)
+            }
         }
     }
     
@@ -285,10 +356,10 @@ extension ResponseMapper {
         case textsWithIconHorizontal = "TEXTS_WITH_ICON_HORIZONTAL"
         case productInfo = "PRODUCT_INFO"
         case productSelect = "PRODUCT_SELECT"
-        case separator = "SEPARATOR_START_OPERATOR"
         case selector = "VERTICAL_SELECTOR"
         case citySelector = "CITY_SELECTOR"
         case officeSelector = "OFFICE_SELECTOR"
+        case separator = "SEPARATOR_START_OPERATOR"
         case endSeparator = "SEPARATOR_END_OPERATOR"
         case separatorSubGroup = "SEPARATOR_FIELD_SUB_GROUP"
     }
