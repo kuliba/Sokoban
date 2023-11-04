b05b14bec4decde516f60882601368d3c056f4de
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-11-04 16:01:27 +0300

    add `activate` test to `CVVPINFunctionalityActivationServiceTests`

---
+++ swift-forabank/Tests/Services/CVVPIN_ServicesTests/CVVPINFunctionalityActivationService/CVVPINFunctionalityActivationServiceTests.swift
@@ -0,0 +1,326 @@
+//
+//  CVVPINFunctionalityActivationServiceTests.swift
+//  
+//
+//  Created by Igor Malyarov on 04.11.2023.
+//
+
+import CVVPIN_Services
+import XCTest
+
+final class CVVPINFunctionalityActivationServiceTests: XCTestCase {
+    
+    func test_init_shouldNotCallCollaborators() {
+        
+        let (_, getCodeSpy, formSessionKeySpy, bindKeySpy) = makeSUT()
+        
+        XCTAssertNoDiff(getCodeSpy.callCount, 0)
+        XCTAssertNoDiff(formSessionKeySpy.callCount, 0)
+        XCTAssertNoDiff(bindKeySpy.callCount, 0)
+    }
+    
+    // MARK: - activate
+    
+    func test_activate_shouldDeliverErrorOnGetCodeInvalidFailure() {
+        
+        let statusCode = 500
+        let invalidData = anyData()
+        let (sut, getCodeSpy, _, _) = makeSUT()
+        
+        expectActivate(sut, toDeliver: [
+            .failure(.invalid(statusCode: statusCode, data: invalidData))
+        ], on: {
+            getCodeSpy.complete(with: .failure(.invalid(statusCode: statusCode, data: invalidData)))
+        })
+    }
+    
+    func test_activate_shouldDeliverErrorOnGetCodeNetworkFailure() {
+        
+        let (sut, getCodeSpy, _, _) = makeSUT()
+        
+        expectActivate(sut, toDeliver: [.failure(.network)], on: {
+            
+            getCodeSpy.complete(with: .failure(.network))
+        })
+    }
+    
+    func test_activate_shouldDeliverErrorOnGetCodeServerFailure() {
+        
+        let statusCode = 500
+        let errorMessage = "Get Code Error"
+        let (sut, getCodeSpy, _, _) = makeSUT()
+        
+        expectActivate(sut, toDeliver: [
+            .failure(.server(statusCode: statusCode, errorMessage: errorMessage))
+        ], on: {
+            getCodeSpy.complete(with: .failure(.server(statusCode: statusCode, errorMessage: errorMessage)))
+        })
+    }
+    
+    func test_activate_shouldDeliverErrorOnFormSessionKeyInvalidFailure() {
+        
+        let statusCode = 500
+        let invalidData = anyData()
+        let (sut, getCodeSpy, formSessionKeySpy, _) = makeSUT()
+        
+        expectActivate(sut, toDeliver: [
+            .failure(.invalid(statusCode: statusCode, data: invalidData))
+        ], on: {
+            getCodeSpy.complete(with: anySuccess())
+            formSessionKeySpy.complete(with: .failure(.invalid(statusCode: statusCode, data: invalidData)))
+        })
+    }
+    
+    func test_activate_shouldDeliverErrorOnFormSessionKeyNetworkFailure() {
+        
+        let (sut, getCodeSpy, formSessionKeySpy, _) = makeSUT()
+        
+        expectActivate(sut, toDeliver: [.failure(.network)], on: {
+            
+            getCodeSpy.complete(with: anySuccess())
+            formSessionKeySpy.complete(with: .failure(.network))
+        })
+    }
+    
+    func test_activate_shouldDeliverErrorOnFormSessionKeyServerFailure() {
+        
+        let statusCode = 500
+        let errorMessage = "Forn Session Key Error"
+        let (sut, getCodeSpy, formSessionKeySpy, _) = makeSUT()
+        
+        expectActivate(sut, toDeliver: [
+            .failure(.server(statusCode: statusCode, errorMessage: errorMessage))
+        ], on: {
+            getCodeSpy.complete(with: anySuccess())
+            formSessionKeySpy.complete(with: .failure(.server(statusCode: statusCode, errorMessage: errorMessage)))
+        })
+    }
+    
+    func test_activate_shouldDeliverErrorOnFormSessionKeyServiceFailure() {
+        
+        let (sut, getCodeSpy, formSessionKeySpy, _) = makeSUT()
+        
+        expectActivate(sut, toDeliver: [.failure(.serviceFailure)], on: {
+            
+            getCodeSpy.complete(with: anySuccess())
+            formSessionKeySpy.complete(with: .failure(.serviceFailure))
+        })
+    }
+    
+    func test_activate_shouldDeliverPhoneOnSuccess() {
+        
+        let phone = UUID().uuidString
+        let (sut, getCodeSpy, formSessionKeySpy, _) = makeSUT()
+        
+        expectActivate(sut, toDeliver: [.success(.init(phoneValue: phone))], on: {
+            
+            getCodeSpy.complete(with: anySuccess(phone: phone))
+            formSessionKeySpy.complete(with: anySuccess())
+        })
+    }
+    
+    // MARK: - Helpers
+    
+    private typealias SUT = CVVPINFunctionalityActivationService
+    
+    private func makeSUT(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (
+        sut: SUT,
+        getCodeSpy: GetCodeSpy,
+        formSessionKeySpy: FormSessionKeySpy,
+        bindKeySpy: BindKeySpy
+    ) {
+        let getCodeSpy = GetCodeSpy()
+        let formSessionKeySpy = FormSessionKeySpy()
+        let bindKeySpy = BindKeySpy()
+        let sut = SUT(
+            getCode: getCodeSpy.get(completion:),
+            formSessionKey: formSessionKeySpy.form(completion:),
+            bindPublicKeyWithEventID: bindKeySpy.bind(_:completion:)
+        )
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(getCodeSpy, file: file, line: line)
+        trackForMemoryLeaks(formSessionKeySpy, file: file, line: line)
+        trackForMemoryLeaks(bindKeySpy, file: file, line: line)
+        
+        return (sut, getCodeSpy, formSessionKeySpy, bindKeySpy)
+    }
+    
+    private func expectActivate(
+        _ sut: SUT,
+        toDeliver expectedResults: [SUT.ActivateResult],
+        on action: @escaping () -> Void,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        var receivedResults = [SUT.ActivateResult]()
+        let exp = expectation(description: "wait for completion")
+        
+        sut.activate {
+            
+            receivedResults.append($0)
+            exp.fulfill()
+        }
+        
+        action()
+        
+        wait(for: [exp], timeout: 1.0)
+        
+        assert(
+            receivedResults.mapToEquatable(),
+            equals: expectedResults.mapToEquatable(),
+            file: file, line: line
+        )
+    }
+    
+    private final class GetCodeSpy {
+        
+        private(set) var completions = [SUT.GetCodeCompletion]()
+        
+        var callCount: Int { completions.count }
+        
+        func get(
+            completion: @escaping SUT.GetCodeCompletion
+        ) {
+            completions.append(completion)
+        }
+        
+        func complete(
+            with result: SUT.GetCodeResult,
+            at index: Int = 0
+        ) {
+            completions[index](result)
+        }
+    }
+    
+    private final class FormSessionKeySpy {
+        
+        private(set) var completions = [SUT.FormSessionKeyCompletion]()
+        
+        var callCount: Int { completions.count }
+        
+        func form(
+            completion: @escaping SUT.FormSessionKeyCompletion
+        ) {
+            completions.append(completion)
+        }
+        
+        func complete(
+            with result: SUT.FormSessionKeyResult,
+            at index: Int = 0
+        ) {
+            completions[index](result)
+        }
+    }
+    
+    private final class BindKeySpy {
+        
+        typealias Message = (payload: SUT.OTP, completion: SUT.BindPublicKeyWithEventIDCompletion)
+        
+        private(set) var messages = [Message]()
+        
+        var callCount: Int { messages.count }
+        
+        func bind(
+            _ payload: SUT.OTP,
+            completion: @escaping SUT.BindPublicKeyWithEventIDCompletion
+        ) {
+            messages.append((payload, completion))
+        }
+        
+        func complete(
+            with result: SUT.BindPublicKeyWithEventIDResult,
+            at index: Int = 0
+        ) {
+            messages[index].completion(result)
+        }
+    }
+}
+
+private extension Array where Element == CVVPINFunctionalityActivationService.ActivateResult {
+    
+    func mapToEquatable() -> [CVVPINFunctionalityActivationService.ActivateResult.EquatableActivateResult] {
+        
+        map { $0.mapToEquatable() }
+    }
+}
+
+private extension CVVPINFunctionalityActivationService.ActivateResult {
+    
+    func mapToEquatable() -> EquatableActivateResult {
+        
+        self
+            .map(EquatableConfirmResponse.init)
+            .mapError(EquatableError.init)
+    }
+    
+    typealias EquatableActivateResult = Swift.Result<EquatableConfirmResponse, EquatableError>
+    
+    struct EquatableConfirmResponse: Equatable {
+        
+        let phone: String
+        
+        init(response: CVVPINFunctionalityActivationService.Phone) {
+            
+            self.phone = response.phoneValue
+        }
+    }
+    
+    enum EquatableError: Error & Equatable {
+        
+        case invalid(statusCode: Int, data: Data)
+        case network
+        case server(statusCode: Int, errorMessage: String)
+        case serviceFailure
+        
+        init(error: CVVPINFunctionalityActivationService.ActivateError) {
+            
+            switch error {
+            case let .invalid(statusCode: statusCode, data: data):
+                self = .invalid(statusCode: statusCode, data: data)
+                
+            case .network:
+                self = .network
+                
+            case let .server(statusCode: statusCode, errorMessage: errorMessage):
+                self = .server(statusCode: statusCode, errorMessage: errorMessage)
+                
+            case .serviceFailure:
+                self = .serviceFailure
+            }
+        }
+        
+        enum ServiceError {
+            
+            case checkSessionFailure
+            case decryptionFailure
+            case makeJSONFailure
+        }
+    }
+}
+
+private func anySuccess(
+    code: String = UUID().uuidString,
+    phone: String = UUID().uuidString
+) -> CVVPINFunctionalityActivationService.GetCodeResult {
+    
+    .success(.init(code: code, phone: phone))
+}
+
+private func anySuccess(
+    sessionKeyValue: Data = anyData(),
+    eventIDValue: String = UUID().uuidString,
+    sessionTTL: Int = 23
+) -> CVVPINFunctionalityActivationService.FormSessionKeyResult {
+    
+    .success(
+        .init(
+            sessionKey: .init(sessionKeyValue: sessionKeyValue),
+            eventID: .init(eventIDValue: eventIDValue),
+            sessionTTL: sessionTTL
+        )
+    )
+}
