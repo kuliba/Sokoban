4ad0de2a9bdbfebce021c721fe536954b92b8bea
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-11-06 13:38:04 +0300

    extract `Fetcher` to production

--- swift-forabank/Tests/Infra/FetcherTests/FetcherCacheDecorator/FetcherCacheDecoratorTests.swift
+++ swift-forabank/Tests/Infra/FetcherTests/FetcherCacheDecorator/FetcherCacheDecoratorTests.swift
@@ -5,31 +5,8 @@
 //  Created by Igor Malyarov on 06.11.2023.
 //
 
+import Fetcher
 import XCTest
-
-protocol Fetcher<Payload, Success, Failure> {
-    
-    associatedtype Payload
-    associatedtype Success
-    associatedtype Failure: Error
-    
-    typealias FetchResult = Result<Success, Failure>
-    typealias FetchCompletion = (FetchResult) -> Void
-    
-    func fetch(
-        _ payload: Payload,
-        completion: @escaping FetchCompletion
-    )
-}
-
-extension Fetcher where Payload == Void {
-    
-    func fetch(
-        completion: @escaping FetchCompletion
-    ) {
-        fetch((), completion: completion)
-    }
-}
 
 final class FetcherCacheDecorator<Payload, Success, Failure: Error>: Fetcher {
     
