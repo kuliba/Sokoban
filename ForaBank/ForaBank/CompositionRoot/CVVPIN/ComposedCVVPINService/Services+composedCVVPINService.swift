kuliba/Sokoban

commit 388690deac5282ac8c6ad6512d7f1734479609f5

replace `RSAKeyPairCacheCleaningBindPublicKeyWithEventIDServiceDecorator` with `FetcherCacheDecorator`

ForaBank/ForaBank/CompositionRoot/CVVPIN/ComposedCVVPINService/Services+composedCVVPINService.swift
@@ -101,9 +101,9 @@ extension Services {
             process: process(payload:completion:)
         )
         
-        let rsaKeyPairCacheCleaningBindPublicKeyWithEventIDService = RSAKeyPairCacheCleaningBindPublicKeyWithEventIDServiceDecorator(
+        let rsaKeyPairCacheCleaningBindPublicKeyWithEventIDService = FetcherCacheDecorator(
             decoratee: bindPublicKeyWithEventIDService,
-            clearCache: rsaKeyPairStore.deleteCacheIgnoringResult
+            cache: rsaKeyPairStore.deleteCacheIgnoringResult
         )
         
         let activationService = CVVPINFunctionalityActivationService(
@@ -364,8 +364,8 @@ extension Services {
             otp: CVVPINFunctionalityActivationService.OTP,
             completion: @escaping CVVPINFunctionalityActivationService.BindPublicKeyWithEventIDCompletion
         ) {
-            rsaKeyPairCacheCleaningBindPublicKeyWithEventIDService.bind(
-                otp: .init(otpValue: otp.otpValue)
+            rsaKeyPairCacheCleaningBindPublicKeyWithEventIDService.fetch(
+                .init(otpValue: otp.otpValue)
             ) {
                 completion($0.mapError(CVVPINFunctionalityActivationService.BindPublicKeyError.init))
             }

commit fa6f669ac2b77b6725b38609cb906665dd47fbd7

fix bug with RSA keys deletion introduced in 388690deac5282ac8c6ad6512d7f1734479609f5

ForaBank/ForaBank/CompositionRoot/CVVPIN/ComposedCVVPINService/Services+composedCVVPINService.swift
@@ -103,7 +103,8 @@ extension Services {
         
         let rsaKeyPairCacheCleaningBindPublicKeyWithEventIDService = FetcherDecorator(
             decoratee: bindPublicKeyWithEventIDService,
-            cache: rsaKeyPairStore.deleteCacheIgnoringResult
+            handleSuccess: { _ in },
+            handleFailure: clearRSACacheOnError
         )
         
         let activationService = CVVPINFunctionalityActivationService(
@@ -318,6 +319,15 @@ extension Services {
             bindPublicKeyWithEventIDRemoteService.process(payload) {
                 
                 completion($0.mapError { .init($0) })
+            }
+        }
+        
+        func clearRSACacheOnError(
+            _ error: BindPublicKeyWithEventIDService.Failure
+        ) {
+            // clear cache if retryAttempts == 0
+            if case .server = error {
+                rsaKeyPairStore.deleteCacheIgnoringResult()
             }
         }
         