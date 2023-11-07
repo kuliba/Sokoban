281c69699f58d3641affc6098445ac3332da42f3
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-11-07 11:03:41 +0300

    add `confirmWith` tests

--- ForaBank/ForaBankTests/CompositionRoot/CVVPIN/ComposedCVVPINService/ComposedCVVPINService/ComposedCVVPINServiceTests.swift
+++ ForaBank/ForaBankTests/CompositionRoot/CVVPIN/ComposedCVVPINService/ComposedCVVPINService/ComposedCVVPINServiceTests.swift
@@ -61,6 +61,69 @@ final class ComposedCVVPINServiceTests: XCTestCase {
         expectActivate(sut, toDeliver: .success(.init(phone)))
     }
     
+    // MARK: - confirmWith
+    
+    func test_confirmWith_shouldDeliverServiceErrorOnConfirmInvalidFailure() {
+        
+        let statusCode = 500
+        let invalidData = anyData()
+        let sut = makeSUT(
+            confirmActivationResult: .failure(.invalid(statusCode: statusCode, data: invalidData))
+        )
+        
+        expectConfirmWith(sut, toDeliver: .failure(.serviceFailure))
+    }
+    
+    func test_confirmWith_shouldDeliverServiceErrorOnConfirmNetworkFailure() {
+        
+        let sut = makeSUT(
+            confirmActivationResult: .failure(.network)
+        )
+        
+        expectConfirmWith(sut, toDeliver: .failure(.serviceFailure))
+    }
+    
+    func test_confirmWith_shouldDeliverRetryErrorOnConfirmRetryFailure() {
+        
+        let statusCode = 500
+        let errorMessage = "Confirm Failure. Retry"
+        let retryAttempts = 5
+        let sut = makeSUT(
+            confirmActivationResult: .failure(.retry(statusCode: statusCode, errorMessage: errorMessage, retryAttempts: retryAttempts))
+        )
+        
+        expectConfirmWith(sut, toDeliver: .failure(.retry(statusCode: statusCode, errorMessage: errorMessage, retryAttempts: retryAttempts)))
+    }
+    
+    func test_confirmWith_shouldDeliverServerErrorOnConfirmServerFailure() {
+        
+        let statusCode = 500
+        let errorMessage = "Confirm Failure. Retry"
+        let sut = makeSUT(
+            confirmActivationResult: .failure(.server(statusCode: statusCode, errorMessage: errorMessage))
+        )
+        
+        expectConfirmWith(sut, toDeliver: .failure(.server(statusCode: statusCode, errorMessage: errorMessage)))
+    }
+    
+    func test_confirmWith_shouldDeliverServiceErrorOnConfirmServiceFailure() {
+        
+        let sut = makeSUT(
+            confirmActivationResult: .failure(.serviceFailure)
+        )
+        
+        expectConfirmWith(sut, toDeliver: .failure(.serviceFailure))
+    }
+    
+    func test_confirmWith_shouldDeliverVoidOnConfirmSuccess() {
+        
+        let sut = makeSUT(
+            confirmActivationResult: .success(())
+        )
+        
+        expectConfirmWith(sut, toDeliver: .success(()))
+    }
+    
     // MARK: - Helpers
     
     private typealias SUT = ComposedCVVPINService
@@ -113,6 +176,37 @@ final class ComposedCVVPINServiceTests: XCTestCase {
                 .success(received)
             ):
                 XCTAssertNoDiff(expected, received, file: file, line: line)
+                
+            default:
+                XCTFail("Expected \(expectedResult), but got \(receivedResult).", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        })
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    private func expectConfirmWith(
+        _ sut: SUT,
+        toDeliver expectedResult: ActivateCVVPINClient.ConfirmationResult,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        let exp = expectation(description: "wait for completion")
+        
+        sut.confirmWith(otp: UUID().uuidString, completion: { receivedResult in
+            
+            switch (expectedResult, receivedResult) {
+                
+            case let (
+                .failure(expected as NSError),
+                .failure(received as NSError)
+            ):
+                XCTAssertNoDiff(expected, received, file: file, line: line)
+                
+            case (.success(()), .success(())):
+                break
                 
             default:
                 XCTFail("Expected \(expectedResult), but got \(receivedResult).", file: file, line: line)
