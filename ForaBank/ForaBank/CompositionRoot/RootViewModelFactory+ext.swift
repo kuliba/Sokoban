9500ec6b5ce4ad90cea595a16fd2d487bb9df6b0
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-11-07 16:08:41 +0300

    move extension to separate file

---
+++ ForaBank/ForaBank/CompositionRoot/RootViewModelFactory+ext.swift
@@ -0,0 +1,118 @@
+//
+//  RootViewModelFactory+ext.swift
+//  ForaBank
+//
+//  Created by Igor Malyarov on 07.11.2023.
+//
+
+import Foundation
+
+extension RootViewModelFactory {
+    
+    static func make(
+        model: Model,
+        logger: LoggerAgentProtocol
+    ) -> RootViewModel {
+        
+        let httpClient = model.authenticatedHTTPClient()
+        
+        let (productProfileViewModelFactory, onExit) = make(
+            httpClient: httpClient,
+            logger: logger,
+            model: model
+        )
+        
+        return make(
+            model: model,
+            productProfileViewModelFactory: productProfileViewModelFactory,
+            onExit: onExit
+        )
+    }
+}
+
+// TODO: needs better naming
+private extension RootViewModelFactory {
+    
+    typealias MakeProductProfileViewModelFactory = (ProductData, String, @escaping () -> Void) -> ProductProfileViewModel?
+    typealias OnExit = () -> Void
+    
+    static func make(
+        model: Model,
+        productProfileViewModelFactory: @escaping MakeProductProfileViewModelFactory,
+        onExit: @escaping OnExit
+    ) -> RootViewModel {
+        
+        let mainViewModel = MainViewModel(
+            model,
+            productProfileViewModelFactory: productProfileViewModelFactory,
+            onExit: onExit
+        )
+        
+        let paymentsViewModel = PaymentsTransfersViewModel(
+            model: model,
+            productProfileViewModelFactory: productProfileViewModelFactory,
+            onExit: onExit
+        )
+        
+        let chatViewModel = ChatViewModel()
+        
+        let informerViewModel = InformerView.ViewModel(model)
+        
+        return .init(
+            mainViewModel: mainViewModel,
+            paymentsViewModel: paymentsViewModel,
+            chatViewModel: chatViewModel,
+            informerViewModel: informerViewModel,
+            model,
+            onExit: onExit
+        )
+    }
+    
+    static func make(
+        httpClient: HTTPClient,
+        logger: LoggerAgentProtocol,
+        model: Model
+    ) -> (MakeProductProfileViewModelFactory, OnExit) {
+        
+        let rsaKeyPairStore = makeLoggingStore(
+            logger: logger
+        )
+        
+        let onExit = rsaKeyPairStore.deleteCacheIgnoringResult
+        
+        let cvvPINServicesClient = Services.cvvPINServicesClient(
+            httpClient: httpClient,
+            logger: logger,
+            rsaKeyPairStore: rsaKeyPairStore
+        )
+        
+        let productProfileViewModelFactory = {
+            
+            ProductProfileViewModel(
+                model,
+                cvvPINServicesClient: cvvPINServicesClient,
+                onExit: onExit,
+                product: $0,
+                rootView: $1,
+                dismissAction: $2
+            )
+        }
+        
+        return (productProfileViewModelFactory, onExit)
+    }
+    
+    static func makeLoggingStore(
+        logger: LoggerAgentProtocol
+    ) -> any Store<RSADomain.KeyPair> {
+        
+        let log = { logger.log(level: $0, category: .cache, message: $1, file: $2, line: $3) }
+        
+        let store = KeyTagKeyChainStore<RSADomain.KeyPair>(keyTag: .rsa)
+        let rsaKeyPairStore = LoggingStoreDecorator(
+            decoratee: store,
+            log: log
+        )
+        
+        return rsaKeyPairStore
+    }
+}
