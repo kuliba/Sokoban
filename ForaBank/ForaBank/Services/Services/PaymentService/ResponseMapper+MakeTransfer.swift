710b38cffd9aeef4a4a9d5ea9af96b5f71a0c136
Dmitriy Savushkin <da.savushkin4@gmail.com>
2023-11-09 09:30:11 +0400

    add `mapMakeTransferResponse`

--- ForaBank/ForaBank/Services/Services/PaymentService/ResponseMapper+MakeTransfer.swift
+++ ForaBank/ForaBank/Services/Services/PaymentService/ResponseMapper+MakeTransfer.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+extension ResponseMapper {
+    
+    typealias MakeTransferResult = Result<MakeTransferResponse, MakeTransferError>
+    
+    static func mapMakeTransferResponse(
+        _ data: Data,
+        _ httpURLResponse: HTTPURLResponse
+    ) -> MakeTransferResult {
+        
+        do {
+            
+            switch httpURLResponse.statusCode {
+            case 200:
+                    
+                let commissionProductTransfer = try JSONDecoder().decode(MakeTransferResponse.self, from: data)
+                return .success(commissionProductTransfer)
+                
+            default:
+                
+                let serverError = try JSONDecoder().decode(ServerError.self, from: data)
+                return .failure(.error(
+                    statusCode: serverError.statusCode,
+                    errorMessage: serverError.errorMessage
+                ))
+            }
+            
+        } catch {
+            return .failure(.invalidData(
+                statusCode: httpURLResponse.statusCode, data: data
+            ))
+            
+        }
+    }
+    
+    enum MakeTransferError: Error , Equatable{
+        
+        case error(
+            statusCode: Int,
+            errorMessage: String
+        )
+        case invalidData(statusCode: Int, data: Data)
+    }
+}
