0da6668b3b3e908d0a61631792e064390b4a037b
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-11-10 10:47:27 +0300

    add tests to `Services_makeAuthWithPublicKeyServiceTests`

--- ForaBank/ForaBankTests/CompositionRoot/CVVPIN/ComposedCVVPINService/CVVPINServicesFactory/Services+makeAuthWithPublicKeyServiceTests.swift
+++ ForaBank/ForaBankTests/CompositionRoot/CVVPIN/ComposedCVVPINService/CVVPINServicesFactory/Services+makeAuthWithPublicKeyServiceTests.swift
@@ -22,6 +22,110 @@ final class Services_makeAuthWithPublicKeyServiceTests: XCTestCase {
         XCTAssertEqual(cacheSpy.callCount, 0)
     }
     
+    func test_fetch_shouldDeliverErrorOnPrepareExchangeFailure() {
+        
+        let (sut, prepareKeyExchangeSpy, _, _, _) = makeSUT()
+        
+        expect(sut, toDeliver: .failure(.serviceError(.prepareKeyExchangeFailure)), on: {
+            
+            prepareKeyExchangeSpy.complete(with: .failure(anyError()))
+        })
+    }
+    
+    func test_fetch_shouldDeliverErrorOnAuthRemoteServiceCreateRequestFailure() {
+        
+        let (sut, prepareKeyExchangeSpy, authRemoteServiceSpy, _, _) = makeSUT()
+        
+        expect(sut, toDeliver: .failure(.network), on: {
+            
+            prepareKeyExchangeSpy.complete(with: .success(anyData()))
+            authRemoteServiceSpy.complete(with: .failure(.createRequest(anyError())))
+        })
+    }
+    
+    func test_fetch_shouldDeliverErrorOnAuthRemoteServicePerformRequestFailure() {
+        
+        let (sut, prepareKeyExchangeSpy, authRemoteServiceSpy, _, _) = makeSUT()
+        
+        expect(sut, toDeliver: .failure(.network), on: {
+            
+            prepareKeyExchangeSpy.complete(with: .success(anyData()))
+            authRemoteServiceSpy.complete(with: .failure(.performRequest(anyError())))
+        })
+    }
+    
+    func test_fetch_shouldDeliverErrorOnAuthRemoteServiceMapResponseInvalidFailure() {
+        
+        let statusCode = 500
+        let invalidData = anyData()
+        let (sut, prepareKeyExchangeSpy, authRemoteServiceSpy, _, _) = makeSUT()
+        
+        expect(sut, toDeliver: .failure(.invalid(statusCode: statusCode, data: invalidData)), on: {
+            
+            prepareKeyExchangeSpy.complete(with: .success(anyData()))
+            authRemoteServiceSpy.complete(with: .failure(.mapResponse(.invalid(statusCode: statusCode, data: invalidData))))
+        })
+    }
+    
+    func test_fetch_shouldDeliverErrorOnAuthRemoteServiceMapResponseNetworkFailure() {
+        
+        let (sut, prepareKeyExchangeSpy, authRemoteServiceSpy, _, _) = makeSUT()
+        
+        expect(sut, toDeliver: .failure(.network), on: {
+            
+            prepareKeyExchangeSpy.complete(with: .success(anyData()))
+            authRemoteServiceSpy.complete(with: .failure(.mapResponse(.network)))
+        })
+    }
+    
+    func test_fetch_shouldDeliverErrorOnAuthRemoteServiceMapResponseServerFailure() {
+        
+        let statusCode = 500
+        let errorMessage = "Map Response Failure"
+        let (sut, prepareKeyExchangeSpy, authRemoteServiceSpy, _, _) = makeSUT()
+        
+        expect(sut, toDeliver: .failure(.server(statusCode: statusCode, errorMessage: errorMessage)), on: {
+            
+            prepareKeyExchangeSpy.complete(with: .success(anyData()))
+            authRemoteServiceSpy.complete(with: .failure(.mapResponse(.server(statusCode: statusCode, errorMessage: errorMessage))))
+        })
+    }
+    
+    func test_fetch_shouldDeliverErrorOnMakeSessionKeyFailure() {
+        
+        let (sut, prepareKeyExchangeSpy, authRemoteServiceSpy, makeSessionKeySpy, _) = makeSUT()
+        
+        expect(sut, toDeliver: .failure(.serviceError(.makeSessionKeyFailure)), on: {
+            
+            prepareKeyExchangeSpy.complete(with: .success(anyData()))
+            authRemoteServiceSpy.complete(with: anySuccess())
+            makeSessionKeySpy.complete(with: .failure(anyError()))
+        })
+    }
+    
+    func test_fetch_shouldDeliverValueOnSuccess() {
+        
+        let sessionIDValue = UUID().uuidString
+        let publicServerSessionKeyValue = UUID().uuidString
+        let sessionTTL = 11
+        let sessionKeyValue = anyData()
+        let (sut, prepareKeyExchangeSpy, authRemoteServiceSpy, makeSessionKeySpy, _) = makeSUT()
+        
+        expect(sut, toDeliver: .success(.init(
+            sessionID: .init(sessionIDValue: sessionIDValue),
+            sessionKey: .init(sessionKeyValue: sessionKeyValue),
+            sessionTTL: sessionTTL
+        )), on: {
+            
+            prepareKeyExchangeSpy.complete(with: .success(anyData()))
+            authRemoteServiceSpy.complete(with: anySuccess(
+                sessionIDValue: sessionIDValue,
+                sessionTTL: sessionTTL
+            ))
+            makeSessionKeySpy.complete(with: .success(.init(sessionKeyValue: sessionKeyValue)))
+        })
+    }
+    
     // MARK: - Helpers
     
     private typealias SUT = Services.AuthWithPublicKeyService
@@ -58,6 +162,65 @@ final class Services_makeAuthWithPublicKeyServiceTests: XCTestCase {
         return (sut, prepareKeyExchangeSpy, authRemoteServiceSpy, makeSessionKeySpy, cacheSpy)
     }
     
+    private func expect(
+        _ sut: any SUT,
+        toDeliver expectedResult: Result<AuthenticateWithPublicKeyService.Success, AuthenticateWithPublicKeyService.Error>,
+        on action: @escaping () -> Void,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        let exp = expectation(description: "wait for completion")
+        
+        sut.fetch { receivedResult in
+            
+            switch (receivedResult, expectedResult) {
+            case let (
+                .failure(.invalid(statusCode: receivedStatusCode, data: receivedInvalidData)),
+                .failure(.invalid(statusCode: expectedStatusCode, data: expectedInvalidData))
+            ):
+                XCTAssertNoDiff(receivedStatusCode, expectedStatusCode, file: file, line: line)
+                XCTAssertNoDiff(receivedInvalidData, expectedInvalidData, file: file, line: line)
+                
+            case (.failure(.network), .failure(.network)):
+                break
+                
+            case let (
+                .failure(.server(statusCode: receivedStatusCode, errorMessage: receivedErrorMessage)),
+                .failure(.server(statusCode: expectedStatusCode, errorMessage: expectedErrorMessage))
+            ):
+                XCTAssertNoDiff(receivedStatusCode, expectedStatusCode, file: file, line: line)
+                XCTAssertNoDiff(receivedErrorMessage, expectedErrorMessage, file: file, line: line)
+                
+            case let (
+                .failure(.serviceError(receivedServiceError)),
+                .failure(.serviceError(expectedServiceError))
+            ):
+                switch (receivedServiceError, expectedServiceError) {
+                case (.activationFailure, .activationFailure),
+                    (.makeSessionKeyFailure, .makeSessionKeyFailure),
+                    (.missingRSAPublicKey, .missingRSAPublicKey),
+                    (.prepareKeyExchangeFailure, .prepareKeyExchangeFailure):
+                    break
+                    
+                default:
+                    XCTFail("\nExpected \(expectedServiceError), but got \(receivedServiceError) instead.", file: file, line: line)
+                }
+                
+            case let (.success(received), .success(expected)):
+                XCTAssertNoDiff(received.equatable, expected.equatable, file: file, line: line)
+                
+            default:
+                XCTFail("\nExpected \(expectedResult), but got \(receivedResult) instead.", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+        
+        action()
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     private final class CacheSpy {
         
         private(set) var callCount: Int = 0
@@ -68,4 +231,36 @@ final class Services_makeAuthWithPublicKeyServiceTests: XCTestCase {
             callCount += 1
         }
     }
+}
+
+private extension AuthenticateWithPublicKeyService.Success {
+    
+    var equatable: EquatableSuccess {
+        
+        .init(
+            sessionIDValue: sessionID.sessionIDValue,
+            sessionKeyValue: sessionKey.sessionKeyValue,
+            sessionTTL: sessionTTL
+        )
+    }
+    
+    struct EquatableSuccess: Equatable {
+        
+        let sessionIDValue: String
+        let sessionKeyValue: Data
+        let sessionTTL: Int
+    }
+}
+
+private func anySuccess(
+    sessionIDValue: String = UUID().uuidString,
+    publicServerSessionKeyValue: String = UUID().uuidString,
+    sessionTTL: Int = 45
+) -> Result<AuthenticateWithPublicKeyService.Response, MappingRemoteServiceError<AuthenticateWithPublicKeyService.APIError>> {
+    
+    .success(.init(
+        sessionID: sessionIDValue,
+        publicServerSessionKey: publicServerSessionKeyValue,
+        sessionTTL: sessionTTL
+    ))
 }
