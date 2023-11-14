ebe026803ee3a2b8e58e60e01d0de7252ad0d5e6
Tasha Andryushina <natalykn@gmail.com>
2023-11-14 13:00:45 +0300

    add tests

--- ForaBank/ForaBankTests/Model/Payments/Model+AntifraudDataTests.swift
+++ ForaBank/ForaBankTests/Model/Payments/Model+AntifraudDataTests.swift
@@ -11,11 +11,11 @@ import XCTest
 final class Model_AntifraudDataTests: XCTestCase {
     
     // MARK: - paymentsAntifraudData
-
+    
     func test_paymentsAntifraudData_serviceNotSfp_shouldReturnNil() {
         
         let operation: Payments.Operation = .init(service: .avtodor)
-
+        
         let sut: Model = .mockWithEmptyExcept()
         
         let result = sut.paymentsAntifraudData(for: operation)
@@ -26,35 +26,111 @@ final class Model_AntifraudDataTests: XCTestCase {
     func test_paymentsAntifraudData_serviceSfp_sfpAntifraudNil_shouldReturnNil() {
         
         let operation: Payments.Operation = .init(service: .sfp)
-
+        
         let sut: Model = .mockWithEmptyExcept()
         
         let result = sut.paymentsAntifraudData(for: operation)
         
         XCTAssertNil(result)
-    }  
+    }
     
     func test_paymentsAntifraudData_serviceSfp_sfpAntifraudValueG_shouldReturnNil() {
         
-        let paramOne = Payments.ParameterMock(
-            id: .sfpAntifraudId,
-            value: "G")
-        let stepOne = Payments.Operation.Step(
-            parameters: [paramOne],
-            front: .empty(),
-            back: .empty())
-
-        let operation: Payments.Operation = .init(
-            service: .sfp,
-            source: nil,
-            steps: [stepOne],
-            visible: [])
-
+        let operation = operation(parameters: [paramSfpAntifraudG])
+        
         let sut: Model = .mockWithEmptyExcept()
         
         let result = sut.paymentsAntifraudData(for: operation)
         
         XCTAssertNil(result)
+    }
+    
+    func test_paymentsAntifraudData_serviceSfp_sfpAntifraudValueNotGWithoutAmount_shouldReturnNil() {
+        
+        let operation = operation(parameters: [paramSfpAntifraud, paramPhone, paramRecipient])
+        
+        let sut: Model = .mockWithEmptyExcept()
+        
+        let result = sut.paymentsAntifraudData(for: operation)
+        
+        XCTAssertNil(result)
+    }
+    
+    func test_paymentsAntifraudData_serviceSfp_sfpAntifraudValueNotGWithoutPhone_shouldReturnNil() {
+        
+        let operation = operation(parameters: [paramSfpAntifraud, paramAmount, paramPhone])
+        
+        let sut: Model = .mockWithEmptyExcept()
+        
+        let result = sut.paymentsAntifraudData(for: operation)
+        
+        XCTAssertNil(result)
+    }
+    
+    func test_paymentsAntifraudData_serviceSfp_sfpAntifraudValueNotGWithoutRecipient_shouldReturnNil() {
+        
+        let operation = operation(parameters: [paramSfpAntifraud, paramAmount, paramRecipient])
+        
+        let sut: Model = .mockWithEmptyExcept()
+        
+        let result = sut.paymentsAntifraudData(for: operation)
+        
+        XCTAssertNil(result)
+    }
+    
+    func test_paymentsAntifraudData_serviceSfp_sfpAntifraudValueNotGWithAllParameters_shouldReturnData() {
+        
+        let operation = operation(parameters: [paramSfpAntifraud, paramAmount, paramPhone, paramRecipient])
+        
+        let sut: Model = .mockWithEmptyExcept()
+        
+        let result = sut.paymentsAntifraudData(for: operation)
+        
+        let antifraudData: Payments.AntifraudData? = .init(
+            payeeName: "Иванов",
+            phone: "+7 963 000-00-00",
+            amount: "- 1231 P")
+        
+        XCTAssertNoDiff(result?.equatable, antifraudData?.equatable)
+    }
+    
+    // MARK: - Helpers
+    
+    let paramSfpAntifraud = Payments.ParameterMock(
+        id: .sfpAntifraudId,
+        value: "G1")
+    
+    let paramSfpAntifraudG = Payments.ParameterMock(
+        id: .sfpAntifraudId,
+        value: "G")
+    
+    let paramAmount = Payments.ParameterMock(
+        id: .amountParameterId,
+        value: "1231 P")
+    
+    let paramPhone = Payments.ParameterMock(
+        id: .phoneParameterId,
+        value: "79630000000")
+    
+    let paramRecipient = Payments.ParameterMock(
+        id: .recipientParameterId,
+        value: "Иванов")
+    
+    func operation(
+        service: Payments.Service = .sfp,
+        parameters: [any PaymentsParameterRepresentable]
+    ) -> Payments.Operation {
+        
+        let stepOne = Payments.Operation.Step(
+            parameters: parameters,
+            front: .empty(),
+            back: .empty())
+        
+        return .init(
+            service: service,
+            source: nil,
+            steps: [stepOne],
+            visible: [])
     }
 }
 
@@ -64,4 +140,24 @@ private extension String {
     static let phoneParameterId: Self = Payments.Parameter.Identifier.sfpPhone.rawValue
     static let recipientParameterId: Self = Payments.Parameter.Identifier.sftRecipient.rawValue
     static let sfpAntifraudId: Self = Payments.Parameter.Identifier.sfpAntifraud.rawValue
+}
+
+private extension Payments {
+    
+    struct EquatableAntifraudData: Equatable {
+        
+        let payeeName: String
+        let phone: String
+        let amount: String
+    }
+}
+
+private extension Payments.AntifraudData {
+    
+    var equatable: Payments.EquatableAntifraudData {
+        .init(
+            payeeName: self.payeeName,
+            phone: self.phone,
+            amount: self.amount)
+    }
 }
