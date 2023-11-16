2fb40cf2896e5d69dbb2ab695de4eb5b5e60bc1e
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-11-16 09:04:32 +0300

    add `QRReaderButton`

---
+++ ForaBank/PreviewApps/SberQRPreview/Buttons/QRReaderButton.swift
@@ -0,0 +1,26 @@
+//
+//  QRReaderButton.swift
+//  ForaBank
+//
+//  Created by Igor Malyarov on 16.11.2023.
+//
+
+import SwiftUI
+
+struct QRReaderButton: View {
+    
+    let action: () -> Void
+    
+    var body: some View {
+        
+        Button(action: action) {
+            Image(systemName: "qrcode.viewfinder")
+        }
+    }
+}
+
+struct QRReaderButton_Previews: PreviewProvider {
+    static var previews: some View {
+        QRReaderButton {}
+    }
+}
