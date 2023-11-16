1505043ebfe32ebedd4b532ccc96d6921462f12e
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-11-16 08:37:54 +0300

    add `SberQRPreviewApp`

---
+++ ForaBank/PreviewApps/SberQRPreview/SberQRPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SberQRPreviewApp.swift
+//  SberQRPreview
+//
+//  Created by Igor Malyarov on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SberQRPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
