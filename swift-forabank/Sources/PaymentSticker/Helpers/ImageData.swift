aff4c0c1f468d3830bd192555be7d66d45456337
Dmitriy Savushkin <da.savushkin4@gmail.com>
2023-11-17 16:54:35 +0400

    add `ImageData`

--- swift-forabank/Sources/PaymentSticker/Helpers/ImageData.swift
+++ swift-forabank/Sources/PaymentSticker/Helpers/ImageData.swift
@@ -1,8 +1,40 @@
 //
-//  File.swift
+//  ImageData.swift
 //  
 //
 //  Created by Дмитрий Савушкин on 17.11.2023.
 //
 
 import Foundation
+
+public enum ImageData: Hashable {
+    
+    case data(Data?)
+    case named(String)
+}
+
+extension ImageData {
+    
+    var data: Data? {
+    
+        guard case let .data(data) = self
+        else { return nil }
+        
+        return data
+    }
+    
+    var name: String? {
+    
+        guard case let .named(name) = self
+        else { return nil }
+        
+        return name
+    }
+    
+    static let empty: Self = .data(.empty)
+}
+
+extension Data {
+    
+    static let empty: Data = .init()
+}
