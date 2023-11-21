e345a367e9ed4d033c7c33fbeed2d62305542f0d
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-11-21 12:17:19 +0300

    implement `ButtonWithSheet`

--- swift-forabank/Sources/UI/ButtonWithSheet/ButtonWithSheet.swift
+++ swift-forabank/Sources/UI/ButtonWithSheet/ButtonWithSheet.swift
@@ -7,28 +7,109 @@
 
 import SwiftUI
 
-public struct ButtonWithSheet<ButtonLabel: View>: View {
+public struct ButtonWithSheet<ButtonLabel, SheetState, SheetStateView>: View
+where ButtonLabel: View,
+      SheetState: Identifiable & Hashable,
+      SheetStateView: View {
     
-    private let action: () -> Void
-    private let label: () -> ButtonLabel
+    public typealias GetSheetState = (@escaping (SheetState) -> Void) -> Void
+    public typealias MakeButtonLabel = () -> ButtonLabel
+    public typealias MakeSheetStateView = (SheetState) -> SheetStateView
+    
+    @State private var sheetState: SheetState?
+    
+    private let label: MakeButtonLabel
+    private let getSheetState: GetSheetState
+    private let sheetStateView: MakeSheetStateView
     
     public init(
-        action: @escaping () -> Void,
-        label: @escaping () -> ButtonLabel
+        label: @escaping MakeButtonLabel,
+        getSheetState: @escaping GetSheetState,
+        sheetStateView: @escaping MakeSheetStateView
     ) {
-        self.action = action
         self.label = label
+        self.getSheetState = getSheetState
+        self.sheetStateView = sheetStateView
     }
     
     public var body: some View {
         
-        Button(action: action, label: label)
+        Button(action: updateState, label: label)
+            .sheet(item: $sheetState, content: sheetStateView)
+    }
+    
+    private func updateState() {
+        
+        getSheetState { sheetState = $0 }
     }
 }
 
 #Preview {
-    ButtonWithSheet(action: {}) {
+    
+    HStack(spacing: 32) {
         
-        Image(systemName: "square.and.arrow.up.on.square")
+        Group {
+            
+            buttonWithSheet(
+                title: "sync",
+                systemName: "flag.2.crossed",
+                getSheetState: { completion in
+                    
+                    completion(Item.preview)
+                }
+            )
+            
+            buttonWithSheet(
+                title: "async",
+                systemName: "flag.2.crossed.circle",
+                getSheetState: { completion in
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        
+                        completion(Item.preview)
+                    }
+                }
+            )
+        }
+        .imageScale(.large)
+        .padding()
+        .background(Color.gray.opacity(0.2))
+        .clipShape(Capsule())
     }
+}
+
+private func buttonWithSheet(
+    title: String,
+    systemName: String,
+    getSheetState: @escaping (@escaping (Item) -> Void) -> Void
+) -> some View {
+    
+    ButtonWithSheet(
+        label: { Label(title, systemImage: systemName) },
+        getSheetState: getSheetState,
+        sheetStateView: { item in
+            
+            VStack(spacing: 48) {
+                
+                Text(item.title)
+                    .font(.title.bold())
+                
+                Text(item.subtitle)
+                    .foregroundColor(.secondary)
+            }
+        }
+    )
+}
+
+private struct Item: Identifiable & Hashable {
+    
+    let title: String
+    let subtitle: String
+    
+    var id: String { title }
+    
+    static let preview: Self = .init(
+        title: "Item Title",
+        subtitle: "This is Item Subtitle"
+    )
 }
