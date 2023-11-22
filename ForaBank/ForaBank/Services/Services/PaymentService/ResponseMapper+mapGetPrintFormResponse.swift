adf6d4b67294ea785747d8045459e0f71353a53e
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-11-22 15:15:06 +0300

    add `ResponseMapper+mapGetPrintFormResponse`

---
+++ ForaBank/ForaBank/Services/Services/PaymentService/ResponseMapper+mapGetPrintFormResponse.swift
@@ -0,0 +1,52 @@
+//
+//  ResponseMapper+mapGetPrintFormResponse.swift
+//  ForaBank
+//
+//  Created by Igor Malyarov on 22.11.2023.
+//
+
+import Foundation
+
+extension ResponseMapper {
+    
+#warning("PDF???")
+    typealias GetPrintFormResult = Result<Data, GetPrintFormError>
+    
+    static func mapGetPrintFormResponse(
+        _ data: Data,
+        _ httpURLResponse: HTTPURLResponse
+    ) -> GetPrintFormResult {
+        
+        do {
+            switch httpURLResponse.statusCode {
+            case 200:
+                let response = try JSONDecoder().decode(Response.self, from: data)
+                let data = try response.data.get(orThrow: NSError(domain: "Empty data", code: -1))
+                if let errorMessage = response.errorMessage {
+                    return .failure(.server(statusCode: response.statusCode, errorMessage: errorMessage))
+                } else {
+                    return .success(data)
+                }
+                
+            default:
+                let error = try JSONDecoder().decode(ServerError.self, from: data)
+                return .failure(.server(statusCode: error.statusCode, errorMessage: error.errorMessage))
+            }
+        } catch {
+            return .failure(.invalidData(statusCode: httpURLResponse.statusCode, data: data))
+        }
+    }
+    
+    enum GetPrintFormError: Error {
+        
+        case invalidData(statusCode: Int, data: Data)
+        case server(statusCode: Int, errorMessage: String)
+    }
+    
+    private struct Response: Decodable {
+        
+        let statusCode: Int
+        let errorMessage: String?
+        let data: Data?
+    }
+}
