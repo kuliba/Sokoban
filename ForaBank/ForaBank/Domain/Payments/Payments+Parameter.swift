0f7445c2feac56908e53aedebb80532b371d4f0c
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-11-24 23:18:01 +0300

    change default `placement` for `ParameterSuccessAdditionalButtons` to `bottom`

--- ForaBank/ForaBank/Domain/Payments/Payments+Parameter.swift
+++ ForaBank/ForaBank/Domain/Payments/Payments+Parameter.swift
@@ -1556,7 +1556,7 @@ extension Payments {
         let options: [Option]
         let placement: Payments.Parameter.Placement
         
-        init(options: [Option], placement: Payments.Parameter.Placement = .feed) {
+        init(options: [Option], placement: Payments.Parameter.Placement = .bottom) {
             
             self.parameter = .init(id: Payments.Parameter.Identifier.successAdditionalButtons.rawValue, value: nil)
             self.options = options
