6260d7d6827c7eb0f88d7c211873e2e15dae45d1
Dmitriy Savushkin <da.savushkin4@gmail.com>
2023-11-28 12:44:41 +0400

    restruct helper

--- swift-forabank/Sources/PaymentSticker/Views/InputView.swift
+++ swift-forabank/Sources/PaymentSticker/Views/InputView.swift
@@ -79,33 +79,6 @@ struct InputView: View {
     }
 }
 
-extension ReducerTextFieldViewModel {
-    
-    var text: String? { state.text }
-    
-    func textPublisher(
-        scheduler: AnySchedulerOfDispatchQueue = .makeMain()
-    ) -> AnyPublisher<String?, Never> {
-        
-        $state
-            .map(\.text)
-            .eraseToAnyPublisher()
-    }
-    
-    func isEditing(
-        scheduler: AnySchedulerOfDispatchQueue = .makeMain()
-    ) -> AnyPublisher<Bool, Never> {
-        
-        $state
-            .map(\.isEditing)
-            .eraseToAnyPublisher()
-    }
-    
-    // MARK: - Support existing API
-    
-    var hasValue: Bool { text != "" && text != nil }
-}
-
 public struct InputConfiguration {
 
     let titleFont: Font
