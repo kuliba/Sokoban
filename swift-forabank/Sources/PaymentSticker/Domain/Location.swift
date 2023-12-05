cab7a13a17b80183a0a27ad1abac433d3217a3d0
Dmitriy Savushkin <da.savushkin4@gmail.com>
2023-12-05 16:49:03 +0400

    location extract to file

--- swift-forabank/Sources/PaymentSticker/Domain/Location.swift
+++ swift-forabank/Sources/PaymentSticker/Domain/Location.swift
@@ -1,8 +1,17 @@
 //
-//  File.swift
+//  Location.swift
 //  
 //
 //  Created by Дмитрий Савушкин on 05.12.2023.
 //
 
 import Foundation
+
+public struct Location: Hashable, Identifiable {
+    
+    public let id: String
+    
+    public init(id: String) {
+        self.id = id
+    }
+}
