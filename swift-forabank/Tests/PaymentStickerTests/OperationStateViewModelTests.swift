bd45a52aae980cbee1675e46f33b969fda96d2a2
Dmitriy Savushkin <da.savushkin4@gmail.com>
2023-12-06 16:33:01 +0400

    make sut for operationStateViewModel

--- swift-forabank/Tests/PaymentStickerTests/OperationStateViewModelTests.swift
+++ swift-forabank/Tests/PaymentStickerTests/OperationStateViewModelTests.swift
@@ -19,6 +19,22 @@ final class OperationStateViewModelTests {
     //MARK: Helpers
     
     func makeSUT() -> OperationStateViewModel {
+    func makeSUT(
+        parameters: [Parameter] = [],
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> OperationStateViewModel {
+        
+        let sut = OperationStateViewModel(
+            state: .operation(.init(parameters: parameters)),
+            blackBoxGet: { _,_ in }
+        )
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+
+        return sut
+    }
+    
         
         return .init(state: .operation(.init(parameters: [])), blackBoxGet: { _,_ in })
     }
