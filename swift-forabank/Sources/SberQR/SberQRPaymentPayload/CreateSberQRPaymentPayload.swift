633d68a6319d512da4d5031b6fec0ce888d4e6b2
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-12-07 10:37:19 +0300

    extract `CreateSberQRPaymentPayload` to production

---
+++ swift-forabank/Sources/SberQR/SberQRPaymentPayload/CreateSberQRPaymentPayload.swift
@@ -0,0 +1,56 @@
+//
+//  CreateSberQRPaymentPayload.swift
+//
+//
+//  Created by Igor Malyarov on 06.12.2023.
+//
+
+import Foundation
+import Tagged
+
+public struct CreateSberQRPaymentPayload {
+    
+    public let qrLink: URL
+    public let product: Product
+    public let amount: Amount?
+    
+    public init(
+        qrLink: URL,
+        product: Product,
+        amount: Amount?
+    ) {
+        self.qrLink = qrLink
+        self.product = product
+        self.amount = amount
+    }
+}
+
+public extension CreateSberQRPaymentPayload {
+    
+    enum Product {
+        
+        case card(CardID)
+        case account(AccountID)
+        
+        public typealias CardID = Tagged<_CardID, Int>
+        public enum _CardID {}
+        
+        public typealias AccountID = Tagged<_AccountID, Int>
+        public enum _AccountID {}
+    }
+    
+    struct Amount {
+        
+        public let amount: Decimal
+        public let currency: Currency
+        
+        public init(amount: Decimal, currency: Currency) {
+            
+            self.amount = amount
+            self.currency = currency
+        }
+        
+        public typealias Currency = Tagged<_Currency, String>
+        public enum _Currency {}
+    }
+}
