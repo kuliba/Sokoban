74285e87dcaf69936c40e6ef1bf508bde74e1af1
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-12-07 08:50:37 +0300

    rename file

--- ForaBank/ForaBank/ViewModel/Main/QRTypealiases.swift
+++ ForaBank/ForaBank/ViewModel/Main/QRTypealiases.swift
@@ -1,12 +1,12 @@
 //
-//  SberQR.swift
+//  QRTypealiases.swift
 //  ForaBank
 //
 //  Created by Igor Malyarov on 05.12.2023.
 //
 
 import Foundation
-// QRScanner
+
 typealias MakeQRScannerModel = (@escaping () -> Void) -> QRViewModel
 
 typealias GetSberQRDataResult = (Result<Data, Error>)
