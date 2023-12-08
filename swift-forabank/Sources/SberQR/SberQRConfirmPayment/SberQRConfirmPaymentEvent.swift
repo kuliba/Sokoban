8852e6ce3ec7cf53ba7e1aba342c0c3e2e4579d4
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-12-08 11:07:57 +0300

    move events into one file

--- swift-forabank/Sources/SberQR/SberQRConfirmPayment/SberQRConfirmPaymentEvent.swift
+++ swift-forabank/Sources/SberQR/SberQRConfirmPayment/SberQRConfirmPaymentEvent.swift
@@ -5,8 +5,28 @@
 //  Created by Igor Malyarov on 07.12.2023.
 //
 
+import Foundation
+
 public enum SberQRConfirmPaymentEvent {
     
     case editable(EditableAmountEvent)
     case fixed(FixedAmountEvent)
+}
+
+public extension SberQRConfirmPaymentEvent {
+    
+    enum EditableAmountEvent {
+        
+        case editAmount(Decimal)
+        case toggleProductSelect
+        case pay
+        case select(ProductSelect.Product.ID)
+    }
+    
+    enum FixedAmountEvent {
+        
+        case toggleProductSelect
+        case pay
+        case select(ProductSelect.Product.ID)
+    }
 }
