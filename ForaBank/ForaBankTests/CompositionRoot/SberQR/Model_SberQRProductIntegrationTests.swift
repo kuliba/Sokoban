5676dc74c216a0b62893e3b48d3f8905db9926b3
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-12-09 13:24:02 +0300

    extract helpers into `SberQRProductTests`

--- ForaBank/ForaBankTests/CompositionRoot/SberQR/Model_SberQRProductIntegrationTests.swift
+++ ForaBank/ForaBankTests/CompositionRoot/SberQR/Model_SberQRProductIntegrationTests.swift
@@ -9,7 +9,7 @@
 import SberQR
 import XCTest
 
-final class Model_SberQRProductIntegrationTests: XCTestCase {
+final class Model_SberQRProductIntegrationTests: SberQRProductTests {
     
     func test_sberQRProducts_shouldReturnEmptyOnEmptyAllProducts() {
         
@@ -159,73 +159,5 @@ final class Model_SberQRProductIntegrationTests: XCTestCase {
         XCTAssertNoDiff(products.map(\.id), [3, 5])
         XCTAssertNoDiff(products.count, 2)
         XCTAssertNoDiff(sut.allProducts.count, 5)
-    }
-    
-    // MARK: - Helpers
-    
-    private typealias SUT = Model
-    private typealias Parameter = GetSberQRDataResponse.Parameter
-    private typealias FilterProductType = GetSberQRDataResponse.Parameter.ProductSelect.Filter.ProductType
-    private typealias Currency = GetSberQRDataResponse.Parameter.ProductSelect.Filter.Currency
-    
-    private func makeSUT(
-        file: StaticString = #file,
-        line: UInt = #line
-    ) -> SUT {
-        
-        let sut: SUT = .mockWithEmptyExcept()
-        
-        trackForMemoryLeaks(sut, file: file, line: line)
-        
-        return sut
-    }
-    
-    private func makeGetSberQRDataResponse(
-        with parameters: [Parameter],
-        required: [GetSberQRDataResponse.Required] = []
-    ) -> GetSberQRDataResponse {
-        
-        .init(
-            qrcID: UUID().uuidString,
-            parameters: parameters,
-            required: required
-        )
-    }
-    
-    private func amount() -> Parameter {
-        
-        .amount(.init(
-            id: .paymentAmount,
-            value: 123.45,
-            title: "Title",
-            validationRules: [],
-            button: .init(
-                title: "Pay",
-                action: .paySberQR,
-                color: .red
-            )
-        ))
-    }
-    
-    private func header() -> Parameter {
-        
-        .header(.init(id: .title, value: "Title"))
-    }
-    
-    private func productSelect(
-        productTypes: [FilterProductType],
-        currencies: [Currency]
-    ) -> Parameter {
-        
-        .productSelect(.init(
-            id: .init(UUID().uuidString),
-            value: nil,
-            title: "Title",
-            filter: .init(
-                productTypes: productTypes,
-                currencies: currencies,
-                additional: false
-            )
-        ))
     }
 }
