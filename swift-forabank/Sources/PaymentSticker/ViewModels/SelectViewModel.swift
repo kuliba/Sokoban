c27d769b7cd74102ffc988da6532efc04e9c6432
Dmitriy Savushkin <da.savushkin4@gmail.com>
2023-12-13 10:09:29 +0400

    select view model helpers

--- swift-forabank/Sources/PaymentSticker/ViewModels/SelectViewModel.swift
+++ swift-forabank/Sources/PaymentSticker/ViewModels/SelectViewModel.swift
@@ -13,25 +13,62 @@ public struct SelectViewModel {
     public typealias OptionID = ParameterSelect.State.OptionsListViewModel.OptionViewModel
     
     let parameter: ParameterSelect
-    let icon: ImageData
+    let isSearching: Bool
     let tapAction: () -> Void
     let select: (OptionID) -> Void
+    let search: (String) -> Void
     
     public init(
         parameter: SelectViewModel.ParameterSelect,
-        icon: ImageData,
+        isSearching: Bool,
         tapAction: @escaping () -> Void,
-        select: @escaping (SelectViewModel.OptionID) -> Void
+        select: @escaping (SelectViewModel.OptionID) -> Void,
+        search: @escaping (String) -> Void
     ) {
         self.parameter = parameter
-        self.icon = icon
+        self.isSearching = isSearching
         self.tapAction = tapAction
         self.select = select
+        self.search = search
     }
     
     public enum TapAction {
         
         case chevronButtonTapped
         case openBranch
+    }
+}
+
+//MARK: Helper
+
+extension SelectViewModel {
+    
+    var icon: ImageData {
+       
+        switch parameter.id {
+        case .citySelector:
+            return .named("ic24MapPin")
+        case .officeSelector:
+            return .named("ic24Bank")
+        default:
+            return .named("ic24ArrowDownCircle")
+        }
+    }
+}
+
+extension SelectViewModel {
+    
+    public init(
+        parameter: Operation.Parameter.Select,
+        tapAction: @escaping () -> Void,
+        select: @escaping (SelectViewModel.OptionID) -> Void,
+        search: @escaping (String) -> Void
+    ) {
+       
+        self.parameter = parameter
+        self.isSearching = parameter.id == .citySelector ? true : false
+        self.tapAction = tapAction
+        self.select = select
+        self.search = search
     }
 }
