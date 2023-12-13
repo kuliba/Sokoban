15944fca8250f0f501645c494f2444c71386baa5
Dmitriy Savushkin <da.savushkin4@gmail.com>
2023-12-13 10:10:17 +0400

    input view convenience init

--- swift-forabank/Sources/PaymentSticker/Parameters/Input.swift
+++ swift-forabank/Sources/PaymentSticker/Parameters/Input.swift
@@ -12,17 +12,21 @@ public extension Operation.Parameter {
     struct Input: Hashable {
         
         public var value: String
-        public let title: String
+        public let title: Title
         public let warning: String?
         
         public init(
             value: String,
-            title: String,
+            title: Title,
             warning: String?
         ) {
             self.value = value
             self.title = title
             self.warning = warning
+        }
+        
+        public enum Title: String {
+            case code = "Введите код из смс"
         }
     }
 }
