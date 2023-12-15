e1b5f27ee20e8c479374954618e9e22e29536eae
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-12-15 15:31:50 +0300

    tweak `DecimalFormatter`

--- swift-forabank/Sources/SberQR/SberQRConfirmPayment/View/Components/DecimalFormatter.swift
+++ swift-forabank/Sources/SberQR/SberQRConfirmPayment/View/Components/DecimalFormatter.swift
@@ -13,9 +13,14 @@ public struct DecimalFormatter {
     
     private let formatter: NumberFormatter
     
-    public init(currencySymbol: String) {
-        
-        self.formatter = .currency(with: currencySymbol)
+    public init(
+        currencySymbol: String,
+        locale: Locale = .current
+    ) {
+        self.formatter = .currency(
+            with: currencySymbol,
+            locale: locale
+        )
     }
     
     public func format(_ decimal: Decimal) -> String? {
@@ -26,7 +31,7 @@ public struct DecimalFormatter {
     public func number(from string: String?) -> Decimal {
         
         guard let string = string,
-                let number = formatter.number(from: string)
+              let number = formatter.number(from: string)
         else { return .zero }
         
         return number.decimalValue
@@ -39,7 +44,7 @@ public struct DecimalFormatter {
     ) -> String {
         
         let decimalSeparator = formatter.decimalSeparator
-
+        
         var allowedCharacterSet = CharacterSet.decimalDigits
         
         if allowDecimalSeparator, let decimalSeparator {
@@ -55,13 +60,22 @@ public struct DecimalFormatter {
             return allowedCharacterSet.contains(first)
         })
     }
+    
+    /// Returns `true` if `text` has `decimalSeparator`.
+    public func hasDecimalSeparator(
+        _ text: String
+    ) -> Bool {
+        
+        text.contains(formatter.decimalSeparator)
+    }
 }
 
 private extension NumberFormatter {
     
     // NumberFormatter+Extensions.swift:21
     static func currency(
-        with currencySymbol: String
+        with currencySymbol: String,
+        locale: Locale = .current
     ) -> NumberFormatter {
         
         let formatter = NumberFormatter()
@@ -69,7 +83,7 @@ private extension NumberFormatter {
         formatter.currencySymbol = currencySymbol
         formatter.minimumFractionDigits = 0
         formatter.maximumFractionDigits = 2
-        formatter.locale = Locale.current
+        formatter.locale = locale
         return formatter
     }
 }
