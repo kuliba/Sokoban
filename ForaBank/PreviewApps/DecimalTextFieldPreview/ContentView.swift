5d6255dc7fa60ffc7ba132b2f46ca62779c7ab16
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-12-15 15:40:13 +0300

    add empty `DecimalTextFieldPreview` app target

---
+++ ForaBank/PreviewApps/DecimalTextFieldPreview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DecimalTextFieldPreview
+//
+//  Created by Igor Malyarov on 15.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
