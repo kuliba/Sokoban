e269a1cba6b5c607dc5a5f2899cef2cfdced3922
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-12-23 15:27:35 +0300

    add `buttonHeight` to `ButtonConfig`

--- swift-forabank/Sources/UI/Components/PrimitiveComponents/Config/ButtonConfig.swift
+++ swift-forabank/Sources/UI/Components/PrimitiveComponents/Config/ButtonConfig.swift
@@ -11,12 +11,15 @@ public struct ButtonConfig {
     
     public let active: ButtonStateConfig
     public let inactive: ButtonStateConfig
+    public let buttonHeight: CGFloat
     
     public init(
         active: ButtonStateConfig,
-        inactive: ButtonStateConfig
+        inactive: ButtonStateConfig,
+        buttonHeight: CGFloat = 56
     ) {
         self.active = active
         self.inactive = inactive
+        self.buttonHeight = buttonHeight
     }
 }
