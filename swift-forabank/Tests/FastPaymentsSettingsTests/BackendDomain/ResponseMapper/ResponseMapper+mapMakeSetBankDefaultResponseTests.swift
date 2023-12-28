2fff77c39c3cf66dad2f508382bc030b9aa9e7b0
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-12-28 18:48:43 +0300

    extract type to file

--- swift-forabank/Tests/FastPaymentsSettingsTests/BackendDomain/ResponseMapper/ResponseMapper+mapMakeSetBankDefaultResponseTests.swift
+++ swift-forabank/Tests/FastPaymentsSettingsTests/BackendDomain/ResponseMapper/ResponseMapper+mapMakeSetBankDefaultResponseTests.swift
@@ -5,63 +5,6 @@
 //  Created by Igor Malyarov on 28.12.2023.
 //
 
-import Foundation
-
-public enum MakeSetBankDefaultMappingError: Error, Equatable {
-    
-    case invalid(statusCode: Int, data: Data)
-    case retry(errorMessage: String)
-    case server(statusCode: Int, errorMessage: String)
-}
-
-extension ResponseMapper {
-    
-    typealias MakeSetBankDefaultResponseResult = Result<Void, MakeSetBankDefaultMappingError>
-    
-    static func mapMakeSetBankDefaultResponseResponse(
-        _ data: Data,
-        _ httpURLResponse: HTTPURLResponse
-    ) -> MakeSetBankDefaultResponseResult {
-        
-        map(data, httpURLResponse, mapOrThrow: map)
-            .mapError(MakeSetBankDefaultMappingError.init(error:))
-    }
-    
-    private static func map(
-        _ data: _Data
-    ) throws -> Void {
-        
-        if data != nil { throw InvalidResponse() }
-    }
-    
-    private struct InvalidResponse: Error {}
-    
-    private typealias _Data = Data?
-}
-
-private extension MakeSetBankDefaultMappingError {
-    
-    init(error: MappingError) {
-        
-        
-        switch error {
-        case let .invalid(statusCode, data):
-            self = .invalid(statusCode: statusCode, data: data)
-            
-        case .server(_, .retryErrorMessage):
-            self = .retry(errorMessage: .retryErrorMessage)
-            
-        case let .server(statusCode, errorMessage):
-            self = .server(statusCode: statusCode, errorMessage: errorMessage)
-        }
-    }
-}
-
-private extension String {
-    
-    static let retryErrorMessage = "Введен некорректный код. Попробуйте еще раз."
-}
-
 import FastPaymentsSettings
 import XCTest
 
