f6fb6200239948fad27591093f6431468399761d
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-12-28 15:50:54 +0300

    add assert for `Void`

--- swift-forabank/Tests/FastPaymentsSettingsTests/BackendDomain/ResponseMapper/Helpers/assert.swift
+++ swift-forabank/Tests/FastPaymentsSettingsTests/BackendDomain/ResponseMapper/Helpers/assert.swift
@@ -31,3 +31,27 @@ func assert<T: Equatable>(
         XCTFail("\nExpected \(expectedResult), but got \(receivedResult) instead.", file: file, line: line)
     }
 }
+
+func assert(
+    _ receivedResult: Result<Void, MappingError>,
+    equals expectedResult: Result<Void, MappingError>,
+    file: StaticString = #file,
+    line: UInt = #line
+) {
+    switch (receivedResult, expectedResult) {
+    case let (
+        .failure(received),
+        .failure(expected)
+    ):
+        XCTAssertNoDiff(received, expected, file: file, line: line)
+        
+    case (
+        .success(()),
+        .success(())
+    ):
+        break
+        
+    default:
+        XCTFail("\nExpected \(expectedResult), but got \(receivedResult) instead.", file: file, line: line)
+    }
+}
