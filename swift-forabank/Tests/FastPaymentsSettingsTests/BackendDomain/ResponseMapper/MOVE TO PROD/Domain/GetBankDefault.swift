6156511fec92d6c4aa9f8a1ea9238fd3253b2742
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-12-28 15:46:02 +0300

    extract types to files

---
+++ swift-forabank/Tests/FastPaymentsSettingsTests/BackendDomain/ResponseMapper/MOVE TO PROD/Domain/GetBankDefault.swift
@@ -0,0 +1,11 @@
+//
+//  GetBankDefault.swift
+//
+//
+//  Created by Igor Malyarov on 28.12.2023.
+//
+
+import Tagged
+
+typealias GetBankDefault = Tagged<_GetBankDefault, Bool>
+enum _GetBankDefault {}
---
+++ swift-forabank/Tests/FastPaymentsSettingsTests/BackendDomain/ResponseMapper/MOVE TO PROD/ResponseMapper/ResponseMapper+mapGetBankDefaultResponse.swift
@@ -0,0 +1,37 @@
+//
+//  ResponseMapper+mapGetBankDefaultResponse.swift
+//
+//
+//  Created by Igor Malyarov on 28.12.2023.
+//
+
+import FastPaymentsSettings
+import Foundation
+
+extension ResponseMapper {
+    #warning("change error type to add limit error")
+    typealias GetBankDefaultResult = Result<GetBankDefault, MappingError>
+    
+    static func mapGetBankDefaultResponse(
+        _ data: Data,
+        _ httpURLResponse: HTTPURLResponse
+    ) -> GetBankDefaultResult {
+        
+        map(data, httpURLResponse, map: map)
+    }
+    
+    private static func map(
+        _ data: _Data
+    ) throws -> GetBankDefault {
+        
+        .init(data.foraBank)
+    }
+}
+
+private extension ResponseMapper {
+    
+    struct _Data: Decodable {
+        
+        let foraBank: Bool
+    }
+}
