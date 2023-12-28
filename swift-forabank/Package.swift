cb5c4528d10e081107c3d6fec605708ea634d6bc
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-12-28 09:57:26 +0300

    add empty module `FastPaymentsSettings` with tests

--- swift-forabank/Package.swift
+++ swift-forabank/Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         .macOS(.v11),
     ],
     products: [
+        .fastPaymentsSettings,
         .loadableModel,
         .loadableResourceComponent,
         .paymentSticker,
@@ -61,6 +62,8 @@ let package = Package(
         .svgKit,
     ],
     targets: [
+        .fastPaymentsSettings,
+        .fastPaymentsSettingsTests,
         .loadableModel,
         .loadableModelTests,
         .loadableResourceComponent,
@@ -141,6 +144,13 @@ let package = Package(
 )
 
 private extension Product {
+    
+    static let fastPaymentsSettings = library(
+        name: .fastPaymentsSettings,
+        targets: [
+            .fastPaymentsSettings,
+        ]
+    )
     
     static let loadableModel = library(
         name: .loadableModel,
@@ -402,6 +412,26 @@ private extension Product {
 }
 
 private extension Target {
+    
+    static let fastPaymentsSettings = target(
+        name: .fastPaymentsSettings,
+        dependencies: [
+            // external packages
+            .combineSchedulers,
+            .tagged,
+        ]
+    )
+    static let fastPaymentsSettingsTests = testTarget(
+        name: .fastPaymentsSettingsTests,
+        dependencies: [
+            // external packages
+            .combineSchedulers,
+            .customDump,
+            .tagged,
+            // internal modules
+            .fastPaymentsSettings,
+        ]
+    )
     
     static let loadableModel = target(
         name: .loadableModel,
@@ -960,6 +990,10 @@ private extension Target {
 
 private extension Target.Dependency {
     
+    static let fastPaymentsSettings = byName(
+        name: .fastPaymentsSettings
+    )
+    
     static let loadableModel = byName(
         name: .loadableModel
     )
@@ -1108,6 +1142,9 @@ private extension Target.Dependency {
 }
 
 private extension String {
+    
+    static let fastPaymentsSettings = "FastPaymentsSettings"
+    static let fastPaymentsSettingsTests = "FastPaymentsSettingsTests"
     
     static let loadableModel = "LoadableModel"
     static let loadableModelTests = "LoadableModelTests"
