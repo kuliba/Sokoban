a7fd0d79fe30e8271dbe1b9f6fe06d06c05a138a
Igor Malyarov <malyarov_i_a@forabank.ru>
2023-12-29 09:14:35 +0300

    add `RequestFactory+createGetClientConsentMe2MePullRequestTests`

---
+++ ForaBank/ForaBankTests/Services/Services/FastPayments/RequestFactory+createGetClientConsentMe2MePullRequestTests.swift
@@ -0,0 +1,58 @@
+//
+//  RequestFactory+createGetClientConsentMe2MePullRequestTests.swift
+//  ForaBankTests
+//
+//  Created by Igor Malyarov on 29.12.2023.
+//
+
+import Foundation
+
+extension RequestFactory {
+    
+    static func createGetClientConsentMe2MePullRequest(
+        url: URL
+    ) -> URLRequest {
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.cachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+        return request
+    }
+}
+
+@testable import ForaBank
+import XCTest
+
+final class RequestFactory_createGetClientConsentMe2MePullRequestTests: XCTestCase {
+    
+    func test_makeRequest_shouldSetURL() {
+        
+        let url = anyURL()
+        let request = makeRequest(url: url)
+        
+        XCTAssertNoDiff(request.url, url)
+    }
+    
+    func test_makeRequest_shouldSetHTTPMethodToGET() {
+        
+        let request = makeRequest()
+        
+        XCTAssertNoDiff(request.httpMethod, "GET")
+    }
+    
+    func test_makeRequest_shouldSetCachePolicy() {
+        
+        let request = makeRequest()
+        
+        XCTAssertNoDiff(request.cachePolicy, .reloadIgnoringLocalAndRemoteCacheData)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeRequest(
+        url: URL = anyURL(string: "any-url")
+    ) -> URLRequest {
+        
+        RequestFactory.createGetClientConsentMe2MePullRequest(url: url)
+    }
+}
