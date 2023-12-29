a135bd1ee1597e200d3cd4d1953a905b3acc04c2
Dmitriy Savushkin <da.savushkin4@gmail.com>
2023-12-29 04:41:20 +0400

    atm caching fixes

--- ForaBank/ForaBank/CompositionRoot/PaymentSticker/MakeOperationStateViewModel.swift
+++ ForaBank/ForaBank/CompositionRoot/PaymentSticker/MakeOperationStateViewModel.swift
@@ -38,8 +38,8 @@ extension RootViewModelFactory {
                 processMakeTransferService: makeTransferService.makeTransferProcess,
                 processImageLoaderService: imageLoaderService.imageProcess,
                 selectOffice: $0,
-                products: { model.productsMapper(model: model) },
-                cityList: { transferType in model.citiesMapper(transferType, model: model) }
+                products: { model.productsMapper() },
+                cityList: { transferType in model.citiesMapper(transferType) }
             )
             
             return OperationStateViewModel(blackBoxGet: businessLogic.operationResult)
@@ -387,11 +387,9 @@ private extension StickerDictionaryError {
 
 private extension Model {
     
-    func productsMapper(
-        model: Model
-    ) -> [BusinessLogic.Product] {
+    func productsMapper() -> [BusinessLogic.Product] {
         
-        let cards = model.allProducts.compactMap({ $0 as? ProductCardData })
+        let cards = allProducts.compactMap({ $0 as? ProductCardData })
         let products = cards.filter({ $0.isMain ?? false }).filter({ $0.allowDebit == true })
         
         let allProducts = products.map({ BusinessLogic.Product(
@@ -415,39 +413,31 @@ private extension Model {
     }
     
     func citiesMapper(
-        _ transferType: BusinessLogic.TransferType,
-        model: Model
+        _ transferType: BusinessLogic.TransferType
     ) -> [BusinessLogic.City] {
         
-        let cities = model.localAgent.load(type: [AtmCityData].self)
+        let cities = localAgent.load(type: [AtmCityData].self) ?? []
         
         switch transferType {
         case .courier:
             
-            return cities?
-                .filter({ $0.productList?.contains(where: { $0 == .sticker } ) ?? false })
-                .map({ BusinessLogic.City(id: $0.id.description, name: $0.name) })
-                ?? []
+            let filtered = cities
+                .filter { ($0.productList ?? []).contains { $0 == .sticker }}
+                .map { BusinessLogic.City(id: $0.id.description, name: $0.name) }
+            
+            return filtered
             
         case .office:
-            let atmData = model.dictionaryAtmList()?
-                .filter({ $0.serviceIdList.contains(where: { $0 == 140 } ) })
+            let atmData = dictionaryAtmList()?
+                .filter { $0.serviceIdList.contains { $0 == 140 }}
             
-            let atmID = atmData?.compactMap({ $0 }).map({ $0.cityId }) ?? []
+            let atmIDs = (atmData?.map(\.cityId) ?? []).uniqued()
             
-            let atmIDUniq = atmID.uniqued()
+            let filtered = cities
+                .filter { city in atmIDs.contains { $0 == city.id }}
+                .map { BusinessLogic.City(id: $0.id.description, name: $0.name) }
             
-            let filteredCity = cities?.filter({ city in
-                
-                atmIDUniq.contains(where: { $0 == city.id })
-            })
-            
-            let mapCities = (filteredCity?.map({
-                
-                BusinessLogic.City(id: $0.id.description, name: $0.name)
-            })) ?? []
-            
-            return mapCities
+            return filtered
         }
     }
 }
