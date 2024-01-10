6c6dff75ee415fe74404a65388f342240a00ba8e
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-01-10 22:58:46 +0300

    extract to production

--- swift-forabank/Tests/FastPaymentsSettingsTests/Infra/RemoteServiceAdapter/RemoteServiceAdapterTests.swift
+++ swift-forabank/Tests/FastPaymentsSettingsTests/Infra/RemoteServiceAdapter/RemoteServiceAdapterTests.swift
@@ -5,43 +5,6 @@
 //  Created by Igor Malyarov on 10.01.2024.
 //
 
-final class RemoteServiceAdapter<Input, OldOutput, Output, ProcessError>
-where ProcessError: Error {
-    
-    private let service: Service
-    private let adapt: Adapt
-    
-    init(
-        service: Service,
-        adapt: @escaping Adapt
-    ) {
-        self.service = service
-        self.adapt = adapt
-    }
-}
-
-extension RemoteServiceAdapter {
-    
-    func process(
-        _ input: Input,
-        completion: @escaping ProcessCompletion
-    ) {
-        service.process(input) { [adapt] result in
-            
-            completion(result.map(adapt))
-        }
-    }
-}
-
-extension RemoteServiceAdapter {
-    
-    typealias Service = any RemoteServiceInterface<Input, OldOutput, ProcessError>
-    typealias Adapt = (OldOutput) -> Output
-    
-    typealias ProcessResult = Result<Output, ProcessError>
-    typealias ProcessCompletion = (ProcessResult) -> Void
-}
-
 import FastPaymentsSettings
 import XCTest
 
