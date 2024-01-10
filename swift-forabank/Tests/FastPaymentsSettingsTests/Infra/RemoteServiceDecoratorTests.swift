83ab15eddf931b62415e66a44ea7aef1f1c05e8b
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-01-10 22:01:50 +0300

    add `RemoteServiceDecoratorTests`

---
+++ swift-forabank/Tests/FastPaymentsSettingsTests/Infra/RemoteServiceDecoratorTests.swift
@@ -0,0 +1,230 @@
+//
+//  RemoteServiceDecoratorTests.swift
+//
+//
+//  Created by Igor Malyarov on 10.01.2024.
+//
+
+protocol RemoteServiceInterface<Input, Output, ProcessError> {
+    
+    associatedtype Input
+    associatedtype Output
+    associatedtype ProcessError: Error
+    
+    typealias ProcessResult = Result<Output, ProcessError>
+    typealias ProcessCompletion = (ProcessResult) -> Void
+    
+    func process(
+        _ input: Input,
+        completion: @escaping ProcessCompletion
+    )
+}
+
+final class RemoteServiceDecorator<Input, Output, ProcessError>
+where ProcessError: Error {
+    
+    private let decoratee: Decoratee
+    private let decoration: Decoration
+    
+    init(
+        decoratee: Decoratee,
+        decoration: @escaping Decoration
+    ) {
+        self.decoratee = decoratee
+        self.decoration = decoration
+    }
+}
+
+extension RemoteServiceDecorator: RemoteServiceInterface {
+
+    func process(
+        _ input: Input, 
+        completion: @escaping ProcessCompletion
+    ) {
+        decoratee.process(input) { [weak self] result in
+        
+            self?.decoration(result) { completion(result) }
+        }
+    }
+}
+
+extension RemoteServiceDecorator {
+    
+    typealias Decoratee = any RemoteServiceInterface<Input, Output, ProcessError>
+    typealias Decoration = (ProcessResult, @escaping () -> Void) -> Void
+    
+    typealias ProcessResult = Result<Output, ProcessError>
+    typealias ProcessCompletion = (ProcessResult) -> Void
+}
+
+import XCTest
+
+final class RemoteServiceDecoratorTests: XCTestCase {
+    
+    func test_init_shouldNotCallCollaborator() {
+        
+        let (_, decoratorSpy, decorationSpy) = makeSUT()
+        
+        XCTAssertNoDiff(decoratorSpy.callCount, 0)
+        XCTAssertNoDiff(decorationSpy.callCount, 0)
+    }
+    
+    func test_process_shouldPassInputToDecoratee() {
+        
+        let input = makeInput()
+        let (sut, spy, _) = makeSUT()
+        
+        sut.process(input) { _ in }
+        
+        XCTAssertNoDiff(spy.payloads, [input])
+    }
+    
+    func test_process_shouldCompleteWithDecorateeFailure() {
+        
+        let failure = SUT.ProcessError()
+        let (sut, spy, decorationSpy) = makeSUT()
+        
+        expect(sut, toDeliver: .failure(failure), on: {
+            
+            spy.complete(with: .failure(failure))
+            decorationSpy.complete()
+        })
+    }
+    
+    func test_process_shouldCompleteWithDecorateeSuccess() {
+        
+        let success = SUT.Output()
+        let (sut, spy, decorationSpy) = makeSUT()
+        
+        expect(sut, toDeliver: .success(success), on: {
+            
+            spy.complete(with: .success(success))
+            decorationSpy.complete()
+        })
+    }
+    
+    func test_process_shouldDecorateDecorateeFailure() {
+        
+        let failure = SUT.ProcessError()
+        let (sut, decoratorSpy, decorationSpy) = makeSUT()
+        
+        sut.process(makeInput()) { _ in }
+        decoratorSpy.complete(with: .failure(failure))
+
+        XCTAssertNoDiff(decorationSpy.values, [.failure(failure)])
+    }
+    
+    func test_process_shouldDecorateDecorateeSuccess() {
+        
+        let success = SUT.Output()
+        let (sut, decoratorSpy, decorationSpy) = makeSUT()
+        
+        sut.process(makeInput()) { _ in }
+        decoratorSpy.complete(with: .success(success))
+
+        XCTAssertNoDiff(decorationSpy.values, [.success(success)])
+    }
+    
+    // MARK: - Helpers
+    
+    private typealias SUT = RemoteServiceDecorator<Input, Response, ProcessError>
+    private typealias Input = UUID
+    private typealias Result = Swift.Result<SUT.Output, SUT.ProcessError>
+    private typealias DecoratorSpy = Spy<Input, Response, ProcessError>
+    private typealias DecorationSpy = _DecorationSpy<Swift.Result<Response, ProcessError>>
+    
+    private func makeSUT(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (
+        sut: SUT,
+        decoratorSpy: DecoratorSpy,
+        decorationSpy: DecorationSpy
+    ) {
+        let decoratorSpy = DecoratorSpy()
+        let decorationSpy = DecorationSpy()
+        let sut = SUT(
+            decoratee: decoratorSpy,
+            decoration: decorationSpy.process
+        )
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(decoratorSpy, file: file, line: line)
+        trackForMemoryLeaks(decorationSpy, file: file, line: line)
+        
+        return (sut, decoratorSpy, decorationSpy)
+    }
+    
+    private func expect(
+        _ sut: SUT,
+        with payload: Input = makeInput(),
+        toDeliver expectedResult: Result,
+        on action: @escaping () -> Void,
+        timeout: TimeInterval = 0.05,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        let exp = expectation(description: "wait for completion")
+        
+        sut.process(makeInput()) {
+            
+            assert($0, equals: expectedResult, file: file, line: line)
+            exp.fulfill()
+        }
+        
+        action()
+        
+        wait(for: [exp], timeout: timeout)
+    }
+    
+    private struct Response: Equatable {
+        
+        let value: String
+        
+        init(value: String = UUID().uuidString) {
+            
+            self.value = value
+        }
+    }
+    
+    private struct ProcessError: Error, Equatable {
+        
+        let value: UUID
+        
+        init(value: UUID = .init()) {
+            
+            self.value = value
+        }
+    }
+    
+    private final class _DecorationSpy<Value> {
+
+        private(set) var messages = [Message]()
+        
+        var values: [Value] { messages.map(\.value) }
+        var callCount: Int { messages.count }
+        
+        func process(
+            _ value: Value,
+            _ completion: @escaping () -> Void
+        ) {
+            messages.append((value, completion))
+        }
+        
+        func complete(at index: Int = 0) {
+            
+            messages[index].completion()
+        }
+        
+        typealias Message = (value: Value, completion: () -> Void)
+    }
+}
+
+extension Spy: RemoteServiceInterface {}
+
+private func makeInput(
+    _ value: UUID = .init()
+) -> UUID {
+    
+    value
+}
