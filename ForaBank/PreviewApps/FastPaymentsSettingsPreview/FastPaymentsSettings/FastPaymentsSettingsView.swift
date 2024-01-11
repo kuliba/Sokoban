3f5e928e127ec9a3e97ce9a5df1fda574abc3e6a
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-01-11 20:39:08 +0300

    add `FastPaymentsSettingsView & Model` scaffolding

---
+++ ForaBank/PreviewApps/FastPaymentsSettingsPreview/FastPaymentsSettings/FastPaymentsSettingsView.swift
@@ -0,0 +1,26 @@
+//
+//  FastPaymentsSettingsView.swift
+//  FastPaymentsSettingsPreview
+//
+//  Created by Igor Malyarov on 11.01.2024.
+//
+
+import SwiftUI
+
+struct FastPaymentsSettingsView: View {
+    
+    @ObservedObject var viewModel: FastPaymentsSettingsViewModel
+    
+    var body: some View {
+        
+        Text("Fast Payments Settings")
+    }
+}
+
+struct FastPaymentsSettingsView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+ 
+        FastPaymentsSettingsView(viewModel: .init())
+    }
+}
---
+++ ForaBank/PreviewApps/FastPaymentsSettingsPreview/FastPaymentsSettings/FastPaymentsSettingsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FastPaymentsSettingsViewModel.swift
+//  FastPaymentsSettingsPreview
+//
+//  Created by Igor Malyarov on 11.01.2024.
+//
+
+import Foundation
+
+final class FastPaymentsSettingsViewModel: ObservableObject {
+    
+    
+}
