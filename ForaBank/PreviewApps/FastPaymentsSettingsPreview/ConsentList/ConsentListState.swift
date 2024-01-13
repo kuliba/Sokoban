f25bd0109aa5eba8b3b088391e50df5840b3cb8e
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-01-13 16:03:35 +0300

    add `ConsentListState` scaffolding

---
+++ ForaBank/PreviewApps/FastPaymentsSettingsPreview/ConsentList/ConsentListState.swift
@@ -0,0 +1,14 @@
+//
+//  ConsentListState.swift
+//  FastPaymentsSettingsPreview
+//
+//  Created by Igor Malyarov on 13.01.2024.
+//
+
+enum ConsentListState {
+    
+    case collapsed
+    case expanded
+    case collapsedError
+    case expandedError
+}
