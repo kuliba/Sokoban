2bc48bb78f242cfe85f40c1dd1596c262192dbe9
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-01-14 14:12:59 +0300

    add empty `FastPaymentsSettingsPreviewTests` target

---
+++ ForaBank/PreviewApps/FastPaymentsSettingsPreviewTests/FastPaymentsSettingsPreviewTests.swift
@@ -0,0 +1,13 @@
+//
+//  FastPaymentsSettingsPreviewTests.swift
+//  FastPaymentsSettingsPreviewTests
+//
+//  Created by Igor Malyarov on 14.01.2024.
+//
+
+@testable import FastPaymentsSettingsPreview
+import XCTest
+
+final class FastPaymentsSettingsPreviewTests: XCTestCase {
+    
+}
