4d22cc55b75aa69f340e167f93101aad55e72059
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-01-18 20:52:15 +0300

    add initial tests to `TickEffectHandlerTests`

--- swift-forabank/Tests/UI/OTPInputComponentTests/Tick/TickEffectHandlerTests.swift
+++ swift-forabank/Tests/UI/OTPInputComponentTests/Tick/TickEffectHandlerTests.swift
@@ -7,6 +7,12 @@
 
 final class TickEffectHandler {
     
+    private let initiate: Initiate
+    
+    init(initiate: @escaping Initiate) {
+        
+        self.initiate = initiate
+    }
 }
 
 extension TickEffectHandler {
@@ -15,7 +21,23 @@ extension TickEffectHandler {
         _ effect: Effect,
         _ dispatch: @escaping Dispatch
     ) {
+        switch effect {
+        case .initiate:
+            initiate { _ in }
+        }
+    }
+}
+
+extension TickEffectHandler {
+    
+    typealias InitiateResult = Result<Void, Failure>
+    typealias InitiateCompletion = (InitiateResult) -> Void
+    typealias Initiate = (@escaping InitiateCompletion) -> Void
+    
+    enum Failure: Error, Equatable {
         
+        case connectivityError
+        case serverError(String)
     }
 }
 
@@ -32,19 +54,44 @@ import XCTest
 
 final class TickEffectHandlerTests: XCTestCase {
     
+    func test_init_shouldNotCallCollaborators() {
+        
+        let (_, spy) = makeSUT()
+        
+        XCTAssertEqual(spy.callCount, 0)
+    }
+    
+    func test_initiate_shouldCallCollaborator() {
+        
+        let (sut, spy) = makeSUT()
+        
+        sut.handleEffect(.initiate) { _ in }
+        
+        XCTAssertEqual(spy.callCount, 1)
+    }
     // MARK: - Helpers
     
     private typealias SUT = TickEffectHandler
-    
+    private typealias State = SUT.State
+    private typealias Event = SUT.Event
+    private typealias Effect = SUT.Effect
+
+    private typealias InitiateSpy = Spy<Void, SUT.InitiateResult>
+
     private func makeSUT(
         file: StaticString = #file,
         line: UInt = #line
-    ) -> SUT {
+    ) -> (
+        sut: SUT,
+        spy: InitiateSpy
+    ) {
         
-        let sut = SUT()
+        let spy = InitiateSpy()
+        let sut = SUT(initiate: spy.process(completion:))
         
         trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(spy, file: file, line: line)
         
-        return sut
+        return (sut, spy)
     }
 }
