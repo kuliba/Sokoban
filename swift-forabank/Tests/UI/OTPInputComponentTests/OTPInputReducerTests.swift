9a4a147cb89a2bf316623b9165a6b0ec04983c73
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-01-18 18:18:41 +0300

    add `OTPInputReducerTests` scaffolding

---
+++ swift-forabank/Tests/UI/OTPInputComponentTests/OTPInputReducerTests.swift
@@ -0,0 +1,29 @@
+//
+//  OTPInputReducerTests.swift
+//
+//
+//  Created by Igor Malyarov on 18.01.2024.
+//
+
+final class OTPInputReducer {
+    
+}
+
+import XCTest
+
+final class OTPInputReducerTests: XCTestCase {
+    
+    // MARK: - Helpers
+    
+    private typealias SUT = OTPInputReducer
+    
+    private func makeSUT(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> SUT {
+        
+        let sut = SUT()
+        
+        return sut
+    }
+}
