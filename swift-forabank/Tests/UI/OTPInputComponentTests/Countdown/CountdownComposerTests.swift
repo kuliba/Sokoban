df7766133368761959158f362cc6884707b51d4d
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-01-19 14:52:43 +0300

    add `CountdownComposerTests`

--- swift-forabank/Tests/UI/OTPInputComponentTests/Countdown/CountdownComposerTests.swift
+++ swift-forabank/Tests/UI/OTPInputComponentTests/Countdown/CountdownComposerTests.swift
@@ -8,21 +8,33 @@
 import OTPInputComponent
 import XCTest
 
-//final class CountdownComposerTests: XCTestCase {
-//
-//    // MARK: - Helpers
-//
-//    private typealias SUT = CountdownComposer
-//
-//    private func makeSUT(
-//        file: StaticString = #file,
-//        line: UInt = #line
-//    ) -> SUT {
-//
-//        let sut = SUT()
-//
-//        trackForMemoryLeaks(sut, file: file, line: line)
-//
-//        return sut
-//    }
-//}
+final class CountdownComposerTests: XCTestCase {
+    
+    func test_init_shouldNotCallCollaborators() {
+        
+        let (_, spy) = makeSUT()
+        
+        XCTAssertEqual(spy.callCount, 0)
+    }
+    
+    // MARK: - Helpers
+    
+    private typealias SUT = CountdownComposer
+    private typealias ActivateSpy = Spy<Void, SUT.ActivateResult>
+    
+    private func makeSUT(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (
+        sut: SUT,
+        activateSpy: ActivateSpy
+    ) {
+        let activateSpy = ActivateSpy()
+        let sut = SUT(activate: activateSpy.process(completion:))
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(activateSpy, file: file, line: line)
+        
+        return (sut, activateSpy)
+    }
+}
