609338398e058e144f7252f08407018704aafcb7
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-01-19 13:36:47 +0300

    add `OTPInputComponentPreview` app

---
+++ ForaBank/PreviewApps/OTPInputComponentPreview/OTPInputComponentPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTPInputComponentPreviewApp.swift
+//  OTPInputComponentPreview
+//
+//  Created by Igor Malyarov on 19.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct OTPInputComponentPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
