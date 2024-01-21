dcfc2434b173200a30e3a6b164a4e034794eb86c
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-01-21 17:22:26 +0300

    extract reusable `TimerSpy`

--- swift-forabank/Tests/UI/OTPInputComponentTests/- Composition WIP/OTPInputBinderTests.swift
+++ swift-forabank/Tests/UI/OTPInputComponentTests/- Composition WIP/OTPInputBinderTests.swift
@@ -254,42 +254,4 @@ final class OTPInputBinderTests: XCTestCase {
         
         .init(countdown: countdown, otpField: otpField)
     }
-    
-    private final class TimerSpy: TimerProtocol {
-        
-        private let duration: Int
-        private var completions = [Completion]()
-        private(set) var messages = [Message]()
-        
-        init(duration: Int) {
-            
-            self.duration = duration
-        }
-        
-        var callCount: Int { messages.count }
-        
-        func start(
-            every interval: TimeInterval,
-            onRun completion: @escaping Completion
-        ) {
-            completions = .init(repeating: completion, count: duration/Int(interval))
-            messages.append(.start)
-        }
-        
-        func stop() {
-            
-            messages.append(.stop)
-        }
-        
-        func tick() {
-            completions.remove(at: 0)()
-        }
-        
-        typealias Completion = () -> Void
-        
-        enum Message: Equatable {
-            
-            case start, stop
-        }
-    }
 }
