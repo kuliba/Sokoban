6faac14016214084c1594dad09ae2efcea84f35a
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-01-25 21:24:09 +0300

    extract `ContractEvent`

--- swift-forabank/Sources/FastPaymentsSettings/FastPaymentsSettings/Domains/Contract/ContractEvent.swift
+++ swift-forabank/Sources/FastPaymentsSettings/FastPaymentsSettings/Domains/Contract/ContractEvent.swift
@@ -1,18 +1,15 @@
 //
 //  ContractEvent.swift
-//  
+//
 //
 //  Created by Igor Malyarov on 25.01.2024.
 //
 
-public extension FastPaymentsSettingsEvent {
+public typealias ContractUpdateResult = Result<UserPaymentSettings.PaymentContract, ServiceFailure>
+
+public enum ContractEvent: Equatable {
     
-    typealias ContractUpdateResult = Result<UserPaymentSettings.PaymentContract, ServiceFailure>
-    
-    enum ContractEvent: Equatable {
-        
-        case activateContract
-        case deactivateContract
-        case updateContract(ContractUpdateResult)
-    }
+    case activateContract
+    case deactivateContract
+    case updateContract(ContractUpdateResult)
 }
