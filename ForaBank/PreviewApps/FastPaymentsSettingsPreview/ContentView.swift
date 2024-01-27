61dfe3741afff07eae1d40bbf2d798503e984f97
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-01-27 14:53:01 +0300

    cleanup

--- ForaBank/PreviewApps/FastPaymentsSettingsPreview/ContentView.swift
+++ ForaBank/PreviewApps/FastPaymentsSettingsPreview/ContentView.swift
@@ -9,46 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var demo: Demo = .fastPaymentsSettings
-    
     var body: some View {
         
-        ZStack {
-            
-            Color.clear
-            
-            Group {
-                
-                switch demo {
-                case .consent:
-                    ConsentListPrototypeView()
-                    
-                case .fastPaymentsSettings:
-                    FPSPrototypeView()
-                }
-            }
-        }
-        .overlay(alignment: .bottomLeading, content: picker)
-    }
-    
-    private func picker() -> some View {
-        
-        Picker("Select Demo", selection: $demo) {
-            
-            ForEach(Demo.allCases, id: \.self) {
-                
-                Text($0.rawValue).tag($0)
-            }
-        }
-        .pickerStyle(.menu)
-    }
-}
-
-private extension ContentView {
-    
-    enum Demo: String, CaseIterable {
-        
-        case consent, fastPaymentsSettings
+        FPSPrototypeView()
     }
 }
 
