25d84c04d633b9cd4976f9fd8ad3f4967e4bc3f0
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-01-29 21:05:21 +0300

    add `UserAccountNavigationComponent` dependency; fix `navigationDestination` access control

--- swift-forabank/Sources/FastPaymentsSettings/Foundation/Array+ext.swift
+++ swift-forabank/Sources/FastPaymentsSettings/Foundation/Array+ext.swift
@@ -5,7 +5,7 @@
 //  Created by Igor Malyarov on 14.01.2024.
 //
 
-public extension Array where Element: Identifiable {
+extension Array where Element: Identifiable {
     
     subscript(id: Element.ID) -> Element? {
         
