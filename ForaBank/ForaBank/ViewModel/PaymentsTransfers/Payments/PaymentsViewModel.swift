71e51fde39d5fcb2325163902b2be50422db2634
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-01-31 13:02:59 +0400

    fix alignment

--- ForaBank/ForaBank/ViewModel/PaymentsTransfers/Payments/PaymentsViewModel.swift
+++ ForaBank/ForaBank/ViewModel/PaymentsTransfers/Payments/PaymentsViewModel.swift
@@ -53,7 +53,12 @@ class PaymentsViewModel: ObservableObject {
                 switch result {
                 case let .select(selectServiceParameter):
                     // multiple services for category
-                    let serviceViewModel = PaymentsServiceViewModel(category: category, parameters: [selectServiceParameter], model: model, closeAction: closeAction)
+                    let serviceViewModel = PaymentsServiceViewModel(
+                        category: category,
+                        parameters: [selectServiceParameter],
+                        model: model,
+                        closeAction: closeAction
+                    )
                     serviceViewModel.rootActions = rootActions
                     
                     await MainActor.run {
