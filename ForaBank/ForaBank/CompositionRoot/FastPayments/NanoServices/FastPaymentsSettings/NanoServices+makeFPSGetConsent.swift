689c52e30b84509b53dae8f097cfed1cf9857645
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-02-02 13:37:39 +0300

    add `NanoServices.makeFPSGetConsent`

--- ForaBank/ForaBank/CompositionRoot/FastPayments/NanoServices/FastPaymentsSettings/NanoServices+makeFPSGetConsent.swift
+++ ForaBank/ForaBank/CompositionRoot/FastPayments/NanoServices/FastPaymentsSettings/NanoServices+makeFPSGetConsent.swift
@@ -5,4 +5,45 @@
 //  Created by Igor Malyarov on 02.02.2024.
 //
 
+import FastPaymentsSettings
+import Fetcher
 import Foundation
+import GenericRemoteService
+
+extension NanoServices {
+    
+    static func makeFPSGetConsent(
+        httpClient: HTTPClient,
+        log: @escaping (String, StaticString, UInt) -> Void
+    ) -> MicroServices.Facade.GetConsent {
+        
+        let getClientConsentMe2MePullService = loggingRemoteService(
+            createRequest: ForaBank.RequestFactory.createGetClientConsentMe2MePullRequest,
+            httpClient: httpClient,
+            mapResponse: FastPaymentsSettings.ResponseMapper.mapGetClientConsentMe2MePullResponse,
+            log: log
+        )
+        
+        return { completion in
+            
+            getClientConsentMe2MePullService.fetch { //result in
+                
+                completion(Consent.init(result: $0))
+            }
+        }
+    }
+}
+
+extension Consent {
+    
+    init?(result: Result<[ConsentMe2MePull], RemoteServiceError<Error, Error, MappingError>>) {
+        
+        switch result {
+        case .failure:
+            return nil
+            
+        case let .success(consents):
+            self = .init(consents.map(\.bankID).map { .init(rawValue: $0)} )
+        }
+    }
+}
