9bcd80492a533995f5231af85fc0df4959aad69e
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-02-05 10:52:12 +0400

    fix `test_paymentsAntifraudData_serviceSfp_sfpAntifraudValueNotGWithAllParameters_shouldReturnData`

--- ForaBank/ForaBankTests/Model/Payments/Model+AntifraudDataTests.swift
+++ ForaBank/ForaBankTests/Model/Payments/Model+AntifraudDataTests.swift
@@ -87,7 +87,7 @@ final class Model_AntifraudDataTests: XCTestCase {
         let antifraudData: Payments.AntifraudData? = .init(
             payeeName: "Иванов",
             phone: "+7 963 000-00-00",
-            amount: "- 1231 ₽"
+            amount: "- 1231"
         )
         
         try XCTAssertNoDiff(XCTUnwrap(result?.equatable), antifraudData?.equatable)
