0665557104a4f3a5611123fb7e23708ee8cffa0a
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-02-05 09:46:49 +0400

    remove `cancel` action

--- ForaBank/ForaBank/ViewModel/PaymentsTransfers/Payments/PaymentsViewModel.swift
+++ ForaBank/ForaBank/ViewModel/PaymentsTransfers/Payments/PaymentsViewModel.swift
@@ -330,39 +330,6 @@ class PaymentsViewModel: ObservableObject {
                 case _ as PaymentsViewModelAction.EditName:
                     break
                     //TODO: setup open edit name sheet action
-                    
-                case let payload as PaymentsOperationViewModelAction.CancelOperation:
-                    
-                    switch payload.reason {
-                    case .cancel, .none:
-                        //TODO: move to convenience init
-                        let success = Payments.Success(
-                            operation: operationViewModel.operation.value,
-                            parameters: [
-                                Payments.ParameterSuccessStatus(status: .accepted),
-                                Payments.ParameterSuccessText(value: "Перевод отменен!", style: .warning),
-                                Payments.ParameterSuccessText(value: String(payload.amount.dropFirst()), style: .amount),
-                                //TODO: logo: .init(title: "сбп", image: .ic40Sbp)
-                                Payments.ParameterButton.actionButtonMain()
-                            ])
-                        
-                        self.successViewModel = .init(paymentSuccess: success, model)
-                        
-                    case .timeOut:
-                        //TODO: move to convenience init
-                        let success = Payments.Success(
-                            operation: operationViewModel.operation.value,
-                            parameters: [
-                                Payments.ParameterSuccessStatus(status: .accepted),
-                                Payments.ParameterSuccessText(value: "Перевод отменен!", style: .warning),
-                                Payments.ParameterSuccessText(value: "Время на подтверждение перевода вышло", style: .title),
-                                Payments.ParameterSuccessText(value: String(payload.amount.dropFirst()), style: .amount),
-                                //TODO: logo: .init(title: "сбп", image: .ic40Sbp)
-                                Payments.ParameterButton.actionButtonMain()
-                            ])
-                        
-                        self.successViewModel = .init(paymentSuccess: success, model)
-                    }
                 
                 case _ as PaymentsSuccessAction.Button.Repeat:
                     self.content = .operation(operationViewModel)
