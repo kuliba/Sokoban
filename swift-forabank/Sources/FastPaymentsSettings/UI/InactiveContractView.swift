7825e580ffaedf7042941f84853658f43f67696e
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-02-06 14:59:15 +0300

    fix `InactiveContractView` button

--- swift-forabank/Sources/FastPaymentsSettings/UI/InactiveContractView.swift
+++ swift-forabank/Sources/FastPaymentsSettings/UI/InactiveContractView.swift
@@ -16,13 +16,16 @@ struct InactiveContractView: View {
         
         VStack {
             
-            HStack(spacing: 16) {
+            Button(action: action) {
                 
-                Button("Включить переводы СБП", action: action)
-                
-                Spacer()
-                
-                ToggleMockView(status: .inactive)
+                HStack(spacing: 16) {
+                    
+                    Text("Включить переводы СБП")
+                    
+                    Spacer()
+                    
+                    ToggleMockView(status: .inactive)
+                }
             }
             
             AttributedTextView(attributedString: .consent)
