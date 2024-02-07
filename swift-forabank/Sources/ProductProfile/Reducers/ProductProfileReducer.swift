289e8340fcd4ae859f8aa6761ba82480762ee659
Tasha Andryushina <natalykn@gmail.com>
2024-02-07 13:59:13 +0300

    extract alerts

--- swift-forabank/Sources/ProductProfile/Reducers/ProductProfileReducer.swift
+++ swift-forabank/Sources/ProductProfile/Reducers/ProductProfileReducer.swift
@@ -34,98 +34,15 @@ public extension ProductProfileReducer {
             state.modal = nil
         case .showAlertChangePin:
             state.modal = nil
-            effect = .delayAlert(alertChangePin())
+            effect = .delayAlert(Alerts.alertChangePin())
 
         case let .showAlertCardGuardian(status):
             state.modal = nil
-            effect = .delayAlert(alertBlockCard(status))
+            effect = .delayAlert(Alerts.alertBlockCard(status))
         case let .showAlert(alert):
             state.alert = alert
         }
         return (state, effect)
-    }
-    
-    private func alertChangePin(
-    ) -> AlertModelOf<ProductProfileNavigation.Event> {
-        
-        .init(
-            title: "Активируйте сертификат",
-            message: "\nСертификат позволяет просматривать CVV по картам и изменять PIN-код\nв течение 6 месяцев\n\nЭто мера предосторожности во избежание мошеннических операций",
-            primaryButton: .init(
-                type: .cancel,
-                title: "Отмена",
-                event: .closeAlert),
-            secondaryButton: .init(
-                type: .default,
-                title: "Активировать",
-                event: .closeAlert)
-        )
-    }
-    
-    private func alertBlockCard(
-        _ status: CardGuardian.CardGuardianStatus
-    ) -> AlertModelOf<ProductProfileNavigation.Event> {
-        
-        .init(
-            title: titleForAlertCardGuardian(status),
-            message: messageForAlertCardGuardian(status),
-            primaryButton: .init(
-                type: .cancel,
-                title: "Отмена",
-                event: .closeAlert),
-            secondaryButton: .init(
-                type: .default,
-                title: titleSecondaryButtonForAlertCardGuardian(status),
-                event: .closeAlert)
-        )
-    }
-
-    private func titleForAlertCardGuardian(
-        _ status: CardGuardian.CardGuardianStatus
-    ) -> String {
-        
-        switch status {
-        case .active:
-            return "Заблокировать карту?"
-        case .blockedUnlockAvailable:
-            return "Разблокировать карту?"
-        case .blockedUnlockNotAvailable:
-            return "Невозможно разблокировать"
-        case .notActivated:
-            return ""
-        }
-    }
-    
-    private func messageForAlertCardGuardian(
-        _ status: CardGuardian.CardGuardianStatus
-    ) -> String? {
-        
-        switch status {
-        case .active:
-            return "Карту можно будет разблокировать в приложении или в колл-центре"
-        case .blockedUnlockAvailable:
-            return .none
-        case .blockedUnlockNotAvailable:
-            return "Обратитесь в поддержку, чтобы разблокировать карту"
-        case .notActivated:
-            return .none
-        }
-    }
-
-    private func titleSecondaryButtonForAlertCardGuardian(
-        _ status: CardGuardian.CardGuardianStatus
-    ) -> String {
-        
-        switch status {
-        case .active:
-            return "ОК"
-        case .blockedUnlockAvailable:
-            return "Да"
-        case .blockedUnlockNotAvailable:
-            return "Контакты"
-        case .notActivated:
-            return ""
-        }
     }
 }
 
