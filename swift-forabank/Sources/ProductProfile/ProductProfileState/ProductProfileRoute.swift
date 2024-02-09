5798cae4028d8b31a2db0d76237e0df1b1b2b33f
Tasha Andryushina <natalykn@gmail.com>
2024-02-09 11:43:38 +0300

    add _Destination

--- swift-forabank/Sources/ProductProfile/ProductProfileState/ProductProfileRoute.swift
+++ swift-forabank/Sources/ProductProfile/ProductProfileState/ProductProfileRoute.swift
@@ -12,4 +12,26 @@ import Foundation
 public extension ProductProfileNavigation.State {
         
     typealias ProductProfileRoute = GenericRoute<CardGuardianViewModel, ProductProfileNavigation.State.CGDestination, Never, AlertModelOf<ProductProfileNavigation.Event>>
+    
+    private typealias _ProductProfileRoute = _Destination<CardGuardianState, ProductProfileNavigation.State.CGDestination, Never, AlertModelOf<ProductProfileNavigation.Event>>
+}
+
+private struct _Destination<State, Destination, Modal, Alert> {
+    
+    public let state: State
+    public var destination: Destination?
+    public var modal: Modal?
+    public var alert: Alert?
+    
+    public init(
+        state: State,
+        destination: Destination? = nil,
+        modal: Modal? = nil,
+        alert: Alert? = nil
+    ) {
+        self.state = state
+        self.destination = destination
+        self.modal = modal
+        self.alert = alert
+    }
 }
