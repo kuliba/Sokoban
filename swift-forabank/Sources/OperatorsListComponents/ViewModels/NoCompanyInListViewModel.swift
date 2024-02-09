b6c55d06bf0e93f77882237fe1a88da39102e6dc
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-02-09 09:19:13 +0400

    add `NoCompanyInListViewModel`

---
+++ swift-forabank/Sources/OperatorsListComponents/ViewModels/NoCompanyInListViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Дмитрий Савушкин on 09.02.2024.
+//
+
+import Foundation
+
+struct NoCompanyInListViewModel {
+ 
+    let title: String
+    let description: String
+    let subtitle: String
+    let buttons: [ButtonSimpleView.ViewModel]
+}
