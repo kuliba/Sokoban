835cbb604a05d88e8b56801a29049d663a67fef9
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-02-11 12:17:21 +0300

    add `C2BSubscriptionUIPreview` app target with  `C2BSubscriptionUI` dependency

---
+++ ForaBank/PreviewApps/C2BSubscriptionUIPreview/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  C2BSubscriptionUIPreview
+//
+//  Created by Igor Malyarov on 11.02.2024.
+//
+
+import C2BSubscriptionUI
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+ 
+        ContentView()
+    }
+}
