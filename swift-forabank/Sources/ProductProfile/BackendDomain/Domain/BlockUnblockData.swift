e8afc3d096200a1a68ba0d0f11864a9cc2c3f83f
Tasha Andryushina <natalykn@gmail.com>
2024-02-12 13:22:28 +0300

    add mapper

---
+++ swift-forabank/Sources/ProductProfile/BackendDomain/Domain/BlockUnblockData.swift
@@ -0,0 +1,12 @@
+//
+//  BlockUnblockData.swift
+//
+//
+//  Created by Andryusina Nataly on 12.02.2024.
+//
+
+public struct BlockUnblockData: Equatable {
+    
+    let statusBrief: String?
+    let statusDescription: String?
+}
---
+++ swift-forabank/Sources/ProductProfile/BackendDomain/ResponseMapper/ResponseMapper+mapBlockCardResponse.swift
@@ -0,0 +1,50 @@
+//
+//  ResponseMapper+mapBlockCardResponse.swift
+//
+//
+//  Created by Andryusina Nataly on 12.02.2024.
+//
+
+import Foundation
+
+public extension ResponseMapper {
+    
+    static func mapBlockCardResponse(
+        _ data: Data,
+        _ httpURLResponse: HTTPURLResponse
+    ) -> MappingResult<BlockUnblockData?> {
+        
+        map(data, httpURLResponse, mapOrThrow: map)
+    }
+    
+    private static func map(
+        _ data: _Data
+    ) throws -> BlockUnblockData? {
+        
+        data.first?.data
+    }
+}
+
+private extension ResponseMapper {
+    
+    typealias _Data = [_DTO]
+}
+
+private extension ResponseMapper._DTO {
+    
+    var data: BlockUnblockData? {
+                
+        return .init(
+            statusBrief: statusBrief,
+            statusDescription: statusDescription)
+    }
+}
+
+private extension ResponseMapper {
+    
+    struct _DTO: Decodable {
+        
+        let statusDescription: String?
+        let statusBrief: String?
+    }
+}
