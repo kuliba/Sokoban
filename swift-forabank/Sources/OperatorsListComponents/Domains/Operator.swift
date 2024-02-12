0a5fc811bd957f10fd9afec5742aa5a652102afa
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-02-12 10:16:22 +0400

    `Operator` domain

---
+++ swift-forabank/Sources/OperatorsListComponents/Domains/Operator.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Дмитрий Савушкин on 12.02.2024.
+//
+
+import Foundation
+
+public struct Operator {
+    
+    let name: String
+    let inn: String
+    let serviceId: String
+    let md5hash: String
+}
