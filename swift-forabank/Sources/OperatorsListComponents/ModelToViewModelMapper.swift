a77a584e0bdd4e23a9e5f2fe7c74661a271f0033
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-02-12 10:16:47 +0400

    add `ModelToViewModelMapper`

--- swift-forabank/Sources/OperatorsListComponents/ModelToViewModelMapper.swift
+++ swift-forabank/Sources/OperatorsListComponents/ModelToViewModelMapper.swift
@@ -1,8 +1,34 @@
 //
-//  File.swift
-//  
+//  ModelToViewModelMapper.swift
+//
 //
 //  Created by Дмитрий Савушкин on 12.02.2024.
 //
 
 import Foundation
+
+public struct ModelToViewModelMapper {
+    
+    let action: (Event) -> Void
+    
+    public init(
+        action: @escaping (Event) -> Void
+    ) {
+        self.action = action
+    }
+}
+
+public extension ModelToViewModelMapper {
+    
+    func map(
+        _ operator: Operator
+    ) -> OperatorViewModel {
+        
+        return .init(
+            icon: Data(),
+            title: `operator`.name,
+            description: `operator`.inn,
+            action: {}
+        )
+    }
+}
