131813e227c0aee38b79aa74964edeb591093ba0
Tasha Andryushina <natalykn@gmail.com>
2024-02-14 13:29:06 +0300

    cleanup

--- swift-forabank/Sources/ProductProfile/ProductProfileState/ProductProfileNavigation+State.swift
+++ swift-forabank/Sources/ProductProfile/ProductProfileState/ProductProfileNavigation+State.swift
@@ -15,16 +15,13 @@ public extension ProductProfileNavigation {
         
         public var modal: ProductProfileRoute?
         public var alert: AlertModelOf<Event>?
-        public var isLoading: Bool
 
         public init(
             modal: ProductProfileRoute? = nil,
-            alert: AlertModelOf<Event>? = nil,
-            isLoading: Bool = false
+            alert: AlertModelOf<Event>? = nil
         ) {
             self.modal = modal
             self.alert = alert
-            self.isLoading = false
         }
     }
 }
