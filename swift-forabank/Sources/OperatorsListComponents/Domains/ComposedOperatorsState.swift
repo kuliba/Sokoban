89e4bf85f60cb71d5746e79ecf6e9348a17621c2
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-02-19 13:49:35 +0400

    composed operators state

---
+++ swift-forabank/Sources/OperatorsListComponents/Domains/ComposedOperatorsState.swift
@@ -0,0 +1,14 @@
+//
+//  ComposedOperatorsState.swift
+//
+//
+//  Created by Дмитрий Савушкин on 19.02.2024.
+//
+
+import Foundation
+
+public struct ComposedOperatorsState {
+    
+    let operators: [Operator]
+    let latestPayments: [LatestPayment]
+}
