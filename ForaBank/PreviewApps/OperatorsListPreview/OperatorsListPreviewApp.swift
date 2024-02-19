e851ae4ba5ac9f6adb7e2630e89600461c5c69ba
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-02-19 15:38:34 +0400

    create `OperatorsListPreviewApp`

---
+++ ForaBank/PreviewApps/OperatorsListPreview/OperatorsListPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  OperatorsListPreviewApp.swift
+//  OperatorsListPreview
+//
+//  Created by Дмитрий Савушкин on 19.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct OperatorsListPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
