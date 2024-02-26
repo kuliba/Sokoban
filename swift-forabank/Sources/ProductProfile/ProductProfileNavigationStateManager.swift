101ffaee1dc82e5da37b59c8c213735a168bb06e
Tasha Andryushina <natalykn@gmail.com>
2024-02-26 16:13:34 +0300

    cleanup

--- swift-forabank/Sources/ProductProfile/ProductProfileNavigationStateManager.swift
+++ swift-forabank/Sources/ProductProfile/ProductProfileNavigationStateManager.swift
@@ -7,50 +7,6 @@
 
 import Foundation
 
-// TODO: убрать после интегрирования в приложение!!
-     /*public final class ProductProfileViewModel: ObservableObject {
-    
-    @Published public private(set) var state: ProductProfileNavigation.State
-    
-    private let navigationStateManager: ProductProfileNavigationStateManager
-    
-    private let stateSubject = PassthroughSubject<ProductProfileNavigation.State, Never>()
-    private let scheduler: AnySchedulerOfDispatchQueue
-        
-    public init(
-        initialState: ProductProfileNavigation.State,
-        navigationStateManager: ProductProfileNavigationStateManager,
-        scheduler: AnySchedulerOfDispatchQueue = .makeMain()
-    ) {
-        self.state = initialState
-        self.navigationStateManager = navigationStateManager
-        self.scheduler = scheduler
-        
-        stateSubject
-            .removeDuplicates()
-            .receive(on: scheduler)
-            .assign(to: &$state)
-    }
-}
-
-extension ProductProfileViewModel {
-    
-    public func event(
-        _ event: ProductProfileNavigation.Event
-    ) {
-        
-        let (state, effect) = navigationStateManager.reduce(state, event)
-        stateSubject.send(state)
-        
-        if let effect {
-            
-            navigationStateManager.handleEffect(effect) { [weak self] in
-                    self?.event($0)
-            }
-        }
-    }
-}
-*/
 public struct ProductProfileNavigationStateManager {
     
     let reduce: Reduce
