2d2d60b659365e8feb670a6d20a07d28b7d4511e
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-02-28 11:37:56 +0400

    add `RegularFieldViewModel`

---
+++ swift-forabank/Sources/OperatorsListComponents/Extension/RegularFieldViewModel+ext.swift
@@ -0,0 +1,58 @@
+//
+//  RegularFieldViewModel.swift
+//  
+//
+//  Created by Дмитрий Савушкин on 27.02.2024.
+//
+
+import Foundation
+import TextFieldComponent
+import UIKit
+
+public typealias RegularFieldViewModel = ReducerTextFieldViewModel<TextFieldUI.ToolbarViewModel, KeyboardType>
+
+public extension RegularFieldViewModel {
+    
+    static func make(
+        keyboardType: Keyboard,
+        text: String?,
+        placeholderText: String,
+        limit: Int
+    ) -> RegularFieldViewModel {
+        
+        let reducer = TransformingReducer(
+            placeholderText: placeholderText,
+            transform: Transformers.limiting(limit).transform(_:)
+        )
+        
+        let toolbar = ToolbarFactory.makeToolbarViewModel(
+            closeButtonLabel: .image("Close Button"),
+            closeButtonAction: {
+                UIApplication.shared.endEditing()
+            },
+            doneButtonLabel: .title("Готово"),
+            doneButtonAction: {
+                UIApplication.shared.endEditing()
+            }
+        )
+        
+        if let text {
+            
+            return .init(
+                initialState: .noFocus(text),
+                reducer: reducer,
+                keyboardType: keyboardType,
+                toolbar: toolbar
+            )
+            
+        } else {
+            
+            return .init(
+                initialState: .placeholder(placeholderText),
+                reducer: reducer,
+                keyboardType: keyboardType,
+                toolbar: toolbar
+            )
+        }
+    }
+}
