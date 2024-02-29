91c25c965525c20f3c03ff659a2a81cb45dc9ec4
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-02-29 13:39:11 +0300

    Model+loadOperatorsTests

---
+++ ForaBank/ForaBank/CompositionRoot/UtilityPayments/Model+loadOperators.swift
@@ -0,0 +1,53 @@
+//
+//  Model+loadOperators.swift
+//  ForaBank
+//
+//  Created by Igor Malyarov on 21.02.2024.
+//
+
+import Foundation
+import OperatorsListComponents
+
+extension Model {
+    
+    typealias Payload = LoadOperatorsPayload
+    typealias LoadOperatorsResult = Result<[OperatorsListComponents.Operator], Error>
+    typealias LoadOperatorsCompletion = (LoadOperatorsResult) -> Void
+    
+    func loadOperators(
+        _ payload: Payload,
+        _ completion: @escaping LoadOperatorsCompletion
+    ) {
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            
+            guard let self else { return }
+            
+            if let operators = localAgent.load(type: [OperatorGroup].self) {
+                
+                completion(.success(
+                    operators
+                        .paged(with: payload)
+                        .map(OperatorsListComponents.Operator.init)
+                ))
+            } else {
+                
+                completion(.failure(LoadOperatorsFailure()))
+            }
+        }
+    }
+    
+    struct LoadOperatorsFailure: Error {}
+}
+
+private extension OperatorsListComponents.Operator {
+    
+    init(_ operatorGroup: OperatorGroup) {
+        
+        self.init(
+            id: operatorGroup.title,
+            title: operatorGroup.title,
+            subtitle: operatorGroup.description,
+            image: nil
+        )
+    }
+}
---
+++ ForaBank/ForaBankTests/CompositionRoot/UtilityPayments/Model+loadOperatorsTests.swift
@@ -0,0 +1,151 @@
+//
+//  Model+loadOperatorsTests.swift
+//  ForaBankTests
+//
+//  Created by Igor Malyarov on 29.02.2024.
+//
+
+@testable import ForaBank
+import OperatorsListComponents
+import XCTest
+
+private typealias SUT = Model
+
+final class Model_loadOperatorsTests: XCTestCase {
+    
+    func test_loadOperators_shouldDeliverEmptyOnEmpty() throws {
+        
+        let payload = makeLoadOperatorsPayload()
+        let sut = try makeSUT(stub: [])
+        
+        expect(sut, with: payload, toDeliver: .success([]))
+    }
+    
+    func test_loadOperators_shouldDeliverFirstPageOnNilOperatorID() throws {
+        
+        let payload = makeLoadOperatorsPayload(
+            operatorID: nil,
+            pageSize: 2
+        )
+        let sut = try makeSUT(stub: .stub(names: [
+            "b", "a", "c", "d", "e"
+        ]))
+        
+        expect(sut, with: payload, toDeliver: .success(.stub(names: ["b", "a"])))
+    }
+    
+    func test_loadOperators_shouldDeliverPageAfterOperatorID() throws {
+        
+        let payload = makeLoadOperatorsPayload(
+            operatorID: "a",
+            pageSize: 2
+        )
+        let sut = try makeSUT(stub: .stub(names: [
+            "b", "a", "c", "d", "e"
+        ]))
+        
+        expect(sut, with: payload, toDeliver: .success(.stub(names: ["c", "d"])))
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(
+        stub: [OperatorGroup] = .stub(),
+        file: StaticString = #file,
+        line: UInt = #line
+    ) throws -> SUT {
+        
+        let sut: SUT = try .mockWithEmptyExcept(
+            localAgent: LocalAgentStub(
+                type: [OperatorGroup].self,
+                value: stub
+            )
+        )
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return sut
+    }
+    
+    private func makeLoadOperatorsPayload(
+        operatorID: OperatorsListComponents.Operator.ID? = nil,
+        searchText: String = "",
+        pageSize: Int = 10
+    ) -> LoadOperatorsPayload {
+        
+        .init(
+            afterOperatorID: operatorID,
+            searchText: searchText,
+            pageSize: pageSize
+        )
+    }
+    
+    private func expect(
+        _ sut: SUT,
+        with payload: LoadOperatorsPayload,
+        toDeliver expectedResult: SUT.LoadOperatorsResult,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        let exp = expectation(description: "wait for completion")
+        
+        sut.loadOperators(payload) { receivedResult in
+            
+            ForaBankTests.assert(receivedResult, expectedResult, file: file, line: line)
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
+    }
+}
+
+private func assert(
+    _ receivedResult: SUT.LoadOperatorsResult,
+    _ expectedResult: SUT.LoadOperatorsResult,
+    file: StaticString = #file,
+    line: UInt = #line
+) {
+    switch (receivedResult, expectedResult) {
+    case let (
+        .failure(receivedError as NSError),
+        .failure(expectedError as NSError)
+    ):
+        XCTAssertNoDiff(receivedError, expectedError, file: file, line: line)
+        
+    case let (
+        .success(received),
+        .success(expected)
+    ):
+        XCTAssertNoDiff(received, expected, file: file, line: line)
+        
+    default:
+        XCTFail("\nExpected \(expectedResult), but got \(receivedResult) instead.", file: file, line: line)
+    }
+}
+
+private extension Array where Element == OperatorGroup {
+    
+    static func stub(
+        names: [String] = []
+    ) -> Self {
+        
+        names.map {
+            
+            .init(md5hash: "", title: $0, description: "")
+        }
+    }
+}
+
+private extension Array where Element == OperatorsListComponents.Operator {
+    
+    static func stub(
+        names: [String] = []
+    ) -> Self {
+        
+        names.map {
+            
+            .init(id: $0, title: $0, subtitle: "", image: nil)
+        }
+    }
+}
