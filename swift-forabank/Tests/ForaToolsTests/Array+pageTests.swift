1605983a8a064d87052afdacc28c24739bd8f5d2
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-02-29 11:58:07 +0300

    add `Array.page(startingAfter:pageSize:)` with tests

--- swift-forabank/Tests/ForaToolsTests/Array+pageTests.swift
+++ swift-forabank/Tests/ForaToolsTests/Array+pageTests.swift
@@ -10,7 +10,9 @@ import XCTest
 
 final class Array_pageTests: XCTestCase {
     
-    func test_page_shouldReturnEmptyOnMissing() {
+    // MARK: - startingAt
+    
+    func test_page_startingAt_shouldReturnEmptyOnMissing() {
         
         let items = items(count: 100)
         
@@ -19,7 +21,7 @@ final class Array_pageTests: XCTestCase {
         XCTAssertEqual(page, [])
     }
     
-    func test_page_shouldReturnEmptyOnLast() {
+    func test_page_startingAt_shouldReturnEmptyOnLast() {
         
         let items = items(count: 100)
         
@@ -28,7 +30,7 @@ final class Array_pageTests: XCTestCase {
         XCTAssertEqual(page, [])
     }
     
-    func test_page_shouldReturnAllOnPageSizeBiggerThanCount() {
+    func test_page_startingAt_shouldReturnAllOnPageSizeBiggerThanCount() {
         
         let count = 100
         let pageSize = 1_000
@@ -51,7 +53,7 @@ final class Array_pageTests: XCTestCase {
         XCTAssertEqual(page.map(\.id), (0..<10).map { $0 })
     }
     
-    func test_page_shouldReturnPage_02() {
+    func test_page_startingAt_shouldReturnPage_02() {
         
         let startingAt = 80
         let pageSize = 10
@@ -60,6 +62,73 @@ final class Array_pageTests: XCTestCase {
         let page = items.page(startingAt: startingAt, pageSize: pageSize)
         
         XCTAssertEqual(page.map(\.id), (80..<90).map { $0 })
+    }
+    
+    // MARK: - startingAfter
+    
+    func test_page_startingAfter_shouldReturnEmptyOnMissing() {
+        
+        let items = items(count: 100)
+        
+        let page = items.page(startingAfter: 111, pageSize: 100)
+        
+        XCTAssertEqual(page, [])
+    }
+    
+    func test_page_startingAfter_shouldReturnEmptyOnLast() {
+        
+        let items = items(count: 100)
+        
+        let page = items.page(startingAfter: 100, pageSize: 100)
+        
+        XCTAssertEqual(page, [])
+    }
+    
+    func test_page_startingAfter_shouldReturnAllAfterStartingOnPageSizeBiggerThanCount() {
+        
+        let count = 100
+        let pageSize = 1_000
+        let startingAfter = 0
+        let items = items(count: count)
+        
+        let page = items.page(startingAfter: startingAfter, pageSize: pageSize)
+        
+        XCTAssertEqual(page, .init(items.dropFirst()))
+        XCTAssertGreaterThan(pageSize, count)
+    }
+    
+    func test_page_startingAfter_shouldReturnPage_01() {
+        
+        let startingAfter = 0
+        let pageSize = 10
+        let items = items(count: 100)
+        
+        let page = items.page(startingAfter: startingAfter, pageSize: pageSize)
+        
+        XCTAssertEqual(page.map(\.id), (1..<11).map { $0 })
+    }
+    
+    func test_page_startingAfter_shouldReturnPage_02() {
+        
+        let startingAfter = 80
+        let pageSize = 10
+        let items = items(count: 100)
+        
+        let page = items.page(startingAfter: startingAfter, pageSize: pageSize)
+        
+        XCTAssertEqual(page.map(\.id), (81..<91).map { $0 })
+    }
+    
+    func test_page_startingAfter_shouldReturnPage_03() {
+        
+        let startingAfter = 80
+        let pageSize = 20
+        let count = 100
+        let items = items(count: count)
+        
+        let page = items.page(startingAfter: startingAfter, pageSize: pageSize)
+        
+        XCTAssertEqual(page.map(\.id), (81..<count).map { $0 })
     }
     
     private struct Item: Equatable, Identifiable {
