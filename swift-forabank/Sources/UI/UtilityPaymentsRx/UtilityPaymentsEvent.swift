b0bdd9b38eff999403c0e23880a7a129375bf606
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-03-02 11:07:36 +0300

    simplify `UtilityPaymentsEvent` declaration

--- swift-forabank/Sources/UI/UtilityPaymentsRx/UtilityPaymentsEvent.swift
+++ swift-forabank/Sources/UI/UtilityPaymentsRx/UtilityPaymentsEvent.swift
@@ -5,9 +5,8 @@
 //  Created by Igor Malyarov on 19.02.2024.
 //
 
-public enum UtilityPaymentsEvent<LastPayment, Operator>: Equatable
-where LastPayment: Equatable & Identifiable,
-      Operator: Equatable & Identifiable {
+public enum UtilityPaymentsEvent<LastPayment, Operator>
+where Operator: Identifiable {
     
     case didScrollTo(Operator.ID)
     case initiate
@@ -27,3 +26,5 @@ public extension UtilityPaymentsEvent {
         case updated(String)
     }
 }
+
+extension UtilityPaymentsEvent: Equatable where LastPayment: Equatable, Operator: Equatable {}
