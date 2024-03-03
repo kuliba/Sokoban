1ef3ce622da5ccf42983fac699732c0301793b26
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-03-03 11:22:31 +0300

    add `UtilityPaymentPreview` app target

---
+++ ForaBank/PreviewApps/UtilityPaymentPreview/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  UtilityPaymentPreview
+//
+//  Created by Igor Malyarov on 03.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+ 
+        ContentView()
+    }
+}
