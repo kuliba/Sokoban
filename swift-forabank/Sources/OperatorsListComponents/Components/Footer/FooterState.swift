bac359cf7296fb3ea1d29642910602b8579cd670
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-03-05 16:59:56 +0400

    fix optional buttons array

--- swift-forabank/Sources/OperatorsListComponents/Components/Footer/FooterState.swift
+++ swift-forabank/Sources/OperatorsListComponents/Components/Footer/FooterState.swift
@@ -5,11 +5,10 @@
 //  Created by Дмитрий Савушкин on 05.03.2024.
 //
 
-import Foundation
 import SwiftUI
 
 public enum FooterState {
      
-    case footer(title: String, description: String, buttons: [ButtonSimpleViewModel], subtitle: String)
+    case footer(title: String, description: String, buttons: [ButtonSimpleViewModel]?, subtitle: String)
     case searchResult(image: Image, description: String, button: ButtonSimpleViewModel)
 }
