61477b5aee317645c3ff58a67c93ce611c7eaae8
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-03-06 14:09:40 +0400

    add associated type `Footer`, `Failure`

--- swift-forabank/Sources/OperatorsListComponents/Components/Footer/FooterState.swift
+++ swift-forabank/Sources/OperatorsListComponents/Components/Footer/FooterState.swift
@@ -8,7 +8,20 @@
 import SwiftUI
 
 public enum FooterState {
-     
-    case footer(title: String, description: String, subtitle: String)
-    case failure(image: Image, description: String)
+    
+    case footer(Footer)
+    case failure(Failure)
+    
+    public struct Footer {
+        
+        let title: String
+        let description: String
+        let subtitle: String
+    }
+
+    public struct Failure {
+        
+        let image: Image
+        let description: String
+    }
 }
