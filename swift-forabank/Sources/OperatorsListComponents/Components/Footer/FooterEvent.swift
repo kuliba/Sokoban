1b51b067b9441d0614b477a115989711cd7452af
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-03-06 12:53:28 +0400

    add `footerEvent`

---
+++ swift-forabank/Sources/OperatorsListComponents/Components/Footer/FooterEvent.swift
@@ -0,0 +1,14 @@
+//
+//  FooterEvent.swift
+//
+//
+//  Created by Дмитрий Савушкин on 06.03.2024.
+//
+
+import Foundation
+
+public enum FooterEvent {
+    
+    case payByInstruction
+    case addCompany
+}
