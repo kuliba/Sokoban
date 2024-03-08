f782f757446d666b0860b25e55616f8eb3c2a9ba
Tasha Andryushina <natalykn@gmail.com>
2024-03-08 21:30:54 +0300

    add `DetailsReducerTests`

---
+++ swift-forabank/Sources/UI/ProductProfile/AccountDetails/PreviewContent/DetailsState+ext.swift
@@ -0,0 +1,13 @@
+//
+//  DetailsState+ext.swift
+//
+//
+//  Created by Andryusina Nataly on 08.03.2024.
+//
+
+import Foundation
+
+public extension DetailsState {
+    
+    static let initialState = DetailsState(items: .preview)
+}
---
+++ swift-forabank/Tests/UI/ProductProfileTests/AccountDetailsTests/DetailsReducerTests.swift
@@ -0,0 +1,120 @@
+//
+//  DetailsReducerTests.swift
+//  
+//
+//  Created by Andryusina Nataly on 06.03.2024.
+//
+
+import XCTest
+@testable import AccountDetails
+import RxViewModel
+
+final class DetailsReducerTests: XCTestCase {
+
+    func test_appear_shouldSetEffectNone() {
+        
+        assert(.appear, on: .initialState, effect: .none)
+    }
+    
+    func test_appear_shouldUpdateEventOnAppear() {
+                
+        assert(.appear, on: .initialState) {
+            
+            $0.updateEvent(.appear)
+        }
+    }
+    
+    func test_itemTappedLongPress_shouldSetEffectNone() {
+                
+        assert(.itemTapped(.longPress("111","text")), on: .initialState, effect: .none)
+    }
+    
+    func test_itemTappedLongPress_shouldUpdateEventOnItemTap() {
+         
+        assert(.itemTapped(.longPress("111","text")), on: .initialState) {
+            
+            $0.updateEvent(.itemTapped(.longPress("111","text")))
+        }
+    }
+
+    func test_itemTappedIconTap_shouldSetEffectNone() {
+                
+        assert(.itemTapped(.iconTap(.cvv)), on: .initialState, effect: .none)
+    }
+    
+    func test_buttonTappedIconTap_shouldUpdateEventOnItemTap() {
+         
+        assert(.itemTapped(.iconTap(.cvv)), on: .initialState) {
+            
+            $0.updateEvent(.itemTapped(.iconTap(.cvv)))
+        }
+    }
+
+    // MARK: - Helpers
+    
+    private typealias SUT = DetailsReducer
+    private typealias State = SUT.State
+    private typealias Event = SUT.Event
+    private typealias Effect = SUT.Effect
+    private typealias Result = (State, Effect?)
+    
+    private func makeSUT(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> SUT {
+        
+        let sut = SUT()
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut)
+    }
+    
+    private func reduce(
+        _ sut: SUT,
+        _ state: State = .initialState,
+        event: Event
+    ) -> (state: State, effect: Effect?) {
+        
+        return sut.reduce(state, event)
+    }
+    
+    private func assert(
+        sut: SUT? = nil,
+        _ event: Event,
+        on state: State,
+        effect expectedEffect: Effect?,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        
+        let receivedEffect: Effect? = reduce(sut ?? makeSUT(), state, event: event).1
+        
+        XCTAssertNoDiff(
+            receivedEffect,
+            expectedEffect,
+            "\nExpected \(String(describing: expectedEffect)) state, but got \(String(describing: receivedEffect)) instead.",
+            file: file, line: line
+        )
+    }
+    
+    private func assert(
+        sut: SUT? = nil,
+        _ event: Event,
+        on state: State,
+        updateStateToExpected: UpdateStateToExpected<State>? = nil,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        let sut = sut ?? makeSUT()
+        
+        _assertState(
+            sut,
+            event,
+            on: state,
+            updateStateToExpected: updateStateToExpected,
+            file: file, line: line
+        )
+    }
+}
+
+extension DetailsReducer: Reducer { }
