561f29fa1af81b020f466faeb9fd9e1b1d0b3f7c
Tasha Andryushina <natalykn@gmail.com>
2024-03-18 18:38:10 +0300

    add `String+ext`

--- swift-forabank/Sources/UI/ProductProfile/CardUI/Domain/CardInfo.swift
+++ swift-forabank/Sources/UI/ProductProfile/CardUI/Domain/CardInfo.swift
@@ -121,23 +121,3 @@ public extension CardInfo {
         return state != .showFront
     }
 }
-
-public extension String {
-    
-    func formatted(
-        withChunkSize chunkSize: Int = 4,
-        withSeparator separator: String = " "
-    ) -> String {
-        var stringWithAddedSpaces = ""
-        
-        for i in Swift.stride(from: 0, to: self.count, by: 1) {
-            if i > 0 && (i % chunkSize) == 0 {
-                stringWithAddedSpaces.append(contentsOf: separator)
-            }
-            let characterToAdd = self[self.index(self.startIndex, offsetBy: i)]
-            stringWithAddedSpaces.append(characterToAdd)
-        }
-        
-        return stringWithAddedSpaces
-    }
-}
