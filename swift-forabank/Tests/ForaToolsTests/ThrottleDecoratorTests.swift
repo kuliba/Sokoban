b2e3d6f2f17d9d35039185b6023e252d46103b19
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-03-20 10:12:11 +0300

    move `ThrottleDecorator` to prod

--- swift-forabank/Tests/ForaToolsTests/ThrottleDecoratorTests.swift
+++ swift-forabank/Tests/ForaToolsTests/ThrottleDecoratorTests.swift
@@ -5,40 +5,7 @@
 //  Created by Igor Malyarov on 20.03.2024.
 //
 
-import Foundation
-
-final class ThrottleDecorator {
-    
-    private let delay: TimeInterval
-    private var lastCallTime: TimeInterval = 0
-    private let queue = DispatchQueue(label: "com.ThrottleDecorator.queue")
-    
-    init(delay: TimeInterval) {
-        
-        self.delay = delay
-    }
-}
-
-extension ThrottleDecorator {
-    
-    func callAsFunction(
-        block: @escaping () -> Void
-    ) {
-        queue.sync { [weak self] in
-            
-            guard let self else { return }
-            
-            let currentTime = Date().timeIntervalSince1970
-            
-            guard currentTime - lastCallTime >= delay else { return }
-            
-            lastCallTime = currentTime
-            
-            block()
-        }
-    }
-}
-
+import ForaTools
 import XCTest
 
 final class ThrottleDecoratorTests: XCTestCase {
