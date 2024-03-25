d692df1e60a95d24903881d3cb6a95cfdebbe64b
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-03-25 15:44:13 +0300

    reuse `ResponseMapper` from `RemoteServices`

--- swift-forabank/Sources/FastPaymentsSettings/BackendDomain/RequestFactory/RequestFactory+createMakeSetBankDefaultRequest.swift
+++ swift-forabank/Sources/FastPaymentsSettings/BackendDomain/RequestFactory/RequestFactory+createMakeSetBankDefaultRequest.swift
@@ -11,24 +11,13 @@ import Tagged
 
 public extension RequestFactory {
     
-    typealias VerificationCode = Tagged<_VerificationCode, String>
-    enum _VerificationCode {}
-    
     static func createMakeSetBankDefaultRequest(
         url: URL,
         payload: VerificationCode
     ) throws -> URLRequest {
         
-        guard !payload.isEmpty else {
-            throw EmptyVerificationCode()
-        }
-        
-        var request = createEmptyRequest(.post, with: url)
-        request.httpBody = try payload.httpBody
-        return request
+        try createVerificationCodeRequest(url: url, payload: payload)
     }
-    
-    struct EmptyVerificationCode: Error {}
 }
 
 private extension RequestFactory.VerificationCode {
