a2bad174cf6a47751c5f16b58980ce1efec22b46
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-03-28 10:40:38 +0300

    initial scaffolding of `PaymentEffectHandler`

---
+++ swift-forabank/Tests/Payments/AnywayPayment/AnywayPaymentCoreTests/PaymentEffectHandlerTests.swift
@@ -0,0 +1,153 @@
+//
+//  PaymentEffectHandlerTests.swift
+//
+//
+//  Created by Igor Malyarov on 28.03.2024.
+//
+
+enum PaymentEvent<Update> {
+    
+    case update(Update)
+}
+
+extension PaymentEvent: Equatable where Update: Equatable {}
+
+enum PaymentEffect<Digest> {
+    
+    case `continue`(Digest)
+}
+
+extension PaymentEffect: Equatable where Digest: Equatable {}
+
+final class PaymentEffectHandler<Digest, Update> {
+    
+    private let process: Process
+    
+    init(process: @escaping Process) {
+        
+        self.process = process
+    }
+}
+
+extension PaymentEffectHandler {
+    
+    func handleEffect(
+        _ effect: Effect,
+        _ dispatch: @escaping Dispatch
+    ) {
+        switch effect {
+        case let .continue(digest):
+            process(digest) {
+                
+                dispatch(.update($0))
+            }
+        }
+    }
+}
+
+extension PaymentEffectHandler {
+    
+    typealias ProcessCompletion = (Update) -> Void
+    typealias Process = (Digest, @escaping ProcessCompletion) -> Void
+    
+    typealias Dispatch = (Event) -> Void
+    
+    typealias Event = PaymentEvent<Update>
+    typealias Effect = PaymentEffect<Digest>
+}
+
+import XCTest
+
+final class PaymentEffectHandlerTests: XCTestCase {
+    
+    func test_init_shouldNotCallCollaborators() {
+        
+        let (_, processing) = makeSUT()
+        
+        XCTAssertEqual(processing.callCount, 0)
+    }
+    
+    // MARK: - continue
+    
+    func test_continue_shouldCallProcessingWithDigest() {
+        
+        let digest = makeDigest()
+        let (sut, processing) = makeSUT()
+        
+        sut.handleEffect(.continue(digest)) { _ in }
+        
+        XCTAssertNoDiff(processing.payloads, [digest])
+    }
+    
+    func test_continue_shouldDeliverUpdateOnProcessingSuccess() {
+        
+        let update = makeUpdate()
+        expect(toDeliver: .update(update), on: .continue(makeDigest()), with: update)
+    }
+    
+    // MARK: - Helpers
+    
+    private typealias SUT = PaymentEffectHandler<Digest, Update>
+    private typealias Processing = Spy<Digest, Update>
+    
+    private func makeSUT(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (
+        sut: SUT,
+        processing: Processing
+    ) {
+        let processing = Processing()
+        let sut = SUT(process: processing.process)
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(processing, file: file, line: line)
+        
+        return (sut, processing)
+    }
+    
+    private func expect(
+        toDeliver expectedEvent: SUT.Event,
+        on effect: SUT.Effect,
+        with processingResult: Update,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        let (sut, processing) = makeSUT()
+        let exp = expectation(description: "wait for completion")
+        
+        sut.handleEffect(effect) {
+            
+            XCTAssertNoDiff(expectedEvent, $0, "Expected \(expectedEvent), but got \($0) instead.")
+            exp.fulfill()
+        }
+        
+        processing.complete(with: processingResult)
+        
+        wait(for: [exp], timeout: 1)
+    }
+}
+
+private struct Digest: Equatable {
+    
+    let value: String
+}
+
+private struct Update: Equatable {
+    
+    let value: String
+}
+
+private func makeDigest(
+    _ value: String = UUID().uuidString
+) -> Digest {
+    
+    .init(value: value)
+}
+
+private func makeUpdate(
+    _ value: String = UUID().uuidString
+) -> Update {
+    
+    .init(value: value)
+}
