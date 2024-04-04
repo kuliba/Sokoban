d0bb9e130317ba35fffd0c6dfef091550aad955a
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-04-04 20:17:08 +0400

    tests rename to `createBankParameter`

--- ForaBank/ForaBankTests/Model/Payments/Model+PaymensSFP/Model+PaymensSFPTests.swift
+++ ForaBank/ForaBankTests/Model/Payments/Model+PaymensSFP/Model+PaymensSFPTests.swift
@@ -95,7 +95,7 @@ final class Model_PaymensSFPTests: XCTestCase {
             source: .sfp(phone: "phone", bankId: "1")
         )
         
-        let bankParameter = sut.bankParameter(operation, operationPhone: nil)
+        let bankParameter = sut.createBankParameter(operation, operationPhone: nil, banksIds: [])
         
         XCTAssertNoDiff(bankParameter.parameter.id, Self.bankParameterTest.id)
     }
@@ -120,7 +120,7 @@ final class Model_PaymensSFPTests: XCTestCase {
             source: .latestPayment(1)
         )
         
-        let bankParameter = sut.bankParameter(operation, operationPhone: nil)
+        let bankParameter = sut.createBankParameter(operation, operationPhone: nil, banksIds: [])
         
         XCTAssertNoDiff(bankParameter.parameter.id, Self.bankParameterTest.id)
     }
