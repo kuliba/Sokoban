87a9fcfb3874ed0eff09e110a7bcb298bddc923b
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-04-08 13:12:00 +0300

    add `AnywayPaymentSnapshottedTests`

---
+++ swift-forabank/Tests/Payments/AnywayPayment/AnywayPaymentCoreTests/UpdateAnywayPaymentTests/AnywayPaymentSnapshottedTests.swift
@@ -0,0 +1,74 @@
+//
+//  AnywayPaymentSnapshottedTests.swift
+//
+//
+//  Created by Igor Malyarov on 08.04.2024.
+//
+
+extension AnywayPayment {
+    
+    func snapshotted() -> Snapshot {
+        
+        let pairs = elements.compactMap(\.parameterIDValuePair)
+        
+        return .init(uniqueKeysWithValues: pairs)
+    }
+}
+
+private extension AnywayPayment.Element {
+    
+    var parameterIDValuePair: (StringID, Value)? {
+        
+        guard case let .parameter(parameter) = self 
+        else { return nil }
+        
+        return parameter.field.value.map { (parameter.field.id, $0 ) }
+    }
+}
+
+import AnywayPaymentCore
+import XCTest
+
+final class AnywayPaymentSnapshottedTests: XCTestCase {
+    
+    func test_snapshotted_shouldDeliverEmptyOnEmptyElements() {
+        
+        let payment = makeAnywayPayment(elements: [])
+        
+        XCTAssert(payment.snapshotted().isEmpty)
+    }
+    
+    func test_snapshotted_shouldDeliverEmptyOnEmptyParameters() {
+        
+        let payment = makeAnywayPayment(
+            elements: [.field(makeAnywayPaymentField())]
+        )
+        
+        XCTAssert(payment.snapshotted().isEmpty)
+    }
+    
+    func test_snapshotted_shouldDeliverParameterIDs() {
+        
+        let (parameter1, parameter2) = makeTwoParameters()
+        let payment = makeAnywayPayment(elements: [.parameter(parameter1), .parameter(parameter2), .field(makeAnywayPaymentField())]
+        )
+        
+        XCTAssertNoDiff(payment.snapshotted(), [
+            parameter1.field.id: parameter1.field.value,
+            parameter2.field.id: parameter2.field.value
+        ])
+    }
+    
+    // MARK: - Helpers
+    
+    private typealias Parameter = AnywayPayment.Element.Parameter
+    
+    private func makeTwoParameters(
+    ) -> (parameter1: Parameter, parameter2: Parameter) {
+        
+        let parameter1 = makeAnywayPaymentParameter()
+        let parameter2 = makeAnywayPaymentParameter()
+        
+        return (parameter1, parameter2)
+    }
+}
