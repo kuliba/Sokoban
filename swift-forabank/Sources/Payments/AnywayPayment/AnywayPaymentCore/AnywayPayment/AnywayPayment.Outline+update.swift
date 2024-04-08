c9bc218d44bcbba2592d80d4e5e79424167fb16a
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-04-08 13:54:40 +0300

    add `AnywayPayment.Outline.update(with:)`

--- swift-forabank/Sources/Payments/AnywayPayment/AnywayPaymentCore/AnywayPayment/AnywayPayment.Outline+update.swift
+++ swift-forabank/Sources/Payments/AnywayPayment/AnywayPaymentCore/AnywayPayment/AnywayPayment.Outline+update.swift
@@ -1,17 +1,18 @@
 //
-//  AnywayPayment+snapshotted.swift
+//  AnywayPayment.Outline+update.swift
 //
 //
 //  Created by Igor Malyarov on 08.04.2024.
 //
 
-extension AnywayPayment {
+extension AnywayPayment.Outline {
     
-    public func snapshotted() -> Outline {
+    public func update(with payment: AnywayPayment) -> Self {
         
-        let pairs = elements.compactMap(\.parameterIDValuePair)
-        
-        return .init(uniqueKeysWithValues: pairs)
+        merging(
+            payment.elements.compactMap(\.parameterIDValuePair),
+            uniquingKeysWith: { first, _ in first }
+        )
     }
 }
 
--- swift-forabank/Tests/Payments/AnywayPayment/AnywayPaymentCoreTests/UpdateAnywayPaymentTests/AnywayPaymentOutlineTests.swift
+++ swift-forabank/Tests/Payments/AnywayPayment/AnywayPaymentCoreTests/UpdateAnywayPaymentTests/AnywayPaymentOutlineTests.swift
@@ -1,5 +1,5 @@
 //
-//  AnywayPaymentSnapshottedTests.swift
+//  AnywayPaymentOutlineTests.swift
 //
 //
 //  Created by Igor Malyarov on 08.04.2024.
@@ -8,31 +8,43 @@
 import AnywayPaymentCore
 import XCTest
 
-final class AnywayPaymentSnapshottedTests: XCTestCase {
+final class AnywayPaymentOutlineTests: XCTestCase {
     
-    func test_snapshotted_shouldDeliverEmptyOnEmptyElements() {
+    func test_update_shouldDeliverEmptyOnEmptyElementsPayment() {
         
+        let outline = AnywayPayment.Outline()
         let payment = makeAnywayPayment(elements: [])
         
-        XCTAssert(payment.snapshotted().isEmpty)
+        XCTAssert(outline.update(with: payment).isEmpty)
     }
     
-    func test_snapshotted_shouldDeliverEmptyOnEmptyParameters() {
+    func test_update_shouldNotChangeOnEmptyElementsPayment() {
         
+        let outline: AnywayPayment.Outline = ["a": "1"]
+        let payment = makeAnywayPayment(elements: [])
+        
+        XCTAssertNoDiff(outline.update(with: payment), outline)
+    }
+    
+    func test_update_shouldNotChangeOnEmptyParameters() {
+        
+        let outline: AnywayPayment.Outline = ["a": "1"]
         let payment = makeAnywayPayment(
             elements: [.field(makeAnywayPaymentField())]
         )
         
-        XCTAssert(payment.snapshotted().isEmpty)
+        XCTAssertNoDiff(outline.update(with: payment), outline)
     }
     
-    func test_snapshotted_shouldDeliverParameterIDs() {
+    func test_update_shouldDeliverParameterIDs() {
         
+        let outline: AnywayPayment.Outline = ["a": "1"]
         let (parameter1, parameter2) = makeTwoParameters()
         let payment = makeAnywayPayment(elements: [.parameter(parameter1), .parameter(parameter2), .field(makeAnywayPaymentField())]
         )
         
-        XCTAssertNoDiff(payment.snapshotted(), [
+        XCTAssertNoDiff(outline.update(with: payment), [
+            "a": "1",
             parameter1.field.id: parameter1.field.value,
             parameter2.field.id: parameter2.field.value
         ])
