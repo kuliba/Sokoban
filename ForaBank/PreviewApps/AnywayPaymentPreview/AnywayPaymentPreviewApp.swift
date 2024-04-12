5bb215908ae3086f098ca1e6450d67c0e43ca88b
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-04-12 13:23:04 +0300

    add `AnywayPaymentPreview` app

---
+++ ForaBank/PreviewApps/AnywayPaymentPreview/AnywayPaymentPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnywayPaymentPreviewApp.swift
+//  AnywayPaymentPreview
+//
+//  Created by Igor Malyarov on 12.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnywayPaymentPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
