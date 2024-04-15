3268721cbfcf5b278d7d9fb554b00b77652b571b
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-04-15 15:55:57 +0300

    add static preview properties

--- ForaBank/PreviewApps/AnywayPaymentPreview/Preview Content/AnywayPayment.Element+preview.swift
+++ ForaBank/PreviewApps/AnywayPaymentPreview/Preview Content/AnywayPayment.Element+preview.swift
@@ -11,6 +11,8 @@ extension Array where Element == AnywayPayment.Element {
     
     static let preview: Self = [
         .field(.init(id: "1", title: "a", value: "bb")),
+        .parameter(.textInput),
+        .parameter(.select),
         .widget(.otp(nil))
     ]
 }
---
+++ ForaBank/PreviewApps/AnywayPaymentPreview/Preview Content/AnywayPayment.Element.Parameter+preview.swift
@@ -0,0 +1,76 @@
+//
+//  AnywayPayment.Element.Parameter+preview.swift
+//  AnywayPaymentPreview
+//
+//  Created by Igor Malyarov on 15.04.2024.
+//
+
+import AnywayPaymentCore
+
+extension AnywayPayment.Element.Parameter {
+    
+    static let textInput: Self = .init(
+        field: .preview,
+        masking: .preview,
+        validation: .preview,
+        uiAttributes: .textInput
+    )
+    static let select: Self = .init(
+        field: .preview,
+        masking: .preview,
+        validation: .preview,
+        uiAttributes: .select
+    )
+}
+
+private extension AnywayPayment.Element.Parameter.Field {
+    
+    static let preview: Self = .init(id: "123", value: "abc")
+}
+
+private extension AnywayPayment.Element.Parameter.Masking {
+    
+    static let preview: Self = .empty
+    static let empty: Self = .init(inputMask: nil, mask: nil)
+}
+
+private extension AnywayPayment.Element.Parameter.Validation {
+    
+    static let preview: Self = .required
+    static let required: Self = .init(isRequired: true, maxLength: nil, minLength: nil, regExp: "")
+}
+
+private extension AnywayPayment.Element.Parameter.UIAttributes {
+    
+    static let select: Self = preview(
+        dataType: .pairs([.init(key: "0", value: "abc")]),
+        type: .select,
+        viewType: .input
+    )
+    static let textInput: Self = preview(
+        dataType: .string,
+        type: .input,
+        viewType: .input
+    )
+    
+    private static func preview(
+        dataType: AnywayPayment.Element.Parameter.UIAttributes.DataType,
+        type: AnywayPayment.Element.Parameter.UIAttributes.FieldType,
+        viewType: AnywayPayment.Element.Parameter.UIAttributes.ViewType
+    ) -> Self {
+        
+        .init(
+            dataType: dataType,
+            group: nil,
+            isPrint: true,
+            phoneBook: false,
+            isReadOnly: false,
+            subGroup: nil,
+            subTitle: nil,
+            svgImage: nil,
+            title: "title",
+            type: type,
+            viewType: viewType
+        )
+    }
+}
