e0cd066ba4182b6e5a377ad6c48490ebd66ed0b2
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-04-20 09:11:59 +0300

    tweak `RxViewModel`: remove `Equatable` requirements for generic State, Event, Effect

--- swift-forabank/Sources/UI/RxViewModel/RxViewModel+ext.swift
+++ swift-forabank/Sources/UI/RxViewModel/RxViewModel+ext.swift
@@ -5,12 +5,31 @@
 //  Created by Igor Malyarov on 20.01.2024.
 //
 
+public extension RxViewModel where State: Equatable {
+    
+    convenience init(
+        initialState: State,
+        reduce: @escaping Reduce,
+        handleEffect: @escaping HandleEffect,
+        scheduler: AnySchedulerOfDispatchQueue = .makeMain()
+    ) {
+        self.init(
+            initialState: initialState,
+            reduce: reduce,
+            handleEffect: handleEffect,
+            predicate: ==,
+            scheduler: scheduler
+        )
+    }
+}
+
 public extension RxViewModel {
     
     convenience init(
         initialState: State,
         reducer: any Reducer<State, Event, Effect>,
         effectHandler: any EffectHandler<Event, Effect>,
+        predicate: @escaping (State, State) -> Bool = { _,_ in false },
         scheduler: AnySchedulerOfDispatchQueue = .makeMain()
     ) {
         
@@ -18,6 +37,7 @@ public extension RxViewModel {
             initialState: initialState,
             reduce: reducer.reduce(_:_:),
             handleEffect: effectHandler.handleEffect(_:_:),
+            predicate: predicate,
             scheduler: scheduler
         )
     }
