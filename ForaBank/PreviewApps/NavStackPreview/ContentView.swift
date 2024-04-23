59bf034a58f53a67acf78468d26c8b67e0606950
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-04-23 08:05:50 +0300

    add `NavStackPreview` preview app

---
+++ ForaBank/PreviewApps/NavStackPreview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavStackPreview
+//
+//  Created by Igor Malyarov on 23.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
