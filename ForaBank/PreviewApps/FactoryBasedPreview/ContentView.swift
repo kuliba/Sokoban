4177337081bab0837e9fb35e9581e7ca11ed857b
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-04-24 17:55:26 +0300

    add `FactoryBasedPreview` app target
    remove team

---
+++ ForaBank/PreviewApps/FactoryBasedPreview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FactoryBasedPreview
+//
+//  Created by Igor Malyarov on 24.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ForaBank/PreviewApps/FactoryBasedPreview/FactoryBasedPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactoryBasedPreviewApp.swift
+//  FactoryBasedPreview
+//
+//  Created by Igor Malyarov on 24.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct FactoryBasedPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
