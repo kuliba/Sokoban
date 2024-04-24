3bc2ace7b2f4103de889ea8cc9c27e7fdbbdc5c5
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-04-24 11:50:28 +0400

    update select config

---
+++ swift-forabank/Sources/OperatorsListComponents/SelectComponent/SelectConfig.swift
@@ -0,0 +1,86 @@
+//
+//  SelectConfig.swift
+//
+//
+//  Created by Дмитрий Савушкин on 24.04.2024.
+//
+
+import Foundation
+import SwiftUI
+import SharedConfigs
+
+public struct SelectConfig {
+    
+    let title: String
+    let titleConfig: TextConfig
+    
+    let placeholder: String
+    let placeholderConfig: TextConfig
+    
+    let backgroundIcon: Color
+    let foregroundIcon: Color
+    let icon: Image
+    
+    let isSearchable: Bool
+    
+    public init(
+        title: String,
+        titleConfig: TextConfig,
+        placeholder: String,
+        placeholderConfig: TextConfig,
+        backgroundIcon: Color,
+        foregroundIcon: Color,
+        icon: Image,
+        isSearchable: Bool
+    ) {
+        self.title = title
+        self.titleConfig = titleConfig
+        self.placeholder = placeholder
+        self.placeholderConfig = placeholderConfig
+        self.backgroundIcon = backgroundIcon
+        self.foregroundIcon = foregroundIcon
+        self.icon = icon
+        self.isSearchable = isSearchable
+    }
+}
+
+public struct OptionConfig {
+    
+    let icon: Image
+    let foreground: Color
+    let background: Color
+    
+    let selectIcon: Image
+    let selectForeground: Color
+    let selectBackground: Color
+    
+    let mainBackground: Color
+    
+    let kind: Kind
+    
+    public init(
+        icon: Image,
+        foreground: Color,
+        background: Color,
+        selectIcon: Image,
+        selectForeground: Color,
+        selectBackground: Color,
+        mainBackground: Color,
+        kind: Kind
+    ) {
+        self.icon = icon
+        self.foreground = foreground
+        self.background = background
+        self.selectIcon = selectIcon
+        self.selectForeground = selectForeground
+        self.selectBackground = selectBackground
+        self.mainBackground = mainBackground
+        self.kind = kind
+    }
+    
+    public enum Kind: Int {
+        
+        case small = 16
+        case normal = 24
+    }
+}
