7cd9586650f86a0d7ec6b74e1b9228479fcf7988
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-05-03 09:49:14 +0300

    move `PaymentsTransfersFactory`

--- ForaBank/ForaBank/ViewModel/PaymentsTransfers/PaymentsTransfersFactory.swift
+++ ForaBank/ForaBank/ViewModel/PaymentsTransfers/PaymentsTransfersFactory.swift
@@ -2,37 +2,8 @@
 //  PaymentsTransfersFactory.swift
 //  ForaBank
 //
-//  Created by Andryusina Nataly on 29.01.2024.
+//  Created by Igor Malyarov on 03.05.2024.
 //
-
-import Foundation
-import OperatorsListComponents
-import PrePaymentPicker
-
-#warning("replace with type from module")
-final class UtilitiesViewModel: ObservableObject {
-    
-    @Published private(set) var state: State
-    
-    private let loadOperators: LoadOperators
-    
-    init(
-        initialState: State,
-        loadOperators: @escaping LoadOperators
-    ) {
-        self.state = initialState
-        self.loadOperators = loadOperators
-    }
-    
-    // MARK: - types
-    
-    typealias State = PrePaymentOptionsState<OperatorsListComponents.LatestPayment, OperatorsListComponents.Operator>
-    
-    struct Payload {}
-    
-    typealias LoadOperatorsCompletion = ([OperatorsListComponents.Operator]) -> Void
-    typealias LoadOperators = (Payload, @escaping LoadOperatorsCompletion) -> Void
-}
 
 struct PaymentsTransfersFactory {
     
