edc6cd9d9136decdfc81985b584d1a16185776c4
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-05-03 12:21:19 +0300

    add `UtilityServicePaymentFlowPreview`

---
+++ ForaBank/PreviewApps/UtilityServicePaymentFlowPreview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UtilityServicePaymentFlowPreview
+//
+//  Created by Igor Malyarov on 03.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
