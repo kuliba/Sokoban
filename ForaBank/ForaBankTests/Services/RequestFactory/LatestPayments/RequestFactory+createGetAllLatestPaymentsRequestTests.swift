afc5de0e4da99588409da81f3200469cc7b2b8fb
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-05-13 19:21:24 +0400

    add `RequestFactory_createGetAllLatestPaymentsRequestTests`

---
+++ ForaBank/ForaBankTests/Services/RequestFactory/LatestPayments/RequestFactory+createGetAllLatestPaymentsRequestTests.swift
@@ -0,0 +1,36 @@
+//
+//  RequestFactory+createGetAllLatestPaymentsRequestTests.swift
+//  ForaBankTests
+//
+//  Created by Дмитрий Савушкин on 13.05.2024.
+//
+
+@testable import ForaBank
+import RemoteServices
+import XCTest
+
+final class RequestFactory_createGetAllLatestPaymentsRequestTests: XCTestCase {
+    
+    func test_createRequest_shouldSetRequestURL() throws {
+        
+        let request = try createRequest()
+
+        XCTAssertNoDiff(
+            request.url?.absoluteString,
+            "https://pl.forabank.ru/dbo/api/v3/rest/v2/getAllLatestPayments?isServicePayments=true"
+        )
+    }
+    
+    func test_createRequest_shouldSetRequestMethodToPost() throws {
+        
+        let request = try createRequest()
+        
+        XCTAssertEqual(request.httpMethod, "POST")
+    }
+    
+    private func createRequest() throws -> URLRequest {
+        
+        try RequestFactory.getAllLatestPaymentRequest(.service)
+    }
+
+}
