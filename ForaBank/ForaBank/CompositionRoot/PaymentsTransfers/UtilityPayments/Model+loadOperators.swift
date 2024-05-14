18ad91a7ff7afaf1d80959eab699129c860fe3cd
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-05-14 14:29:22 +0300

    move `ArraySlice+page` to ForaTools

--- ForaBank/ForaBank/CompositionRoot/PaymentsTransfers/UtilityPayments/Model+loadOperators.swift
+++ ForaBank/ForaBank/CompositionRoot/PaymentsTransfers/UtilityPayments/Model+loadOperators.swift
@@ -5,6 +5,7 @@
 //  Created by Igor Malyarov on 21.02.2024.
 //
 
+import ForaTools
 import Foundation
 import OperatorsListComponents
 
@@ -105,38 +106,6 @@ extension ArraySlice where Element: Identifiable {
             return page(startingAt: id, pageSize: pageSize)
         }
         
-    }
-}
-
-extension ArraySlice where Element: Identifiable {
-    
-    /// Return a slice of the array starting from the element with given `id`, up to the specified `pageSize`.
-    /// If the element with the given ID is not found, return an empty array.
-    func page(
-        startingAt id: Element.ID,
-        pageSize: Int
-    ) -> Self {
-        
-        guard let startIndex = self.firstIndex(where: { $0.id == id })
-        else { return [] }
-        
-        let endIndex = index(startIndex, offsetBy: pageSize, limitedBy: count) ?? count
-        return self[startIndex..<endIndex]
-    }
-    
-    /// Return a slice of the array starting `after` the element with given `id`, up to the specified `pageSize`.
-    /// If the element with the given ID is not found, return an empty array.
-    func page(
-        startingAfter id: Element.ID,
-        pageSize: Int
-    ) -> Self {
-        
-        guard let startIndex = self.firstIndex(where: { $0.id == id })
-        else { return [] }
-        
-        let nextIndex = index(startIndex, offsetBy: 1, limitedBy: count) ?? count
-        let endIndex = index(nextIndex, offsetBy: pageSize, limitedBy: count) ?? count
-        return self[nextIndex..<endIndex]
     }
 }
 
