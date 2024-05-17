0dd5721e5f84dde5a88407f6debbe19f310e0e24
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-05-17 10:11:33 +0400

    generateDates for Calendar helper

---
+++ swift-forabank/Sources/CalendarUI/Helpers.swift
@@ -0,0 +1,34 @@
+//
+//  Helpers.swift
+//
+//
+//  Created by Дмитрий Савушкин on 17.05.2024.
+//
+
+import Foundation
+
+extension Calendar {
+    func generateDates(
+        inside interval: DateInterval,
+        matching components: DateComponents
+    ) -> [Date] {
+        var dates: [Date] = []
+        dates.append(interval.start)
+
+        enumerateDates(
+            startingAfter: interval.start,
+            matching: components,
+            matchingPolicy: .nextTime
+        ) { date, _, stop in
+            if let date = date {
+                if date < interval.end {
+                    dates.append(date)
+                } else {
+                    stop = true
+                }
+            }
+        }
+
+        return dates
+    }
+}
