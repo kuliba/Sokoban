ab785aa7d283f2d359addd0db9ab91782337912a
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-05-19 16:09:03 +0300

    add `TransactionPreviewApp` target

---
+++ ForaBank/PreviewApps/TransactionPreview/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  TransactionPreview
+//
+//  Created by Igor Malyarov on 19.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        VStack {
+            
+            Image(systemName: "globe")
+                .imageScale(.large)
+            
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
