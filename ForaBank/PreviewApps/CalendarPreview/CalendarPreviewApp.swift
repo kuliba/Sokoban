c6e0d4d99b9d5c9f7b92ec9af99327a5c1476334
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-05-22 18:22:24 +0400

    create CalendarPreviewApp

---
+++ ForaBank/PreviewApps/CalendarPreview/CalendarPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarPreviewApp.swift
+//  CalendarPreview
+//
+//  Created by Дмитрий Савушкин on 22.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalendarPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
