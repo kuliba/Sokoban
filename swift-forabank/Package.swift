a978c503d549586fd2c4829120544875fa3a726c
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-05-23 11:05:19 +0400

    remove code component from `Package`

--- swift-forabank/Package.swift
+++ swift-forabank/Package.swift
@@ -200,7 +200,6 @@ let package = Package(
         .buttonComponent,
         .infoComponent,
         .checkBoxComponent,
-        .codeComponent,
         .footerComponent,
         .nameComponent,
         .selectComponent,
@@ -1556,7 +1555,6 @@ private extension Target {
             .combineSchedulers,
             .tagged,
             // internal modules
-            .paymentComponents,
             .rxViewModel,
             .uiPrimitives,
         ],
@@ -1795,14 +1793,6 @@ private extension Target {
         path: "Sources/UI/Components/\(String.checkBoxComponent)"
     )
     
-    static let codeComponent = target(
-        name: .codeComponent,
-        dependencies: [
-            .sharedConfigs
-        ],
-        path: "Sources/UI/Components/\(String.codeComponent)"
-    )
-    
     static let footerComponent = target(
         name: .footerComponent,
         dependencies: [
@@ -1856,11 +1846,11 @@ private extension Target {
             .amountComponent,
             .buttonComponent,
             .carouselComponent,
-            .codeComponent,
             .infoComponent,
             .checkBoxComponent,
             .footerComponent,
             .nameComponent,
+            .otpInputComponent,
             .selectComponent,
             .inputComponent,
             .inputPhoneComponent,
@@ -2206,10 +2196,6 @@ private extension Target.Dependency {
     static let checkBoxComponent = byName(
         name: .checkBoxComponent
     )
-      
-    static let codeComponent = byName(
-        name: .codeComponent
-    )
     
     static let footerComponent = byName(
         name: .footerComponent
@@ -2473,8 +2459,6 @@ private extension String {
     static let infoComponent = "InfoComponent"
     
     static let checkBoxComponent = "CheckBoxComponent"
-    
-    static let codeComponent = "CodeComponent"
     
     static let footerComponent = "FooterComponent"
     
