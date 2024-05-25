50cf6480b7df5218e415edc90d72420dffb20f81
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-05-25 16:31:17 +0300

    add simple `PaymentsTransfersFlowComposerTests` to check composer

---
+++ ForaBank/ForaBankTests/CompositionRoot/PaymentsTransfers/PaymentsTransfersFlowComposerTests.swift
@@ -0,0 +1,53 @@
+//
+//  PaymentsTransfersFlowComposerTests.swift
+//  ForaBankTests
+//
+//  Created by Igor Malyarov on 25.05.2024.
+//
+
+@testable import ForaBank
+import XCTest
+
+final class PaymentsTransfersFlowComposerTests: XCTestCase {
+    
+    func test_compose_live_shouldNotFail() {
+        
+        _ = makeSUT(flag: .live).compose()
+    }
+    
+    func test_compose_stub_shouldNotFail() {
+        
+        _ = makeSUT(flag: .stub).compose()
+    }
+    
+    // MARK: - Helpers
+    
+    private typealias SUT = PaymentsTransfersFlowComposer
+    
+    private func makeSUT(
+        flag: StubbedFeatureFlag.Option,
+        model: Model? = nil,
+        pageSize: Int = 30,
+        observeLast: Int = 5,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> SUT {
+        
+        let model = model ?? .mockWithEmptyExcept()
+        let httpClient = HTTPClientSpy()
+        let sut = SUT(
+            flag: flag,
+            model: model,
+            httpClient: httpClient,
+            log: { _,_,_ in },
+            pageSize: pageSize,
+            observeLast: observeLast
+        )
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(model, file: file, line: line)
+        trackForMemoryLeaks(httpClient, file: file, line: line)
+        
+        return sut
+    }
+}
