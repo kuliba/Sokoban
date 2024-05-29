b6e960e96e1e26ca8dbf84cd111259b7820425d5
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-05-29 07:31:16 +0300

    add `AutoScrollingListView`

--- ForaBank/PreviewApps/AutoScrollingListPreview/ContentView.swift
+++ ForaBank/PreviewApps/AutoScrollingListPreview/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var items = (1...20).map(Item.init)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-
-            Text("Hello, world!")
+        
+        NavigationView {
+        
+            VStack {
+            
+                AutoScrollingListView(items: items) {
+                    
+                    Text($0.name)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.vertical, 6)
+                }
+                .padding(.horizontal)
+            }
+            .navigationTitle("Auto-Scroll List")
+            .toolbar(content: toolbar)
         }
-        .padding()
+    }
+    
+    private func addItem() {
+        
+        let newID = items.count + 1
+        items.append(.init(id: newID))
+    }
+    
+    private func toolbar() -> some ToolbarContent {
+        
+        ToolbarItem(placement: .navigationBarTrailing) {
+            
+            Button(action: addItem) {
+                
+                Image(systemName: "plus")
+            }
+        }
     }
 }
 
