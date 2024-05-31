a67a26bdec4f2a87571df90364b8c5143c7569fc
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-05-31 11:21:43 +0300

    add docs

--- swift-forabank/Sources/ForaTools/Decorators/ThrottleDecorator.swift
+++ swift-forabank/Sources/ForaTools/Decorators/ThrottleDecorator.swift
@@ -7,12 +7,15 @@
 
 import Foundation
 
+/// A decorator class to throttle function calls, ensuring that they are not executed more frequently than a specified delay.
 public final class ThrottleDecorator {
     
     private let delay: TimeInterval
     private var lastCallTime: TimeInterval = 0
     private let queue = DispatchQueue(label: "com.ThrottleDecorator.queue")
     
+    /// Initialises the ThrottleDecorator with a specified delay.
+    /// - Parameter delay: The minimum time interval between allowed function calls.
     public init(delay: TimeInterval) {
         
         self.delay = delay
@@ -21,12 +24,18 @@ public final class ThrottleDecorator {
 
 public extension ThrottleDecorator {
     
+    /// Calls the provided block, ensuring that it is not executed more frequently than the specified delay.
+    /// Uses a weak reference to self to prevent retain cycles.
+    /// - Parameter block: The closure to be debounced.
     func throttle(
         block: @escaping () -> Void
     ) {
         queue.sync { [weak self] in self?.execute(block) }
     }
     
+    /// Calls the provided block, ensuring that it is not executed more frequently than the specified delay.
+    /// Uses a strong reference to self.
+    /// - Parameter block: The closure to be throttled.
     func callAsFunction(
         block: @escaping () -> Void
     ) {
