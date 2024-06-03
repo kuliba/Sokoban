7a1d9ad53f0af50e52a7df6b21323df46a1951a4
Tasha Andryushina <natalykn@gmail.com>
2024-06-03 15:51:04 +0300

    add `isDarkColor`, add tests

--- ForaBank/ForaBank/Extensions/Color+Extensions.swift
+++ ForaBank/ForaBank/Extensions/Color+Extensions.swift
@@ -57,4 +57,12 @@ extension Color {
         }
         return (r, g, b, a)
     }
+    
+    func isDarkColor() -> Bool {
+        var r, g, b, a: CGFloat
+        (r, g, b, a) = (0, 0, 0, 0)
+        UIColor(self).getRed(&r, green: &g, blue: &b, alpha: &a)
+        let luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b
+        return  luminance < 0.80
+    }
 }
