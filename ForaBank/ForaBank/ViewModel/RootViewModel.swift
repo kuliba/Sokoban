1b1d5fc87233cfa4a84a6ff99fe43cf7106fd342
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-06-03 20:28:41 +0400

    update `AppVersion`

--- ForaBank/ForaBank/ViewModel/RootViewModel.swift
+++ ForaBank/ForaBank/ViewModel/RootViewModel.swift
@@ -293,15 +293,24 @@ class RootViewModel: ObservableObject, Resetable {
                         case let .success(appInfo):
                             LoggerAgent.shared.log(level: .debug, category: .ui, message: "received ModelAction.AppVersion.Response, success, info: \(appInfo)")
                             
-                            if let appVersion = self.infoDictionary?["CFBundleShortVersionString"] as? String, appInfo.version > appVersion {
+                            if let appVersion = self.infoDictionary?["CFBundleShortVersionString"] as? String {
                                 
-                                self.alert = .init(title: "Новая версия", message: "Доступна новая версия \(appInfo.version).", primary: .init(type: .default, title: "Не сейчас", action: {}), secondary: .init(type: .default, title: "Обновить", action: {
-                                    guard let url = URL(string: "\(appInfo.trackViewUrl)") else {
-                                        return
-                                    }
-                                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                                let compareVersion = appInfo.version.compareVersion(to: appVersion)
+                                
+                                switch compareVersion {
+                                case .orderedAscending:
                                     
-                                }))
+                                    self.alert = .init(title: "Новая версия", message: "Доступна новая версия \(appInfo.version).", primary: .init(type: .default, title: "Не сейчас", action: {}), secondary: .init(type: .default, title: "Обновить", action: {
+                                        guard let url = URL(string: "\(appInfo.trackViewUrl)") else {
+                                            return
+                                        }
+                                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                                        
+                                    }))
+                                    
+                                default:
+                                    break
+                                }
                             }
                             
                         case let .failure(error):
