a39b2576e8a2203d9cce588fe76243e4f5664dd8
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-06-05 10:56:29 +0300

    add `scheduler`

--- ForaBank/PreviewApps/CachedModelsStatePreview/RxCachedModelsViewModel.swift
+++ ForaBank/PreviewApps/CachedModelsStatePreview/RxCachedModelsViewModel.swift
@@ -6,27 +6,35 @@
 //
 
 import Combine
+import CombineSchedulers
 import ForaTools
 import Foundation
 import RxViewModel
 
-final class CachedPaymentViewModel: ObservableObject {
+typealias AnySchedulerOfDispatchQueue = AnySchedulerOf<DispatchQueue>
 
+extension AnySchedulerOfDispatchQueue {
+    
+    static func makeMain() -> AnySchedulerOfDispatchQueue { .main }
+}
+
+final class CachedPaymentViewModel: ObservableObject {
+    
     @Published private(set) var state: State
     
     private var cancellables = Set<AnyCancellable>()
     
     init(
         source: Source,
-        map: @escaping Map
+        map: @escaping Map,
+        scheduler: AnySchedulerOfDispatchQueue = .makeMain()
     ) {
         let pairs = source.state.fields.map { ($0.id, map($0)) }
         let initialState = State(pairs: pairs)
         self.state = initialState
         
-        #warning("add Scheduler")
         source.$state
-            .receive(on: DispatchQueue.main)
+            .receive(on: scheduler)
             .sink { [weak self] payment in
                 
                 guard let self else { return }
