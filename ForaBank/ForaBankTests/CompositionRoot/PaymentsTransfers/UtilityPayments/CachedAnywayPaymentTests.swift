5233383bf4c62e4386fbb007f07670648da2427f
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-06-06 12:07:50 +0300

    add `CachedAnywayPaymentTests`: test updating

--- ForaBank/ForaBankTests/CompositionRoot/PaymentsTransfers/UtilityPayments/CachedAnywayPaymentTests.swift
+++ ForaBank/ForaBankTests/CompositionRoot/PaymentsTransfers/UtilityPayments/CachedAnywayPaymentTests.swift
@@ -33,7 +33,7 @@ struct CachedAnywayPayment<ElementModel> {
     
     init(
         _ payment: AnywayPayment,
-        using map: @escaping (Element) -> (ElementModel)
+        using map: @escaping Map
     ) {
         self.init(
             cachedModels: .init(pairs: payment.elements.map { ($0.id, map($0)) }),
@@ -49,6 +49,24 @@ struct CachedAnywayPayment<ElementModel> {
         cachedModels.keyModelPairs.map(IdentifiedModels.init)
     }
     
+    func updating(
+        with payment: AnywayPayment,
+        using map: @escaping Map
+    ) -> Self {
+        
+        let updatedCachedModels = cachedModels.updating(
+            with: payment.elements,
+            using: map
+        )
+        return .init(
+            cachedModels: updatedCachedModels,
+            infoMessage: payment.infoMessage,
+            isFinalStep: payment.isFinalStep,
+            isFraudSuspected: payment.isFraudSuspected,
+            puref: payment.puref
+        )
+    }
+    
     struct IdentifiedModels: Identifiable {
         
         let id: Element.ID
@@ -60,6 +78,8 @@ struct CachedAnywayPayment<ElementModel> {
     
     typealias Puref = AnywayPayment.Puref
     typealias AnywayPayment = AnywayPaymentDomain.AnywayPayment
+    
+    typealias Map = (Element) -> (ElementModel)
 }
 
 @testable import ForaBank
@@ -143,7 +163,108 @@ final class CachedAnywayPaymentTests: XCTestCase {
         let payment = Payment(anywayPayment)
         
         XCTAssertNoDiff(payment.models.map(\.id), [.fieldID(field.id), .parameterID(parameter.field.id), .widgetID(.core)])
-        XCTAssertNoDiff(payment.models.map(\.model), anywayPayment.elements)
+        XCTAssertNoDiff(payment.models.map(\.model), [.field(field), .parameter(parameter), .widget(.core(core))])
+    }
+    
+    // MARK: - updating
+    
+    func test_updating_shouldUpdateWithNilInfoMessageFromAnywayPaymentWithNilInfoMessage() {
+        
+        let payment = Payment(makeAnywayPayment(infoMessage: anyMessage()))
+        XCTAssertNotNil(payment.infoMessage)
+        
+        let updated = updating(payment, with: makeAnywayPayment(infoMessage: nil))
+        
+        XCTAssertNil(updated.infoMessage)
+    }
+    
+    func test_updating_shouldUpdateWithInfoMessageFromAnywayPaymentWithInfoMessage() {
+        
+        let payment = Payment(makeAnywayPayment(infoMessage: nil))
+        XCTAssertNil(payment.infoMessage)
+        
+        let infoMessage = anyMessage()
+        let updated = updating(payment, with: makeAnywayPayment(infoMessage: infoMessage))
+        
+        XCTAssertNoDiff(updated.infoMessage, infoMessage)
+    }
+    
+    func test_updating_shouldUpdateWithIsFinalStepFalseFromAnywayPaymentWithIsFinalStepFalse() {
+        
+        let payment = Payment(makeAnywayPayment(isFinalStep: true))
+        XCTAssertTrue(payment.isFinalStep)
+        
+        let updated = updating(payment, with: makeAnywayPayment(isFinalStep: false))
+        
+        XCTAssertFalse(updated.isFinalStep)
+    }
+    
+    func test_updating_shouldUpdateIsFinalStepTrueFromAnywayPaymentIsFinalStepTrue() {
+        
+        let payment = Payment(makeAnywayPayment(isFinalStep: false))
+        XCTAssertFalse(payment.isFinalStep)
+        
+        let updated = updating(payment, with: makeAnywayPayment(isFinalStep: true))
+        
+        XCTAssertTrue(updated.isFinalStep)
+    }
+    
+    func test_updating_shouldUpdateWithIsFraudSuspectedFalseFromAnywayPaymentWithIsFraudSuspectedFalse() {
+        
+        let payment = Payment(makeAnywayPayment(isFraudSuspected: true))
+        XCTAssertTrue(payment.isFraudSuspected)
+        
+        let updated = updating(payment, with: makeAnywayPayment(isFraudSuspected: false))
+        
+        XCTAssertFalse(updated.isFraudSuspected)
+    }
+    
+    func test_updating_shouldUpdateIsFraudSuspectedTrueFromAnywayPaymentIsFraudSuspectedTrue() {
+        
+        let payment = Payment(makeAnywayPayment(isFraudSuspected: false))
+        XCTAssertFalse(payment.isFraudSuspected)
+        
+        let updated = updating(payment, with: makeAnywayPayment(isFraudSuspected: true))
+        
+        XCTAssertTrue(updated.isFraudSuspected)
+    }
+    
+    func test_updating_shouldUpdateWithPurefFromAnywayPaymentWithPuref() {
+        
+        let puref1 = anyMessage()
+        let payment = Payment(makeAnywayPayment(puref: .init(puref1)))
+        XCTAssertNoDiff(payment.puref, .init(puref1))
+        
+        let puref2 = anyMessage()
+        let updated = updating(payment, with: makeAnywayPayment(puref: .init(puref2)))
+        
+        XCTAssertNoDiff(updated.puref, .init(puref2))
+    }
+    
+    func test_updating_shouldUpdateWithEmptyModelsFromAnywayPaymentWithEmptyElements() {
+        
+        let payment = Payment(makeAnywayPayment(elements: [.field(makeAnywayPaymentField())]))
+        XCTAssertFalse(payment.models.isEmpty)
+        
+        let updated = updating(payment, with: makeAnywayPayment(elements: []))
+        
+        XCTAssertTrue(updated.models.isEmpty)
+    }
+    
+    func test_updating_shouldUpdateWithModelsFromAnywayPaymentWithElements() {
+        
+        let field = makeAnywayPaymentField()
+        let parameter = makeAnywayPaymentParameter()
+        let core = makeAnywayPaymentCoreWidget()
+        let anywayPayment = makeAnywayPayment(
+            elements: [.field(field), .parameter(parameter), .widget(.core(core))]
+        )
+        let payment = Payment(anywayPayment)
+        
+        let updated = updating(payment, with: makeAnywayPayment(elements: [.parameter(parameter), .field(field)]))
+        
+        XCTAssertNoDiff(updated.models.map(\.id), [.parameterID(parameter.field.id), .fieldID(field.id)])
+        XCTAssertNoDiff(updated.models.map(\.model), [.parameter(parameter), .field(field)])
     }
     
     // MARK: - Helpers
@@ -166,6 +287,14 @@ final class CachedAnywayPaymentTests: XCTestCase {
             isFraudSuspected: isFraudSuspected,
             puref: puref
         )
+    }
+    
+    private func updating(
+        _ payment: Payment,
+        with anywayPayment: AnywayPayment
+    ) -> Payment {
+        
+        return payment.updating(with: anywayPayment, using: { $0 })
     }
 }
 
