fed13e2e0c77e7a7b2e67cfd4ecfb906a2dbd0e8
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-06-11 18:00:07 +0300

    add `FraudPaymentCompleteView`

---
+++ ForaBank/ForaBank/View/PaymentsTransfers/FraudCancelledPaymentCompleteView/FraudPaymentCompleteViewConfig.swift
@@ -0,0 +1,20 @@
+//
+//  FraudPaymentCompleteViewConfig.swift
+//  ForaBank
+//
+//  Created by Igor Malyarov on 11.06.2024.
+//
+
+import SharedConfigs
+import SwiftUI
+
+struct FraudPaymentCompleteViewConfig: Equatable {
+    
+    let amountConfig: TextConfig
+    let icon: Image
+    let iconColor: Color
+    let message: String
+    let messageConfig: TextConfig
+    let reason: String
+    let reasonConfig: TextConfig
+}
