f753a658075214bd2f337a78351168bbc2994476
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-06-16 18:09:59 +0300

    Revert "add spinner decoration (NOTE: delayed for unknown reason)"
    This reverts commit 8f7bda826859bfd4b19aa9d255394ea8f1c7bdc3.

--- ForaBank/ForaBank/CompositionRoot/PaymentsTransfers/AnywayTransactionViewModelComposer.swift
+++ ForaBank/ForaBank/CompositionRoot/PaymentsTransfers/AnywayTransactionViewModelComposer.swift
@@ -43,19 +43,11 @@ extension AnywayTransactionViewModelComposer {
         let composer = ReducerComposer()
         let reducer = composer.compose()
         
-        let decorator = HandleEffectDecorator(
-            decoratee: effectHandler.handleEffect(_:_:),
-            decoration: .init(
-                onEffectStart: { self.spinnerActions?.show() },
-                onEffectFinish: { self.spinnerActions?.hide() }
-            )
-        )
-
         return .init(
             transaction: transaction,
             mapToModel: { event in { self.elementMapper.map($0, event) }},
             reduce: reducer.reduce(_:_:),
-            handleEffect: decorator.callAsFunction(_:_:),
+            handleEffect: effectHandler.handleEffect(_:_:),
             observe: notify,
             scheduler: scheduler
         )
