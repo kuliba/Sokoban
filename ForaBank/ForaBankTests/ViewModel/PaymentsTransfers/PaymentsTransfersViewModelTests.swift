2d13bc5cafa6782dcd85bdd8f7ee83f326d9ee9a
Tasha Andryushina <natalykn@gmail.com>
2024-06-18 17:17:29 +0300

    update tests

--- ForaBank/ForaBankTests/ViewModel/PaymentsTransfers/PaymentsTransfersViewModelTests.swift
+++ ForaBank/ForaBankTests/ViewModel/PaymentsTransfers/PaymentsTransfersViewModelTests.swift
@@ -42,7 +42,7 @@ final class PaymentsTransfersViewModelTests: XCTestCase {
         sut.meToMeSendSuccess(model: model)
         _ = XCTWaiter().wait(for: [.init()], timeout: 0.5)
         
-        XCTAssertEqual(spy.values.count, 4)
+        XCTAssertEqual(spy.values.count, 2)
     }
     
 #warning("fix and restore")
