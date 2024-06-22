985d4fc4e3d1829b2cc623ee8b4c5003da6e5156
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-06-22 08:39:05 +0300

    extract `FooterViewModel` to prod

--- swift-forabank/Tests/UI/Components/AmountComponentTests/FooterComponentTests/FooterViewModelTests.swift
+++ swift-forabank/Tests/UI/Components/AmountComponentTests/FooterComponentTests/FooterViewModelTests.swift
@@ -5,91 +5,9 @@
 //  Created by Igor Malyarov on 22.06.2024.
 //
 
-import Combine
-import Foundation
+import AmountComponent
 import TextFieldDomain
 import TextFieldComponent
-
-final class FooterViewModel: ObservableObject {
-    
-    @Published private(set) var state: State
-    
-    let textFieldModel: DecimalTextFieldViewModel
-    
-    private let reduce: Reduce
-    private let formatter: DecimalFormatter
-    private let stateSubject = PassthroughSubject<State, Never>()
-    private var cancellables = Set<AnyCancellable>()
-    
-    init(
-        initialState: State,
-        reduce: @escaping Reduce,
-        formatter: DecimalFormatter,
-        textFieldModel: DecimalTextFieldViewModel,
-        scheduler: AnySchedulerOfDispatchQueue = .makeMain()
-    ) {
-        self.state = initialState
-        self.textFieldModel = textFieldModel
-        self.reduce = reduce
-        self.formatter = formatter
-        
-        textFieldModel.setText(to: formatter.format(initialState.amount))
-        
-        textFieldModel.$state
-        //            .dropFirst(2)
-            .map(formatter.getDecimal)
-            .removeDuplicates()
-            .receive(on: scheduler)
-            .sink { [weak self] in
-                
-                // print(self.map(ObjectIdentifier.init) ?? "", "sink", $0)
-                self?.state.amount = $0
-            }
-            .store(in: &cancellables)
-        
-        stateSubject
-            .receive(on: scheduler)
-            .assign(to: &$state)
-    }
-}
-
-extension FooterViewModel {
-    
-    typealias State = FooterState
-    typealias Event = FooterEvent
-    typealias Effect = FooterEffect
-    
-    typealias Reduce = (State, Event) -> (State, FooterEffect?)
-    typealias GetDecimal = (TextFieldState) -> Decimal
-}
-
-extension FooterViewModel {
-    
-    func event(_ event: Event) {
-        
-        notifyTextField(event)
-        stateSubject.send(reduce(state, event).0)
-    }
-}
-
-private extension FooterViewModel {
-    
-    func notifyTextField(_ event: Event) {
-        
-        switch event {
-        case .button(.tap):
-            textFieldModel.finishEditing()
-            
-        case let .edit(amount):
-            textFieldModel.setText(to: formatter.format(amount))
-            
-        default:
-            break
-        }
-    }
-}
-
-import AmountComponent
 import XCTest
 
 final class FooterViewModelTests: XCTestCase {
