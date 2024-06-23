dbea7a9ea07ebf12a0effed6480903506f961439
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-06-23 09:32:39 +0300

    improve `AnywayPaymentParameterValidatorTests`

--- swift-forabank/Sources/Payments/AnywayPayment/AnywayPaymentCore/AnywayPaymentParameterValidator/AnywayPaymentParameterValidationError.swift
+++ swift-forabank/Sources/Payments/AnywayPayment/AnywayPaymentCore/AnywayPaymentParameterValidator/AnywayPaymentParameterValidationError.swift
@@ -5,7 +5,7 @@
 //  Created by Igor Malyarov on 23.06.2024.
 //
 
-public enum AnywayPaymentParameterValidationError {
+public enum AnywayPaymentParameterValidationError: Equatable {
     
     case emptyRequired
     case regExViolation
