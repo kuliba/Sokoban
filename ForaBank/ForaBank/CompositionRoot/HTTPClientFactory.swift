92676e2636204d36b9998d2017fd4a70902ce368
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-06-24 23:59:47 +0300

    add empty blacklisted httpClient decoration

--- ForaBank/ForaBank/CompositionRoot/HTTPClientFactory.swift
+++ ForaBank/ForaBank/CompositionRoot/HTTPClientFactory.swift
@@ -5,6 +5,9 @@
 //  Created by Igor Malyarov on 24.06.2024.
 //
 
+import ForaTools
+import Foundation
+
 enum HTTPClientFactory {}
 
 extension HTTPClientFactory {
@@ -14,6 +17,40 @@ extension HTTPClientFactory {
         logger: LoggerAgentProtocol
     ) -> HTTPClient {
         
-        model.authenticatedHTTPClient()
+        let httpClient = model.authenticatedHTTPClient()
+        
+        let isBlacklisted: (URLRequest, Int) -> Bool = { _,_ in false }
+        
+        return AnyLoader(httpClient.performRequest(_:completion:))
+            .blacklisted(isBlacklisted: isBlacklisted)
+            .mapError(\.error)
     }
+}
+
+extension AnyLoader: HTTPClient
+where Payload == Request,
+      Response == Result<(Data, HTTPURLResponse), Error> {
+    
+    public func performRequest(
+        _ request: Request,
+        completion: @escaping Completion
+    ) {
+        load(request, completion)
+    }
+}
+
+private extension BlacklistDecorator.Error {
+    
+    var error: Error {
+        
+        switch self {
+        case .blacklistedError:
+            return BlacklistedError()//NSError(domain: "BlacklistedError", code: 1, userInfo: nil)
+            
+        case .loadFailure(let error):
+            return error
+        }
+    }
+    
+    struct BlacklistedError: Error {}
 }
