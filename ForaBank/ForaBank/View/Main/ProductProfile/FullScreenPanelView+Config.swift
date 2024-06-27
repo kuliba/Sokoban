61f9872d3279755aea31f62e53c608f65ba9a126
Tasha Andryushina <natalykn@gmail.com>
2024-06-27 17:24:28 +0300

    add `FullScreenPanelViewConfig+ext`

--- ForaBank/ForaBank/View/Main/ProductProfile/FullScreenPanelView+Config.swift
+++ ForaBank/ForaBank/View/Main/ProductProfile/FullScreenPanelView+Config.swift
@@ -42,16 +42,3 @@ extension FullScreenPanelView {
         let hstack: CGFloat
     }
 }
-
-extension FullScreenPanelView.Config {
-    
-    static let `default`: Self = .init(
-        colors: .init(
-            background: .bgIconGrayLightest,
-            title: .textSecondary,
-            subtitle: .textPlaceholder),
-        height: 56,
-        paddings: .init(horizontal: 16, top: 30),
-        spacings: .init(vstack: 24, hstack: 8),
-        fonts: .init(title: .textBodySR12160()))
-}
