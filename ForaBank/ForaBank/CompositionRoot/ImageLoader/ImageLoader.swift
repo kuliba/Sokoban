d5f9f879da3277e65aef602fe7e88cc428732f5e
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-07-01 07:14:10 +0300

    define `ImageLoader`

---
+++ ForaBank/ForaBank/CompositionRoot/ImageLoader/ImageLoader.swift
@@ -0,0 +1,11 @@
+//
+//  ImageLoader.swift
+//  ForaBank
+//
+//  Created by Igor Malyarov on 30.06.2024.
+//
+
+import ForaTools
+import SwiftUI
+
+typealias ImageLoader = ForaTools.Loader<String, Result<Image, Error>>
