f5054d0dbf5e7af32089b1768eeb07b66f3f27df
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-07-01 15:49:43 +0300

    TDD `RequestCollector`

---
+++ swift-forabank/Tests/ForaToolsTests/Loaders/RequestCollectorTests.swift
@@ -0,0 +1,296 @@
+//
+//  RequestCollectorTests.swift
+//
+//
+//  Created by Igor Malyarov on 01.07.2024.
+//
+
+import CombineSchedulers
+import ForaTools
+import Foundation
+
+#warning("add request cancellation")
+final class RequestCollector<Request, Response>
+where Request: Hashable {
+    
+    private var pendingRequests: Completions
+    private var inflightRequests: Completions
+    private var startTime: Date?
+    private let collectionPeriod: DispatchTimeInterval
+    private let performRequests: PerformRequests
+    private let scheduler: AnySchedulerOf<DispatchQueue>
+    
+    init(
+        pendingRequests: Completions = .init(),
+        inflightRequests: Completions = .init(),
+        startTime: Date? = nil,
+        collectionPeriod: DispatchTimeInterval,
+        performRequests: @escaping PerformRequests,
+        scheduler: AnySchedulerOf<DispatchQueue>
+    ) {
+        self.pendingRequests = pendingRequests
+        self.inflightRequests = inflightRequests
+        self.startTime = startTime
+        self.collectionPeriod = collectionPeriod
+        self.performRequests = performRequests
+        self.scheduler = scheduler
+    }
+    
+    typealias Completion = (Response) -> Void
+    typealias Completions = [Request: [Completion]]
+    
+    typealias PerformRequestsCompletion = ([Request: Response]) -> Void
+    typealias PerformRequests = ([Request], @escaping PerformRequestsCompletion) -> Void
+}
+
+extension RequestCollector {
+    
+    func process(
+        _ request: Request,
+        _ completion: @escaping Completion
+    ) {
+        if let handlers = inflightRequests[request] {
+            inflightRequests[request] = handlers + [completion]
+        } else if let handlers = pendingRequests[request] {
+            pendingRequests[request] = handlers + [completion]
+        } else {
+            pendingRequests[request] = [completion]
+        }
+        
+        if startTime == nil {
+            
+            startTime = Date()
+            scheduleCollaboratorInvocation()
+        }
+    }
+}
+
+#warning("move to separate file")
+extension RequestCollector: Loader {
+    
+    func load(
+        _ request: Request,
+        _ completion: @escaping Completion
+    ) {
+        process(request, completion)
+    }
+}
+
+private extension RequestCollector {
+    
+    func scheduleCollaboratorInvocation() {
+        
+        scheduler.schedule(
+            after: .init(.now().advanced(by: collectionPeriod))
+        ) {
+            self.processRequests()
+        }
+    }
+    
+    func processRequests() {
+        
+        guard !pendingRequests.isEmpty
+        else {
+            startTime = nil
+            return
+        }
+        
+        let requestsToProcess = Array(pendingRequests.keys)
+        inflightRequests = pendingRequests
+        pendingRequests.removeAll()
+        startTime = nil
+        
+        performRequests(requestsToProcess, handleResponses)
+    }
+    
+    func handleResponses(responses: [Request: Response]) {
+        
+        for (request, response) in responses {
+            
+            if let handlers = inflightRequests[request] {
+                
+                for handler in handlers {
+                    handler(response)
+                }
+                inflightRequests.removeValue(forKey: request)
+            }
+        }
+    }
+}
+
+import CombineSchedulers
+import ForaTools
+import XCTest
+
+final class RequestCollectorTests: XCTestCase {
+    
+    // MARK: - init
+    
+    func test_init_shouldNotCallCollaborator() {
+        
+        let (_, spy, _) = makeSUT()
+        
+        XCTAssertEqual(spy.callCount, 0)
+    }
+    
+    // MARK: - process
+    
+    func test_process_shouldNotCallCollaboratorOnNoRequests() {
+        
+        let (_, spy, scheduler) = makeSUT()
+        
+        scheduler.advance(to: .init(.now()))
+        
+        XCTAssertEqual(spy.callCount, 0)
+    }
+    
+    func test_process_shouldCallCollaboratorOnceWithRequestAfterTimeInterval() {
+        
+        let request = anyRequest()
+        let (sut, spy, scheduler) = makeSUT(collectionPeriod: .seconds(100))
+        
+        sut.process(request) { _ in }
+        sut.process(request) { _ in }
+        sut.process(request) { _ in }
+        XCTAssertTrue(spy.payloads.isEmpty)
+        
+        scheduler.advance(to: .init(.now().advanced(by: .seconds(99))))
+        XCTAssertTrue(spy.payloads.isEmpty)
+        
+        scheduler.advance(to: .init(.now().advanced(by: .seconds(100))))
+        XCTAssertNoDiff(spy.payloads, [[request]])
+    }
+    
+    func test_process_shouldNotCallCollaboratorAgainOnRequestAfterTimeIntervalBeforeCollaboratorCompletion() {
+        
+        let request = anyRequest()
+        let (sut, spy, scheduler) = makeSUT(collectionPeriod: .seconds(100))
+        
+        sut.process(request) { _ in }
+        sut.process(request) { _ in }
+        sut.process(request) { _ in }
+        XCTAssertTrue(spy.payloads.isEmpty)
+        
+        scheduler.advance(to: .init(.now().advanced(by: .seconds(100))))
+        XCTAssertNoDiff(spy.payloads, [[request]])
+        
+        sut.process(request) { _ in }
+        scheduler.advance(to: .init(.distantFuture))
+        XCTAssertNoDiff(spy.payloads, [[request]])
+    }
+    
+    func test_process_shouldCallCollaboratorAgainOnRequestAfterTimeIntervalAndCompleted() {
+        
+        let request = anyRequest()
+        let (sut, spy, scheduler) = makeSUT(collectionPeriod: .seconds(100))
+        
+        sut.process(request) { _ in }
+        sut.process(request) { _ in }
+        sut.process(request) { _ in }
+        XCTAssertTrue(spy.payloads.isEmpty)
+        
+        scheduler.advance(to: .init(.now().advanced(by: .seconds(100))))
+        XCTAssertNoDiff(spy.payloads, [[request]])
+        spy.complete(with: [request: anyResponse()])
+        
+        sut.process(request) { _ in }
+        scheduler.advance(to: .init(.now().advanced(by: .seconds(99))))
+        XCTAssertNoDiff(spy.payloads, [[request]])
+        
+        scheduler.advance(to: .init(.now().advanced(by: .seconds(100))))
+        XCTAssertNoDiff(spy.payloads, [[request], [request]])
+    }
+    
+    func test_process_shouldDeliverResponseToMultipleOnSameRequest() {
+        
+        let (request, response) = (anyRequest(), anyResponse())
+        let (sut, spy, scheduler) = makeSUT(collectionPeriod: .seconds(100))
+        var responses = [Response]()
+        
+        sut.process(request) { responses.append($0) }
+        XCTAssertTrue(responses.isEmpty)
+        
+        sut.process(request) { responses.append($0) }
+        XCTAssertTrue(responses.isEmpty)
+        
+        sut.process(request) { responses.append($0) }
+        XCTAssertTrue(responses.isEmpty)
+        
+        scheduler.advance(to: .init(.now().advanced(by: .seconds(99))))
+        XCTAssertTrue(responses.isEmpty)
+        
+        scheduler.advance(to: .init(.now().advanced(by: .seconds(100))))
+        spy.complete(with: [request: response])
+        
+        XCTAssertNoDiff(responses, [response, response, response])
+    }
+    
+    func test_process_shouldDeliverResponses() {
+        
+        let (request1, request2, request3) = (anyRequest(), anyRequest(), anyRequest())
+        let (response1, response2, response3) = (anyResponse(), anyResponse(), anyResponse())
+        let (sut, spy, scheduler) = makeSUT(collectionPeriod: .seconds(1))
+        var responses = [Request: Response]()
+        
+        sut.process(request1) { responses[request1] = $0 }
+        XCTAssertTrue(responses.isEmpty)
+        
+        sut.process(request2) { responses[request2] = $0 }
+        XCTAssertTrue(responses.isEmpty)
+        
+        sut.process(request3) { responses[request3] = $0 }
+        XCTAssertTrue(responses.isEmpty)
+        
+        scheduler.advance(to: .init(.now().advanced(by: .seconds(1))))
+        spy.complete(with: [request1: response1, request2: response2, request3: response3])
+        
+        XCTAssertNoDiff(responses, [request1: response1, request2: response2, request3: response3])
+    }
+    
+    // MARK: - Helpers
+    
+    private typealias SUT = RequestCollector<Request, Response>
+    private typealias Request = Int
+    private typealias Response = String
+    
+    private typealias PerformRequestsSpy = Spy<[Request], [Request: Response]>
+    
+    private func makeSUT(
+        collectionPeriod: DispatchTimeInterval = .milliseconds(100),
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (
+        sut: SUT,
+        spy: PerformRequestsSpy,
+        scheduler: TestSchedulerOfDispatchQueue
+    ) {
+        let spy = PerformRequestsSpy()
+        let scheduler = DispatchQueue.test
+        let sut = SUT(
+            collectionPeriod: collectionPeriod,
+            performRequests: spy.process(_:completion:),
+            scheduler: scheduler.eraseToAnyScheduler()
+        )
+        
+#warning("FIX MEMORY LEAKS")
+        //        trackForMemoryLeaks(sut, file: file, line: line)
+        //        trackForMemoryLeaks(spy, file: file, line: line)
+        //        trackForMemoryLeaks(scheduler, file: file, line: line)
+        
+        return (sut, spy, scheduler)
+    }
+    
+    private func anyRequest(
+        _ value: Int = .random(in: 1...1_000)
+    ) -> Request {
+        
+        return value
+    }
+    
+    private func anyResponse(
+        _ value: String = UUID().uuidString
+    ) -> Response {
+        
+        return value
+    }
+}
