fd4ef1e54a1ec10c8aa881167f1ee7dc3f061f56
Tasha Andryushina <natalykn@gmail.com>
2024-07-04 15:29:10 +0300

    remove equatable

--- ForaBank/ForaBank/ViewModel/Main/ProductProfile/ControlPanel/ControlPanelEffect.swift
+++ ForaBank/ForaBank/ViewModel/Main/ProductProfile/ControlPanel/ControlPanelEffect.swift
@@ -7,25 +7,9 @@
 
 import SwiftUI
 
-enum ControlPanelEffect: Equatable {
-    
-    static func == (lhs: ControlPanelEffect, rhs: ControlPanelEffect) -> Bool {
-        lhs.id == rhs.id
-    }
-    
+enum ControlPanelEffect {
+        
     case delayAlert(Alert.ViewModel, DispatchTimeInterval)
     case blockCard(ProductCardData)
     case unblockCard(ProductCardData)
-
-    var id: String {
-        
-        switch self {
-        case let .delayAlert(viewModel, _):
-            return viewModel.id.uuidString 
-        case let .blockCard(card):
-            return "\(card.id)"
-        case let .unblockCard(card):
-            return "\(card.id)"
-        }
-    }
 }
