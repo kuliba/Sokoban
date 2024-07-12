f6d988d1b165c0ff58f93ea6350185b6cca49436
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-07-12 06:41:45 +0400

    update `InputView`

--- swift-forabank/Sources/UI/Components/InputComponent/InputView.swift
+++ swift-forabank/Sources/UI/Components/InputComponent/InputView.swift
@@ -7,24 +7,45 @@
 
 import SwiftUI
 import SharedConfigs
+import TextFieldComponent
 
 public struct InputView<Icon, IconView: View>: View {
-
+    
+    @StateObject private var regularFieldViewModel: RegularFieldViewModel
+    
     let state: InputState<Icon>
     let event: (InputEvent) -> Void
     let config: InputConfig
+    let commit: (String) -> Void
     let iconView: () -> IconView
+    let isValid: (String) -> Bool
     
     public init(
         state: InputState<Icon>,
         event: @escaping (InputEvent) -> Void,
         config: InputConfig,
-        iconView: @escaping () -> IconView
+        iconView: @escaping () -> IconView,
+        commit: @escaping (String) -> Void,
+        isValid: @escaping (String) -> Bool
     ) {
         self.state = state
         self.event = event
         self.config = config
         self.iconView = iconView
+        self.commit = commit
+        self.isValid = isValid
+        
+        let regularFieldViewModel: RegularFieldViewModel = .make(
+            keyboardType: .init(keyboardType: config.keyboardType),
+            text: state.dynamic.value,
+            placeholderText: config.placeholder,
+            limit: config.limit,
+            regExp: config.regExp
+        )
+        
+        self._regularFieldViewModel = .init(
+            wrappedValue: regularFieldViewModel
+        )
     }
     
     public var body: some View {
@@ -39,18 +60,23 @@ public struct InputView<Icon, IconView: View>: View {
                     
                     state.settings.title.text(withConfig: config.titleConfig)
                     
-                    
-                    TextField(
-                        config.placeholder,
-                        text: .init(
-                            get: { state.dynamic.value },
-                            set: { event(.edit($0)) }
-                        )
+                    TextFieldView(
+                        viewModel: regularFieldViewModel,
+                        textFieldConfig: .init(
+                            font: .systemFont(ofSize: 16),
+                            textColor: config.textFieldFont.textColor,
+                            tintColor: .black,
+                            backgroundColor: .clear,
+                            placeholderColor: .gray.opacity(0.3)
+                        ),
+                        isFirstResponder: false
                     )
+                    .onChange(of: regularFieldViewModel.text ?? "", perform: commit)
+                    
                 }
             }
             
-            if let hint = state.settings.hint {
+            if let hint = state.settings.hint, !isValid(state.dynamic.value) {
                 
                 HStack(alignment: .center, spacing: 16) {
                     
@@ -61,7 +87,29 @@ public struct InputView<Icon, IconView: View>: View {
                         .font(config.hintConfig.textFont)
                         .foregroundColor(config.hintConfig.textColor)
                 }
+                .transition(.asymmetric(insertion: .scale, removal: .opacity))
             }
+        }
+        .transition(.opacity)
+        .animation(.bouncy)
+        .onTapGesture {
+            
+            regularFieldViewModel.send(.startEditing)
+        }
+    }
+}
+
+private extension KeyboardType {
+    
+    init(keyboardType: InputConfig.KeyboardType) {
+        
+        switch keyboardType {
+        case .decimal:
+            self = .decimal
+        case .default:
+            self = .default
+        case .number:
+            self = .number
         }
     }
 }
@@ -80,7 +128,10 @@ public extension InputConfig {
         placeholder: "Введите лицевой счет",
         hintConfig: .init(textFont: .body, textColor: .red),
         backgroundColor: .gray.opacity(0.1),
-        imageSize: 16
+        imageSize: 16,
+        keyboardType: .decimal,
+        limit: 4,
+        regExp: ""
     )
 }
 
@@ -93,7 +144,9 @@ struct InputView_Previews: PreviewProvider {
                 state: .preview,
                 event: { _ in },
                 config: .preview,
-                iconView: { Text("Icon view") }
+                iconView: { Text("Icon view") },
+                commit: { _ in },
+                isValid: { _ in true}
             )
         }
         .padding(20)
@@ -104,7 +157,8 @@ extension InputState where Icon == String {
     
     static var preview: Self = .init(
         dynamic: .preview,
-        settings: .preview
+        settings: .preview,
+        validate: { true }
     )
 }
 
@@ -112,6 +166,7 @@ private extension InputState.Dynamic where Icon == String {
     
     static var preview: Self = .init(
         value: "some value",
+        state: .collapse,
         warning: nil
     )
 }
@@ -123,6 +178,8 @@ private extension InputState.Settings where Icon == String {
         icon: "inputIcon",
         keyboard: .default,
         title: "input title",
-        subtitle: "input subtitle"
+        subtitle: "input subtitle",
+        regExp: "",
+        limit: 1
     )
 }
