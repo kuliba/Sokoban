1fe97924fb657e179200a70746fda64ae10be062
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-07-12 16:52:59 +0400

    add `InputWrapperViewValidatorTests`

--- ForaBank/ForaBankTests/CompositionRoot/AnywayPayment/InputWrapperValidatorTests.swift
+++ ForaBank/ForaBankTests/CompositionRoot/AnywayPayment/InputWrapperValidatorTests.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import XCTest
+@testable import ForaBank
+import SwiftUI
+
+final class InputWrapperViewValidatorTests: XCTestCase {
+
+    func test_isValid_shouldNotDeliverErrorForMatchingRegex() {
+
+        assert(value: "abc123", regExp: "^[a-zA-Z0-9]+$", isMatching: true)
+    }
+
+    func test_isValid_shouldDeliverErrorForNonMatchingRegex() {
+
+        assert(value: "abc-123", regExp: "^[a-zA-Z0-9]+$", isMatching: false)
+    }
+
+    func test_isValid_shouldDeliverErrorForNilRegex() {
+
+        assert(value: "abc-123", regExp: nil, isMatching: true)
+    }
+
+    private func assert(
+        value: String,
+        regExp pattern: String?,
+        isMatching: Bool,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        XCTAssertNoDiff(
+            InputWrapperView<EmptyView>.isValidate(value, regExp: pattern),
+            isMatching,
+            file: file, line: line
+        )
+    }
+}
