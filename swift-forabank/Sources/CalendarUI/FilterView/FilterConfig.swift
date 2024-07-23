842015c2ab8e9a8c2089cc20390eb457d6940040
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-07-23 09:42:54 +0400

    update `FilterConfig`

--- swift-forabank/Sources/CalendarUI/FilterView/FilterConfig.swift
+++ swift-forabank/Sources/CalendarUI/FilterView/FilterConfig.swift
@@ -8,16 +8,49 @@
 import Foundation
 import SwiftUI
 
-struct FilterConfig {
+public struct FilterConfig {
     
+    let periodTitle: FilterView.TitleView.Config
+    let transferTitle: FilterView.TitleView.Config
+    let categoriesTitle: FilterView.TitleView.Config
     let button: ButtonConfig
+    let buttonsContainerConfig: ButtonsContainer.Config
+    let errorConfig: ErrorView.Config
     
-    struct ButtonConfig {
+    public init(
+        periodTitle: FilterView.TitleView.Config,
+        transferTitle: FilterView.TitleView.Config,
+        categoriesTitle: FilterView.TitleView.Config,
+        button: ButtonConfig,
+        buttonsContainerConfig: ButtonsContainer.Config,
+        errorConfig: ErrorView.Config
+    ) {
+        self.periodTitle = periodTitle
+        self.transferTitle = transferTitle
+        self.categoriesTitle = categoriesTitle
+        self.button = button
+        self.buttonsContainerConfig = buttonsContainerConfig
+        self.errorConfig = errorConfig
+    }
+    
+    public struct ButtonConfig {
         
         let selectBackgroundColor: Color
         let notSelectedBackgroundColor: Color
         
         let selectForegroundColor: Color
         let notSelectForegroundColor: Color
+        
+        public init(
+            selectBackgroundColor: Color,
+            notSelectedBackgroundColor: Color,
+            selectForegroundColor: Color,
+            notSelectForegroundColor: Color
+        ) {
+            self.selectBackgroundColor = selectBackgroundColor
+            self.notSelectedBackgroundColor = notSelectedBackgroundColor
+            self.selectForegroundColor = selectForegroundColor
+            self.notSelectForegroundColor = notSelectForegroundColor
+        }
     }
 }
