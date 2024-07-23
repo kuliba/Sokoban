0267a9747cd664b559e1f90566f39319046925a7
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-07-23 15:53:19 +0300

    add `Model.segmentedPaymentProviders(matching:mapping:)`

--- ForaBank/ForaBankTests/Model/Model+QRHelpersTests.swift
+++ ForaBank/ForaBankTests/Model/Model+QRHelpersTests.swift
@@ -7,7 +7,94 @@
 
 @testable import ForaBank
 import XCTest
+
 final class Model_QRHelpersTests: XCTestCase {
+    
+    func test_loadCached_shouldDeliverEmptyOnNonMatchingINN() {
+        
+        let (qrCode, qrMapping) = makeQR(inn: anyMessage())
+        let sut = makeSUT(operators: [makeCachingOperator(inn: anyMessage())])
+        
+        let loaded = sut.loadCached(matching: qrCode, mapping: qrMapping)
+        
+        XCTAssertEqual(loaded, [])
+    }
+    
+    func test_loadCached_shouldDeliverMatchingByINNOperator() {
+        
+        let inn = anyMessage()
+        let (qrCode, qrMapping) = makeQR(inn: inn)
+        let sut = makeSUT(operators: [makeCachingOperator(inn: inn)])
+        
+        let loaded = sut.loadCached(matching: qrCode, mapping: qrMapping)
+        
+        XCTAssertEqual(loaded?.count, 1)
+        XCTAssertEqual(loaded?.first?.inn, inn)
+    }
+    
+    func test_loadCached_shouldDeliverMatchingOperators() {
+        
+        let inn = anyMessage()
+        let (qrCode, qrMapping) = makeQR(inn: inn)
+        let sut = makeSUT(operators: [
+            makeCachingOperator(inn: inn),
+            makeCachingOperator(inn: inn),
+        ])
+        
+        let loaded = sut.loadCached(matching: qrCode, mapping: qrMapping)
+        
+        XCTAssertEqual(loaded?.count, 2)
+        XCTAssertEqual(loaded?.first?.inn, inn)
+        XCTAssertEqual(loaded?.last?.inn, inn)
+    }
+    
+    func test_segmentedPaymentProviders_shouldDeliverMatchingOperators() {
+        
+        let inn = anyMessage()
+        let (qrCode, qrMapping) = makeQR(inn: inn)
+        let sut = makeSUT(operators: [
+            makeCachingOperator(inn: inn),
+            makeCachingOperator(inn: inn),
+        ])
+        
+        let loaded = sut.segmentedPaymentProviders(matching: qrCode, mapping: qrMapping)
+        
+        XCTAssertEqual(loaded?.count, 2)
+        XCTAssertEqual(loaded?.first?.inn, inn)
+        XCTAssertEqual(loaded?.last?.inn, inn)
+    }
+    
+    func test_segmentedPaymentProviders_shouldDeliverMatchingOperators_() {
+        
+        let inn = anyMessage()
+        let qrCode = makeQRCode(inn: inn)
+        let (_, qrMapping) = makeQR(inn: inn)
+        let sut = makeSUT(operators: [
+            makeCachingOperator(inn: inn),
+            makeCachingOperator(inn: inn),
+        ])
+        
+        let loaded = sut.segmentedPaymentProviders(matching: qrCode, mapping: qrMapping)
+        
+        XCTAssertEqual(loaded?.count, 2)
+        XCTAssertEqual(loaded?.first?.inn, inn)
+        XCTAssertEqual(loaded?.last?.inn, inn)
+    }
+    
+    private func makeQR(
+        inn: String
+    ) -> (QRCode, QRMapping) {
+        
+        let qrCode = QRCode(original: "", rawData: ["payeeinn": inn])
+        let qrMapping = QRMapping(
+            parameters: [
+                .init(parameter: .general(.inn), keys: ["payeeinn"], type: .string)
+            ],
+            operators: []
+        )
+        
+        return (qrCode, qrMapping)
+    }
     
     // MARK: - helpers tests
     
@@ -57,7 +144,7 @@ final class Model_QRHelpersTests: XCTestCase {
         inn: String = anyMessage(),
         md5Hash: String? = anyMessage(),
         title: String = anyMessage(),
-        sortedOrder: Int
+        sortedOrder: Int = 0
     ) -> CachingOperator {
         
         return .init(id: id, inn: inn, md5Hash: md5Hash, title: title, sortedOrder: sortedOrder)
@@ -74,6 +161,13 @@ final class Model_QRHelpersTests: XCTestCase {
         
         precondition(operators.count == count)
         return operators
+    }
+    
+    private func makeQRCode(
+        inn: String = "5037008735"
+    ) -> QRCode {
+        
+        return .init(string: "ST00011|Name=Счет по сбору платежей за ЖКУ района Зеленоград|PersonalAcc=40911810100180000361|BankName=Филиал \"Центральный\" Банка ВТБ (ПАО)|BIC=044525411|CorrespAcc=30101810145250000411|PayeeINN=\(inn)|ServiceName=44822770069|PersAcc=123456|PaymPeriod=112021|Amount=738961|Sum=738961|TechCode=02")!
     }
     
     // MARK: - DSL
