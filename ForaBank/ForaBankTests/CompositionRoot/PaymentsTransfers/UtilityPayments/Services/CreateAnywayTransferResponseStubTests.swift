36ab87c02a2efa0691bb56d605d7f15c68c4068f
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-07-30 19:06:06 +0300

    tweak `CreateAnywayTransferResponseStubTests`

--- ForaBank/ForaBankTests/CompositionRoot/PaymentsTransfers/UtilityPayments/Services/CreateAnywayTransferResponseStubTests.swift
+++ ForaBank/ForaBankTests/CompositionRoot/PaymentsTransfers/UtilityPayments/Services/CreateAnywayTransferResponseStubTests.swift
@@ -13,13 +13,13 @@ import XCTest
 
 final class CreateAnywayTransferResponseStubTests: XCTestCase {
     
-    func test_shouldMapToAnywayPaymentUpdate() {
+    func test_step4_shouldMapToAnywayPaymentUpdate() {
         
-        let update = AnywayPaymentUpdate(stub)
+        let update = AnywayPaymentUpdate(step4)
         XCTAssertNotNil(update)
     }
 
     // MARK: - Helpers
     
-    private let stub = RemoteServices.ResponseMapper.CreateAnywayTransferResponse.step4
+    private let step4 = RemoteServices.ResponseMapper.CreateAnywayTransferResponse.step4
 }
