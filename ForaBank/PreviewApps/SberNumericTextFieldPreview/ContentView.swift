6861c7c7b797f43d99c8cfa162d980f26667eb46
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-08-07 19:20:04 +0300

    add `SberNumericTextFieldPreview` app

---
+++ ForaBank/PreviewApps/SberNumericTextFieldPreview/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SberNumericTextFieldPreview
+//
+//  Created by Igor Malyarov on 07.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+
+        VStack {
+
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
