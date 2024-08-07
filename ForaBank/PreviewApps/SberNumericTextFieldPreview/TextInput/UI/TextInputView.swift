7322e02db3292e89cbaed3382fb7bba21d71efda
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-08-07 21:31:31 +0300

    extract `TextInputViewDemo`

--- ForaBank/PreviewApps/SberNumericTextFieldPreview/TextInput/UI/TextInputView.swift
+++ ForaBank/PreviewApps/SberNumericTextFieldPreview/TextInput/UI/TextInputView.swift
@@ -5,10 +5,10 @@
 //  Created by Igor Malyarov on 07.08.2024.
 //
 
-import TextFieldDomain
-import TextFieldComponent
 import SharedConfigs
 import SwiftUI
+import TextFieldDomain
+import TextFieldComponent
 
 struct TextInputView<IconView: View>: View {
     
@@ -68,11 +68,9 @@ private extension TextInputView {
         
         ToolbarFactory.makeToolbarViewModel(
             closeButtonLabel: .image(config.toolbar.closeImage),
-            closeButtonAction: {
-                UIApplication.shared.endEditing()
-            },
+            closeButtonAction: UIApplication.shared.endEditing,
             doneButtonLabel: .title(config.toolbar.doneTitle),
-            doneButtonAction: { UIApplication.shared.endEditing() }
+            doneButtonAction: UIApplication.shared.endEditing
         )
     }
     
@@ -106,97 +104,7 @@ struct TextInputView_Previews: PreviewProvider {
     
     static var previews: some View {
         
-        ScrollView(showsIndicators: false) {
-            
-            VStack {
-                
-                textInputView(.placeholderPreview, nil)
-                textInputView(.placeholderPreview, .hintPreview)
-                textInputView(.placeholderPreview, .warningPreview)
-                
-                textInputView(.editingPreview, nil)
-                textInputView(.editingPreview, .hintPreview)
-                textInputView(.editingPreview, .warningPreview)
-                
-                textInputView(.noFocusPreview, nil)
-                textInputView(.noFocusPreview, .hintPreview)
-                textInputView(.noFocusPreview, .warningPreview)
-            }
-        }
+        TextInputViewDemo()
     }
-    
-    private static func textInputView(
-        keyboard: KeyboardType = .decimal,
-        title: String = "Text Field Title",
-        _ textField: TextFieldState,
-        _ message: TextInputState.Message? = nil
-    ) -> some View {
-        
-        TextInputView(
-            state: .init(
-                keyboard: keyboard,
-                title: title,
-                textField: textField,
-                message: message
-            ),
-            event: { print($0) },
-            config: .preview
-        ) {
-            Image(systemName: "photo")
-        }
-    }
-}
-
-extension TextFieldState {
-    
-    static let placeholderPreview: Self = .placeholder("placeholder")
-    static let editingPreview: Self = .editing(.init("tex"))
-    static let noFocusPreview: Self = .noFocus("entered text.")
-}
-
-extension TextInputState.Message {
-    
-    static let hintPreview: Self = .hint("Long hint describing how to fill up this field without errors.")
-    static let warningPreview: Self = .warning("This text describes text input validation error.")
-}
-
-extension TextInputConfig {
-    
-    static let preview: Self = .init(
-        hint: .init(
-            textFont: .footnote,
-            textColor: .orange
-        ),
-        imageWidth: 24,
-        textField: .preview,
-        title: .init(
-            textFont: .subheadline,
-            textColor: .green
-        ),
-        toolbar: .preview,
-        warning: .init(
-            textFont: .footnote,
-            textColor: .red
-        )
-    )
-}
-
-extension TextFieldView.TextFieldConfig {
-    
-    static let preview: Self = .init(
-        font: .systemFont(ofSize: 18),
-        textColor: .black,
-        tintColor: .red,
-        backgroundColor: .clear,
-        placeholderColor: .pink
-    )
-}
-
-extension ToolbarConfig {
-    
-    static let preview: Self = .init(
-        closeImage: "closeImage",
-        doneTitle: "Done!"
-    )
 }
 #endif
