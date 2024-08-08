757edeb4da23614befab49cf13872e85de1c4a8e
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-08-08 07:52:32 +0300

    move `keyboard` and `title` from `TextInputState`to  `TextInputConfig`

--- ForaBank/ForaBank/CompositionRoot/Configs/TextInputConfig+iFora.swift
+++ ForaBank/ForaBank/CompositionRoot/Configs/TextInputConfig+iFora.swift
@@ -6,27 +6,37 @@
 //
 
 import PaymentComponents
+import TextFieldUI
 
 extension TextInputConfig {
     
-    static let iFora: Self = .init(
-        hint: .init(
-            textFont: .textBodySR12160(),
-            textColor: .textPlaceholder
-        ),
-        imageWidth: 24,
-        textField: .black16,
-        title: .init(
-            textFont: .textBodyMR14180(),
-            textColor: .textPlaceholder
-        ),
-        toolbar: .init(
-            closeImage: "Close Button",
-            doneTitle: "Готово"
-        ),
-        warning: .init(
-            textFont: .textBodySR12160(),
-            textColor: .systemColorError
+    static func iFora(
+        keyboard: KeyboardType, 
+        title: String
+    ) -> Self {
+        
+        return .init(
+            hint: .init(
+                textFont: .textBodySR12160(),
+                textColor: .textPlaceholder
+            ),
+            imageWidth: 24,
+            keyboard: keyboard,
+            placeholder: "Введите значение",
+            textField: .black16,
+            title: title,
+            titleConfig: .init(
+                textFont: .textBodyMR14180(),
+                textColor: .textPlaceholder
+            ),
+            toolbar: .init(
+                closeImage: "Close Button",
+                doneTitle: "Готово"
+            ),
+            warning: .init(
+                textFont: .textBodySR12160(),
+                textColor: .systemColorError
+            )
         )
-    )
+    }
 }
--- swift-forabank/Sources/UI/Components/InputComponent/TextInput/UI/TextInputConfig.swift
+++ swift-forabank/Sources/UI/Components/InputComponent/TextInput/UI/TextInputConfig.swift
@@ -13,23 +13,32 @@ public struct TextInputConfig: Equatable {
     
     public let hint: TextConfig
     public let imageWidth: CGFloat
+    public let keyboard: KeyboardType
+    public let placeholder: String
     public let textField: TextFieldConfig
-    public let title: TextConfig
+    public let title: String
+    public let titleConfig: TextConfig
     public let toolbar: ToolbarConfig
     public let warning: TextConfig
     
     public init(
         hint: TextConfig,
         imageWidth: CGFloat,
+        keyboard: KeyboardType,
+        placeholder: String,
         textField: TextFieldConfig,
-        title: TextConfig,
+        title: String,
+        titleConfig: TextConfig,
         toolbar: ToolbarConfig,
         warning: TextConfig
     ) {
         self.hint = hint
         self.imageWidth = imageWidth
+        self.keyboard = keyboard
+        self.placeholder = placeholder
         self.textField = textField
         self.title = title
+        self.titleConfig = titleConfig
         self.toolbar = toolbar
         self.warning = warning
     }
