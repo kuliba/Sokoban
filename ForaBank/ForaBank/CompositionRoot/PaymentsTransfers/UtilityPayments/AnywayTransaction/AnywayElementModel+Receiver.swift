867c314b860be1c1f073d5a90963f4aa64139843
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-08-12 12:50:06 +0300

    (possible fix for IOS-134): fix `AnywayElementModel: Receiver` conformance: send events to inputs and select // no need to send validation from AnywayTransactionViewModel to model - UI component is constructed with validation from parameter

--- ForaBank/ForaBank/CompositionRoot/PaymentsTransfers/UtilityPayments/AnywayTransaction/AnywayElementModel+Receiver.swift
+++ ForaBank/ForaBank/CompositionRoot/PaymentsTransfers/UtilityPayments/AnywayTransaction/AnywayElementModel+Receiver.swift
@@ -16,8 +16,34 @@ extension AnywayElementModel: Receiver {
         case let (.widget(.otp(otp)), .otpWarning(warning)):
             otp.event(.otpField(.failure(.serverError(warning))))
             
+        case let (.parameter(parameter), .updateValueTo(value)):
+            switch parameter.type {
+            case let .numberInput(numberInput):
+                numberInput.event(.textField(.setTextTo(value)))
+                
+            case let .select(select):
+                self.select(value, in: select)
+                
+            case let .textInput(textInput):
+                textInput.event(.textField(.setTextTo(value)))
+                
+            default:
+                break
+            }
+            
         default:
-#warning("FIX ME")
+            break
         }
+    }
+    
+    private func select(
+        _ key: String,
+        in select: ObservingSelectorViewModel
+    ) {
+        let options = select.state.selector.options
+        guard let toSelect = options.first(where: { $0.key == key })
+        else { return }
+        
+        select.event(.selectOption(.init(key: key, value: toSelect.value)))
     }
 }
