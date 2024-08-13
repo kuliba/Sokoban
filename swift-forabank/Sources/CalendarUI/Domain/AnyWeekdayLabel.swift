9d277b966281cad3d5dbacd65676d70541ebbc71
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-08-13 05:18:08 +0400

    fix formatting

--- swift-forabank/Sources/CalendarUI/Domain/AnyWeekdayLabel.swift
+++ swift-forabank/Sources/CalendarUI/Domain/AnyWeekdayLabel.swift
@@ -11,6 +11,9 @@ public struct AnyWeekdayLabel: View {
     
     private let internalView: AnyView
     
-    init<V: WeekdayLabel>(_ view: V) { internalView = view.erased() }
+    init<V: WeekdayLabel>(_ view: V) {
+        internalView = view.erased()
+    }
+    
     public var body: some View { internalView }
 }
