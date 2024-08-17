898179264bfb3f783a10fdb165ec157a2b3ff154
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-08-17 12:03:45 +0300

    add `Array.assignIDs(_:_:)`; `Identified`

---
+++ swift-forabank/Sources/Payments/PayHub/Content/Supporting/Identified.swift
@@ -0,0 +1,23 @@
+//
+//  Identified.swift
+//  
+//
+//  Created by Igor Malyarov on 17.08.2024.
+//
+
+/// A structure that associates an element with a unique identifier.
+public struct Identified<ID, Element> where ID: Hashable {
+    
+    public let id: ID
+    public let element: Element
+    
+    public init(
+        id: ID,
+        element: Element
+    ) {
+        self.id = id
+        self.element = element
+    }
+}
+
+extension Identified: Equatable where Element: Equatable {}
