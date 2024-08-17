3cab06e185afb5aa86d2299600ef6f72246fbd33
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-08-17 19:23:23 +0300

    move `uiItems` to `PayHubState`

--- ForaBank/PreviewApps/PayHubPreview/PayHub/Content/PayHubContentView.swift
+++ ForaBank/PreviewApps/PayHubPreview/PayHub/Content/PayHubContentView.swift
@@ -98,34 +98,6 @@ private extension PayHubState {
         guard case .placeholders = loadState else { return false }
         return true
     }
-    
-    typealias PayHubUIItem = Identified<UUID, UIItem<Element>>
-    
-    var uiItems: [PayHubUIItem] {
-        
-        let templates = PayHubUIItem(.selectable(.templates))
-        let exchange = PayHubUIItem(.selectable(.exchange))
-        
-        var uiItems = [templates, exchange]
-        
-        switch loadState {
-        case let .loaded(loaded):
-            uiItems.append(contentsOf: loaded.map { .init(id: $0.id, element: .selectable(.latest($0.element))) })
-            
-        case let .placeholders(ids):
-            uiItems.append(contentsOf: ids.map { .init(id: $0, element: .placeholder($0)) })
-        }
-        
-        return uiItems
-    }
-}
-
-private extension Identified where ID == UUID {
-    
-    init(_ element: Element) {
-        
-        self.init(id: .init(), element: element)
-    }
 }
 
 private extension View {
