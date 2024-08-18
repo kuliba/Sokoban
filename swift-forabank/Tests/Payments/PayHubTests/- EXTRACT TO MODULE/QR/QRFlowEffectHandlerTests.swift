6fac25f8a28ed7f7052bbfdd055e56552ae060cb
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-08-18 10:12:20 +0300

    TDD `QRFlowEffectHandlerTests`

--- swift-forabank/Tests/Payments/PayHubTests/- EXTRACT TO MODULE/QR/QRFlowEffectHandlerTests.swift
+++ swift-forabank/Tests/Payments/PayHubTests/- EXTRACT TO MODULE/QR/QRFlowEffectHandlerTests.swift
@@ -1,20 +1,32 @@
 //
 //  QRFlowEffectHandlerTests.swift
-//  
+//
 //
 //  Created by Igor Malyarov on 18.08.2024.
 //
 
 enum QRFlowEvent: Equatable {}
 
-enum QRFlowEffect: Equatable {}
+enum QRFlowEffect<ScanResult> {
+    
+    case processScanResult(ScanResult)
+}
 
-struct QRFlowEffectHandlerMicroServices {}
+extension QRFlowEffect: Equatable where ScanResult: Equatable {}
 
-extension QRFlowEffectHandlerMicroServices {}
+struct QRFlowEffectHandlerMicroServices<ScanResult> {
+    
+    let processScanResult: ProcessScanResult
+}
 
-final class QRFlowEffectHandler {
+extension QRFlowEffectHandlerMicroServices {
+    
+    typealias ProcessScanResultCompletion = (()) -> Void
+    typealias ProcessScanResult = (ScanResult, @escaping ProcessScanResultCompletion) -> Void
+}
 
+final class QRFlowEffectHandler<ScanResult> {
+    
     private let microServices: MicroServices
     
     init(
@@ -23,7 +35,7 @@ final class QRFlowEffectHandler {
         self.microServices = microServices
     }
     
-    typealias MicroServices = QRFlowEffectHandlerMicroServices
+    typealias MicroServices = QRFlowEffectHandlerMicroServices<ScanResult>
 }
 
 extension QRFlowEffectHandler {
@@ -33,7 +45,8 @@ extension QRFlowEffectHandler {
         _ dispatch: @escaping Dispatch
     ) {
         switch effect {
-            
+        case let .processScanResult(scanResult):
+            microServices.processScanResult(scanResult) { _ in }
         }
     }
 }
@@ -43,7 +56,7 @@ extension QRFlowEffectHandler {
     typealias Dispatch = (Event) -> Void
     
     typealias Event = QRFlowEvent
-    typealias Effect = QRFlowEffect
+    typealias Effect = QRFlowEffect<ScanResult>
 }
 
 
@@ -51,21 +64,60 @@ import XCTest
 
 final class QRFlowEffectHandlerTests: XCTestCase {
     
+    // MARK: - init
+    
+    func test_shouldNotCallCollaborators() {
+        
+        let (_, processScanResult) = makeSUT()
+        
+        XCTAssertEqual(processScanResult.callCount, 0)
+    }
+    
+    // MARK: - processScanResult
+    
+    func test_processScanResult_shouldCallProcessScanResultWithPayload() {
+        
+        let scanResult = makeScanResult()
+        let (sut, processScanResult) = makeSUT()
+        
+        sut.handleEffect(.processScanResult(scanResult)) { _ in }
+        
+        XCTAssertNoDiff(processScanResult.payloads, [scanResult])
+    }
+    
     // MARK: - Helpers
     
-    private typealias SUT = QRFlowEffectHandler
+    private typealias SUT = QRFlowEffectHandler<ScanResult>
+    private typealias ProcessScanResultSpy = Spy<ScanResult, Void>
     
     private func makeSUT(
         file: StaticString = #file,
         line: UInt = #line
-    ) -> SUT {
-        
+    ) -> (
+        sut: SUT,
+        processScanResult: ProcessScanResultSpy
+    ) {
+        let processScanResult = ProcessScanResultSpy()
         let sut = SUT(microServices: .init(
+            processScanResult: processScanResult.process
         ))
         
         trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(processScanResult, file: file, line: line)
         
-        return sut
+        return (sut, processScanResult)
+    }
+    
+    private struct ScanResult: Equatable {
+        
+        let value: String
+    }
+    
+    private func makeScanResult(
+        _ value: String = anyMessage()
+    ) -> ScanResult {
+        
+        return .init(value: value)
     }
     
     private func expect(
@@ -76,7 +128,7 @@ final class QRFlowEffectHandlerTests: XCTestCase {
         file: StaticString = #file,
         line: UInt = #line
     ) {
-        let sut = sut ?? makeSUT()
+        let sut = sut ?? makeSUT().sut
         let exp = expectation(description: "wait for completion")
         exp.expectedFulfillmentCount = expectedEvents.count
         var receivedEvents = [SUT.Event]()
