def54d6f0f2525f2340043ec9aabd46bfc47c9f8
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-08-23 10:00:34 +0300

    extract `PaymentsTransfersToolbarBinderComposer` to prod

--- swift-forabank/Tests/Payments/PayHubUITests/PaymentsTransfersToolbar/Binder/PaymentsTransfersToolbarBinderComposerTests.swift
+++ swift-forabank/Tests/Payments/PayHubUITests/PaymentsTransfersToolbar/Binder/PaymentsTransfersToolbarBinderComposerTests.swift
@@ -6,107 +6,8 @@
 //
 
 import CombineSchedulers
-import Foundation
-
-final class PaymentsTransfersToolbarBinderComposer<Profile, QR> {
-    
-    private let microServices: MicroServices
-    private let scheduler: AnySchedulerOf<DispatchQueue>
-    
-    init(
-        microServices: MicroServices,
-        scheduler: AnySchedulerOf<DispatchQueue>
-    ) {
-        self.microServices = microServices
-        self.scheduler = scheduler
-    }
-    
-    typealias MicroServices = PaymentsTransfersToolbarFlowEffectHandlerMicroServices<Profile, QR>
-}
-
-extension PaymentsTransfersToolbarBinderComposer {
-    
-    func compose(
-        selection: PaymentsTransfersToolbarState.Selection? = nil
-    ) -> Binder<Content, Flow> {
-        
-        return .init(
-            content: makeContent(selection: selection),
-            flow: makeFlow()
-        ) { content, flow in
-            
-            let flowNavigation = flow.$state.map(\.navigation)
-            let dismiss = flowNavigation
-                .combineLatest(flowNavigation.dropFirst())
-                .filter { $0.0 != nil && $0.1 == nil }
-                .debounce(for: .milliseconds(100), scheduler: scheduler)
-                .sink { _ in content.event(.select(nil)) }
-            
-            let select = content.$state
-                .compactMap(\.selection)
-                .sink {
-                    
-                    switch $0 {
-                    case .profile:
-                        flow.event(.select(.profile))
-                    case .qr:
-                        flow.event(.select(.qr))
-                    }
-                }
-            
-            return [dismiss, select]
-        }
-    }
-    
-    typealias Content = RxViewModel<PaymentsTransfersToolbarState, PaymentsTransfersToolbarEvent, PaymentsTransfersToolbarEffect>
-    typealias Flow = RxViewModel<PaymentsTransfersToolbarFlowState<Profile, QR>, PaymentsTransfersToolbarFlowEvent<Profile, QR>, PaymentsTransfersToolbarFlowEffect>
-}
-
-// MARK: - Content
-
-private extension PaymentsTransfersToolbarBinderComposer {
-    
-    func makeContent(
-        selection: PaymentsTransfersToolbarState.Selection?
-    ) -> Content {
-        
-        let reducer = PaymentsTransfersToolbarReducer()
-        let effectHandler = PaymentsTransfersToolbarEffectHandler(
-            microServices: .init()
-        )
-        
-        return .init(
-            initialState: .init(selection: selection),
-            reduce: reducer.reduce(_:_:),
-            handleEffect: effectHandler.handleEffect(_:_:),
-            scheduler: scheduler
-        )
-    }
-}
-
-// MARK: - Flow
-
-private extension PaymentsTransfersToolbarBinderComposer {
-    
-    func makeFlow() -> Flow {
-        
-        let reducer = PaymentsTransfersToolbarFlowReducer<Profile, QR>()
-        let effectHandler = PaymentsTransfersToolbarFlowEffectHandler(
-            microServices: microServices
-        )
-        
-        return .init(
-            initialState: .init(),
-            reduce: reducer.reduce(_:_:),
-            handleEffect: effectHandler.handleEffect(_:_:),
-            scheduler: scheduler
-        )
-    }
-}
-
-import Combine
 import PayHub
-import RxViewModel
+import PayHubUI
 import XCTest
 
 final class PaymentsTransfersToolbarBinderComposerTests: XCTestCase {
@@ -134,13 +35,13 @@ final class PaymentsTransfersToolbarBinderComposerTests: XCTestCase {
         let flowSpy = ValueSpy(sut.flow.$state.map(\.navigation))
         XCTAssertNoDiff(contentSpy.values, [nil])
         XCTAssertNoDiff(flowSpy.values, [nil])
-
+        
         sut.content.event(.select(.profile))
         scheduler.advance()
         
         XCTAssertNoDiff(contentSpy.values, [nil, .profile])
         XCTAssertNoDiff(flowSpy.values, [nil, nil])
-
+        
         makeProfile.complete(with: profile)
         scheduler.advance()
         
@@ -156,13 +57,13 @@ final class PaymentsTransfersToolbarBinderComposerTests: XCTestCase {
         let flowSpy = ValueSpy(sut.flow.$state.map(\.navigation))
         XCTAssertNoDiff(contentSpy.values, [nil])
         XCTAssertNoDiff(flowSpy.values, [nil])
-
+        
         sut.content.event(.select(.qr))
         scheduler.advance()
         
         XCTAssertNoDiff(contentSpy.values, [nil, .qr])
         XCTAssertNoDiff(flowSpy.values, [nil, nil])
-
+        
         makeQR.complete(with: qr)
         scheduler.advance()
         
@@ -182,13 +83,13 @@ final class PaymentsTransfersToolbarBinderComposerTests: XCTestCase {
         
         makeQR.complete(with: qr)
         scheduler.advance()
-
+        
         XCTAssertNoDiff(contentSpy.values, [nil, .qr])
         XCTAssertNoDiff(flowSpy.values, [nil, nil, .qr(qr)])
         
         sut.flow.event(.dismiss)
         scheduler.advance(by: .milliseconds(100))
-
+        
         XCTAssertNoDiff(contentSpy.values, [nil, .qr, nil])
         XCTAssertNoDiff(flowSpy.values, [nil, nil, .qr(qr), nil])
     }
