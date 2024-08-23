cd5c83d97889cd89488a3dc4a67c2e1ae5e215d2
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-08-23 16:16:36 +0300

    extract to prod

---
+++ swift-forabank/Sources/Payments/PayHub/CategoryPickerSection/Flow/Logic/CategoryPickerSectionFlowEffectHandler.swift
@@ -0,0 +1,43 @@
+//
+//  CategoryPickerSectionFlowEffectHandler.swift
+//
+//
+//  Created by Igor Malyarov on 23.08.2024.
+//
+
+public final class CategoryPickerSectionFlowEffectHandler<Category, CategoryModel, CategoryList> {
+    
+    private let microServices: MicroServices
+    
+    public init(
+        microServices: MicroServices
+    ) {
+        self.microServices = microServices
+    }
+    
+    public typealias MicroServices = CategoryPickerSectionFlowEffectHandlerMicroServices<Category, CategoryModel, CategoryList>
+}
+
+public extension CategoryPickerSectionFlowEffectHandler {
+    
+    func handleEffect(
+        _ effect: Effect,
+        _ dispatch: @escaping Dispatch
+    ) {
+        switch effect {
+        case .showAll:
+            microServices.showAll { dispatch(.receive(.list($0))) }
+            
+        case let .showCategory(category):
+            microServices.showCategory(category) { dispatch(.receive(.category($0))) }
+        }
+    }
+}
+
+public extension CategoryPickerSectionFlowEffectHandler {
+    
+    typealias Dispatch = (Event) -> Void
+    
+    typealias Event = CategoryPickerSectionFlowEvent<Category, CategoryModel, CategoryList>
+    typealias Effect = CategoryPickerSectionFlowEffect<Category>
+}
