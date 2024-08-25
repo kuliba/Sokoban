bb98e44de97aabcb1d62e8bbd3aa673a07dd85e8
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-08-25 23:25:25 +0300

    replace `CategoryPickerSectionContentWrapperView` with `RxWrapperView`

--- swift-forabank/Sources/Payments/PayHubUI/CategoryPickerSection/Content/CategoryPickerSectionContentWrapperView.swift
+++ swift-forabank/Sources/Payments/PayHubUI/CategoryPickerSection/Content/CategoryPickerSectionContentWrapperView.swift
@@ -5,33 +5,7 @@
 //  Created by Igor Malyarov on 21.08.2024.
 //
 
+import RxViewModel
 import SwiftUI
 
-public struct CategoryPickerSectionContentWrapperView<ContentView, ServiceCategory>: View
-where ContentView: View {
-    
-    @ObservedObject private var model: Model
-    
-    private let makeContentView: MakeContentView
-    
-    public init(
-        model: Model, 
-        makeContentView: @escaping MakeContentView
-    ) {
-        self.model = model
-        self.makeContentView = makeContentView
-    }
-    
-    public var body: some View {
-        
-        makeContentView(model.state, model.event(_:))
-    }
-}
-
-public extension CategoryPickerSectionContentWrapperView {
-    
-    typealias Model = CategoryPickerSectionContent<ServiceCategory>
-    typealias State = CategoryPickerSectionState<ServiceCategory>
-    typealias Event = CategoryPickerSectionEvent<ServiceCategory>
-    typealias MakeContentView = (State, @escaping (Event) -> Void) -> ContentView
-}
+public typealias CategoryPickerSectionContentWrapperView<ContentView, ServiceCategory> = RxWrapperView<ContentView, CategoryPickerSectionState<ServiceCategory>, CategoryPickerSectionEvent<ServiceCategory>, CategoryPickerSectionEffect> where ContentView: View
