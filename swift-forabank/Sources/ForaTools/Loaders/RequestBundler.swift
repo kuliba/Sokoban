d8f1b0a955167c8da501a019649e34e4d24ddd1c
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-08-25 12:28:29 +0300

    add scaffolding for `RequestCollectorBundlerTests`

--- swift-forabank/Sources/ForaTools/Loaders/RequestBundler.swift
+++ swift-forabank/Sources/ForaTools/Loaders/RequestBundler.swift
@@ -65,3 +65,42 @@ extension RequestBundler: Loader {
         }
     }
 }
+
+// TODO: add tests
+public extension RequestBundler {
+    
+    /// Loads multiple requests and calls the completion handlers when the responses are received.
+    /// Each request is batched and performed only once if multiple identical requests are found.
+    ///
+    /// - Parameters:
+    ///   - requests: An array of requests to be loaded.
+    ///   - completion: A completion handler that is called for each individual response.
+    func load(
+        _ requests: [Request],
+        _ completion: @escaping (Request, Response) -> Void
+    ) {
+        for request in requests {
+            
+            load(request) { completion(request, $0) }
+        }
+    }
+}
+
+// TODO: add tests
+public extension RequestBundler {
+    
+    /// A typealias for a closure that performs a request and provides the response via a completion handler.
+    ///
+    /// This closure takes a `Request` and a completion handler as its parameters.
+    /// The completion handler is expected to be called with the `Response` once the request is fulfilled.
+    typealias PerformRequest = (Request, @escaping (Response) -> Void) -> Void
+    
+    /// Convenience initialiser that accepts a closure to perform requests instead of a `Performer`.
+    ///
+    /// - Parameter performRequest: A closure that performs the request and returns the response.
+    convenience init(
+        performRequest: @escaping PerformRequest
+    ) {
+        self.init(performer: AnyLoader(performRequest))
+    }
+}
---
+++ swift-forabank/Tests/ForaToolsTests/Loaders/RequestCollectorBundlerTests.swift
@@ -0,0 +1,122 @@
+//
+//  RequestCollectorBundlerTests.swift
+//
+//
+//  Created by Igor Malyarov on 25.08.2024.
+//
+
+import CombineSchedulers
+import Foundation
+
+public final class RequestCollectorBundler<Request: Hashable, Response> {
+    
+    private let requestCollector: RequestCollector<Request, Response>
+    
+    /// Initialises a new instance of `RequestCollectorBundler`.
+    ///
+    /// - Parameters:
+    ///   - collectionPeriod: The period over which requests are collected before processing.
+    ///   - scheduler: The scheduler used to manage the timing of request processing.
+    ///   - performRequest: A closure that performs the request and returns the response.
+    public init(
+        collectionPeriod: DispatchTimeInterval,
+        scheduler: AnySchedulerOf<DispatchQueue>,
+        performRequest: @escaping RequestBundler<Request, Response>.PerformRequest
+    ) {
+        self.requestCollector = .init(
+            collectionPeriod: collectionPeriod,
+            performRequests: { requests, completion in
+                
+                let bundler = RequestBundler(performRequest: performRequest)
+                for request in requests {
+                    
+                    bundler.load(request) { completion([request: $0]) }
+                }
+            },
+            scheduler: scheduler
+        )
+    }
+}
+
+extension RequestCollectorBundler {
+    
+    /// Processes a new request by first collecting it, then bundling and executing it.
+    ///
+    /// - Parameters:
+    ///   - request: The request to be processed.
+    ///   - completion: The completion handler to be called with the response.
+    public func process(
+        _ request: Request,
+        _ completion: @escaping (Response) -> Void
+    ) {
+        requestCollector.process(request, completion)
+    }
+}
+
+import ForaTools
+import XCTest
+
+final class RequestCollectorBundlerTests: XCTestCase {
+    
+    // MARK: - init
+    
+    func test_init_shouldNotCallPerformRequest() {
+        
+        let (_,_, performRequest) = makeSUT()
+        
+        XCTAssertEqual(performRequest.callCount, 0)
+    }
+    
+    // MARK: - Helpers
+    
+    private typealias SUT = RequestCollectorBundler<Request, Response>
+    private typealias PerformRequestSpy = Spy<Request, Response>
+    
+    private func makeSUT(
+        collectionPeriod: DispatchTimeInterval = .milliseconds(500),
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (
+        sut: SUT,
+        scheduler: TestSchedulerOf<DispatchQueue>,
+        performRequest: PerformRequestSpy
+    ) {
+        let scheduler = DispatchQueue.test
+        let performRequest = PerformRequestSpy()
+        let sut = SUT(
+            collectionPeriod: collectionPeriod,
+            scheduler: scheduler.eraseToAnyScheduler(),
+            performRequest: performRequest.process(_:completion:)
+        )
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(scheduler, file: file, line: line)
+        trackForMemoryLeaks(performRequest, file: file, line: line)
+        
+        return (sut, scheduler, performRequest)
+    }
+    
+    private struct Request: Hashable {
+        
+        let value: String
+    }
+    
+    private func makeRequest(
+        _ value: String = anyMessage()
+    ) -> Request {
+        
+        return .init(value: value)
+    }
+    
+    private struct Response: Hashable {
+        
+        let value: String
+    }
+    
+    private func makeResponse(
+        _ value: String = anyMessage()
+    ) -> Response {
+        
+        return .init(value: value)
+    }
+}
