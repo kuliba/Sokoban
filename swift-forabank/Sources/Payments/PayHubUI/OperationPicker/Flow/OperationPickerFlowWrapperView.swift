1667c97c46421c592e78b560d9a3cc1a5647938e
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-08-26 13:48:45 +0300

    add `OperationPickerFlowWrapperView` (typealias)

---
+++ swift-forabank/Sources/Payments/PayHubUI/OperationPicker/Flow/OperationPickerFlowWrapperView.swift
@@ -0,0 +1,12 @@
+//
+//  OperationPickerFlowWrapperView.swift
+//
+//
+//  Created by Igor Malyarov on 26.08.2024.
+//
+
+import PayHub
+import RxViewModel
+import SwiftUI
+
+public typealias OperationPickerFlowWrapperView<ContentView, Exchange, Latest, LatestFlow, Status, Templates> = RxWrapperView<ContentView, OperationPickerFlowState<Exchange, Latest, Status, Templates>, OperationPickerFlowEvent<Exchange, Latest, LatestFlow, Status, Templates>, OperationPickerFlowEffect<Latest>> where ContentView: View
