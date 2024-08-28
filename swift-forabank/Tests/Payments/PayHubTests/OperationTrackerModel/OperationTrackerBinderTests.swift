eb4d5a784f99ecdb8afd6cd932373b85a6f66a2b
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-08-28 22:15:25 +0300

    add test

--- swift-forabank/Tests/Payments/PayHubTests/OperationTrackerModel/OperationTrackerBinderTests.swift
+++ swift-forabank/Tests/Payments/PayHubTests/OperationTrackerModel/OperationTrackerBinderTests.swift
@@ -5,6 +5,7 @@
 //  Created by Igor Malyarov on 28.08.2024.
 //
 
+import CombineSchedulers
 import PayHub
 import XCTest
 
@@ -22,10 +23,88 @@ final class OperationTrackerBinderTests: XCTestCase {
         XCTAssertNoDiff(responseSpy.payloads, [response])
     }
     
+    func test_tracker_binder_false() {
+        
+        typealias ProcessSpy = Spy<Bool, Value>
+        typealias ReceiveSpy = CallSpy<Value, Void>
+        
+        let startSpy = StartSpy()
+        let processSpy = ProcessSpy()
+        let receiveSpy = ReceiveSpy()
+        let scheduler: AnySchedulerOf<DispatchQueue> = .immediate
+        
+        let tracker = OperationTrackerModel(
+            start: startSpy.process(completion:),
+            scheduler: scheduler
+        )
+        let publisher = tracker.$state
+            .compactMap(\.isTerminated)
+            .eraseToAnyPublisher()
+        
+        let binder = StateValueBinder(
+            publisher: publisher,
+            process: processSpy.process(_:completion:),
+            receive: receiveSpy.call(payload:)
+        )
+        let binding = binder.bind()
+        
+        tracker.event(.start)
+        
+        let response = makeResponse()
+        let value = makeValue()
+        
+        startSpy.complete(with: nil)
+        processSpy.complete(with: value)
+        
+        XCTAssertNoDiff(processSpy.payloads, [false])
+        XCTAssertNoDiff(receiveSpy.payloads, [value])
+        
+        XCTAssertNotNil(binding)
+    }
+    
+    func test_tracker_binder_true() {
+        
+        typealias ProcessSpy = Spy<Bool, Value>
+        typealias ReceiveSpy = CallSpy<Value, Void>
+        
+        let startSpy = StartSpy()
+        let processSpy = ProcessSpy()
+        let receiveSpy = ReceiveSpy()
+        let scheduler: AnySchedulerOf<DispatchQueue> = .immediate
+        
+        let tracker = OperationTrackerModel(
+            start: startSpy.process(completion:),
+            scheduler: scheduler
+        )
+        let publisher = tracker.$state
+            .compactMap(\.isTerminated)
+            .eraseToAnyPublisher()
+        
+        let binder = StateValueBinder(
+            publisher: publisher,
+            process: processSpy.process(_:completion:),
+            receive: receiveSpy.call(payload:)
+        )
+        let binding = binder.bind()
+        
+        tracker.event(.start)
+        
+        let response = makeResponse()
+        let value = makeValue()
+        
+        startSpy.complete(with: response)
+        processSpy.complete(with: value)
+        
+        XCTAssertNoDiff(processSpy.payloads, [true])
+        XCTAssertNoDiff(receiveSpy.payloads, [value])
+        
+        XCTAssertNotNil(binding)
+    }
+    
     // MARK: - Helpers
     
     private typealias SUT = OperationTrackerBinder<ResponseSpy, Response>
-    private typealias StartSpy = Spy<Void, Response>
+    private typealias StartSpy = Spy<Void, Response?>
     private typealias ResponseSpy = CallSpy<Response?, Void>
     
     private func makeSUT(
@@ -60,6 +139,18 @@ final class OperationTrackerBinderTests: XCTestCase {
     private func makeResponse(
         _ value: String = anyMessage()
     ) -> Response {
+        
+        return .init(value: value)
+    }
+    
+    private struct Value: Equatable {
+        
+        let value: String
+    }
+    
+    private func makeValue(
+        _ value: String = anyMessage()
+    ) -> Value {
         
         return .init(value: value)
     }
