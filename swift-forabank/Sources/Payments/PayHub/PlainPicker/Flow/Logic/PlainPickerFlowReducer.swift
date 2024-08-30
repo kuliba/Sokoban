f8920fb2b284d754d4cd0ab60a45755bc49129f1
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-08-30 13:18:22 +0300

    add `PlainPickerFlow` domain and logic

---
+++ swift-forabank/Sources/Payments/PayHub/PlainPicker/Flow/Logic/PlainPickerFlowReducer.swift
@@ -0,0 +1,43 @@
+//
+//  PlainPickerFlowReducer.swift
+//
+//
+//  Created by Igor Malyarov on 30.08.2024.
+//
+
+public final class PlainPickerFlowReducer<Element, Navigation> {
+    
+    public init() {}
+}
+
+public extension PlainPickerFlowReducer {
+    
+    func reduce(
+        _ state: State,
+        _ event: Event
+    ) -> (State, Effect?) {
+        
+        var state = state
+        var effect: Effect?
+        
+        switch event {
+        case .dismiss:
+            state.navigation = nil
+            
+        case let .navigation(navigation):
+            state.navigation = navigation
+            
+        case let .select(element):
+            effect = .select(element)
+        }
+        
+        return (state, effect)
+    }
+}
+
+public extension PlainPickerFlowReducer {
+    
+    typealias State = PlainPickerFlowState<Navigation>
+    typealias Event = PlainPickerFlowEvent<Element, Navigation>
+    typealias Effect = PlainPickerFlowEffect<Element>
+}
