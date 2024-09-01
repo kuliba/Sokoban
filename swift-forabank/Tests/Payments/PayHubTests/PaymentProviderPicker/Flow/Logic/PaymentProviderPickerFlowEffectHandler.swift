61782e64d6f59e46f0e44aaf4583a5841d61f8fd
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-09-01 07:22:29 +0300

    extract `PaymentProviderPickerFlowEffectHandler.handle(_:_:)`

--- swift-forabank/Tests/Payments/PayHubTests/PaymentProviderPicker/Flow/Logic/PaymentProviderPickerFlowEffectHandler.swift
+++ swift-forabank/Tests/Payments/PayHubTests/PaymentProviderPicker/Flow/Logic/PaymentProviderPickerFlowEffectHandler.swift
@@ -26,34 +26,7 @@ extension PaymentProviderPickerFlowEffectHandler {
     ) {
         switch effect {
         case let .select(select):
-            switch select {
-            case let .latest(latest):
-                microServices.initiatePayment(latest) {
-                    
-                    dispatch(.initiatePaymentResult($0))
-                }
-                
-            case .payByInstructions:
-                microServices.makePayByInstructions {
-                    
-                    dispatch(.payByInstructions($0))
-                }
-                
-            case let .provider(provider):
-                microServices.processProvider(provider) {
-                    
-                    switch $0 {
-                    case let .initiatePaymentResult(result):
-                        dispatch(.initiatePaymentResult(result))
-                        
-                    case let .services(services):
-                        dispatch(.loadServices(services))
-                        
-                    case .servicesFailure:
-                        dispatch(.loadServices(nil))
-                    }
-                }
-            }
+            self.handle(select, dispatch)
         }
     }
 }
@@ -66,3 +39,39 @@ extension PaymentProviderPickerFlowEffectHandler {
     typealias Effect = PaymentProviderPickerFlowEffect<Latest, Provider>
 }
 
+private extension PaymentProviderPickerFlowEffectHandler {
+    
+    func handle(
+        _ select: Effect.Select,
+        _ dispatch: @escaping Dispatch
+    ) {
+        switch select {
+        case let .latest(latest):
+            microServices.initiatePayment(latest) {
+                
+                dispatch(.initiatePaymentResult($0))
+            }
+            
+        case .payByInstructions:
+            microServices.makePayByInstructions {
+                
+                dispatch(.payByInstructions($0))
+            }
+            
+        case let .provider(provider):
+            microServices.processProvider(provider) {
+                
+                switch $0 {
+                case let .initiatePaymentResult(result):
+                    dispatch(.initiatePaymentResult(result))
+                    
+                case let .services(services):
+                    dispatch(.loadServices(services))
+                    
+                case .servicesFailure:
+                    dispatch(.loadServices(nil))
+                }
+            }
+        }
+    }
+}
