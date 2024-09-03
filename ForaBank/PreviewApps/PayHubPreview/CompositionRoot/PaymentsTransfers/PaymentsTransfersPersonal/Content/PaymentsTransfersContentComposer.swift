43ca6a08cd171796af2094aa80333fbf1ec01363
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-09-03 11:43:00 +0300

    extract `PaymentFlowMicroServiceComposerNanoServicesComposer` and `StandardSelectedCategoryDestinationNanoServicesComposer`

--- ForaBank/PreviewApps/PayHubPreview/CompositionRoot/PaymentsTransfers/PaymentsTransfersPersonal/Content/PaymentsTransfersContentComposer.swift
+++ ForaBank/PreviewApps/PayHubPreview/CompositionRoot/PaymentsTransfers/PaymentsTransfersPersonal/Content/PaymentsTransfersContentComposer.swift
@@ -96,59 +96,14 @@ private extension PaymentsTransfersContentComposer {
         category: ServiceCategory,
         completion: @escaping (SelectedCategoryDestination) -> Void
     ) {
-        let loadLatest = { completion in self.loadLatest(category, completion) }
-        let loadOperators = { completion in self.loadOperators(category, completion) }
-        
-        let standardFlowComposer = StandardSelectedCategoryDestinationMicroServiceComposer<Latest, ServiceCategory, Operator, SelectedCategoryStub, Error>(
-            nanoServices: .init(
-                loadLatest: loadLatest,
-                loadOperators: loadOperators,
-                makeFailure: { $0(NSError(domain: "Failure", code: -1)) },
-                makeSuccess: { payload, completion in
-                    
-                    completion(.init(
-                        category: category,
-                        latest: payload.latest,
-                        operators: payload.operators
-                    ))
-                }
-            )
-        )
-        let standardMicroService = standardFlowComposer.compose()
+        let composer = PaymentFlowMicroServiceComposerNanoServicesComposer()
+        let nanoServices = composer.compose(category: category)
         let paymentFlowComposer = PaymentFlowMicroServiceComposer(
-            nanoServices: .init(
-                makeMobile: { $0(MobileBinderStub()) },
-                makeQR: { $0(QRBinderStub()) },
-                makeStandard: { standardMicroService.makeDestination(category, $0) },
-                makeTax: { $0(TaxBinderStub()) },
-                makeTransport: { $0(TransportBinderStub()) }
-            )
+            nanoServices: nanoServices
         )
         let microService = paymentFlowComposer.compose()
         
         microService.makePaymentFlow(category.paymentFlowID, completion)
-    }
-    
-    func loadLatest(
-        _ category: ServiceCategory,
-        _ completion: @escaping (Result<[Latest], Error>) -> Void
-    ) {
-        if category.paymentFlowID == .mobile {
-            completion(.success([.init(id: UUID().uuidString)]))
-        } else {
-            completion(.success([]))
-        }
-    }
-    
-    func loadOperators(
-        _ category: ServiceCategory,
-        _ completion: @escaping (Result<[Operator], Error>) -> Void
-    ) {
-        if category.name == "Failure" {
-            completion(.success([]))
-        } else {
-            completion(.success([.init(), .init()]))
-        }
     }
 }
 
