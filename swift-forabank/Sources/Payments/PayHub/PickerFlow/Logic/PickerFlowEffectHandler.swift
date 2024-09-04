c308d8df07503ce40b0af22c71c52d6269995307
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-09-04 22:34:08 +0300

    tweak `MakeNavigation`: take `Dispatch`; add convenience init with simple make navigation closure

--- swift-forabank/Sources/Payments/PayHub/PickerFlow/Logic/PickerFlowEffectHandler.swift
+++ swift-forabank/Sources/Payments/PayHub/PickerFlow/Logic/PickerFlowEffectHandler.swift
@@ -14,8 +14,6 @@ public final class PickerFlowEffectHandler<Element, Navigation> {
     ) {
         self.makeNavigation = makeNavigation
     }
-    
-    public typealias MakeNavigation = (Element, @escaping (Navigation) -> Void) -> Void
 }
 
 public extension PickerFlowEffectHandler {
@@ -26,13 +24,14 @@ public extension PickerFlowEffectHandler {
     ) {
         switch effect {
         case let .select(element):
-            makeNavigation(element) { dispatch(.navigation($0)) }
+            makeNavigation(element, dispatch) { dispatch(.navigation($0)) }
         }
     }
 }
 
 public extension PickerFlowEffectHandler {
     
+    typealias MakeNavigation = (Element, @escaping Dispatch, @escaping (Navigation) -> Void) -> Void
     typealias Dispatch = (Event) -> Void
     
     typealias Event = PickerFlowEvent<Element, Navigation>
