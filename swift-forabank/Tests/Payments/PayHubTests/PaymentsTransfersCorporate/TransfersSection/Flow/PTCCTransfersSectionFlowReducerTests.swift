ab5c242a8e406e7926104aebbfde703e399a87e2
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-09-04 15:33:01 +0300

    add scaffolding for `PTCCTransfersSectionFlowReducerTests`

---
+++ swift-forabank/Tests/Payments/PayHubTests/PaymentsTransfersCorporate/TransfersSection/Flow/PTCCTransfersSectionFlowReducerTests.swift
@@ -0,0 +1,113 @@
+//
+//  PTCCTransfersSectionFlowReducerTests.swift
+//
+//
+//  Created by Igor Malyarov on 04.09.2024.
+//
+
+struct PTCCTransfersSectionFlowState: Equatable {}
+enum PTCCTransfersSectionFlowEvent: Equatable {}
+enum PTCCTransfersSectionFlowEffect: Equatable {}
+
+final class PTCCTransfersSectionFlowReducer {
+    
+    init() {}
+}
+
+extension PTCCTransfersSectionFlowReducer {
+    
+    func reduce(
+        _ state: State,
+        _ event: Event
+    ) -> (State, Effect?) {
+        
+        var state = state
+        var effect: Effect?
+        
+        switch event {
+            
+        }
+        
+        return (state, effect)
+    }
+}
+
+extension PTCCTransfersSectionFlowReducer {
+    
+    typealias State = PTCCTransfersSectionFlowState
+    typealias Event = PTCCTransfersSectionFlowEvent
+    typealias Effect = PTCCTransfersSectionFlowEffect
+}
+
+import XCTest
+
+final class PTCCTransfersSectionFlowReducerTests: PTCCTransfersSectionFlowTests {
+    
+    // MARK: - Helpers
+    
+    private typealias SUT = PTCCTransfersSectionFlowReducer
+    
+    private func makeSUT(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> SUT {
+        
+        let sut = SUT()
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return sut
+    }
+    
+    @discardableResult
+    private func assert(
+        sut: SUT? = nil,
+        _ state: SUT.State,
+        event: SUT.Event,
+        updateStateToExpected: ((inout SUT.State) -> Void)? = nil,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> SUT.State {
+        
+        let sut = sut ?? makeSUT(file: file, line: line)
+        
+        var expectedState = state
+        updateStateToExpected?(&expectedState)
+        
+        let (receivedState, _) = sut.reduce(state, event)
+        
+        XCTAssertNoDiff(
+            receivedState,
+            expectedState,
+            "\nExpected \(String(describing: expectedState)), but got \(String(describing: receivedState)) instead.",
+            file: file, line: line
+        )
+        
+        return receivedState
+    }
+    
+    @discardableResult
+    private func assert(
+        sut: SUT? = nil,
+        _ state: SUT.State,
+        event: SUT.Event,
+        delivers expectedEffect: SUT.Effect?,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> SUT.Effect? {
+        
+        let sut = sut ?? makeSUT(file: file, line: line)
+        
+        let (_, receivedEffect) = sut.reduce(state, event)
+        
+        XCTAssertNoDiff(
+            receivedEffect,
+            expectedEffect,
+            "\nExpected \(String(describing: expectedEffect)), but got \(String(describing: receivedEffect)) instead.",
+            file: file, line: line
+        )
+        
+        return receivedEffect
+    }
+    
+}
---
+++ swift-forabank/Tests/Payments/PayHubTests/PaymentsTransfersCorporate/TransfersSection/Flow/PTCCTransfersSectionFlowTests.swift
@@ -0,0 +1,12 @@
+//
+//  PTCCTransfersSectionFlowTests.swift
+//  
+//
+//  Created by Igor Malyarov on 04.09.2024.
+//
+
+import XCTest
+
+class PTCCTransfersSectionFlowTests: XCTestCase {
+
+}
