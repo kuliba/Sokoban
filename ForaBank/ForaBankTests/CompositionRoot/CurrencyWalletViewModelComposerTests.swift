250c204612bad2f90162672ad7fbca0e908bbddd
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-09-05 09:01:35 +0300

    move `CurrencyWalletViewModelComposer` to prod

--- ForaBank/ForaBankTests/CompositionRoot/CurrencyWalletViewModelComposerTests.swift
+++ ForaBank/ForaBankTests/CompositionRoot/CurrencyWalletViewModelComposerTests.swift
@@ -5,35 +5,6 @@
 //  Created by Igor Malyarov on 05.09.2024.
 //
 
-final class CurrencyWalletViewModelComposer {
-    
-    private let model: Model
-    
-    init(model: Model) {
-        
-        self.model = model
-    }
-}
-
-extension CurrencyWalletViewModelComposer {
-    
-    func compose(
-        dismiss: @escaping () -> Void,
-        currencyOperation: CurrencyOperation = .buy
-    ) -> CurrencyWalletViewModel? {
-        
-        guard let currency = model.currencyWalletList.value.first
-        else { return nil }
-        
-        return .init(
-            currency: .init(description: currency.code),
-            currencyOperation: currencyOperation,
-            model: model,
-            dismissAction: dismiss
-        )
-    }
-}
-
 @testable import ForaBank
 import XCTest
 
