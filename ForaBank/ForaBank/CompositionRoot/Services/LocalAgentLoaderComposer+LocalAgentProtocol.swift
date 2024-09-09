ab70e1ec00e095b2e5897a972baaab21f1e1c853
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-09-09 22:21:39 +0300

    add `LocalAgentLoaderComposer.compose(agent:)`

---
+++ ForaBank/ForaBank/CompositionRoot/Services/LocalAgentLoaderComposer+LocalAgentProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  LocalAgentLoaderComposer+LocalAgentProtocol.swift
+//  ForaBank
+//
+//  Created by Igor Malyarov on 09.09.2024.
+//
+
+extension LocalAgentLoaderComposer
+where Model: Codable,
+      LoadPayload == Model.Type {
+    
+    func compose(agent: LocalAgentProtocol) -> Loader {
+        
+        self.compose(load: agent.load, save: agent.store)
+    }
+}
