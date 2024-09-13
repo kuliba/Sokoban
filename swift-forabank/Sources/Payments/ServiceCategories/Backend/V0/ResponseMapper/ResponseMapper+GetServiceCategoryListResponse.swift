3959720cbeec671a3d160ce87dfa2c435ee96526
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-09-13 07:59:19 +0300

    fix order

--- swift-forabank/Sources/Payments/ServiceCategories/Backend/V0/ResponseMapper/ResponseMapper+GetServiceCategoryListResponse.swift
+++ swift-forabank/Sources/Payments/ServiceCategories/Backend/V0/ResponseMapper/ResponseMapper+GetServiceCategoryListResponse.swift
@@ -55,15 +55,15 @@ extension ResponseMapper {
             case repaymentLoansAndAccounts
             case security
             case socialAndGames
-            case transport
             case taxAndStateService
+            case transport
         }
         
         public enum LatestPaymentsCategory: Equatable {
             
             case charity
-            case education
             case digitalWallets
+            case education
             case internet
             case mobile
             case networkMarketing
