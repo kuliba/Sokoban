2db36997242faf343230d9e112bd4a42cd8c9ecd
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-09-19 09:20:56 +0300

    change `PaymentProviderPickerContentComposer` to `PaymentProviderListSearchBinder`

--- swift-forabank/Tests/Payments/PayHubTests/PaymentProviderPicker/Content/PaymentProviderListSearchBinderTests.swift
+++ swift-forabank/Tests/Payments/PayHubTests/PaymentProviderPicker/Content/PaymentProviderListSearchBinderTests.swift
@@ -1,5 +1,5 @@
 //
-//  PaymentProviderPickerContentComposerTests.swift
+//  PaymentProviderListSearchBinderTests.swift
 //
 //
 //  Created by Igor Malyarov on 02.09.2024.
@@ -9,62 +9,64 @@ import Combine
 import PayHub
 import XCTest
 
-final class PaymentProviderPickerContentComposerTests: XCTestCase {
+final class PaymentProviderListSearchBinderTests: XCTestCase {
     
     func test_providerList_shouldReceiveTextEmittedBySearch() {
         
         let text = anyMessage()
-        let sut = makeSUT()
+        let (search, providerList, subscription) = makeSUT()
         
-        sut.search?.emit(text)
+        search.emit(text)
         
-        XCTAssertNoDiff(sut.providerList.messages, [text])
+        XCTAssertNoDiff(providerList.messages, [text])
+        XCTAssertNotNil(subscription)
     }
     
     func test_providerList_shouldReceiveEmptyTextEmittedBySearch() {
         
-        let sut = makeSUT()
+        let (search, providerList, subscription) = makeSUT()
         
-        sut.search?.emit("")
+        search.emit("")
         
-        XCTAssertNoDiff(sut.providerList.messages, [""])
+        XCTAssertNoDiff(providerList.messages, [""])
+        XCTAssertNotNil(subscription)
     }
     
     func test_providerList_shouldReceiveTextsEmittedBySearch() {
         
-        let sut = makeSUT()
+        let (search, providerList, subscription) = makeSUT()
         
-        sut.search?.emit("")
-        sut.search?.emit("abc")
-        sut.search?.emit("123")
+        search.emit("")
+        search.emit("abc")
+        search.emit("123")
         
-        XCTAssertNoDiff(sut.providerList.messages, ["", "abc", "123"])
+        XCTAssertNoDiff(providerList.messages, ["", "abc", "123"])
+        XCTAssertNotNil(subscription)
     }
     
     // MARK: - Helpers
     
-    private typealias Composer = PaymentProviderPickerContentComposer<Receiver, Emitter<String>>
-    private typealias SUT = PaymentProviderPickerContent<Receiver, Emitter<String>>
+    private typealias Binder = PaymentProviderListSearchBinder<Receiver, Emitter<String>>
     
     private func makeSUT(
         file: StaticString = #file,
         line: UInt = #line
-    ) -> SUT {
-        
-        let composer = Composer()
+    ) -> (
+        emitter: Emitter<String>,
+        receiver: Receiver,
+        subscription: Set<AnyCancellable>
+    ) {
         let emitter = Emitter<String>()
         let receiver = Receiver()
-        let sut = composer.compose(
+        let subscription = Binder.bind(
             providerList: .init(model: receiver, makeReceive: { $0.receive(_:) }),
             search: .init(model: emitter, makePublisher: { $0.publisher })
         )
         
-        trackForMemoryLeaks(composer, file: file, line: line)
-        trackForMemoryLeaks(sut, file: file, line: line)
         trackForMemoryLeaks(emitter, file: file, line: line)
         trackForMemoryLeaks(receiver, file: file, line: line)
         
-        return sut
+        return (emitter, receiver, subscription)
     }
     
     private final class Receiver {
