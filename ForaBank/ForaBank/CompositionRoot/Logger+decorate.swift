5c082d6680e8cc6467da9c85c25fdd4dea82fd67
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-09-20 11:02:26 +0300

    improve logging decorator

--- ForaBank/ForaBank/CompositionRoot/Logger+decorate.swift
+++ ForaBank/ForaBank/CompositionRoot/Logger+decorate.swift
@@ -12,6 +12,7 @@ extension LoggerAgentProtocol {
     func decorate<T>(
         _ f: @escaping () throws -> T,
         with category: LoggerAgentCategory,
+        messagePrefixLength: Int = 300,
         file: StaticString = #file,
         line: UInt = #line
     ) -> () throws -> T {
@@ -20,10 +21,10 @@ extension LoggerAgentProtocol {
             
             do {
                 let t = try f()
-                self.log(level: .info, category: category, message: "=== " + String(describing: t), file: file, line: line)
+                self.log(level: .info, category: category, message: "RemoteService: " + String(describing: t).prefix(messagePrefixLength), file: file, line: line)
                 return t
             } catch {
-                self.log(level: .error, category: category, message: "=== " + String(describing: error), file: file, line: line)
+                self.log(level: .error, category: category, message: "RemoteService: " + String(describing: error), file: file, line: line)
                 throw error
             }
         }
@@ -33,6 +34,7 @@ extension LoggerAgentProtocol {
     func decorate<T, V>(
         _ f: @escaping (V) throws -> T,
         with category: LoggerAgentCategory,
+        messagePrefixLength: Int = 300,
         file: StaticString = #file,
         line: UInt = #line
     ) -> (V) throws -> T {
@@ -41,10 +43,10 @@ extension LoggerAgentProtocol {
             
             do {
                 let t = try f(v)
-                self.log(level: .info, category: category, message: "=== " + String(describing: t), file: file, line: line)
+                self.log(level: .info, category: category, message: "RemoteService: " + String(describing: t).prefix(messagePrefixLength), file: file, line: line)
                 return t
             } catch {
-                self.log(level: .error, category: category, message: "=== " + String(describing: error), file: file, line: line)
+                self.log(level: .error, category: category, message: "RemoteService: " + String(describing: error), file: file, line: line)
                 throw error
             }
         }
@@ -53,6 +55,7 @@ extension LoggerAgentProtocol {
     func decorate<T, V, E: Error>(
         f: @escaping (V) -> Result<T, E>,
         with category: LoggerAgentCategory,
+        messagePrefixLength: Int = 300,
         file: StaticString = #file,
         line: UInt = #line
     ) -> (V) -> Result<T, E> {
@@ -61,13 +64,41 @@ extension LoggerAgentProtocol {
             
             switch f($0) {
             case let .failure(failure):
-                self.log(level: .error, category: category, message: "=== " + String(describing: failure), file: file, line: line)
+                self.log(level: .error, category: category, message: "RemoteService: " + String(describing: failure), file: file, line: line)
                 return(.failure(failure))
                 
             case let .success(t):
-                self.log(level: .info, category: category, message: "=== " + String(describing: t), file: file, line: line)
+                self.log(level: .info, category: category, message: "RemoteService: " + String(describing: t).prefix(messagePrefixLength), file: file, line: line)
                 return .success(t)
             }
+        }
+    }
+    
+    typealias MapResponse<T, F: Error> = (Data, HTTPURLResponse) -> Result<T, F>
+    
+    func decorate<T, F: Error>(
+        mapResponse: @escaping MapResponse<T, F>,
+        with category: LoggerAgentCategory,
+        messagePrefixLength: Int = 300,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> MapResponse<T, F> {
+        
+        return { data, response in
+            
+            let mapped = mapResponse(data, response)
+            
+            self.log(level: .debug, category: category, message: "RemoteService: response \(String(describing: String(data: data, encoding: .utf8)).prefix(messagePrefixLength))", file: #file, line: #line)
+            
+            switch mapped {
+            case let .failure(failure):
+                self.log(level: .error, category: category, message: "RemoteService: response mapping failure \(failure)", file: #file, line: #line)
+                
+            case let .success(success):
+                self.log(level: .debug, category: category, message: "RemoteService: response mapping success: \(String(describing: success).prefix(messagePrefixLength)).", file: #file, line: #line)
+            }
+            
+            return mapped
         }
     }
 }
