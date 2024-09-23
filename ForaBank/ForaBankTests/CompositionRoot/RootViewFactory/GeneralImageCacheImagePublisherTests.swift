c711a60d9c5ce8f08f510f61a4826cc6fb63840e
Tasha Andryushina <natalykn@gmail.com>
2024-09-23 10:18:52 +0300

    update tests

--- ForaBank/ForaBankTests/CompositionRoot/RootViewFactory/GeneralImageCacheImagePublisherTests.swift
+++ ForaBank/ForaBankTests/CompositionRoot/RootViewFactory/GeneralImageCacheImagePublisherTests.swift
@@ -10,54 +10,46 @@ import SwiftUI
 import XCTest
 
 final class GeneralImageCacheImagePublisherTests: XCTestCase {
-
-    func test_local_shouldDeliverImage() {
-        
-        let key = "1"
-        let (_, imageCache) = makeSUT()
-        let spy = ValueSpy(imageCache.image(forKey: .init(key)))
-
-        XCTAssertFalse(spy.values.isEmpty)
-    }
     
     func test_remote_shouldDeliverImageOnEmptyImages() {
         
         let key = "dict/getBannerCatalogImage?image=banner_1"
-        let (model, imageCache) = makeSUT()
-        let spy = ValueSpy(imageCache.image(forKey: .init(key)))
+        let (model, imagesSpy) = makeSUT(key: key)
         
-        XCTAssertFalse(spy.values.isEmpty)
+        XCTAssertNoDiff(imagesSpy.values.count, 1)
         XCTAssertNil(model.images.value[key])
     }
     
     func test_remote_shouldDeliverImageOnUpdatedImages() {
         
         let key = "dict/getBannerCatalogImage?image=banner_1"
-        let (model, imageCache) = makeSUT()
-        let spy = ValueSpy(imageCache.image(forKey: .init(key)))
+        let image = ImageData(named: "ic24IconMessage")
+        let (model, imagesSpy) = makeSUT(key: key)
 
-        model.updateImage(for: key, with: .init(named: "ic24IconMessage"))
+        model.updateImage(for: key, with: image)
         
-        XCTAssertFalse(spy.values.isEmpty)
-        XCTAssertNotNil(model.images.value[key])
+        XCTAssertNoDiff(imagesSpy.values.count, 1)
+        XCTAssertNoDiff(model.images.value[key], image)
     }
     
     // MARK: - Helpers
     
     private func makeSUT(
+        key: String,
         file: StaticString = #file,
         line: UInt = #line
     ) -> (
         model: Model,
-        imageCache: ImageCache
+        imageSpy: ValueSpy<Image>
     ) {
         let model: Model = .mockWithEmptyExcept()
         let imageCache = model.generalImageCache()
-        
+        let spy = ValueSpy(imageCache.image(forKey: .init(key)))
+
         trackForMemoryLeaks(model, file: file, line: line)
         trackForMemoryLeaks(imageCache, file: file, line: line)
         
-        return (model, imageCache)
+        return (model, spy)
     }
 }
 
