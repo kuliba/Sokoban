4d4ab956e29a47c9641b673c2a2090fecd515fa4
Nikolay Pochekuev <pochekuev_n_e@forabank.ru>
2024-10-03 16:08:54 +0400

    add tests for nil, empty fields

--- swift-forabank/Tests/Services/GetClientInformDataServicesTests/GetAuthorizedZoneClientInformData/ResponseMapper/ResponseMapper+mapGetAuthorizedZoneClientInformDataRequestTests.swift
+++ swift-forabank/Tests/Services/GetClientInformDataServicesTests/GetAuthorizedZoneClientInformData/ResponseMapper/ResponseMapper+mapGetAuthorizedZoneClientInformDataRequestTests.swift
@@ -83,6 +83,106 @@ final class ResponseMapper_mapGetAuthorizedZoneClientInformDataRequestTests: XCT
         )
     }
     
+    func test_map_shouldDeliverInvalidFailureWithNoSerial() {
+       
+        XCTAssertNoDiff(
+            map(.nullSerialResponse),
+            .failure(.invalid(statusCode: 200, data: .nullSerialResponse))
+        )
+    }
+
+    func test_map_shouldDeliverInvalidFailureWithNoAuthorizedList() {
+        
+        XCTAssertNoDiff(
+            map(.nullAuthorizedListResponse),
+            .failure(.invalid(statusCode: 200, data: .nullAuthorizedListResponse))
+        )
+    }
+
+    func test_map_withNilCategory_shouldOmitItem() throws {
+        
+        let mapped = try mapResult(.withNilCategory)
+        
+        XCTAssert(mapped.list.isEmpty)
+    }
+
+    func test_map_withNilTitle_shouldOmitItem() throws {
+        
+        let mapped = try mapResult(.withNilTitle)
+        
+        XCTAssert(mapped.list.isEmpty)
+    }
+    
+    func test_map_withNilImage_shouldOmitItem() throws {
+        
+        let mapped = try mapResult(.withNilImage)
+        
+        XCTAssert(mapped.list.isEmpty)
+    }
+
+    func test_map_withNilText_shouldOmitItem() throws {
+        
+        let mapped = try mapResult(.withNilText)
+        
+        XCTAssert(mapped.list.isEmpty)
+    }
+
+    func test_map_withEmptyCategory_shouldDeliverValidCategory() throws {
+        
+        let mapped = try mapResult(.withEmptyCategory)
+        
+        XCTAssertNoDiff(mapped.list.first?.category, "")
+    }
+
+    func test_map_withEmptyTitle_shouldDeliverValidTitle() throws {
+        
+        let mapped = try mapResult(.withEmptyTitle)
+        
+        XCTAssertNoDiff(mapped.list.first?.title, "")
+    }
+
+    func test_map_withEmptyImage_shouldDeliverValidImage() throws {
+        
+        let mapped = try mapResult(.withEmptyImage)
+        
+        XCTAssertNoDiff(mapped.list.first?.svg_image, "")
+    }
+    
+    func test_map_withEmptyText_shouldDeliverValidText() throws {
+        
+        let mapped = try mapResult(.withEmptyText)
+        
+        XCTAssertNoDiff(mapped.list.first?.text, "")
+    }
+
+    func test_map_shouldOmitItemWithNilCategory() throws {
+        
+        let mapped = try mapResult(.withNilCategoryInOne)
+        
+        XCTAssertNoDiff(mapped.list.first?.category, "CATEGORY")
+    }
+    
+    func test_map_shouldOmitItemWithNilTitle() throws {
+        
+        let mapped = try mapResult(.withNilTitleInOne)
+        
+        XCTAssertNoDiff(mapped.list.first?.title, "TITLE")
+    }
+    
+    func test_map_shouldOmitItemWithNilImage() throws {
+        
+        let mapped = try mapResult(.withNilImageInOne)
+        
+        XCTAssertNoDiff(mapped.list.first?.svg_image, "SVG")
+    }
+    
+    func test_map_shouldOmitItemWithNilText() throws {
+        
+        let mapped = try mapResult(.withNilTextInOne)
+        
+        XCTAssertNoDiff(mapped.list.first?.text, "TEXT")
+    }
+
     func test_map_shouldDeliverResponseWithAuthorized() throws {
         
         try assert(.authorized, .authorized)
@@ -101,6 +201,14 @@ final class ResponseMapper_mapGetAuthorizedZoneClientInformDataRequestTests: XCT
         ResponseMapper.mapGetAuthorizedZoneClientInformDataResponse(data, httpURLResponse)
     }
     
+    private func mapResult(
+        _ data: Data,
+        _ httpURLResponse: HTTPURLResponse = anyHTTPURLResponse()
+    ) throws -> Response {
+
+        try ResponseMapper.mapGetAuthorizedZoneClientInformDataResponse(data, httpURLResponse).get()
+    }
+
     private func assert(
         _ data: Data,
         _ response: Response,
@@ -142,8 +250,22 @@ private extension Data {
     static let emptyDataResponse: Data = String.emptyDataResponse.json
     static let emptyListResponse: Data = String.emptyList.json
     static let nullServerResponse: Data = String.nullServerResponse.json
+    static let nullSerialResponse: Data = String.nullSerialResponse.json
+    static let nullAuthorizedListResponse: Data = String.nullAuthorizedListResponse.json
     static let serverError: Data = String.serverError.json
     static let authorized: Data = String.authorized.json
+    static let withNilCategory: Data = String.withNilCategory.json
+    static let withNilTitle: Data = String.withNilTitle.json
+    static let withNilImage: Data = String.withNilImage.json
+    static let withNilText: Data = String.withNilText.json
+    static let withEmptyCategory: Data = String.withEmptyCategory.json
+    static let withEmptyTitle: Data = String.withEmptyTitle.json
+    static let withEmptyImage: Data = String.withEmptyImage.json
+    static let withEmptyText: Data = String.withEmptyText.json
+    static let withNilCategoryInOne: Data = String.withNilCategoryInOne.json
+    static let withNilTitleInOne: Data = String.withNilTitleInOne.json
+    static let withNilImageInOne: Data = String.withNilImageInOne.json
+    static let withNilTextInOne: Data = String.withNilTextInOne.json
 }
 
 private extension String {
@@ -194,7 +316,29 @@ private extension String {
   }
 }
 """
-    
+
+    static let nullSerialResponse = """
+{
+    "statusCode": 0,
+    "errorMessage": null,
+    "data": {
+        "serial": null,
+        "authorized": []
+    }
+}
+"""
+
+    static let nullAuthorizedListResponse = """
+{
+    "statusCode": 0,
+    "errorMessage": null,
+    "data": {
+        "serial": "1bebd140bc2660211fbba306105479ae",
+        "authorized": null
+    }
+}
+"""
+
     static let authorized = """
 {
   "statusCode": 0,
@@ -213,6 +357,245 @@ private extension String {
         "title": "Приятного аппетита",
         "svg_image": "svg",
         "text": "Специальное предложение для тех кто любит поострей. Вклад в аргентинских песо - 300% годовых. Открыть https://link_click."
+      }
+    ]
+  }
+}
+"""
+    
+    static let withNilCategory = """
+{
+  "statusCode": 0,
+  "errorMessage": null,
+  "data": {
+    "serial": "1bebd140bc2660211fbba306105479ae",
+    "authorized": [
+      {
+        "category": null,
+        "title": "TITLE",
+        "svg_image": "SVG",
+        "text": "TEXT"
+      }
+    ]
+  }
+}
+"""
+    
+    static let withNilTitle = """
+{
+  "statusCode": 0,
+  "errorMessage": null,
+  "data": {
+    "serial": "1bebd140bc2660211fbba306105479ae",
+    "authorized": [
+      {
+        "category": "CATEGORY",
+        "title": null,
+        "svg_image": "SVG",
+        "text": "TEXT"
+      }
+    ]
+  }
+}
+"""
+    static let withNilImage = """
+{
+  "statusCode": 0,
+  "errorMessage": null,
+  "data": {
+    "serial": "1bebd140bc2660211fbba306105479ae",
+    "authorized": [
+      {
+        "category": "CATEGORY",
+        "title": "TITLE",
+        "svg_image": null,
+        "text": "TEXT"
+      }
+    ]
+  }
+}
+"""
+
+    static let withNilText = """
+{
+  "statusCode": 0,
+  "errorMessage": null,
+  "data": {
+    "serial": "1bebd140bc2660211fbba306105479ae",
+    "authorized": [
+      {
+        "category": "CATEGORY",
+        "title": "TITLE",
+        "svg_image": "SVG",
+        "text": null
+      }
+    ]
+  }
+}
+"""
+    
+    static let withEmptyCategory = """
+{
+  "statusCode": 0,
+  "errorMessage": null,
+  "data": {
+    "serial": "1bebd140bc2660211fbba306105479ae",
+    "authorized": [
+      {
+        "category": "",
+        "title": "TITLE",
+        "svg_image": "SVG",
+        "text": "TEXT"
+      }
+    ]
+  }
+}
+"""
+
+    static let withEmptyTitle = """
+{
+  "statusCode": 0,
+  "errorMessage": null,
+  "data": {
+    "serial": "1bebd140bc2660211fbba306105479ae",
+    "authorized": [
+      {
+        "category": "CATEGORY",
+        "title": "",
+        "svg_image": "SVG",
+        "text": "TEXT"
+      }
+    ]
+  }
+}
+"""
+    
+    static let withEmptyImage = """
+{
+  "statusCode": 0,
+  "errorMessage": null,
+  "data": {
+    "serial": "1bebd140bc2660211fbba306105479ae",
+    "authorized": [
+      {
+        "category": "CATEGORY",
+        "title": "TITLE",
+        "svg_image": "",
+        "text": "TEXT"
+      }
+    ]
+  }
+}
+"""
+    
+    static let withEmptyText = """
+{
+  "statusCode": 0,
+  "errorMessage": null,
+  "data": {
+    "serial": "1bebd140bc2660211fbba306105479ae",
+    "authorized": [
+      {
+        "category": "CATEGORY",
+        "title": "TITLE",
+        "svg_image": "SVG",
+        "text": ""
+      }
+    ]
+  }
+}
+"""
+    
+    static let withNilCategoryInOne = """
+{
+  "statusCode": 0,
+  "errorMessage": null,
+  "data": {
+    "serial": "1bebd140bc2660211fbba306105479ae",
+    "authorized": [
+      {
+        "category": null,
+        "title": "TITLE",
+        "svg_image": "SVG",
+        "text": "TEXT"
+      },
+      {
+        "category": "CATEGORY",
+        "title": "TITLE",
+        "svg_image": "SVG",
+        "text": "TEXT"
+      }
+    ]
+  }
+}
+"""
+
+    static let withNilTitleInOne = """
+{
+  "statusCode": 0,
+  "errorMessage": null,
+  "data": {
+    "serial": "1bebd140bc2660211fbba306105479ae",
+    "authorized": [
+      {
+        "category": "CATEGORY",
+        "title": null,
+        "svg_image": "SVG",
+        "text": "TEXT"
+      },
+      {
+        "category": "CATEGORY",
+        "title": "TITLE",
+        "svg_image": "SVG",
+        "text": "TEXT"
+      }
+    ]
+  }
+}
+"""
+    
+    static let withNilImageInOne = """
+{
+  "statusCode": 0,
+  "errorMessage": null,
+  "data": {
+    "serial": "1bebd140bc2660211fbba306105479ae",
+    "authorized": [
+      {
+        "category": "CATEGORY",
+        "title": "TITLE",
+        "svg_image": null,
+        "text": "TEXT"
+      },
+      {
+        "category": "CATEGORY",
+        "title": "TITLE",
+        "svg_image": "SVG",
+        "text": "TEXT"
+      }
+    ]
+  }
+}
+"""
+    
+    static let withNilTextInOne = """
+{
+  "statusCode": 0,
+  "errorMessage": null,
+  "data": {
+    "serial": "1bebd140bc2660211fbba306105479ae",
+    "authorized": [
+      {
+        "category": "CATEGORY",
+        "title": "TITLE",
+        "svg_image": "SVG",
+        "text": null
+      },
+      {
+        "category": "CATEGORY",
+        "title": "TITLE",
+        "svg_image": "SVG",
+        "text": "TEXT"
       }
     ]
   }
