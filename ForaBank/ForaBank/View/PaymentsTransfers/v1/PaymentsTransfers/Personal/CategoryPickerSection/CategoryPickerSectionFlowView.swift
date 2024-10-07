4b2408f02a5d2d8d67867faf3e19b5b424be25e2
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-10-07 11:55:08 +0300

    move UI mapping to `SelectedCategoryNavigation`

--- ForaBank/ForaBank/View/PaymentsTransfers/v1/PaymentsTransfers/Personal/CategoryPickerSection/CategoryPickerSectionFlowView.swift
+++ ForaBank/ForaBank/View/PaymentsTransfers/v1/PaymentsTransfers/Personal/CategoryPickerSection/CategoryPickerSectionFlowView.swift
@@ -50,82 +50,16 @@ private extension CategoryPickerSection.FlowDomain.State {
     
     var destination: CategoryPickerSectionNavigation.Destination? {
         
-        switch navigation {
-        case .failure, .none, .qrNavigation:
-            return nil
-            
-        case let .list(list):
-            return .list(list)
-            
-        case let .paymentFlow(paymentFlow):
-            switch paymentFlow {
-            case let .mobile(mobile):
-                return .mobile(mobile)
-                
-            case .qr:
-                return nil
-                
-            case let .standard(standard):
-                return .standard(standard)
-                
-            case let .taxAndStateServices(taxAndStateServices):
-                return .taxAndStateServices(taxAndStateServices)
-                
-            case let .transport(transport):
-                return .transport(transport)
-            }
-        }
+        navigation?.destination
     }
     
     var failure: SelectedCategoryFailure? {
         
-        guard case let .failure(failure) = navigation else { return nil }
-        
-        return failure
+        navigation?.failure
     }
     
     var fullScreenCover: CategoryPickerSectionNavigation.FullScreenCover? {
         
-        guard case let .paymentFlow(.qr(qr)) = navigation else { return nil }
-        
-        return .init(id: .init(), qr: qr.model)
-    }
-}
-
-extension CategoryPickerSectionNavigation {
-    
-    struct FullScreenCover: Identifiable {
-        
-        let id: UUID
-        let qr: QRModel
-    }
-    
-    enum Destination: Identifiable {
-        
-        case list(CategoryListModelStub)
-        case mobile(ClosePaymentsViewModelWrapper)
-        case standard(StandardSelectedCategoryDestination)
-        case taxAndStateServices(ClosePaymentsViewModelWrapper)
-        case transport(TransportPaymentsViewModel)
-        
-        var id: ID {
-            
-            switch self {
-            case .list:                return .list
-            case .mobile:              return .mobile
-            case .standard:            return .standard
-            case .taxAndStateServices: return .taxAndStateServices
-            case .transport:           return .transport
-            }
-        }
-        
-        enum ID: Hashable {
-            
-            case list
-            case mobile
-            case standard
-            case taxAndStateServices
-            case transport
-        }
+        navigation?.fullScreenCover
     }
 }
--- ForaBank/ForaBank/View/PaymentsTransfers/v1/PaymentsTransfers/Personal/CategoryPickerSection/SelectedCategoryNavigation.swift
+++ ForaBank/ForaBank/View/PaymentsTransfers/v1/PaymentsTransfers/Personal/CategoryPickerSection/SelectedCategoryNavigation.swift
@@ -32,3 +32,89 @@ struct SelectedCategoryFailure: Error, Identifiable {
 typealias StandardSelectedCategoryDestination = Result<PaymentProviderPicker.Binder, FailedPaymentProviderPickerStub>
 
 final class FailedPaymentProviderPickerStub: Error {}
+
+// MARK: - UI mapping
+
+extension SelectedCategoryNavigation {
+    
+    var destination: Destination? {
+        
+        switch self {
+        case .failure, .qrNavigation:
+            return nil
+            
+        case let .list(list):
+            return .list(list)
+            
+        case let .paymentFlow(paymentFlow):
+            switch paymentFlow {
+            case let .mobile(mobile):
+                return .mobile(mobile)
+                
+            case .qr:
+                return nil
+                
+            case let .standard(standard):
+                return .standard(standard)
+                
+            case let .taxAndStateServices(taxAndStateServices):
+                return .taxAndStateServices(taxAndStateServices)
+                
+            case let .transport(transport):
+                return .transport(transport)
+            }
+        }
+    }
+    
+    var failure: SelectedCategoryFailure? {
+        
+        guard case let .failure(failure) = self else { return nil }
+        
+        return failure
+    }
+    
+    var fullScreenCover: FullScreenCover? {
+        
+        guard case let .paymentFlow(.qr(qr)) = self else { return nil }
+        
+        return .init(id: .init(), qr: qr.model)
+    }
+}
+
+extension SelectedCategoryNavigation {
+    
+    struct FullScreenCover: Identifiable {
+        
+        let id: UUID
+        let qr: QRModel
+    }
+    
+    enum Destination: Identifiable {
+        
+        case list(List)
+        case mobile(ClosePaymentsViewModelWrapper)
+        case standard(StandardSelectedCategoryDestination)
+        case taxAndStateServices(ClosePaymentsViewModelWrapper)
+        case transport(TransportPaymentsViewModel)
+        
+        var id: ID {
+            
+            switch self {
+            case .list:                return .list
+            case .mobile:              return .mobile
+            case .standard:            return .standard
+            case .taxAndStateServices: return .taxAndStateServices
+            case .transport:           return .transport
+            }
+        }
+        
+        enum ID: Hashable {
+            
+            case list
+            case mobile
+            case standard
+            case taxAndStateServices
+            case transport
+        }
+    }
+}
