c998300a5a5f94d9233ae87bd1947e1cbac1566e
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-10-14 23:28:21 +0300

    refactor `SerialLoaderComposer`

--- swift-forabank/Tests/WIPTests/SerialLoaderComposerTests.swift
+++ swift-forabank/Tests/WIPTests/SerialLoaderComposerTests.swift
@@ -9,7 +9,6 @@ import EphemeralStores
 import ForaTools
 import GenericLoader
 
-// protocol MonolithicStampedStore<T> {
 protocol MonolithicStore<Value> {
     
     associatedtype Value
@@ -27,42 +26,38 @@ where Serial: Equatable {
     
     private let ephemeral: any Ephemeral
     private let persistent: any Persistent
+    private let remoteLoad: RemoteLoad<[T]>
+    private let fromModel: (Model) -> T
+    private let toModel: (T) -> Model
     
     init(
         ephemeral: any Ephemeral,
-        persistent: any Persistent
+        persistent: any Persistent,
+        remoteLoad: @escaping RemoteLoad<[T]>,
+        fromModel: @escaping (Model) -> T,
+        toModel: @escaping (T) -> Model
     ) {
         self.ephemeral = ephemeral
         self.persistent = persistent
+        self.remoteLoad = remoteLoad
+        self.fromModel = fromModel
+        self.toModel = toModel
     }
     
     typealias Ephemeral = MonolithicStore<[T]>
     typealias Persistent = MonolithicStore<SerialStamped<Serial, Model>>
+    
+    typealias RemoteLoadCompletion<Value> = (Result<ForaTools.SerialStamped<Serial, Value>, Error>) -> Void
+    typealias RemoteLoad<Value> = (Serial?, @escaping RemoteLoadCompletion<Value>) -> Void
 }
 
 extension SerialLoaderComposer {
     
-    typealias RemoteLoadCompletion<Value>  = (Result<ForaTools.SerialStamped<Serial, Value>, Error>) -> Void
-    typealias RemoteLoad<Value> = (Serial?, @escaping RemoteLoadCompletion<Value>) -> Void
-    
-    typealias CacheCompletion = () -> Void
-    typealias Cache<Value> = (ForaTools.SerialStamped<Serial, Value>, @escaping CacheCompletion) -> Void
-    
     @inlinable
-    func compose(
-        remoteLoad: @escaping RemoteLoad<[T]>,
-        fromModel: @escaping (Model) -> T,
-        toModel: @escaping (T) -> Model
-    ) -> (load: Load<T>, reload: Load<T>) {
+    func compose() -> (load: Load<T>, reload: Load<T>) {
         
-        let localLoad = makeLocalLoad(fromModel: fromModel)
-        
-        let reload = makeReload(
-            localLoad: localLoad,
-            getSerial: getSerial,
-            remoteLoad: remoteLoad,
-            cache: cache(toModel: toModel)
-        )
+        let localLoad = makeLocalLoad()
+        let reload = makeReload(localLoad: localLoad)
         let strategy = Strategy(primary: localLoad, fallback: reload)
         
         return (strategy.load(completion:), reload)
@@ -71,14 +66,17 @@ extension SerialLoaderComposer {
 
 extension SerialLoaderComposer {
     
+    typealias CacheCompletion = () -> Void
+    typealias Cache<Value> = (ForaTools.SerialStamped<Serial, Value>, @escaping CacheCompletion) -> Void
+    
     @inlinable
     func cache(
         toModel: @escaping (T) -> Model
-    ) -> (ForaTools.SerialStamped<Serial, [T]>, @escaping () -> Void) -> Void {
+    ) -> Cache<[T]> {
         
         return { [ephemeral, persistent] payload, completion in
             
-            ephemeral.insert(payload.value) { [persistent] _ in
+            ephemeral.insert(payload.value) { _ in
                 
                 let stamped = SerialStamped(
                     list: payload.value.map(toModel),
@@ -90,20 +88,11 @@ extension SerialLoaderComposer {
     }
     
     @inlinable
-    func getSerial(
-        completion: @escaping (Serial?) -> Void
-    ) {
-        persistent.retrieve { completion($0?.serial) }
-    }
-    
-    @inlinable
-    func makeLocalLoad(
-        fromModel: @escaping (Model) -> T
-    ) -> Load<T> {
+    func makeLocalLoad() -> Load<T> {
         
         let strategy = Strategy(
             primary: ephemeral.retrieve,
-            fallback: decoratedPersistent(fromModel: fromModel)
+            fallback: decoratedPersistent
         )
         
         return strategy.load(completion:)
@@ -111,36 +100,25 @@ extension SerialLoaderComposer {
     
     @inlinable
     func decoratedPersistent(
-        fromModel: @escaping (Model) -> T
-    ) -> Load<T> {
-        
-        return { [ephemeral, persistent] completion in
+        completion: @escaping LoadCompletion<T>
+    ) {
+        persistent.retrieve { value in
             
-            persistent.retrieve { value in
-                
-                switch value {
-                case .none:
-                    completion(nil)
-                    
-                case let .some(stamped):
-                    let value = stamped.list.map(fromModel)
-                    ephemeral.insert(value) { _ in completion(value) }
-                }
-            }
+            guard let value else { return completion(nil) }
+            
+            let list = value.list.map(self.fromModel)
+            self.ephemeral.insert(list) { _ in completion(list) }
         }
     }
     
     @inlinable
     func makeReload(
-        localLoad: @escaping Load<T>,
-        getSerial: @escaping (@escaping (Serial?) -> Void) -> Void,
-        remoteLoad: @escaping RemoteLoad<[T]>,
-        cache: @escaping Cache<[T]>
+        localLoad: @escaping Load<T>
     ) -> Load<T> {
         
         let caching = SerialStampedCachingDecorator(
             decoratee: remoteLoad,
-            cache: cache
+            cache: cache(toModel: toModel)
         )
         let fallback = SerialFallback(
             primary: caching.decorated,
@@ -148,11 +126,17 @@ extension SerialLoaderComposer {
         )
         let decoratedRemote = { completion in
             
-            getSerial { fallback(payload: $0, completion: completion) }
+            self.getSerial { fallback(payload: $0, completion: completion) }
         }
-        let strategy = Strategy(primary: decoratedRemote, fallback: localLoad)
         
-        return strategy.load(completion:)
+        return decoratedRemote
+    }
+    
+    @inlinable
+    func getSerial(
+        completion: @escaping (Serial?) -> Void
+    ) {
+        persistent.retrieve { completion($0?.serial) }
     }
 }
 
