4fa5d452bcf6223d37a7387885e5115456163b96
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-10-15 16:45:52 +0300

    add `SerialLoaderComposer+extWithLocalAgentTests`

--- ForaBank/ForaBankTests/CompositionRoot/SerialServices/SerialLoaderComposer+extWithLocalAgentTests.swift
+++ ForaBank/ForaBankTests/CompositionRoot/SerialServices/SerialLoaderComposer+extWithLocalAgentTests.swift
@@ -1,133 +1,28 @@
 //
-//  SerialLoaderComposer+extTests.swift
+//  SerialLoaderComposer+extWithLocalAgentTests.swift
 //  ForaBankTests
 //
 //  Created by Igor Malyarov on 15.10.2024.
 //
-
-import CombineSchedulers
-import EphemeralStores
-import ForaTools
-import SerialComponents
-
-extension SerialComponents.SerialLoaderComposer
-where Serial == String,
-      Model: Codable {
-    
-    convenience init(
-        localAgent: any LocalAgentProtocol,
-        remoteLoad: @escaping RemoteLoad<[T]>,
-        fromModel: @escaping (Model) -> T,
-        toModel: @escaping (T) -> Model
-    ) {
-        self.init(
-            ephemeral: EphemeralStores.InMemoryStore<[T]>(),
-            persistent: LocalAgentWrapper(localAgent: localAgent),
-            remoteLoad: remoteLoad,
-            fromModel: fromModel,
-            toModel: toModel
-        )
-    }
-}
-
-extension EphemeralStores.InMemoryStore: MonolithicStore {
-    
-    nonisolated public func insert(
-        _ value: Value,
-        _ completion : @escaping InsertCompletion
-    ) {
-        Task {
-            
-            await self.insert(value)
-            completion(.success(()))
-        }
-    }
-    
-    nonisolated public func retrieve(
-        _ completion : @escaping RetrieveCompletion
-    ) {
-        Task {
-            
-            let cache = await self.retrieve()
-            completion(cache)
-        }
-    }
-}
-
-actor LocalAgentWrapper<Value: Codable> {
-    
-    private let localAgent: LocalAgentProtocol
-    
-    init(localAgent: LocalAgentProtocol) {
-        
-        self.localAgent = localAgent
-    }
-    
-    func insert(
-        _ stamped: SerialStamped<String, Value>
-    ) throws {
-        
-        try localAgent.store(stamped.value, serial: stamped.serial)
-    }
-    
-    func retrieve() -> SerialStamped<String, Value>? {
-        
-        let value = localAgent.load(type: Value.self)
-        let serial = localAgent.serial(for: Value.self)
-        
-        guard let value, let serial else { return nil }
-        
-        return .init(value: value, serial: serial)
-    }
-}
-
-extension LocalAgentWrapper: MonolithicStore {
-    
-    nonisolated func insert(
-        _ stamped: SerialStamped<String, Value>,
-        _ completion: @escaping InsertCompletion
-    ) {
-        Task {
-            do {
-                try await insert(stamped)
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    nonisolated func retrieve(
-        _ completion: @escaping RetrieveCompletion
-    ) {
-        Task {
-            
-            let value = await retrieve()
-            completion(value)
-        }
-    }
-}
 
 import ForaTools
 @testable import ForaBank
 import SerialComponents
 import XCTest
 
-final class SerialLoaderComposer_extTests: XCTestCase {
+final class SerialLoaderComposer_extWithLocalAgentTests: LocalAgentTests {
     
     func test_init_shouldNotCallCollaborators() {
         
-        let (sut, localAgent, remoteLoad) = makeSUT()
+        let (sut, remoteLoad) = makeSUT()
         
-        XCTAssertEqual(localAgent.loadCallCount, 0)
-        XCTAssertEqual(localAgent.storeCallCount, 0)
         XCTAssertEqual(remoteLoad.callCount, 0)
         XCTAssertNotNil(sut)
     }
     
     func test_load_shouldCallRemote() {
         
-        let (sut, _, remoteLoad) = makeSUT()
+        let (sut, remoteLoad) = makeSUT()
         let exp = expectation(description: "wait for load completion")
         
         sut.compose().load { _ in exp.fulfill() }
@@ -140,7 +35,7 @@ final class SerialLoaderComposer_extTests: XCTestCase {
     
     func test_load_shouldNotCallRemoteOnSecondLoad() {
         
-        let (sut, _, remoteLoad) = makeSUT()
+        let (sut, remoteLoad) = makeSUT()
         let first = expectation(description: "wait for first load completion")
         
         sut.compose().load { _ in first.fulfill() }
@@ -162,7 +57,7 @@ final class SerialLoaderComposer_extTests: XCTestCase {
     
     func test_reload_shouldCallRemote() {
         
-        let (sut, _, remoteLoad) = makeSUT()
+        let (sut, remoteLoad) = makeSUT()
         let exp = expectation(description: "wait for reload completion")
         
         sut.compose().reload { _ in exp.fulfill() }
@@ -177,7 +72,6 @@ final class SerialLoaderComposer_extTests: XCTestCase {
     
     private typealias Serial = String
     private typealias SUT = SerialComponents.SerialLoaderComposer<Serial, Value, Model>
-    private typealias LocalAgentSpy = ForaBankTests.LocalAgentSpy<SerialStamped<Serial, [Model]>>
     private typealias RemoteLoadSpy = Spy<Serial?, SerialStamped<Serial, [Value]>, Error>
     
     private func makeSUT(
@@ -188,17 +82,11 @@ final class SerialLoaderComposer_extTests: XCTestCase {
         line: UInt = #line
     ) -> (
         sut: SUT,
-        localAgentSpy: LocalAgentSpy,
         remoteLoadSpy: RemoteLoadSpy
     ) {
-        let localAgentSpy = LocalAgentSpy(
-            loadStub: loadStub,
-            storeStub: storeStub,
-            serialStub: serialStub
-        )
         let remoteLoadSpy = RemoteLoadSpy()
         let sut = SUT(
-            localAgent: localAgentSpy,
+            localAgent: localAgent,
             remoteLoad: remoteLoadSpy.process(_:completion:),
             fromModel: { .init(value: $0.value) },
             toModel: { .init(value: $0.value) }
@@ -209,7 +97,7 @@ final class SerialLoaderComposer_extTests: XCTestCase {
         // trackForMemoryLeaks(localAgentSpy, file: file, line: line)
         // trackForMemoryLeaks(remoteLoadSpy, file: file, line: line)
         
-        return (sut, localAgentSpy, remoteLoadSpy)
+        return (sut, remoteLoadSpy)
     }
     
     private struct Value: Equatable {
