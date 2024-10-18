51711be54c2bfdb2da31bb587a5f5b5be23fbad0
Nikolay Pochekuev <pochekuev_n_e@forabank.ru>
2024-10-18 15:23:26 +0400

    add iFora Config

---
+++ ForaBank/ForaBank/CompositionRoot/Configs/PlainClientInformBottomSheetConfig+iFora.swift
@@ -0,0 +1,47 @@
+//
+//  PlainClientInformBottomSheetConfig+iFora.swift
+//  ForaBank
+//
+//  Created by Nikolay Pochekuev on 14.10.2024.
+//
+
+import PlainClientInformBottomSheet
+
+extension PlainClientInformBottomSheetConfig {
+    
+    static let iFora: Self = .init(
+        colors: .init(
+            grayGrabber: .mainColorsGrayMedium,
+            grayBackground: .mainColorsGrayLightest
+        ),
+        strings: .init(
+            titlePlaceholder: "Информация"
+        ),
+        titleConfig: .init(
+            textFont: .textH3Sb18240(),
+            textColor: .textSecondary
+        ),
+        textConfig: .init(
+            textFont: .textH3Sb18240(),
+            textColor: .textSecondary
+        ),
+        sizes: .init(
+            iconSize: 64,
+            rowIconSize: 40,
+            navBarHeight: 59,
+            navBarMaxWidth: 48,
+            grabberWidth: 48,
+            grabberHeight: 5,
+            grabberCornerRadius: 3,
+            spacing: 24
+        ),
+        paddings: .init(
+            topGrabber: 8,
+            topImage: 20,
+            horizontal: 20,
+            vertical: 12
+        ),
+        image: .cardPlaceholder,
+        rowImage: .checkImage
+    )
+}
