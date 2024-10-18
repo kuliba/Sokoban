30866f04b5c19a66e322ef3cd2ba092368a1fa4e
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-10-18 10:57:36 +0300

    TDD `LoggingRemoteNanoServiceComposer.composeSerial`

--- ForaBank/ForaBankTests/CompositionRoot/LoggingRemoteNanoServicesComposer/LoggingRemoteNanoServiceComposer+composeSerial.swift
+++ ForaBank/ForaBankTests/CompositionRoot/LoggingRemoteNanoServicesComposer/LoggingRemoteNanoServiceComposer+composeSerial.swift
@@ -5,9 +5,22 @@
 //  Created by Igor Malyarov on 18.10.2024.
 //
 
+import SerialComponents
+
 extension LoggingRemoteNanoServiceComposer {
     
-    func composeSerial() {}
+    typealias SerialLoadCompletion<T> = (String?, @escaping (SerialStamped<String, T>) -> Void) -> Void
+    typealias SerialLoad<T> = (String?, @escaping SerialLoadCompletion<T>) -> Void
+    
+    func composeSerial<T>(
+        createRequest: @escaping (String?) throws -> URLRequest
+    ) -> SerialLoad<T> {
+        
+        return { serial, completion in
+            
+            _ = try? createRequest(serial)
+        }
+    }
 }
 
 @testable import ForaBank
@@ -24,9 +37,37 @@ final class LoggingRemoteNanoServiceComposer_composeSerial: XCTestCase {
         XCTAssertNotNil(sut)
     }
     
+    func test_composeSerial_shouldCallCreateRequestWithNilOnNilSerial() {
+        
+        let serial: String? = nil
+        let createRequestSpy = CreateRequestSpy(stubs: [anyURLRequest()])
+        let composed: SerialLoad = makeSUT().sut.composeSerial(
+            createRequest: createRequestSpy.call(payload:)
+        )
+        
+        composed(serial) { _,_ in }
+        
+        XCTAssertNoDiff(createRequestSpy.payloads, [serial])
+    }
+    
+    func test_composeSerial_shouldCallCreateRequestWithSerial() {
+        
+        let serial = anyMessage()
+        let createRequestSpy = CreateRequestSpy(stubs: [anyURLRequest()])
+        let composed: SerialLoad = makeSUT().sut.composeSerial(
+            createRequest: createRequestSpy.call(payload:)
+        )
+        
+        composed(serial) { _,_ in }
+        
+        XCTAssertNoDiff(createRequestSpy.payloads, [serial])
+    }
+    
     // MARK: - Helpers
     
     private typealias SUT = LoggingRemoteNanoServiceComposer
+    private typealias SerialLoad = SUT.SerialLoad<Value>
+    private typealias CreateRequestSpy = CallSpy<String?, URLRequest>
     
     private func makeSUT(
         file: StaticString = #file,
@@ -48,5 +89,17 @@ final class LoggingRemoteNanoServiceComposer_composeSerial: XCTestCase {
         trackForMemoryLeaks(loggerSpy, file: file, line: line)
         
         return (sut, httpClientSpy, loggerSpy)
+    }
+    
+    private struct Value: Equatable {
+        
+        let value: String
+    }
+    
+    private func makeValue(
+        _ value: String = anyMessage()
+    ) -> Value {
+        
+        return .init(value: value)
     }
 }
