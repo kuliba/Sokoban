bf3dbc1b1bba66f53dd990e7e08764dc32f42597
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-10-21 15:08:01 +0300

    move `RootViewModelFactory.decorate(decoratee:with:)` to prod

--- ForaBank/ForaBankTests/CompositionRoot/RootViewModelFactory+decorateTests.swift
+++ ForaBank/ForaBankTests/CompositionRoot/RootViewModelFactory+decorateTests.swift
@@ -5,42 +5,6 @@
 //  Created by Igor Malyarov on 21.10.2024.
 //
 
-extension RootViewModelFactory {
-    
-    typealias ServiceCategoryBatchService = ([ServiceCategory], @escaping ([ServiceCategory]) -> Void) -> Void
-    
-    func decorate(
-        decoratee: @escaping Load<[ServiceCategory]>,
-        with decoration: @escaping ServiceCategoryBatchService
-    ) -> Load<[ServiceCategory]> {
-        
-        return { [log = logger.log] completion in
-            
-            decoratee {
-                
-                switch $0 {
-                case .none:
-                    completion(nil)
-                    
-                case .some([]):
-                    completion([])
-                    
-                case let .some(categories):
-                    decoration(categories) { failedCategories in
-                        
-                        if !failedCategories.isEmpty {
-                            
-                            log(.error, .network, "Fail to load categories named: \(failedCategories.map(\.name)).", #fileID, #line)
-                        }
-                    }
-                    
-                    completion(categories)
-                }
-            }
-        }
-    }
-}
-
 @testable import ForaBank
 import XCTest
 
