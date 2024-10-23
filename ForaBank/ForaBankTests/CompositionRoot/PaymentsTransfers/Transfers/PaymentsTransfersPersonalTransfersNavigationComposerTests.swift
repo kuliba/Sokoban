87f5b71ce0ea18c5fd6a9b7a13704d1caec0c2fc
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-10-23 08:35:44 +0300

    TDD `PaymentsTransfersPersonalTransfersNavigationComposer`

--- ForaBank/ForaBankTests/CompositionRoot/PaymentsTransfers/Transfers/PaymentsTransfersPersonalTransfersNavigationComposerTests.swift
+++ ForaBank/ForaBankTests/CompositionRoot/PaymentsTransfers/Transfers/PaymentsTransfersPersonalTransfersNavigationComposerTests.swift
@@ -12,17 +12,20 @@ enum PaymentsTransfersPersonalTransfersDomain {
     enum Navigation {
         
         case contacts(Node<ContactsViewModel>)
+        case meToMe(PaymentsMeToMeViewModel)
     }
 }
 
 struct PaymentsTransfersPersonalTransfersNavigationComposerNanoServices {
-
+    
     let makeContacts: MakeContactsViewModel
+    let makeMeToMe: MakeMeToMe
 }
 
 extension PaymentsTransfersPersonalTransfersNavigationComposerNanoServices{
     
     typealias MakeContactsViewModel = () -> Node<ContactsViewModel>
+    typealias MakeMeToMe = () -> PaymentsMeToMeViewModel?
 }
 
 final class PaymentsTransfersPersonalTransfersNavigationComposer {
@@ -30,7 +33,7 @@ final class PaymentsTransfersPersonalTransfersNavigationComposer {
     private let nanoServices: NanoServices
     
     init(nanoServices: NanoServices) {
-     
+        
         self.nanoServices = nanoServices
     }
     
@@ -41,9 +44,12 @@ extension PaymentsTransfersPersonalTransfersNavigationComposer {
     
     func compose(
         _ element: Domain.Element
-    ) -> Domain.Navigation {
+    ) -> Domain.Navigation? {
         
         switch element {
+        case .betweenSelf:
+            return nanoServices.makeMeToMe().map { .meToMe($0) }
+            
         case .byPhoneNumber:
             return .contacts(nanoServices.makeContacts())
             
@@ -65,7 +71,48 @@ final class PaymentsTransfersPersonalTransfersNavigationComposerTests: XCTestCas
         let (sut, spies) = makeSUT()
         
         XCTAssertEqual(spies.makeContacts.callCount, 0)
+        XCTAssertEqual(spies.makeMeToMe.callCount, 0)
         XCTAssertNotNil(sut)
+    }
+    
+    func test_betweenSelf_shouldCallMakeContacts() {
+        
+        let (sut, spies) = makeSUT()
+        
+        _ = sut.compose(.betweenSelf)
+        
+        XCTAssertEqual(spies.makeMeToMe.callCount, 1)
+    }
+    
+    func test_betweenSelf_shouldDeliverNilOn___() {
+        
+        let (sut, _) = makeSUT()
+        
+        let navigation = sut.compose(.betweenSelf)
+        
+        switch navigation {
+        case .none:
+            break
+            
+        default:
+            XCTFail("Expected nil navigation, got \(String(describing: navigation)) instead.")
+        }
+    }
+    
+    func test_betweenSelf_shouldDeliverMeToMeOn___() throws {
+        
+        let meToMe = try makePaymentsMeToMeViewModel()
+        let (sut, _) = makeSUT(meToMe: meToMe)
+        
+        let navigation = sut.compose(.betweenSelf)
+        
+        switch navigation {
+        case let .meToMe(receivedMeToMe):
+            XCTAssert(meToMe === receivedMeToMe)
+            
+        default:
+            XCTFail("Expected nil navigation, got \(String(describing: navigation)) instead.")
+        }
     }
     
     func test_byPhone_shouldCallMakeContacts() {
@@ -87,6 +134,9 @@ final class PaymentsTransfersPersonalTransfersNavigationComposerTests: XCTestCas
         switch navigation {
         case let .contacts(receivedContacts):
             XCTAssert(contacts === receivedContacts.model)
+            
+        default:
+            XCTFail("Expected contacts, got \(String(describing: navigation)) instead.")
         }
     }
     
@@ -94,14 +144,17 @@ final class PaymentsTransfersPersonalTransfersNavigationComposerTests: XCTestCas
     
     private typealias SUT = PaymentsTransfersPersonalTransfersNavigationComposer
     private typealias MakeContacts = CallSpy<Void, Node<ContactsViewModel>>
+    private typealias MakeMeToMe = CallSpy<Void, PaymentsMeToMeViewModel?>
     
     private struct Spies {
         
         let makeContacts: MakeContacts
+        let makeMeToMe: MakeMeToMe
     }
     
     private func makeSUT(
         contacts: ContactsViewModel? = nil,
+        meToMe: PaymentsMeToMeViewModel? = nil,
         file: StaticString = #file,
         line: UInt = #line
     ) -> (
@@ -113,10 +166,12 @@ final class PaymentsTransfersPersonalTransfersNavigationComposerTests: XCTestCas
                 makeContactsViewModelNode(
                     contacts ?? makeContactsViewModel()
                 )
-            ])
+            ]),
+            makeMeToMe: .init(stubs: [meToMe])
         )
         let sut = SUT(nanoServices: .init(
-            makeContacts: spies.makeContacts.call
+            makeContacts: spies.makeContacts.call,
+            makeMeToMe: spies.makeMeToMe.call
         ))
         
         trackForMemoryLeaks(sut, file: file, line: line)
@@ -134,5 +189,30 @@ final class PaymentsTransfersPersonalTransfersNavigationComposerTests: XCTestCas
     ) -> Node<ContactsViewModel> {
         
         return .init(model: model, cancellables: [])
+    }
+    
+    private func makePaymentsMeToMeViewModel(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) throws -> PaymentsMeToMeViewModel {
+        
+        let model: Model = .mockWithEmptyExcept()
+        try model.addMeToMeProduct(file: file, line: line)
+        
+        return try XCTUnwrap(PaymentsMeToMeViewModel(model, mode: .demandDeposit), "Expected PaymentsMeToMeViewModel, got nil instead.", file: file, line: line)
+    }
+}
+
+// MARK: - DSL
+
+extension Model {
+    
+    func addMeToMeProduct(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) throws {
+        
+        products.value.append(element: .accountActiveRub, toValueOfKey: .account)
+        _ = try XCTUnwrap(PaymentsMeToMeViewModel(self, mode: .demandDeposit), "Fail to add product for MeToMe (Expected PaymentsMeToMeViewModel, got nil instead).", file: file, line: line)
     }
 }
