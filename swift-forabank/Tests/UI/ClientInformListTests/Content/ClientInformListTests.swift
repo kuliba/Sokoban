db743d5a0d4d2ed097794ab49519327d466d24ef
Nikolay Pochekuev <pochekuev_n_e@forabank.ru>
2024-10-24 16:23:25 +0400

    delete merge duplicates

--- swift-forabank/Tests/UI/ClientInformListTests/Content/ClientInformListTests.swift
+++ swift-forabank/Tests/UI/ClientInformListTests/Content/ClientInformListTests.swift
@@ -5,9 +5,5 @@
 //  Created by Nikolay Pochekuev on 04.10.2024.
 //
 
-<<<<<<<< Updated upstream:swift-forabank/Tests/UI/ClientInformListTests/Content/ClientInformListTests.swift
 import ClientInformList
-========
-import СlientInformList
->>>>>>>> Stashed changes:swift-forabank/Tests/UI/СlientInformListTests/Content/СlientInformListTests.swift
 import XCTest
