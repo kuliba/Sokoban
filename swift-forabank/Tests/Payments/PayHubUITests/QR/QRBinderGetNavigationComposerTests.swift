f290a612d703a487f056059f979ebeec32339f2d
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-10-29 08:51:50 +0300

    TDD QRBinderGetNavigationComposerTests

---
+++ swift-forabank/Tests/Payments/PayHubUITests/QR/QRBinderGetNavigationComposerTests.swift
@@ -0,0 +1,188 @@
+//
+//  QRBinderGetNavigationComposerTests.swift
+//
+//
+//  Created by Igor Malyarov on 29.10.2024.
+//
+
+import PayHub
+
+struct QRBinderGetNavigationComposerMicroServices<Payments> {
+    
+    let makePayments: MakePayments
+}
+
+extension QRBinderGetNavigationComposerMicroServices {
+    
+    typealias MakePayments = (MakePaymentsPayload) -> Payments
+
+    enum MakePaymentsPayload: Equatable {
+        
+        case c2bSubscribe(URL)
+    }
+}
+
+final class QRBinderGetNavigationComposer<Operator, Provider, Payments, QRCode, QRMapping, Source> {
+    
+    private let microServices: MicroServices
+    
+    init(microServices: MicroServices) {
+        self.microServices = microServices
+    }
+    
+    typealias MicroServices = QRBinderGetNavigationComposerMicroServices<Payments>
+}
+
+extension QRBinderGetNavigationComposer {
+    
+    func getNavigation(
+        qrResult: QRResult,
+        completion: @escaping (QRNavigation) -> Void
+    ) {
+        switch qrResult {
+        case let .c2bSubscribeURL(url):
+            _ = microServices.makePayments(.c2bSubscribe(url))
+            
+        default:
+            fatalError()
+        }
+    }
+    
+    typealias QRResult = QRModelResult<Operator, Provider, QRCode, QRMapping, Source>
+    
+    enum QRNavigation {
+        
+        case payments(Payments)
+    }
+}
+
+import XCTest
+
+final class QRBinderGetNavigationComposerTests: XCTestCase {
+    
+    // MARK: - init
+    
+    func test_init_shouldNotCallCollaborators() {
+        
+        let (sut, spies) = makeSUT()
+        
+        XCTAssertEqual(spies.makePayments.callCount, 0)
+        XCTAssertNotNil(sut)
+    }
+    
+    // MARK: - c2bSubscribeURL
+    
+    func test_getNavigation_shouldCallMakePaymentsOnC2BSubscribeURL() {
+        
+        let url = anyURL()
+        let (sut, spies) = makeSUT()
+        
+        sut.getNavigation(qrResult: .c2bSubscribeURL(url))
+        
+        XCTAssertNoDiff(spies.makePayments.payloads, [.c2bSubscribe(url)])
+    }
+    
+    // MARK: - Helpers
+    
+    private typealias SUT = QRBinderGetNavigationComposer<Operator, Provider, Payments, QRCode, QRMapping, Source>
+    private typealias MakePaymentsPayload = SUT.MicroServices.MakePaymentsPayload
+    private typealias MakePayments = CallSpy<MakePaymentsPayload, Payments>
+    
+    private struct Spies {
+        
+        let makePayments: MakePayments
+    }
+    
+    private func makeSUT(
+        payments: Payments = .init(),
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (
+        sut: SUT,
+        spies: Spies
+    ) {
+        let spies = Spies(
+            makePayments: .init(stubs: [payments])
+        )
+        let sut = SUT(microServices: .init(
+            makePayments: spies.makePayments.call
+        ))
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, spies)
+    }
+    
+    private struct Operator: Equatable {
+        
+        let value: String
+    }
+    
+    private func makeOperator(
+        _ value: String = anyMessage()
+    ) -> Operator {
+        
+        return .init(value: value)
+    }
+    
+    private struct Provider: Equatable {
+        
+        let value: String
+    }
+    
+    private func makeProvider(
+        _ value: String = anyMessage()
+    ) -> Provider {
+        
+        return .init(value: value)
+    }
+    
+    private struct QRCode: Equatable {
+        
+        let value: String
+    }
+    
+    private func makeQRCode(
+        _ value: String = anyMessage()
+    ) -> QRCode {
+        
+        return .init(value: value)
+    }
+    
+    private struct QRMapping: Equatable {
+        
+        let value: String
+    }
+    
+    private func makeQRMapping(
+        _ value: String = anyMessage()
+    ) -> QRMapping {
+        
+        return .init(value: value)
+    }
+    
+    private struct Source: Equatable {
+        
+        let value: String
+    }
+    
+    private func makeSource(
+        _ value: String = anyMessage()
+    ) -> Source {
+        
+        return .init(value: value)
+    }
+    
+    private final class Payments {}
+}
+
+// MARK: - DSL
+
+private extension QRBinderGetNavigationComposer {
+    
+    func getNavigation(
+        qrResult: QRResult
+    ) {
+        self.getNavigation(qrResult: qrResult, completion: { _ in })
+    }
+}
