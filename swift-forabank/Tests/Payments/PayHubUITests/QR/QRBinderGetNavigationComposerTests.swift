6203ebab0e492a601141c88da6e4454e1c37c504
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-10-29 15:50:18 +0300

    move `QRBinderGetNavigationComposer`, 11QRNavigation` to prod

--- swift-forabank/Tests/Payments/PayHubUITests/QR/QRBinderGetNavigationComposerTests.swift
+++ swift-forabank/Tests/Payments/PayHubUITests/QR/QRBinderGetNavigationComposerTests.swift
@@ -5,63 +5,6 @@
 //  Created by Igor Malyarov on 29.10.2024.
 //
 
-import PayHub
-
-struct QRBinderGetNavigationComposerMicroServices<Payments> {
-    
-    let makePayments: MakePayments
-}
-
-extension QRBinderGetNavigationComposerMicroServices {
-    
-    typealias MakePayments = (MakePaymentsPayload) -> Payments
-    
-    enum MakePaymentsPayload: Equatable {
-        
-        case c2bSubscribe(URL)
-    }
-}
-
-final class QRBinderGetNavigationComposer<Operator, Provider, Payments, QRCode, QRMapping, Source> {
-    
-    private let microServices: MicroServices
-    
-    init(microServices: MicroServices) {
-        self.microServices = microServices
-    }
-    
-    typealias MicroServices = QRBinderGetNavigationComposerMicroServices<Payments>
-}
-
-extension QRBinderGetNavigationComposer {
-    
-    func getNavigation(
-        qrResult: QRResult,
-        notify: @escaping Notify,
-        completion: @escaping (Navigation) -> Void
-    ) {
-        switch qrResult {
-        case let .c2bSubscribeURL(url):
-            let payments = microServices.makePayments(.c2bSubscribe(url))
-            completion(.payments(payments))
-            
-        default:
-            fatalError()
-        }
-    }
-    
-    typealias FlowDomain = PayHubUI.FlowDomain<QRResult, Navigation>
-    typealias Notify = (FlowDomain.NotifyEvent) -> Void
-    
-    typealias QRResult = QRModelResult<Operator, Provider, QRCode, QRMapping, Source>
-    typealias Navigation = QRNavigation<Payments>
-}
-
-enum QRNavigation<Payments> {
-    
-    case payments(Payments)
-}
-
 import PayHubUI
 import XCTest
 
