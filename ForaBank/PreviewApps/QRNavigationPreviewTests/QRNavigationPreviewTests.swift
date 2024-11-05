9749ed64d1a8837970fb1bd85d36ae25f287a68d
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-11-05 20:46:43 +0300

    add scaffolding for `QRNavigationPreviewTests`

---
+++ ForaBank/PreviewApps/QRNavigationPreviewTests/QRNavigationPreviewTests.swift
@@ -0,0 +1,15 @@
+//
+//  QRNavigationPreviewTests.swift
+//  QRNavigationPreviewTests
+//
+//  Created by Igor Malyarov on 05.11.2024.
+//
+
+@testable import QRNavigationPreview
+import XCTest
+
+final class QRNavigationPreviewTests: XCTestCase {
+
+    // func test() { XCTFail("unimplemented") }
+
+}
