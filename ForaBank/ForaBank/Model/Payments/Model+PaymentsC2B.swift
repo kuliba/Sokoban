f12df2378611d0629f153dfea1bb9553af2c1481
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-11-11 08:02:25 +0300

    extract `makeButtons`

--- ForaBank/ForaBank/Model/Payments/Model+PaymentsC2B.swift
+++ ForaBank/ForaBank/Model/Payments/Model+PaymentsC2B.swift
@@ -45,6 +45,35 @@ extension Model {
         return (parameters, visible)
     }
     
+    func makeButtons(
+        _ responseParameters: [PaymentsParameterRepresentable],
+        _ precondition: Payments.ParameterSubscribe.Button.Precondition?
+    ) -> Payments.ParameterSubscribe {
+        if responseParameters.contains(where: { $0.value == "Такая привязка счета уже существует!" }) {
+            
+            let subscribeParameter = Payments.ParameterSubscribe(
+                buttons: [
+                    .init(title: "Сохранить", style: .secondary, action: .confirm, precondition: precondition),
+                    .init(title: "На главный", style: .primary, action: .main, precondition: nil)
+                ],
+                icon: "ic72Sbp"
+            )
+            
+            return subscribeParameter
+            
+        } else {
+            let subscribeParameter = Payments.ParameterSubscribe(
+                buttons: [
+                    .init(title: "Привязать счет", style: .primary, action: .confirm, precondition: precondition),
+                    .init(title: "Пока нет", style: .secondary, action: .deny, precondition: nil)
+                ],
+                icon: "ic72Sbp"
+            )
+            
+            return subscribeParameter
+        }
+    }
+    
     func paymentsStepC2B(
         _ operation: Payments.Operation,
         for stepIndex: Int
@@ -94,28 +123,7 @@ extension Model {
                 // subscribe
                 let precondition = response.parameters.precondition
                 
-                if responseParameters.contains(where: { $0.value == "Такая привязка счета уже существует!" }) {
-                    
-                    let subscribeParameter = Payments.ParameterSubscribe(
-                        buttons: [
-                            .init(title: "Сохранить", style: .secondary, action: .confirm, precondition: precondition),
-                            .init(title: "На главный", style: .primary, action: .main, precondition: nil)
-                        ],
-                        icon: "ic72Sbp"
-                    )
-                    
-                    parameters.append(subscribeParameter)
-                    
-                } else {
-                    let subscribeParameter = Payments.ParameterSubscribe(
-                        buttons: [
-                            .init(title: "Привязать счет", style: .primary, action: .confirm, precondition: precondition),
-                            .init(title: "Пока нет", style: .secondary, action: .deny, precondition: nil)
-                        ],
-                        icon: "ic72Sbp"
-                    )
-                    parameters.append(subscribeParameter)
-                }
+                parameters.append(makeButtons(responseParameters, precondition))
                                 
                 let visible = parameters.map(\.id)
                 
