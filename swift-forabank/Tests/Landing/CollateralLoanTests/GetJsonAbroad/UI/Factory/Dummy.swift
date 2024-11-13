a4b88809daa2dd19dba9796e49872598489e4ad8
Valentin Ozerov <ozerov_v_v@forabank.ru>
2024-11-13 20:58:01 +0300

    Added testplan and scheme

---
+++ swift-forabank/Tests/Landing/CollateralLoanTests/GetJsonAbroad/UI/Factory/Dummy.swift
@@ -0,0 +1,16 @@
+//
+//  Dummy.swift
+//
+//
+//  Created by Valentin Ozerov on 13.11.2024.
+//
+
+import XCTest
+
+final class DummyTest: XCTest {
+    
+    func test_test() {
+        
+        XCTAssertTrue(true)
+    }
+}
