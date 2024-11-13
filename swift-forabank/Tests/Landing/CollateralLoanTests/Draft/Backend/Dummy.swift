fdb7f6df77899dd55b2a6a4dd8f751b5968cd95d
Valentin Ozerov <ozerov_v_v@forabank.ru>
2024-11-13 21:03:21 +0300

    Fix testplans

---
+++ swift-forabank/Tests/Landing/CollateralLoanTests/Draft/Backend/Dummy.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Valentin Ozerov on 13.11.2024.
+//
+
+import XCTest
+
+final class DummyTest: XCTest {
+    
+    func test_test() {
+        
+        XCTAssertTrue(true)
+    }
+}
