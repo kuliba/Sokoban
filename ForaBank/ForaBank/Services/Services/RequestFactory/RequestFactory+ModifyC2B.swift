f08f2f8b92db47df2ef2cc8d80b83e7725ca4532
Dmitriy Savushkin <da.savushkin4@gmail.com>
2024-11-14 12:38:54 +0300

    add service `modifyC2BDataRequest`

--- ForaBank/ForaBank/Services/Services/RequestFactory/RequestFactory+ModifyC2B.swift
+++ ForaBank/ForaBank/Services/Services/RequestFactory/RequestFactory+ModifyC2B.swift
@@ -7,49 +7,20 @@
 
 import Foundation
 import RemoteServices
+import ModifyC2BSubscriptionService
 
 extension RequestFactory {
     
     static func modifyC2BDataRequest(
-        _ modifyC2BSubscription: ModifyC2BSubscription
+        payload: ModifyC2BSubscription
     ) throws -> URLRequest {
-
-        let base = Config.serverAgentEnvironment.baseURL
-        let endpoint = modifyC2BSubscription.productType == .card ? Services.Endpoint.modifyC2BSubCardData : Services.Endpoint.modifyC2BSubAccData
-        let url = try! endpoint.url(withBase: base)
         
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.httpBody = modifyC2BSubscription.json
+        let base = Config.serverAgentEnvironment.baseURL
+        let endpoint: Services.Endpoint = payload.productType == .card ? .modifyC2BSubCardData : .modifyC2BSubAccData
+        let endpointURL = try! endpoint.url(withBase: base)
+        
+        var request = try RemoteServices.RequestFactory.modifyC2BDataRequest(url: endpointURL, payload)
         request.cachePolicy = .reloadIgnoringLocalAndRemoteCacheData
         return request
-    }
-}
-
-public struct ModifyC2BSubscription {
-    
-    let productId: Int
-    let productType: ProductType
-    let subscriptionToken: String
-    
-    init(
-        productId: Int,
-        productType: ProductType,
-        subscriptionToken: String
-    ) {
-        self.productId = productId
-        self.productType = productType
-        self.subscriptionToken = subscriptionToken
-    }
-}
-
-private extension ModifyC2BSubscription {
-    
-    var json: Data? {
-        
-        try? JSONSerialization.data(withJSONObject: [
-            "subscriptionToken": self.subscriptionToken,
-            "productId": self.productId
-        ] as [String: Any])
     }
 }
