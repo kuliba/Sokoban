0357444bf45a7deba8fc28590ddbc1f0be6f9017
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-11-15 16:36:48 +0300

    add preview app

---
+++ ForaBank/CollateralLoanLandingPreview/CollateralLoanLandingPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollateralLoanLandingPreviewApp.swift
+//  CollateralLoanLandingPreview
+//
+//  Created by Valentin Ozerov on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CollateralLoanLandingPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
