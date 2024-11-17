0e37f2436fb15f0028c408a35a607fb99d983451
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-11-17 15:29:32 +0300

    improve test helper

--- ForaBank/ForaBankTests/TestHelpers/ModelRootFactory+immediateEmpty.swift
+++ ForaBank/ForaBankTests/TestHelpers/ModelRootFactory+immediateEmpty.swift
@@ -6,6 +6,7 @@
 //
 
 @testable import ForaBank
+import PayHubUI
 
 extension ModelRootFactory {
     
@@ -16,6 +17,21 @@ extension ModelRootFactory {
             logger: LoggerSpy(),
             model: .mockWithEmptyExcept(),
             schedulers: .immediate
+        )
+    }
+    
+    static func test(
+        httpClientFactory: any HTTPClientFactory = HTTPClientFactorySpy(),
+        logger: any LoggerAgentProtocol = LoggerSpy(),
+        model: Model = .mockWithEmptyExcept(),
+        schedulers: Schedulers
+    ) -> ModelRootFactory {
+        
+        return .init(
+            httpClientFactory: httpClientFactory, 
+            logger: logger,
+            model: model,
+            schedulers: schedulers
         )
     }
 }
