fcd1a594842154662d92319e3c412910b05ae076
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-11-17 08:03:57 +0300

    simplify `SceneDelegate.window` setter

--- ForaBank/ForaBank/SceneDelegate.swift
+++ ForaBank/ForaBank/SceneDelegate.swift
@@ -35,8 +35,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
+        window = .init(windowScene: windowScene)
         
         configureWindow()
         
