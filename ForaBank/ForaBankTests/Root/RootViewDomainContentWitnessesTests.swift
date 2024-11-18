0f1809b38bb9632c5081d44c84a9f4590780108c
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-11-18 12:02:37 +0300

    extract `RootViewDomain.ContentWitnesses.init(isFlagActive:)` to prod

--- ForaBank/ForaBankTests/Root/RootViewDomainContentWitnessesTests.swift
+++ ForaBank/ForaBankTests/Root/RootViewDomainContentWitnessesTests.swift
@@ -5,26 +5,6 @@
 //  Created by Igor Malyarov on 18.11.2024.
 //
 
-import Combine
-
-extension RootViewDomain.ContentWitnesses {
-    
-    init(isFlagActive: Bool) {
-        
-        self.init(
-            emitting: {
-            
-                if isFlagActive {
-                    $0.rootEventPublisher
-                } else {
-                    Empty().eraseToAnyPublisher()
-                }
-            },
-            receiving: { _ in {}}
-        )
-    }
-}
-
 @testable import ForaBank
 import XCTest
 
