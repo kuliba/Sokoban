fcf39f70e9497209de5c02c32c4497facb7f8432
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-11-22 09:03:37 +0300

    move `PaymentsTransfersPersonalToolbarDomain` to app target

--- ForaBank/ForaBank/View/PaymentsTransfers/v1/PaymentsTransfers/Personal/Toolbar/PaymentsTransfersPersonalToolbarDomain.swift
+++ ForaBank/ForaBank/View/PaymentsTransfers/v1/PaymentsTransfers/Personal/Toolbar/PaymentsTransfersPersonalToolbarDomain.swift
@@ -5,6 +5,35 @@
 //  Created by Igor Malyarov on 22.11.2024.
 //
 
+import PayHub
 import PayHubUI
 
-typealias PaymentsTransfersPersonalToolbarDomain = PayHubUI.PaymentsTransfersPersonalToolbarDomain<ProfileModelStub, QRModelStub>
+/// A namespace.
+enum PaymentsTransfersPersonalToolbarDomain {}
+
+extension PaymentsTransfersPersonalToolbarDomain {
+    
+    // MARK: - Binder
+    typealias Binder = PayHub.Binder<Content, Flow>
+    typealias Composer = BinderComposer<Content, Select, Navigation>
+    
+    // MARK: - Content
+    
+    typealias Content = PaymentsTransfersPersonalToolbarContent
+    
+    // MARK: - Flow
+    
+    typealias FlowDomain = PayHubUI.FlowDomain<Select, Navigation>
+    typealias Flow = FlowDomain.Flow
+    
+    enum Select {
+        
+        case profile, qr
+    }
+    
+    enum Navigation {
+        
+        case profile(ProfileModelStub)
+        case qr(QRModelStub)
+    }
+}
