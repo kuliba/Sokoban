c6e73aa3a9885fd861f8e37ee2adba7e3480cf30
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-11-28 13:23:04 +0300

    add copies of types to WIPTests

---
+++ swift-forabank/Tests/WIPTests/ClientInformAlerts/ClientInformAlerts+ext.swift
@@ -0,0 +1,58 @@
+//
+//  ClientInformAlerts+ext.swift
+//  ForaBank
+//
+//  Created by Nikolay Pochekuev on 06.11.2024.
+//
+
+import Foundation
+import SwiftUI
+
+enum ClientInformActionType: String {
+    
+    case required
+    case optional
+    
+    init(updateType: String) {
+        
+        self = ClientInformActionType(rawValue: updateType) ?? .required
+    }
+}
+
+enum AlertType: Identifiable {
+    
+    var id: UUID {
+        switch self {
+        case .clientInformAlerts(let alert): return alert.id
+        case .alertViewModel(let alert): return alert.id
+        }
+    }
+    
+    case clientInformAlerts(ClientInformAlerts.Alert)
+    case alertViewModel(Alert.ViewModel)
+}
+
+extension ClientInformAlerts {
+
+    mutating func next() {
+            
+        if informAlerts.isEmpty {
+
+            if let updateAlert {
+                
+                self.updateAlert = .init(
+                    id: .init(), // Restore with different ID
+                    title: updateAlert.title,
+                    text: updateAlert.text,
+                    link: updateAlert.link,
+                    version: updateAlert.version,
+                    actionType: updateAlert.actionType
+                )
+            }
+        } else {
+            informAlerts = .init(
+                informAlerts.dropFirst()
+            )
+        }
+    }
+}
