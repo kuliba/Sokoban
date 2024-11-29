46f5f1954703ad796e71074e001e747817a48a21
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-11-29 11:19:37 +0300

    extract `RootViewFactory.makePaymentsTransfersToolbarView(toolbar:)`

--- ForaBank/ForaBank/View/Root/RootView.swift
+++ ForaBank/ForaBank/View/Root/RootView.swift
@@ -309,7 +309,7 @@ private extension RootView {
                         factory: .init(
                             makeCategoryPickerView: rootViewFactory.makeCategoryPickerSectionView,
                             makeOperationPickerView: rootViewFactory.makeOperationPickerView,
-                            makeToolbarView: makePaymentsTransfersToolbarView,
+                            makeToolbarView: rootViewFactory.makePaymentsTransfersToolbarView,
                             makeTransfersView: makePaymentsTransfersTransfersView
                         ),
                         config: .iFora
@@ -322,61 +322,6 @@ private extension RootView {
                 makeDestinationView: { _ in
                 
                     Text("TBD: DestinationView")
-                }
-            )
-        )
-    }
-    
-    @ViewBuilder
-    func makePaymentsTransfersToolbarView(
-        toolbar: PayHubUI.PaymentsTransfersPersonalToolbar
-    ) -> some View {
-        
-        if let binder = toolbar.toolbarBinder {
-            
-            makePaymentsTransfersToolbarView(binder: binder)
-            
-        } else {
-            
-            Text("Unexpected toolbar type \(String(describing: toolbar))")
-                .foregroundColor(.red)
-        }
-    }
-    
-    func makePaymentsTransfersToolbarView(
-        binder: PaymentsTransfersPersonalToolbarDomain.Binder
-    ) -> some View {
-        
-        ComposedPaymentsTransfersPersonalToolbarView(
-            binder: binder,
-            factory: .init(
-                makeDestinationView: {
-                    
-                    switch $0 {
-                    case let .profile(profileModel):
-                        Text(String(describing: profileModel))
-                    }
-                },
-                makeFullScreenView: {
-                    
-                    switch $0 {
-                    case let .qr(qrModel):
-                        VStack(spacing: 32) {
-                            
-                            Text(String(describing: qrModel))
-                        }
-                    }
-                },
-                makeProfileLabel: {
-                    
-                    HStack {
-                        Image(systemName: "person.circle")
-                        Text("Profile")
-                    }
-                },
-                makeQRLabel: {
-                    
-                    Image(systemName: "qrcode")
                 }
             )
         )
