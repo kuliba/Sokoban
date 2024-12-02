2f3f51404625bb4c9aa3ae73c294f80788a1e433
Андрюшина Наталья Юрьевна <andryushina_n_y@forabank.ru>
2024-12-02 13:14:38 +0000

    DBSNEW-13040 add `SavingsAccountDetails`

---
+++ swift-forabank/Sources/UI/SavingsAccount/Config/SavingsAccountDetailsConfig.swift
@@ -0,0 +1,50 @@
+//
+//  SavingsAccountDetailsConfig.swift
+//
+//
+//  Created by Andryusina Nataly on 02.12.2024.
+//
+
+import SharedConfigs
+import SwiftUI
+
+struct SavingsAccountDetailsConfig: Equatable {
+    
+    let chevronDown: Image
+    let colors: Colors
+    let cornerRadius: CGFloat
+    let days: TextConfig
+    let header: TextWithConfig
+    let heights: Heights
+    let info: Image
+    let interestDate: TextConfig
+    let padding: CGFloat
+    let period: TextConfig
+    let progressColors: [Color]
+    let texts: Texts
+    
+    struct Heights: Equatable {
+        
+        let big: CGFloat
+        let header: CGFloat
+        let interest: CGFloat
+        let period: CGFloat
+        let progress: CGFloat
+        let small: CGFloat
+    }
+    
+    struct Colors: Equatable {
+        
+        let background: Color
+        let chevron: Color
+        let progress: Color
+        let shimmering: Color
+    }
+    
+    struct Texts: Equatable {
+        
+        let days: String
+        let interestDate: String
+        let period: String
+    }
+}
