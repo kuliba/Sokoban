29d2961f28375526227404fe0e73255ba115dfea
Tasha Andryushina <natalykn@gmail.com>
2024-12-04 12:54:30 +0300

    update `FlowEffectHandler`

--- swift-forabank/Sources/UI/SavingsAccount/Flow/Logic/FlowEffectHandler.swift
+++ swift-forabank/Sources/UI/SavingsAccount/Flow/Logic/FlowEffectHandler.swift
@@ -9,3 +9,22 @@ public final class FlowEffectHandler<Destination, InformerPayload> {
         
     public init() {}
 }
+
+public extension FlowEffectHandler {
+    
+    func handleEffect(
+        _ effect: Effect,
+        _ dispatch: @escaping Dispatch
+    ) {
+        switch effect {
+        }
+    }
+}
+
+public extension FlowEffectHandler {
+    
+    typealias Dispatch = (Event) -> Void
+    
+    typealias Event = FlowEvent<Destination, InformerPayload>
+    typealias Effect = FlowEffect
+}
