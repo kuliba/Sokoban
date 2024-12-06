5d2d85e5926281716c69181bd095423e26a5d6cc
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-12-06 17:24:01 +0300

    add more to `RootViewModelFactory_loadOperatorsTests`

--- ForaBank/ForaBankTests/CompositionRoot/RootViewModelFactory/RootViewModelFactory+loadOperatorsTests.swift
+++ ForaBank/ForaBankTests/CompositionRoot/RootViewModelFactory/RootViewModelFactory+loadOperatorsTests.swift
@@ -32,10 +32,58 @@ final class RootViewModelFactory_loadOperatorsTests: RootViewModelFactoryTests {
         expect(withLoadStub: nil, payload: makePayload(), toDeliver: [])
     }
         
+    func test_loadOperators_shouldDeliverEmptyOnEmptyValue() {
+        
+        expect(withLoadStub: [], payload: makePayload(), toDeliver: [])
+    }
+        
+    func test_loadOperators_shouldDeliverEmptyOnNonMatchingCategoryType() {
+        
+        let (_, model) = makeOperatorWithModel(type: .charity)
+        let (sut, _) = makeSUT(loadStub: [model])
+        
+        expect(
+            sut: sut, 
+            withLoadStub: [model],
+            payload: makePayload(for: .digitalWallets),
+            toDeliver: []
+        )
+    }
+        
+    func test_loadOperators_shouldDeliverOneOnValueOfOneForMatchingCategoryType() {
+        
+        let categoryType: CategoryType = .networkMarketing
+        let (`operator`, model) = makeOperatorWithModel(type: categoryType)
+        let (sut, _) = makeSUT(loadStub: [model])
+        
+        expect(
+            sut: sut, 
+            withLoadStub: [model],
+            payload: makePayload(for: categoryType),
+            toDeliver: [`operator`]
+        )
+    }
+        
+    func test_loadOperators_shouldDeliverTwoOnValueOfOneForMatchingCategoryType() {
+        
+        let categoryType: CategoryType = .socialAndGames
+        let (operator1, model1) = makeOperatorWithModel(type: categoryType)
+        let (operator2, model2) = makeOperatorWithModel(type: categoryType)
+        let (sut, _) = makeSUT(loadStub: [model1, model2])
+        
+        expect(
+            sut: sut, 
+            withLoadStub: [model1, model2],
+            payload: makePayload(for: categoryType),
+            toDeliver: [operator1, operator2]
+        )
+    }
+        
     // MARK: - Helpers
     
     private typealias LocalAgent = LocalAgentSpy<[CodableServicePaymentOperator]>
     private typealias Payload = UtilityPrepaymentNanoServices<PaymentServiceOperator>.LoadOperatorsPayload
+    private typealias CategoryType = ServiceCategory.CategoryType
     
     private func makeSUT(
         loadStub: [CodableServicePaymentOperator]? = nil,
@@ -67,6 +115,35 @@ final class RootViewModelFactory_loadOperatorsTests: RootViewModelFactoryTests {
         return .init(afterOperatorID: id, for: type, searchText: searchText, pageSize: pageSize)
     }
     
+    private func makePaymentServiceOperator(
+        id: String = anyMessage(),
+        inn: String = anyMessage(),
+        md5Hash: String? = anyMessage(),
+        name: String = anyMessage(),
+        type: String = anyMessage()
+    ) -> PaymentServiceOperator {
+        
+        return .init(id: id, inn: inn, md5Hash: md5Hash, name: name, type: type)
+    }
+    
+    private func codable(
+        _ `operator`: PaymentServiceOperator,
+        sortedOrder: Int = .random(in: 1...100)
+    ) -> CodableServicePaymentOperator {
+        
+        return .init(id: `operator`.id, inn: `operator`.inn, md5Hash: `operator`.md5Hash, name: `operator`.name, type: `operator`.type, sortedOrder: sortedOrder)
+    }
+    
+    private func makeOperatorWithModel(
+        type: ServiceCategory.CategoryType = .repaymentLoansAndAccounts,
+        sortedOrder: Int = .random(in: 1...100)
+    ) -> (PaymentServiceOperator, CodableServicePaymentOperator) {
+        
+        let `operator` = makePaymentServiceOperator(type: type.name)
+        
+        return (`operator`, codable(`operator`, sortedOrder: sortedOrder))
+    }
+    
     private func expect(
         sut: SUT? = nil,
         withLoadStub loadStub: [CodableServicePaymentOperator]?,
@@ -79,7 +156,7 @@ final class RootViewModelFactory_loadOperatorsTests: RootViewModelFactoryTests {
         let sut = sut ?? makeSUT(loadStub: loadStub).sut
         let exp = expectation(description: "wait for load completion")
         
-        sut.loadOperators(payload: makePayload()) {
+        sut.loadOperators(payload: payload) {
             
             XCTAssertNoDiff($0, expectedValue, "Expected \(String(describing: expectedValue)), but got \(String(describing: $0)) instead.", file: file, line: line)
             
