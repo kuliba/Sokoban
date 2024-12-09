adbaeeb7978290dcbf439c6b99b65da968df5ec8
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-12-09 16:20:33 +0300

    add `ServiceCategoryFailureDomain`

---
+++ ForaBank/ForaBank/View/ServiceCategoryFailure/ServiceCategoryFailureDomain.swift
@@ -0,0 +1,39 @@
+//
+//  ServiceCategoryFailureDomain.swift
+//  ForaBank
+//
+//  Created by Igor Malyarov on 09.12.2024.
+//
+
+import PayHub
+import PayHubUI
+
+/// A namespace.
+enum ServiceCategoryFailureDomain {}
+
+extension ServiceCategoryFailureDomain {
+    
+    // MARK: - Binder
+    
+    typealias Binder = PayHub.Binder<Content, Flow>
+    typealias BinderComposer = PayHubUI.BinderComposer
+    
+    // MARK: - Content
+    
+    typealias Content = ()
+    
+    // MARK: - Flow
+    
+    typealias FlowDomain = PayHubUI.FlowDomain<Select, Navigation>
+    typealias Flow = FlowDomain.Flow
+    
+    enum Select: Equatable {
+        
+        case detailPayment
+    }
+    
+    enum Navigation {
+        
+        case detailPayment(PaymentsViewModel)
+    }
+}
