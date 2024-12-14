9411c0e0f7d54e70c8ea8e1338ceb1ea4ce19ce7
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-12-14 07:50:21 +0300

    add `BinderComposerPreview` app target

---
+++ ForaBank/PreviewApps/BinderComposerPreview/BinderComposerPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinderComposerPreviewApp.swift
+//  BinderComposerPreview
+//
+//  Created by Igor Malyarov on 14.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct BinderComposerPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ForaBank/PreviewApps/BinderComposerPreview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BinderComposerPreview
+//
+//  Created by Igor Malyarov on 14.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        Text("Hello, world!")
+    }
+}
+
+#Preview {
+    
+    ContentView()
+}
