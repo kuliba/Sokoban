1b399a982f862486022f0b4efd2b8e15475c928c
Tasha Andryushina <natalykn@gmail.com>
2024-12-22 15:04:25 +0300

    refactoring

--- Vortex/Vortex/Extensions/String+Extensions.swift
+++ Vortex/Vortex/Extensions/String+Extensions.swift
@@ -310,18 +310,8 @@ extension String {
 }
 
 extension String {
-    var lowercasingFirst: String { prefix(1).lowercased() + dropFirst() }
-    var uppercasingFirst: String { prefix(1).uppercased() + dropFirst() }
 
     var camelCased: String {
-        
-        guard !isEmpty else { return "" }
-        
-        let parts = components(separatedBy: .alphanumerics.inverted)
-        let first = parts.first!.lowercasingFirst
-        let rest = parts.dropFirst().map { $0.uppercasingFirst }
-
-       // return ([first] + rest).joined()
         
         let result = self
             .split(separator: "_")  // split to components
