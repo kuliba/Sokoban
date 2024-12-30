d348ffa35836ba6f70599806b5b703f2f7e9030d
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-12-30 10:14:53 +0300

    add `BinderComposerPreview` app target with tests

---
+++ Vortex/BinderComposerPreview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BinderComposerPreview
+//
+//  Created by Igor Malyarov on 30.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vortex/BinderComposerPreviewTests/BinderComposerPreviewTests.swift
@@ -0,0 +1,14 @@
+//
+//  BinderComposerPreviewTests.swift
+//  BinderComposerPreviewTests
+//
+//  Created by Igor Malyarov on 30.12.2024.
+//
+
+@testable import BinderComposerPreview
+import XCTest
+
+final class BinderComposerPreviewTests: XCTestCase {
+
+    
+}
