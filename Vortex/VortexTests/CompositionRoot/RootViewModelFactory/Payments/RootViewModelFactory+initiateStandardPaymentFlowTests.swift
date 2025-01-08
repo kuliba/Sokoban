454145cd5bbfb795bc5dca6c4b558dac983002f3
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-01-08 09:10:47 +0300

    add initial `RootViewModelFactory_initiateStandardPaymentFlowTests`

---
+++ Vortex/VortexTests/CompositionRoot/RootViewModelFactory/Payments/RootViewModelFactory+initiateStandardPaymentFlowTests.swift
@@ -0,0 +1,87 @@
+//
+//  RootViewModelFactory+initiateStandardPaymentFlowTests.swift
+//  VortexTests
+//
+//  Created by Igor Malyarov on 08.01.2025.
+//
+
+@testable import Vortex
+import XCTest
+
+final class RootViewModelFactory_initiateStandardPaymentFlowTests: RootViewModelFactoryTests {
+    
+    func test_shouldCallLogger_onMissingCategory() {
+        
+        let (sut, _, logger) = makeSUT()
+        XCTAssertEqual(logger.callCount, 0)
+        
+        sut.initiateStandardPaymentFlow(ofType: makeServiceCategoryType()) { _ in }
+        
+        XCTAssertEqual(logger.callCount, 1)
+    }
+    
+    func test_shouldDeliverMissingCategoryOfTypeFailure_onMissingCategory() {
+        
+        let type = makeServiceCategoryType()
+        let (sut, _,_) = makeSUT()
+        
+        expect(sut: sut, with: type, toDeliver: .missingCategoryOfType(type))
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeServiceCategoryType(
+    ) -> ServiceCategory.CategoryType {
+        
+        return anyMessage()
+    }
+    
+    private func equatable(
+        _ result: SUT.StandardPaymentResult
+    ) -> StandardPaymentResult {
+        
+        switch result {
+        case let .failure(failure):
+            switch failure {
+            case .makeStandardPaymentFailure:
+                return .makeStandardPaymentFailure
+                
+            case let .missingCategoryOfType(type):
+                return .missingCategoryOfType(type)
+            }
+            
+        case .success:
+            return .success
+        }
+    }
+    
+    private enum StandardPaymentResult: Equatable {
+        
+        case makeStandardPaymentFailure
+        case missingCategoryOfType(ServiceCategory.CategoryType)
+        case success
+    }
+    
+    private func expect(
+        sut: SUT,
+        with type: ServiceCategory.CategoryType? = nil,
+        toDeliver expectedResult: StandardPaymentResult,
+        on action: () -> Void = {},
+        timeout: TimeInterval = 1.0,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        let type = type ?? makeServiceCategoryType()
+        let exp = expectation(description: "wait for completion")
+        
+        sut.initiateStandardPaymentFlow(ofType: type) {
+            
+            XCTAssertNoDiff(self.equatable($0), expectedResult, "Expected \(expectedResult), but got \($0) instead.", file: file, line: line)
+            exp.fulfill()
+        }
+        
+        action()
+        
+        wait(for: [exp], timeout: timeout)
+    }
+}
