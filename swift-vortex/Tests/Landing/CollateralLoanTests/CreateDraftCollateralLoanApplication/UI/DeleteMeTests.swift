d283de1f9593503fc061f6647e5c5abc57fefce6
Valentin Ozerov <ozerov_v_v@forabank.ru>
2025-01-09 21:23:52 +0300

    Added public DeleteMe struct in CollateralLoanLandingCreateDraftCollateralLoanApplicationUI module

---
+++ swift-vortex/Tests/Landing/CollateralLoanTests/CreateDraftCollateralLoanApplication/UI/DeleteMeTests.swift
@@ -0,0 +1,19 @@
+//
+//  DeleteMeTests.swift
+//
+//
+//  Created by Valentin Ozerov on 09.01.2025.
+//
+
+import XCTest
+import CollateralLoanLandingCreateDraftCollateralLoanApplicationUI
+
+final class DeleteMeTests: XCTestCase {
+    
+    func test_DeleteMe() {
+        
+        _ = DeleteMe()
+        
+        XCTFail()
+    }
+}
