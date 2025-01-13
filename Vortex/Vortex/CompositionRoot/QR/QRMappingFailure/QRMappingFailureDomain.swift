3abf9b9295c064c42be85213966a113f984029f7
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-01-13 11:15:12 +0300

    add `QRMappingFailureDomain`

---
+++ Vortex/Vortex/CompositionRoot/QR/QRMappingFailure/QRMappingFailureDomain.swift
@@ -0,0 +1,43 @@
+//
+//  QRMappingFailureDomain.swift
+//  Vortex
+//
+//  Created by Igor Malyarov on 13.01.2025.
+//
+
+/// A namespace.
+enum QRMappingFailureDomain {}
+
+extension QRMappingFailureDomain {
+    
+    // MARK: - Binder
+    
+    typealias Binder = Vortex.Binder<Content, Flow>
+    typealias BinderComposer = Vortex.BinderComposer<Content, Select, Navigation>
+    
+    // MARK: - Content
+    
+    typealias Content = Void
+    
+    // MARK: - Flow
+    
+    typealias FlowDomain = Vortex.FlowDomain<Select, Navigation>
+    typealias Flow = FlowDomain.Flow
+    
+    typealias NotifyEvent = FlowDomain.NotifyEvent
+    typealias Notify = (NotifyEvent) -> Void
+    
+    enum Select: Equatable {
+        
+        case back
+        case manualSearch
+        case detailPayment
+    }
+    
+    enum Navigation {
+        
+        case back
+        case manualSearch
+        case detailPayment
+    }
+}
