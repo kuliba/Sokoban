d5624094a04fd1048df5aa717262cb34552f204e
Valentin Ozerov <ozerov_v_v@forabank.ru>
2025-01-14 08:24:55 +0300

    Fix review comments

--- swift-vortex/Sources/Landing/CollateralLoan/GetCollateralLanding/UI/Domain/GetCollateralLandingDomain+Event.swift
+++ swift-vortex/Sources/Landing/CollateralLoan/GetCollateralLanding/UI/Domain/GetCollateralLandingDomain+Event.swift
@@ -10,12 +10,9 @@ extension GetCollateralLandingDomain {
     public enum Event: Equatable {
         
         case changeDesiredAmount(UInt)
-        case closeBottomSheet
         case createDraftApplication
         case selectCollateral(String)
         case selectMonthPeriod(UInt)
-        case showCollateralBottomSheet
-        case showPeriodBottomSheet
         case toggleIHaveSalaryInCompany(Bool)
     }
 }
