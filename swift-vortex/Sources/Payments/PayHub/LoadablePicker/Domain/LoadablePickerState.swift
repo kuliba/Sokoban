f797abc79de7d93f3bdf8bf9174a43c2495e7eff
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-01-16 21:59:06 +0300

    improve `LoadablePickerState` API

--- swift-vortex/Sources/Payments/PayHub/LoadablePicker/Domain/LoadablePickerState.swift
+++ swift-vortex/Sources/Payments/PayHub/LoadablePicker/Domain/LoadablePickerState.swift
@@ -1,6 +1,6 @@
 //
 //  LoadablePickerState.swift
-//  
+//
 //
 //  Created by Igor Malyarov on 20.08.2024.
 //
@@ -33,14 +33,25 @@ public extension LoadablePickerState {
     
     var items: [Item] { prefix + suffix }
     
-    var elements: [Identified<ID, Element>] {
+    var elements: [Element] {
         
         items.compactMap {
             
-            guard case let .element(element) = $0
+            guard case let .element(identified) = $0
             else { return nil }
             
-            return element
+            return identified.element
+        }
+    }
+    
+    var identifiedElements: [Identified<ID, Element>] {
+        
+        items.compactMap {
+            
+            guard case let .element(identified) = $0
+            else { return nil }
+            
+            return identified
         }
     }
     
@@ -65,8 +76,8 @@ extension LoadablePickerState.Item: Identifiable {
     public var id: ID {
         
         switch self {
-        case let .element(element): return element.id
-        case let .placeholder(id):  return id
+        case let .element(identified): return identified.id
+        case let .placeholder(id):     return id
         }
     }
 }
