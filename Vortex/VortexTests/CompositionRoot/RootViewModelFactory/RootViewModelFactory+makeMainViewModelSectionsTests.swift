d558dc3fb82d9f59424f26d8bf1f7f397ccc94cb
Tasha Andryushina <natalykn@gmail.com>
2025-01-16 16:20:45 +0300

    add tests for collateralLoanLandingFlag, savingsAccountFlag

--- Vortex/VortexTests/CompositionRoot/RootViewModelFactory/RootViewModelFactory+makeMainViewModelSectionsTests.swift
+++ Vortex/VortexTests/CompositionRoot/RootViewModelFactory/RootViewModelFactory+makeMainViewModelSectionsTests.swift
@@ -7,14 +7,12 @@
 
 @testable import Vortex
 import XCTest
-import RxViewModel
-import Banners
 
 final class RootViewModelFactory_makeMainViewModelSectionsTests: RootViewModelFactoryTests {
     
     func test_shouldMakeMainViewModelSections() {
         
-        let sut = makeSUT().sut
+        let (sut, _, _) = makeSUT()
         let bannersBinder = BannersBinder.immediate
         
         let sections = sut.makeMainViewModelSections(
@@ -30,31 +28,5 @@ final class RootViewModelFactory_makeMainViewModelSectionsTests: RootViewModelFa
         XCTAssertTrue(sections[3] is MainSectionCurrencyMetallView.ViewModel)
         XCTAssertTrue(sections[4] is MainSectionOpenProductView.ViewModel)
         XCTAssertTrue(sections[5] is MainSectionAtmView.ViewModel)
-    }
-    
-    func test_makeOpenNewProductButtons() {
-        
-        let sut = makeSUT().sut
-        let buttons = sut.makeOpenNewProductButtons(
-            collateralLoanLandingFlag: .active, 
-            savingsAccountFlag: .inactive,
-            action: { _ in }
-        )
-        
-        let ids = [
-            "CARD",
-            "DEPOSIT",
-            "ACCOUNT",
-            "STICKER",
-            "LOAN",
-            "INSURANCE",
-            "MORTGAGE"
-        ]
-        
-        XCTAssertTrue(buttons.count == 7)
-
-        buttons.indices.forEach {
-            XCTAssertTrue(buttons[$0].id == ids[$0])
-        }
     }
 }
