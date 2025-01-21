b4faea03dfa0c170f5812600f53907418a77fc3d
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-01-21 22:33:04 +0300

    extract `ImageCache.imagesDictionaryPublisher(for:)`

--- Vortex/Vortex/CompositionRoot/RootViewModelFactory/Factories/RootViewModelFactory+makeOperationPicker.swift
+++ Vortex/Vortex/CompositionRoot/RootViewModelFactory/Factories/RootViewModelFactory+makeOperationPicker.swift
@@ -174,35 +174,3 @@ extension Latest: KeyProviding, ValueUpdatable {
         updating(with: value)
     }
 }
-
-extension ImageCache {
-    
-    func imagesDictionaryPublisher(
-        for keys: [String]
-    ) -> AnyPublisher<[String: Image], Never> {
-        
-        // map into (key, image) tuples
-        let perItemPublishers: [AnyPublisher<(String, Image), Never>] = keys.map { key in
-            
-            image(forKey: .init(key))
-                .map { image in (key, image) }
-                .handleEvents(receiveOutput: {
-                    
-                    print("####### image forKey", $0.0, $0.1)
-                })
-                .eraseToAnyPublisher()
-        }
-        
-        // merge into a dictionary
-        return Publishers.MergeMany(perItemPublishers)
-            .scan([String: Image]()) { dict, tuple in
-                
-                var dict = dict
-                let (md5, img) = tuple
-                dict[md5] = img
-                
-                return dict
-            }
-            .eraseToAnyPublisher()
-    }
-}
