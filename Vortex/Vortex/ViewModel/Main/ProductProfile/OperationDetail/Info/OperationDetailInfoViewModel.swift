kuliba/Sokoban

commit 39d68e31270b75cd087516395fcbc8f5b7381fdd

extract helpers to remove duplication in `OperationDetailInfoViewModel`

Vortex/Vortex/ViewModel/Main/ProductProfile/OperationDetail/Info/OperationDetailInfoViewModel.swift
@@ -176,33 +176,9 @@ final class OperationDetailInfoViewModel: Identifiable {
             
         case .betweenTheir:
             
-            if let payeeCardId = operation?.payeeCardId {
+            if let creditAccount = creditAccount(payeeCardID: operation?.payeeCardId) {
                 
-                for i in model.products.value {
-                    
-                    if let card = i.value.first(where: { $0.id == payeeCardId }) {
-                        
-                        if let description = card.number?.suffix(4),
-                           let balance = card.balance,
-                           let balanceFormatted = model.amountFormatted(
-                            amount: balance,
-                            currencyCode: card.currency,
-                            style: .clipped
-                           ),
-                           let icon = model.images.value[card.smallDesignMd5hash]?.image,
-                           let additional = card.additionalField {
-                            
-                            cells.append(ProductCellViewModel(
-                                title: "Счет пополнения",
-                                icon: icon,
-                                name: card.displayName,
-                                iconPaymentService: model.images.value[card.paymentSystemMd5Hash]?.image,
-                                balance: balanceFormatted,
-                                description: "· \(description) · \(additional)"
-                            ))
-                        }
-                    }
-                }
+                cells.append(creditAccount)
             }
             
             if let operationCategoryCell { cells.append(operationCategoryCell) }
@@ -225,33 +201,9 @@ final class OperationDetailInfoViewModel: Identifiable {
                 cells.append(comissionCell)
             }
             
-            if let payerCardId = operation?.payerCardId {
+            if let debitAccount = debitAccount(payeeCardID: operation?.payeeCardId) {
                 
-                for i in model.products.value {
-                    
-                    if let card = i.value.first(where: { $0.id == payerCardId }) {
-                        
-                        if let description = card.number?.suffix(4),
-                           let balance = card.balance,
-                           let balanceFormatted = model.amountFormatted(
-                            amount: balance,
-                            currencyCode: card.currency,
-                            style: .clipped
-                           ),
-                           let icon = model.images.value[card.smallDesignMd5hash]?.image,
-                           let additional = card.additionalField {
-                            
-                            cells.append(ProductCellViewModel(
-                                title: "Счет списания",
-                                icon: icon,
-                                name: card.displayName,
-                                iconPaymentService: model.images.value[card.paymentSystemMd5Hash]?.image,
-                                balance: balanceFormatted,
-                                description: "· \(description) · \(additional)"
-                            ))
-                        }
-                    }
-                }
+                cells.append(debitAccount)
             }
             
             cells.append(dateTimeCell)
@@ -1254,6 +1206,64 @@ final class OperationDetailInfoViewModel: Identifiable {
         self.logo = logo
         //FIXME: why dismissAction is commented?
         //        self.dismissAction = dismissAction
+    }
+}
+
+extension OperationDetailInfoViewModel {
+    
+    func creditAccount(
+        payeeCardID: Int?,
+        title: String = "Счет пополнения"
+    ) -> ProductCellViewModel? {
+        
+        guard let payeeCardID else { return nil }
+        
+        return model.account(matching: payeeCardID, title: title)
+    }
+    
+    func debitAccount(
+        payeeCardID: Int?,
+        title: String = "Счет списания"
+    ) -> ProductCellViewModel? {
+        
+        guard let payeeCardID else { return nil }
+        
+        return model.account(matching: payeeCardID, title: title)
+    }
+}
+
+extension Model {
+    
+    func account(
+        matching payeeCardID: Int,
+        title: String
+    ) -> OperationDetailInfoViewModel.ProductCellViewModel? {
+        
+        for product in products.value {
+            
+            if let card = product.value.first(matching: payeeCardID),
+               let description = card.number?.suffix(4),
+               let balance = card.balance,
+               let balanceFormatted = amountFormatted(
+                amount: balance,
+                currencyCode: card.currency,
+                style: .clipped
+               ),
+               let icon = images.value[card.smallDesignMd5hash]?.image,
+               let additional = card.additionalField {
+                
+                return .init(
+                    title: title,
+                    icon: icon,
+                    name: card.displayName,
+                    iconPaymentService: images.value[card.paymentSystemMd5Hash]?.image,
+                    balance: balanceFormatted,
+                    description: "· \(description) · \(additional)"
+                )
+            }
+        }
+        
+        return nil
     }
 }
 

commit 00b7c9a21d7d00e227f58ed43165fb9382dd746b

fix `debitAccount` creation (bug introduced in 39d68e3) (GD#125)

Vortex/Vortex/ViewModel/Main/ProductProfile/OperationDetail/Info/OperationDetailInfoViewModel.swift
@@ -201,7 +201,7 @@ final class OperationDetailInfoViewModel: Identifiable {
                 cells.append(comissionCell)
             }
             
-            if let debitAccount = debitAccount(payeeCardID: operation?.payeeCardId) {
+            if let debitAccount = debitAccount(payeeCardID: operation?.payerCardId) {
                 
                 cells.append(debitAccount)
             }