ad014278fccb178ee37904d156f6c4bcf8c50ccb
Valentin Ozerov <ozerov_v_v@forabank.ru>
2025-01-29 16:28:42 +0300

    DBSNEW-12678. Залоговый кредит. OTP

---
+++ Vortex/Vortex/CompositionRoot/RootViewModelFactory/CollateralLoanLanding/RootViewModelFactory+makeCollateralLoanLandingOTP.swift
@@ -0,0 +1,8 @@
+//
+//  RootViewModelFactory+makeCollateralLoanLandingOTP.swift
+//  Vortex
+//
+//  Created by Valentin Ozerov on 29.01.2025.
+//
+
+import Foundation
---
+++ swift-vortex/Sources/Landing/CollateralLoan/GetShowcase/UI/Helpers/GetShowcaseOTPWarningView.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Valentin Ozerov on 29.01.2025.
+//
+
+import Foundation
