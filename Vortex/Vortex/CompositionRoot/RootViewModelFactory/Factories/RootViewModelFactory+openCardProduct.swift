5854dbd4d99fc9f229fae9f9cef03e4f7efad2c0
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-02-06 22:26:31 +0300

    WIP add `OpenCardDomain.Content` NEED REBASE UPON OPENCARD

--- Vortex/Vortex/CompositionRoot/RootViewModelFactory/Factories/RootViewModelFactory+openCardProduct.swift
+++ Vortex/Vortex/CompositionRoot/RootViewModelFactory/Factories/RootViewModelFactory+openCardProduct.swift
@@ -5,6 +5,10 @@
 //  Created by Igor Malyarov on 06.02.2025.
 //
 
+import Combine
+// import OpenCardBackend
+import RemoteServices
+
 extension RootViewModelFactory {
     
     @inlinable
@@ -12,13 +16,40 @@ extension RootViewModelFactory {
         notify: @escaping () -> Void
     ) -> OpenProduct.OpenCard {
         
+        let content: OpenCardDomain.Content = makeContent()
+        content.event(.load)
+        
         return composeBinder(
-            content: (),
+            content: content,
             delayProvider: delayProvider,
             getNavigation: getNavigation,
             witnesses: witnesses()
         )
     }
+    
+    // MARK: - Content
+    
+    @inlinable
+    func makeContent(
+        initialState: OpenCardDomain.State = .init()
+    ) -> OpenCardDomain.Content {
+        
+        let reducer = OpenCardDomain.Reducer()
+        let effectHandler = OpenCardDomain.EffectHandler(
+            load: load,
+            loadConfirmation: loadConfirmation,
+            orderCard: orderCard
+        )
+        
+        return .init(
+            initialState: initialState,
+            reduce: reducer.reduce,
+            handleEffect: effectHandler.handleEffect,
+            scheduler: schedulers.main
+        )
+    }
+    
+    // MARK: - Flow
     
     @inlinable
     func delayProvider(
@@ -36,9 +67,103 @@ extension RootViewModelFactory {
         
     }
     
+    // MARK: - Bind
+    
     @inlinable
     func witnesses() -> OpenCardDomain.Witnesses {
         
-        return .empty // TODO: replace with (a) emitting failure (alert/informer) (b) dismissing failure
+        return .init(
+            emitting: {
+                
+                $0.$state.compactMap(\.failure)
+                    .map { .select(.failure($0)) }
+                    .eraseToAnyPublisher()
+            },
+            dismissing: { _ in {} } // TODO: add dismissing failure
+        )
+    }
+    
+    // MARK: - Services
+    
+    @inlinable
+    func load(
+        completion: @escaping (OpenCardDomain.LoadResult) -> Void
+    ) {
+//        let service = nanoServiceComposer.compose(
+//            makeRequest: RequestFactory.createGetCardOrderFormRequest,
+//            mapResponse: RemoteServices.ResponseMapper.mapGetCardOrderFormResponse
+//        )
+        let service: (@escaping (OpenCardDomain.LoadResult) -> Void) -> Void = { [weak self] completion in
+            
+            self?.schedulers.background.delay(for: .seconds(2)) {
+             
+                completion(.failure(.init(message: "Server failure", type: .alert)))
+            }
+        }
+        
+        schedulers.background.schedule {
+            
+            service { [service] in
+                
+                completion($0)
+                _ = service
+            }
+        }
+    }
+    
+    @inlinable
+    func loadConfirmation(
+        completion: @escaping (OpenCardDomain.LoadConfirmationResult) -> Void
+    ) {
+//        #warning("/verify/getVerificationCode")
+//        let service = nanoServiceComposer.compose(
+//            makeRequest: RequestFactory.createGetVerificationCodeVerifyRequest,
+//            mapResponse: RemoteServices.ResponseMapper.mapGetVerificationCodeVerifyResponse
+//        )
+        
+        let service: (@escaping (OpenCardDomain.LoadConfirmationResult) -> Void) -> Void = { [weak self] completion in
+            
+            self?.schedulers.background.delay(for: .seconds(2)) {
+                
+                completion(.failure(.init(message: "Server failure", type: .alert)))
+            }
+        }
+        
+        schedulers.background.schedule {
+            
+            service { [service] in
+                
+                completion($0)
+                _ = service
+            }
+        }
+    }
+    
+    @inlinable
+    func orderCard(
+        payload: OpenCardDomain.OrderCardPayload,
+        completion: @escaping (OpenCardDomain.OrderCardResult) -> Void
+    ) {
+//        let service = nanoServiceComposer.compose(
+//            makeRequest: RequestFactory.createOrderRequest,
+//            mapResponse: RemoteServices.ResponseMapper.mapOrderCardResponse
+//        )
+        
+        let service: (@escaping (OpenCardDomain.OrderCardResult) -> Void) -> Void = { [weak self] completion in
+            
+            self?.schedulers.background.delay(for: .seconds(2)) {
+                
+                completion(true)
+            }
+        }
+
+        schedulers.background.schedule {
+            
+            service { [service] in
+                
+                completion($0)
+                _ = service
+            }
+        }
     }
 }
--- Vortex/Vortex/CompositionRoot/RootViewModelFactory/OpenProduct/OpenCard/OpenCardDomain.swift
+++ Vortex/Vortex/CompositionRoot/RootViewModelFactory/OpenProduct/OpenCard/OpenCardDomain.swift
@@ -6,6 +6,10 @@
 //
 
 import FlowCore
+// import OrderCard
+import OTPInputComponent
+import PayHub
+import RxViewModel
 
 /// A namespace.
 enum OpenCardDomain {
@@ -16,7 +20,14 @@ enum OpenCardDomain {
     
     // MARK: - Content
     
-    typealias Content = Void // TODO: replace with RxViewModel
+    typealias Content = RxViewModel<State, Event, Effect>
+    
+    typealias State = OrderCard.State<Confirmation>
+    typealias Event = OrderCard.Event<Confirmation>
+    typealias Effect = OrderCard.Effect
+    
+    typealias Reducer = OrderCard.Reducer<Confirmation>
+    typealias EffectHandler = OrderCard.EffectHandler<Confirmation>
     
     // MARK: - Flow
     
@@ -26,7 +37,210 @@ enum OpenCardDomain {
     
     typealias Witnesses = ContentWitnesses<Content, FlowEvent<Select, Never>>
     
-    enum Select {}
+    enum Select {
+        
+        case failure(OrderCard.LoadFailure)
+    }
     
     enum Navigation {}
+    
+    // MARK: - Other
+    
+    typealias LoadConfirmationResult = OrderCard.LoadConfirmationResult<Confirmation>
+    
+    struct Confirmation {
+        
+        let otp: TimedOTPInputViewModel
+        var consent: Consent
+        
+        struct Consent {
+            
+            var check: Bool
+        }
+    }
+    
+    typealias LoadResult = OrderCard.LoadResult<Confirmation>
+    typealias LoadFailure = OrderCard.LoadFailure
+    
+    typealias OrderCardPayload = OrderCard.OrderCardPayload
+    typealias OrderCardResult = OrderCard.OrderCardResult
+}
+
+extension OpenCardDomain.State {
+    
+    var failure: OpenCardDomain.LoadFailure? {
+        
+        switch result {
+        case let .failure(failure):
+            return failure
+            
+        case let .success(form):
+            switch form.confirmation {
+            case let .failure(failure):
+                return failure
+                
+            default:
+                return nil
+            }
+            
+        default:
+            return nil
+        }
+    }
+}
+
+enum OrderCard { // TODO: replace stub with types from module
+    
+    struct State<Confirmation> {
+        
+        var result: LoadResult<Confirmation>?
+    }
+    
+    struct Form<Confirmation> {
+        
+        var confirmation: LoadConfirmationResult<Confirmation>?
+        let product: Int // Product
+        let type: String //ProductType
+        var messages: Bool // SMS
+    }
+    
+    typealias LoadResult<Confirmation> = Result<Form<Confirmation>, LoadFailure>
+    typealias LoadConfirmationResult<Confirmation> = Result<Confirmation, LoadFailure>
+    
+    struct LoadFailure: Error, Equatable {
+        
+        let message: String
+        let type: FailureType
+        
+        enum FailureType {
+            
+            case alert, informer
+        }
+    }
+    
+    enum Event<Confirmation> {
+        
+        case `continue`
+        case load
+        case loaded(LoadResult<Confirmation>)
+        case loadConfirmation(LoadConfirmationResult<Confirmation>)
+        case messages(MessagesEvent)
+        case orderCardResult(Bool)
+        
+        public enum MessagesEvent: Equatable {
+            
+            case toggle
+        }
+    }
+    
+    enum Effect: Equatable {
+        
+        case load
+        case loadConfirmation
+        case orderCard(OrderCardPayload)
+    }
+    
+    struct OrderCardPayload: Equatable {}
+    
+    typealias OrderCardResult = Bool
+    
+    final class Reducer<Confirmation> {
+        
+        init() {}
+        
+        func reduce(
+            _ state: State,
+            _ event: Event
+        ) -> (State, Effect?) {
+            
+            var state = state
+            var effect: Effect?
+            
+            switch event {
+            case .continue:
+                switch state.result {
+                case .none, .failure:
+                    break
+                    
+                case let .success(form):
+                    switch form.confirmation {
+                    case .none:
+                        // TODO: change state to inflight/isLoading
+                        effect = .loadConfirmation
+                        
+                    case .some:
+                        // TODO: change state to inflight/isLoading
+                        effect = .orderCard(.init())
+                    }
+                }
+                
+            case .load:
+                effect = .load
+                
+            case let .loadConfirmation(confirmation):
+                switch (state.result, confirmation) {
+                case (.none, _), (.failure, _):
+                    break // impossible cases
+                    
+                case var (.success(form), confirmationResult):
+                    form.confirmation = confirmationResult
+                    state.result = .success(form)
+                    
+                }
+            case let .loaded(result):
+                state.result = result
+                
+            case let .messages(value):
+                break // TODO: ignoring here but should not in real type
+                
+            case let .orderCardResult(orderCardResult):
+#warning("UNIMPLEMENTED")
+            }
+            
+            return (state, effect)
+        }
+        
+        typealias State = OrderCard.State<Confirmation>
+        typealias Event = OrderCard.Event<Confirmation>
+    }
+    
+    final class EffectHandler<Confirmation> {
+        
+        private let load: Load
+        private let loadConfirmation: LoadConfirmation
+        private let orderCard: OrderCard
+        
+        init(
+            load: @escaping Load,
+            loadConfirmation: @escaping LoadConfirmation,
+            orderCard: @escaping OrderCard
+        ) {
+            self.load = load
+            self.loadConfirmation = loadConfirmation
+            self.orderCard = orderCard
+        }
+        
+        typealias Load = (@escaping (LoadResult<Confirmation>) -> Void) -> Void
+        typealias LoadConfirmation = (@escaping (LoadConfirmationResult<Confirmation>) -> Void) -> Void
+        typealias OrderCard = (OrderCardPayload, @escaping (Bool) -> Void) -> Void
+        
+        func handleEffect(
+            _ effect: Effect,
+            _ dispatch: @escaping Dispatch
+        ) {
+            switch effect {
+            case .load:
+                load { dispatch(.loaded($0)) }
+                
+            case .loadConfirmation:
+                loadConfirmation { dispatch(.loadConfirmation($0)) }
+                
+            case let .orderCard(payload):
+                orderCard(payload) { dispatch(.orderCardResult($0)) }
+            }
+        }
+        
+        typealias Dispatch = (Event) -> Void
+        typealias Event = Vortex.OrderCard.Event<Confirmation>
+    }
 }
