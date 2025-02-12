63367b0871ca55496ddd7de91adf88e5943c59a8
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-02-12 14:28:22 +0300

    add `UINInputState`

--- Vortex/PreviewApps/C2GPreviewApp/ContentView.swift
+++ Vortex/PreviewApps/C2GPreviewApp/ContentView.swift
@@ -9,19 +9,93 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var uinInputState: UINInputState = .init()
+    
     var body: some View {
         
-        VStack {
+        ZStack {
             
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                
+                Spacer()
+                
+                continueButton()
+                sbpIcon()
+            }
+            
+            settings
+        }
+    }
+    
+    @ViewBuilder
+    private func continueButton() -> some View {
+        
+        if !uinInputState.isEditing {
+            
+            Button(action: {}) {
+                
+                Text("Continue")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 44)
+            }
+            .buttonStyle(.borderedProminent)
+            .padding()
+            .disabled(!uinInputState.isValid)
+        }
+    }
+    
+    private func sbpIcon() -> some View {
+        
+        Image(systemName: "envelope")
+            .imageScale(.large)
+    }
+    
+    private var settings: some View {
+        
+        VStack(spacing: 16) {
+            
+            Text("UIN Input State")
+                .font(.headline)
+            
+            HStack {
+                
+                Text(uinInputState.isEditing ? "editing" : "noFocus")
+                Divider()
+                Text(uinInputState.isValid ? "valid" : "not valid")
+                    .foregroundStyle(uinInputState.isValid ? .green : .red)
+            }
+            .fixedSize()
+            
+            HStack {
+                
+                Button("editing") { uinInputState.isEditing = true }
+                Divider()
+                Button("no focus") { uinInputState.isEditing = false }
+            }
+            .fixedSize()
+            
+            HStack {
+                
+                Button("valid") { uinInputState.isValid = true }
+                    .foregroundStyle(.green)
+                Divider()
+                Button("invalid") { uinInputState.isValid = false }
+                    .foregroundStyle(.red)
+            }
+            .fixedSize()
         }
         .padding()
+    }
+    
+    private struct UINInputState {
+        
+        var isEditing: Bool = false
+        var isValid: Bool = false
     }
 }
 
 #Preview {
+    
     ContentView()
 }
