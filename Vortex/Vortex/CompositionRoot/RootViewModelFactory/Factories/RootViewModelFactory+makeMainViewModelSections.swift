bc770a39206eadde6ae960f4361174283315e985
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-02-12 07:50:54 +0300

    MainSectionFastOperationView.ViewModel: extract and inject items creation

--- Vortex/Vortex/CompositionRoot/RootViewModelFactory/Factories/RootViewModelFactory+makeMainViewModelSections.swift
+++ Vortex/Vortex/CompositionRoot/RootViewModelFactory/Factories/RootViewModelFactory+makeMainViewModelSections.swift
@@ -9,22 +9,23 @@ import Foundation
 
 extension RootViewModelFactory {
     
+    @inlinable
     func makeMainViewModelSections(
         bannersBinder: BannersBinder,
         collateralLoanLandingFlag: CollateralLoanLandingFlag,
         savingsAccountFlag: SavingsAccountFlag
     ) -> [MainSectionViewModel] {
-                
+        
         var sections = [
             MainSectionProductsView.ViewModel(
                 model,
                 promoProducts: nil
             ),
-            MainSectionFastOperationView.ViewModel(),
+            MainSectionFastOperationView.ViewModel(createItems: createItems),
             MainSectionPromoView.ViewModel(model),
             //    BannerPickerSectionBinderWrapper.init(binder: binder),
             MainSectionCurrencyMetallView.ViewModel(model),
-            MainSectionOpenProductView.ViewModel(
+            MainSectionOpenProductView.ViewModel( // TODO: extract
                 model,
                 makeButtons: { [weak self] in
                     
@@ -39,11 +40,88 @@ extension RootViewModelFactory {
             ),
             MainSectionAtmView.ViewModel.initial
         ]
+        
         if updateInfoStatusFlag.isActive {
+            
             if !model.updateInfo.value.areProductsUpdated {
-                sections.insert(UpdateInfoViewModel.init(content: .updateInfoText), at: 0)
+                
+                sections.insert(
+                    UpdateInfoViewModel.init(content: .updateInfoText),
+                    at: 0
+                )
             }
         }
+        
         return sections
+    }
+    
+    @inlinable
+    func createItems(
+        action: @escaping (MainSectionFastOperationView.ViewModel.FastOperations) -> Void
+    ) -> [ButtonIconTextView.ViewModel] {
+        
+        let displayButtonsTypes: [MainSectionFastOperationView.ViewModel.FastOperations] = [.byQr, .byPhone, .utility, .templates]
+        
+        return displayButtonsTypes.map { type in
+            
+            createButtonViewModel(for: type) { action(type) }
+        }
+    }
+    
+    @inlinable
+    func createButtonViewModel(
+        for type: MainSectionFastOperationView.ViewModel.FastOperations,
+        action: @escaping () -> Void
+    ) -> ButtonIconTextView.ViewModel {
+        
+        switch type {
+        case .utility: return .utility(action)
+        default:       return .default(for: type, action)
+        }
+    }
+}
+
+extension ButtonIconTextView.ViewModel {
+    
+    static func `default`(
+        for type: MainSectionFastOperationView.ViewModel.FastOperations,
+        _ action: @escaping () -> Void
+    ) -> ButtonIconTextView.ViewModel {
+        
+        return .init(
+            icon: .init(
+                image: type.icon,
+                background: .circle
+            ),
+            title: .init(text: type.title),
+            orientation: .vertical,
+            action: action
+        )
+    }
+    
+    static func utility(
+        _ action: @escaping () -> Void
+    ) -> ButtonIconTextView.ViewModel {
+        
+        let type: MainSectionFastOperationView.ViewModel.FastOperations = .utility
+        
+        return .init(
+            icon: .init(
+                image: type.icon,
+                style: .original,
+                background: .circle,
+                badge: .init(
+                    text: .init(
+                        title: "0%",
+                        font: .textBodySR12160(),
+                        fontWeight: .bold
+                    ),
+                    backgroundColor: .mainColorsRed,
+                    textColor: .white)
+            ),
+            title: .init(text: type.title),
+            orientation: .vertical,
+            action: action
+        )
     }
 }
