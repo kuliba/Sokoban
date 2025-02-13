3ecf6edfcc11136f31d63d835b9e9945a75a28f0
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-02-13 23:16:04 +0300

    add `paddedRoundedBackground` overload

--- Vortex/Vortex/CompositionRoot/PaymentsTransfers/UtilityPayments/AnywayTransaction/AnywayPaymentElementView/View+paddedRoundedBackground.swift
+++ Vortex/Vortex/CompositionRoot/PaymentsTransfers/UtilityPayments/AnywayTransaction/AnywayPaymentElementView/View+paddedRoundedBackground.swift
@@ -9,6 +9,20 @@ import SwiftUI
 
 extension View {
     
+    @inlinable
+    func paddedRoundedBackground(
+        edgeInsets: EdgeInsets = .init(top: 13, leading: 16, bottom: 13, trailing: 12),
+        color: Color = .mainColorsGrayLightest,
+        cornerRadius: CGFloat = 12
+    ) -> some View {
+        
+        self
+            .padding(edgeInsets)
+            .background(color)
+            .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+    }
+    
+    @_disfavoredOverload
     func paddedRoundedBackground(
         horizontal: CGFloat = 16,
         vertical: CGFloat = 13,
