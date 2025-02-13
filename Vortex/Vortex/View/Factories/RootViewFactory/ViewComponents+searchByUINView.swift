800ffdd5ec5b57f128af9cd8a60669348b57b474
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-02-13 09:20:35 +0300

    `SearchByUINFlowView`

--- Vortex/Vortex/View/Factories/RootViewFactory/ViewComponents+searchByUINView.swift
+++ Vortex/Vortex/View/Factories/RootViewFactory/ViewComponents+searchByUINView.swift
@@ -14,45 +14,127 @@ extension ViewComponents {
         _ binder: SearchByUINDomain.Binder
     ) -> some View {
         
-        searchByUINFlowView(flow: binder.flow) {
+        VStack(spacing: 16) {
             
-            VStack(spacing: 16) {
+            Text("TBD: Search by UIN")
+                .font(.headline)
+            
+            Button("connectivityFailure") {
                 
-                Text("TBD: Search by UIN")
-                    .font(.headline)
-                
-                Button("connectivityFailure") {
-                    
-                    binder.flow.event(.select(.uin(.init(value: "connectivityFailure"))))
-                }
-                
-                Button("serverFailure") {
-                    
-                    binder.flow.event(.select(.uin(.init(value: "serverFailure"))))
-                }
-                
-                Button("success") {
-                    
-                    binder.flow.event(.select(.uin(.init(value: UUID().uuidString))))
-                }
+                binder.flow.event(.select(.uin(.init(value: "connectivityFailure"))))
             }
-            .padding()
-            .frame(maxHeight: .infinity, alignment: .top)
-            .buttonBorderShape(.roundedRectangle)
-            .buttonStyle(.bordered)
-            .frame(maxHeight: .infinity, alignment: .center)
+            
+            Button("serverFailure") {
+                
+                binder.flow.event(.select(.uin(.init(value: "serverFailure"))))
+            }
+            
+            Button("success") {
+                
+                binder.flow.event(.select(.uin(.init(value: UUID().uuidString))))
+            }
         }
+        .padding()
+        .frame(maxHeight: .infinity, alignment: .top)
+        .buttonBorderShape(.roundedRectangle)
+        .buttonStyle(.bordered)
+        .frame(maxHeight: .infinity, alignment: .center)
+        .background(searchByUINFlowView(flow: binder.flow))
     }
     
     func searchByUINFlowView(
-        flow: SearchByUINDomain.Flow,
-        contentView: @escaping () -> some View
+        flow: SearchByUINDomain.Flow
     ) -> some View {
         
         RxWrapperView(model: flow) { state, event in
             
-            contentView()
-               // .disablingLoading(isLoading: state.isLoading)
+            SearchByUINFlowView(
+                state: state.navigation,
+                dismiss: { flow.event(.dismiss) }
+            )
         }
+    }
+}
+
+struct SearchByUINFlowView: View {
+    
+    let state: State?
+    let dismiss: () -> Void
+    
+    var body: some View {
+        
+        Color.clear
+            .alert(item: state?.backendFailure, content: alert)
+            .navigationDestination(
+                destination: state?.destination,
+                content: destinationView
+            )
+    }
+}
+
+extension SearchByUINFlowView {
+    
+    typealias State = SearchByUINDomain.Navigation
+}
+
+private extension SearchByUINFlowView {
+    
+    func alert(
+        backendFailure: BackendFailure
+    ) -> Alert {
+        
+        return backendFailure.alert(action: dismiss)
+    }
+    
+    @ViewBuilder
+    func destinationView(
+        destination: SearchByUINDomain.Navigation.Destination
+    ) -> some View {
+        
+        switch destination {
+        case let .c2gPayment(c2gPayment):
+            // TODO: extract to components
+            Text("TBD c2gPayment:" + String(describing: c2gPayment))
+                .frame(maxHeight: .infinity, alignment: .top)
+                .navigationBarWithBack(title: "Оплата", dismiss: dismiss)
+        }
+    }
+}
+
+extension SearchByUINDomain.Navigation {
+    
+    var backendFailure: BackendFailure? {
+        
+        guard case let .failure(backendFailure) = self else { return nil }
+        
+        return backendFailure
+    }
+    
+    var destination: Destination? {
+        
+        guard case let .payment(c2gPayment) = self else { return nil }
+        
+        return .c2gPayment(c2gPayment)
+    }
+    
+    enum Destination {
+        
+        case c2gPayment(C2GPayment)
+    }
+}
+
+extension SearchByUINDomain.Navigation.Destination: Identifiable {
+    
+    var id: ID {
+        
+        switch self {
+        case let .c2gPayment(c2gPayment):
+            return .c2gPayment
+        }
+    }
+    
+    enum ID: Hashable { // TODO: improve with ObjectIdentifier
+        
+        case c2gPayment
     }
 }
