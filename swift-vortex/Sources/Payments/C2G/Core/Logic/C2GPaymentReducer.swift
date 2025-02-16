b822259aac2d5a9b18779cab599cd62a8444db3d
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-02-16 17:01:32 +0300

    add C2GPayment Domain and `C2GPaymentReducer`

---
+++ swift-vortex/Sources/Payments/C2G/Core/Logic/C2GPaymentReducer.swift
@@ -0,0 +1,49 @@
+//
+//  C2GPaymentReducer.swift
+//
+//
+//  Created by Igor Malyarov on 16.02.2025.
+//
+
+import PaymentComponents
+
+public final class C2GPaymentReducer {
+    
+    private let productSelectReduce: ProductSelectReduce
+    
+    public init(
+        productSelectReduce: @escaping ProductSelectReduce
+    ) {
+        self.productSelectReduce = productSelectReduce
+    }
+    
+    public typealias ProductSelectReduce = (ProductSelect, ProductSelectEvent) -> ProductSelect
+}
+
+public extension C2GPaymentReducer {
+    
+    func reduce(
+        _ state: State,
+        _ event: Event
+    ) -> (State, Effect?) {
+        
+        var state = state
+        
+        switch event {
+        case let .productSelect(productSelectEvent):
+            state.productSelect = productSelectReduce(state.productSelect, productSelectEvent)
+            
+        case .termsToggle:
+            state.termsCheck.toggle()
+        }
+        
+        return (state, nil)
+    }
+}
+
+public extension C2GPaymentReducer {
+    
+    typealias State = C2GPaymentState
+    typealias Event = C2GPaymentEvent
+    typealias Effect = C2GPaymentEffect
+}
