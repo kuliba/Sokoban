41259a080ea1e794ce4fe04e0bb5028eb914ba9a
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-02-18 16:05:35 +0300

    add empty `ShareSheetPreviewApp`

---
+++ Vortex/PreviewApps/ShareSheetPreviewApp/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  ShareSheetPreviewApp
+//
+//  Created by Igor Malyarov on 18.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        VStack {
+            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+
+    ContentView()
+}
