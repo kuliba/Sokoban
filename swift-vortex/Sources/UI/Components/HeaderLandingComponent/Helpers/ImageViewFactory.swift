32af48ed4a33c5f46f0cc9361fcc3b0472b5e773
Dmitriy Savushkin <da.savushkin4@gmail.com>
2025-02-19 13:12:55 +0300

    remove images from image factory

--- swift-vortex/Sources/UI/Components/HeaderLandingComponent/Helpers/ImageViewFactory.swift
+++ swift-vortex/Sources/UI/Components/HeaderLandingComponent/Helpers/ImageViewFactory.swift
@@ -29,33 +29,16 @@ public extension ImageViewFactory {
 extension ImageViewFactory {
     
     static let `default`: Self = .init(
-        makeIconView: {
-            switch $0 {
-    case "1":
-        return  .init(
-            image: .bolt,
-            publisher: Just(.bolt).eraseToAnyPublisher()
-        )
-        
-    case "2":
-        return  .init(
-            image: .shield,
-            publisher: Just(.shield).eraseToAnyPublisher()
-        )
-        
-    default:
-        return .init(
-            image: .flag,
-            publisher: Just(.flag).eraseToAnyPublisher()
-        )
-    }}
+        makeIconView: { _ in 
+            return  .init(
+                image: .bolt,
+                publisher: Just(.bolt).eraseToAnyPublisher()
+            )
+        }
     )
 }
 
 extension Image {
     
     static let bolt: Self = .init(systemName: "bolt")
-    static let percent: Self = .init(systemName: "percent")
-    static let shield: Self = .init(systemName: "shield")
-    static let flag: Self = .init(systemName: "flag")
 }
