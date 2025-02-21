cf5b853bb5012dcde02299ef99b64f33d34c3d65
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-02-21 18:08:39 +0300

    add empty module `GetOperationDetailService`

---
+++ swift-vortex/Tests/Services/GetOperationDetailServiceTests/DeleteMeTests.swift
@@ -0,0 +1,18 @@
+//
+//  DeleteMeTests.swift
+//  
+//
+//  Created by Igor Malyarov on 21.02.2025.
+//
+
+import GetOperationDetailService
+import XCTest
+
+final class DeleteMeTests: XCTestCase {
+
+    func test() { 
+        
+        _ = DeleteMe()
+    }
+
+}
