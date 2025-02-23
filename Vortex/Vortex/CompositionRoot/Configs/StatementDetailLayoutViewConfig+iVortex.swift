6c2c7b549e21d4f378a13a663d5a6ac1d3359435
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-02-23 10:55:52 +0300

    add `StatementDetailLayoutView`

---
+++ Vortex/Vortex/CompositionRoot/Configs/StatementDetailLayoutViewConfig+iVortex.swift
@@ -0,0 +1,16 @@
+//
+//  StatementDetailLayoutViewConfig+iVortex.swift
+//  Vortex
+//
+//  Created by Igor Malyarov on 23.02.2025.
+//
+
+import SwiftUI
+
+extension StatementDetailLayoutViewConfig {
+    
+    static let iVortex: Self = .init(
+        insets: .init(top: 40, leading: 0, bottom: 32, trailing: 0),
+        spacing: 32
+    )
+}
--- Vortex/Vortex/View/Factories/ViewComponents/ViewComponents+makeStatementDetailView.swift
+++ Vortex/Vortex/View/Factories/ViewComponents/ViewComponents+makeStatementDetailView.swift
@@ -14,11 +14,41 @@ extension ViewComponents {
         _ details: StatementDetails
     ) -> some View {
         
-        VStack {
+        StatementDetailLayoutView(config: .iVortex) {
             
             makeC2GPaymentCompleteButtonsView(details.model)
+        } content: {
+            
+            EmptyView()
         }
-        .padding(.vertical, 40)
-        .edgesIgnoringSafeArea(.bottom)
+        .border(.red)
     }
+}
+
+struct StatementDetailLayoutView<Buttons: View, Content: View>: View {
+    
+    let config: Config
+    let buttons: () -> Buttons
+    let content: () -> Content
+    
+    var body: some View {
+        
+        VStack(spacing: config.spacing) {
+            
+            buttons()
+            content()
+        }
+        .padding(config.insets)
+    }
+}
+
+extension StatementDetailLayoutView {
+    
+    typealias Config = StatementDetailLayoutViewConfig
+}
+
+struct StatementDetailLayoutViewConfig: Equatable {
+    
+    let insets: EdgeInsets
+    let spacing: CGFloat
 }
