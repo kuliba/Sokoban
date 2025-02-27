6e42d314f2ec660bf9be4a9be8c8e2b7635bfc18
Dmitriy Savushkin <da.savushkin4@gmail.com>
2025-02-27 22:11:10 +0300

    rename to `ProductItem`

--- swift-vortex/Sources/Landing/OrderCardLandingBackend/ResponseMapper/ResponseMapper+mapOrderCardLandingResponse.swift
+++ swift-vortex/Sources/Landing/OrderCardLandingBackend/ResponseMapper/ResponseMapper+mapOrderCardLandingResponse.swift
@@ -60,10 +60,10 @@ private extension ResponseMapper {
     
     struct _Data: Decodable {
         
-        let products: [Item1]
+        let products: [ProductItem]
         
-        struct Item1: Decodable {
-            //        let id: String?
+        struct ProductItem: Decodable {
+            
             let header: Header?
             let theme: String?
             let product: Product?
