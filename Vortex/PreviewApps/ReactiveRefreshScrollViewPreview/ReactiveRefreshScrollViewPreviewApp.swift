ae698e4ce5d540b3eeeb13f1602110eb5a940107
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-03-01 18:14:51 +0300

    add `ReactiveRefreshScrollViewPreview` app

---
+++ Vortex/PreviewApps/ReactiveRefreshScrollViewPreview/ReactiveRefreshScrollViewPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactiveRefreshScrollViewPreviewApp.swift
+//  ReactiveRefreshScrollViewPreview
+//
+//  Created by Igor Malyarov on 01.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ReactiveRefreshScrollViewPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
