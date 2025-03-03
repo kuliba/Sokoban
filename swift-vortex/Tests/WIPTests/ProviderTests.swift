e2f1a3f50024aba4863c24f3ae46e36ee57a9716
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-03-03 17:01:15 +0300

    call fallback

--- swift-vortex/Tests/WIPTests/ProviderTests.swift
+++ swift-vortex/Tests/WIPTests/ProviderTests.swift
@@ -19,14 +19,24 @@ protocol Cache<Resource> {
     func value(forKey key: String) -> Resource?
 }
 
+protocol Fallback<Resource> {
+    
+    associatedtype Resource
+    
+    func value(forKey key: String) -> Resource
+}
+
 final class WIPImageProvider<Resource>: Provider {
     
     let cache: any Cache<Resource>
+    let fallback: any Fallback<Resource>
     
     init(
-        cache: any Cache<Resource>
+        cache: any Cache<Resource>,
+        fallback: any Fallback<Resource>
     ) {
         self.cache = cache
+        self.fallback = fallback
     }
 }
 
@@ -34,7 +44,7 @@ extension WIPImageProvider {
     
     func value(forKey key: String) -> Resource {
         
-        cache.value(forKey: key) ?? { fatalError() }()
+        cache.value(forKey: key) ?? fallback.value(forKey: key)
     }
 }
 
@@ -47,9 +57,10 @@ final class ProviderTests: XCTestCase {
     
     func test_init_shouldNotCallCollaborators() {
         
-        let (sut, cache, loader) = makeSUT()
+        let (sut, cache, fallback, loader) = makeSUT()
         
         XCTAssertEqual(cache.callCount, 0)
+        XCTAssertEqual(fallback.callCount, 0)
         XCTAssertEqual(loader.callCount, 0)
         // TODO: add other collaborators
         XCTAssertNotNil(sut)
@@ -60,7 +71,7 @@ final class ProviderTests: XCTestCase {
     func test_shouldCallCacheWithKey() {
         
         let key = anyMessage()
-        let (sut, cache, _) = makeSUT()
+        let (sut, cache, _,_) = makeSUT()
         
         _ = sut.value(forKey: key)
         
@@ -70,34 +81,56 @@ final class ProviderTests: XCTestCase {
     func test_shouldDeliverCached_onCacheHit() {
         
         let (key, cached) = (anyMessage(), makeResource())
-        let (sut, _,_) = makeSUT(cacheStubs: cached)
+        let (sut, _,_,_) = makeSUT(cacheStubs: cached)
         
         XCTAssertNoDiff(sut.value(forKey: key), cached)
+    }
+    
+    func test_shouldCallFallback_onCacheMiss() {
+        
+        let key = anyMessage()
+        let (sut, _, fallback, _) = makeSUT(cacheStubs: nil)
+        
+        _ = sut.value(forKey: key)
+        
+        XCTAssertNoDiff(fallback.payloads, [key])
+    }
+    
+    func test_shouldDeliverFallback_onCacheMiss() {
+        
+        let (key, backed) = (anyMessage(), makeResource())
+        let (sut, _,_,_) = makeSUT(fallbackStubs: backed)
+        
+        XCTAssertNoDiff(sut.value(forKey: key), backed)
     }
     
     // MARK: - Helpers
     
     private typealias SUT = WIPImageProvider<Resource>
     private typealias Cache = CallSpy<String, Resource?>
+    private typealias Fallback = CallSpy<String, Resource>
     private typealias Loader = CallSpy<String, Void>
     
     private func makeSUT(
         cacheStubs: Resource?...,
+        fallbackStubs: Resource...,
         file: StaticString = #file,
         line: UInt = #line
     ) -> (
         sut: SUT,
         cache: Cache,
+        fallback: Fallback,
         loader: Loader
     ) {
-        let cache = Cache(stubs: cacheStubs.isEmpty ? [makeResource()] : cacheStubs)
+        let cache = Cache(stubs: cacheStubs.isEmpty ? [nil] : cacheStubs)
+        let fallback = Fallback(stubs: fallbackStubs.isEmpty ? [makeResource()] : fallbackStubs)
         let loader = Loader(stubs: .init(repeating: (), count: 10))
-        let sut = SUT(cache: cache)
+        let sut = SUT(cache: cache, fallback: fallback)
         
         trackForMemoryLeaks(sut, file: file, line: line)
         trackForMemoryLeaks(loader, file: file, line: line)
         
-        return (sut, cache, loader)
+        return (sut, cache, fallback, loader)
     }
     
     struct Resource: Equatable {
@@ -116,6 +149,16 @@ final class ProviderTests: XCTestCase {
 extension CallSpy: Cache
 where Payload == String,
       Response == ProviderTests.Resource? {
+    
+    func value(forKey key: String) -> Response {
+        
+        call(payload: key)
+    }
+}
+
+extension CallSpy: Fallback
+where Payload == String,
+      Response == ProviderTests.Resource {
     
     func value(forKey key: String) -> Response {
         
