bf17d90f0d5ac0ca89b337251462ef8251986151
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-03-08 11:34:18 +0300

    add `SplashScreen` feature file structure

---
+++ swift-vortex/Sources/Feature/SplashScreen/UI/DeleteMe.swift
@@ -0,0 +1,11 @@
+//
+//  DeleteMe.swift
+//
+//
+//  Created by Igor Malyarov on 08.03.2025.
+//
+
+public struct DeleteMe {
+    
+    public init() {}
+}
---
+++ swift-vortex/Tests/Feature/SplashScreen/Backend/DeleteMeTests.swift
@@ -0,0 +1,18 @@
+//
+//  DeleteMeTests.swift
+//  
+//
+//  Created by Igor Malyarov on 08.03.2025.
+//
+
+import SplashScreenBackend
+import XCTest
+
+final class DeleteMeTests: XCTestCase {
+
+    func test() {
+     
+        _ = DeleteMe()
+        XCTFail("unimplemented")
+    }
+}
