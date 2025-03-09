33222c090827e69ad9357e0b63e8df3be0bcf389
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-03-09 17:40:07 +0300

    rename typealias to reflect details

--- Vortex/Vortex/CompositionRoot/RootViewModelFactory/RootViewModelFactory+composeLoaders.swift
+++ Vortex/Vortex/CompositionRoot/RootViewModelFactory/RootViewModelFactory+composeLoaders.swift
@@ -10,17 +10,17 @@ import SerialComponents
 
 extension RootViewModelFactory {
     
-    typealias SerialLoaderComposer<T, Model> = SerialComponents.SerialLoaderComposer<String, T, Model> where Model: Codable
+    typealias CodableSerialLoaderComposer<T, Model> = SerialComponents.SerialLoaderComposer<String, T, Model> where Model: Codable
     
     @inlinable
     func composeLoaders<T, Model>(
         localAgent: (any LocalAgentProtocol)? = nil,
-        remoteLoad: @escaping SerialLoaderComposer<T, Model>.RemoteLoad,
+        remoteLoad: @escaping CodableSerialLoaderComposer<T, Model>.RemoteLoad,
         fromModel: @escaping (Model) -> T,
         toModel: @escaping (T) -> Model
     ) -> (load: Load<[T]>, reload: Load<[T]>) {
         
-        let composer = SerialLoaderComposer(
+        let composer = CodableSerialLoaderComposer(
             localAgent: localAgent ?? model.localAgent,
             remoteLoad: remoteLoad,
             fromModel: fromModel,
