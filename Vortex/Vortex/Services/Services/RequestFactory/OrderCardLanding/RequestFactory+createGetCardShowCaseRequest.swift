5d50ed48381eb45fc18e6929ad571d76cb2f8ec1
Dmitriy Savushkin <da.savushkin4@gmail.com>
2025-03-10 11:29:08 +0300

    add `createGetCardShowCaseRequest`

---
+++ Vortex/Vortex/Services/Services/RequestFactory/OrderCardLanding/RequestFactory+createGetCardShowCaseRequest.swift
@@ -0,0 +1,21 @@
+//
+//  RequestFactory+createGetCardShowCaseRequest.swift
+//  Vortex
+//
+//  Created by Дмитрий Савушкин on 10.03.2025.
+//
+
+import Foundation
+import RemoteServices
+
+extension RequestFactory {
+    
+    static func createGetCardShowCaseRequest() throws -> URLRequest {
+        
+        let base = Config.serverAgentEnvironment.baseURL
+        let endpoint = Services.Endpoint.getCardShowCase
+        let endpointURL = try! endpoint.url(withBase: base)
+        
+        return RemoteServices.RequestFactory.createEmptyRequest(.get, with: endpointURL)
+    }
+}
