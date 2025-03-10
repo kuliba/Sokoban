aa36d4aeaa5fd51764343a144a50b7a03fb43e91
Dmitriy Savushkin <da.savushkin4@gmail.com>
2025-03-10 13:25:34 +0300

    add `CardShowCaseResponse`

--- swift-vortex/Sources/Landing/OrderCardLandingBackend/Domain/CardShowCaseResponse.swift
+++ swift-vortex/Sources/Landing/OrderCardLandingBackend/Domain/CardShowCaseResponse.swift
@@ -9,6 +9,12 @@ public struct CardShowCaseResponse: Equatable {
     
     public let products: [Product]
     
+    public init(
+        products: [CardShowCaseResponse.Product]
+    ) {
+        self.products = products
+    }
+    
     public struct Product: Equatable {
         
         let theme: String
@@ -18,21 +24,64 @@ public struct CardShowCaseResponse: Equatable {
         let terms: String
         let cardShowcaseAction: Action
         
-        public struct Action: Equatable {
+        public init(
+            theme: String,
+            name: [Name],
+            features: Features,
+            image: String,
+            terms: String,
+            cardShowcaseAction: Action
+        ) {
+            self.theme = theme
+            self.name = name
+            self.features = features
+            self.image = image
+            self.terms = terms
+            self.cardShowcaseAction = cardShowcaseAction
+        }
         
+        public struct Action: Equatable {
+            
             let type: String
             let target: String
             let fallbackUrl: String?
+            
+            public init(
+                type: String,
+                target: String,
+                fallbackUrl: String? = nil
+            ) {
+                self.type = type
+                self.target = target
+                self.fallbackUrl = fallbackUrl
+            }
         }
         
         public struct Features: Equatable {
             
+            let header: String?
             let list: [Item]
+            
+            public init(
+                header: String? = nil,
+                list: [Item]
+            ) {
+                self.header = header
+                self.list = list
+            }
             
             public struct Item: Equatable {
                 
                 let bullet: Bool
                 let text: String
+                
+                public init(
+                    bullet: Bool,
+                    text: String
+                ) {
+                    self.bullet = bullet
+                    self.text = text
+                }
             }
         }
         
@@ -40,6 +89,14 @@ public struct CardShowCaseResponse: Equatable {
             
             let text: String
             let isBold: Bool
+            
+            public init(
+                text: String,
+                isBold: Bool
+            ) {
+                self.text = text
+                self.isBold = isBold
+            }
         }
     }
 }
