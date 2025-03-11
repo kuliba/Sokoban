765bc8a4e4ed6576491e6f080ba6ff55700ef8c2
Dmitriy Savushkin <da.savushkin4@gmail.com>
2025-03-11 09:39:04 +0300

    fix `ProductsLanding`

---
+++ Vortex/Vortex/CompositionRoot/RootViewModelFactory/OrderCardLanding/Domain/ProductsLandingDomain.swift
@@ -0,0 +1,39 @@
+//
+//  ProductsLandingDomain.swift
+//  Vortex
+//
+//  Created by Дмитрий Савушкин on 11.03.2025.
+//
+
+import Foundation
+
+enum ProductsLandingDomain {}
+
+extension ProductsLandingDomain {
+    
+    // MARK: - Binder
+    
+    typealias Binder = Vortex.Binder<Content, Flow>
+    typealias BinderComposer = Vortex.BinderComposer<Content, Select, Navigation>
+    
+    // MARK: - Content
+    
+    typealias Content = ProductsLanding
+    
+    // MARK: - Flow
+    
+    typealias FlowDomain = Vortex.FlowDomain<Select, Navigation>
+    typealias Flow = FlowDomain.Flow
+    
+    typealias NotifyEvent = FlowDomain.NotifyEvent
+    typealias Notify = (NotifyEvent) -> Void
+    
+    enum Select: Equatable {
+        case `continue`
+    }
+    
+    enum Navigation {
+
+        case `continue`
+    }
+}
--- Vortex/Vortex/View/Factories/RootViewFactory/OpenCardLanding/Domain/ProductsLanding.swift
+++ Vortex/Vortex/View/Factories/RootViewFactory/OpenCardLanding/Domain/ProductsLanding.swift
@@ -9,23 +9,34 @@ import SharedConfigs
 import Foundation
 
 struct ProductsLanding {
+   
+    let products: [Product]
     
-    let title: TitleConfig
-    let items: [Item]
-    let imageUrl: URL
-    let terms: URL
-    let action: Action
-    
-    struct Action {
+    struct Product {
         
-        let type: String
-        let target: String
-        let fallbackUrl: URL
-    }
-    
-    struct Item {
-        
-        let bullet: Bool
         let title: String
+        let items: [Item]
+        let imageURL: URL
+        let terms: URL
+        let action: Action
+        
+        struct Action {
+            
+            let type: String
+            let target: String
+            let fallbackURL: URL
+        }
+        
+        struct Item {
+            
+            let bullet: Bool
+            let title: String
+        }
     }
+}
+
+struct ProductLandingConfig {
+    
+    let title: TextConfig
+    let item: TextConfig
 }
