cdbc65bf124c156781db9b197cdb7c617b97bc45
Valentin Ozerov <ozerov_v_v@forabank.ru>
2025-03-12 16:10:49 +0300

    Fix completion screen

--- Vortex/Vortex/View/CollateralLoanLanding/CreateDraftCollateralLoanApplication/Complete/PaymentCompleteView.Config+CollateralLoanLanding.swift
+++ Vortex/Vortex/View/CollateralLoanLanding/CreateDraftCollateralLoanApplication/Complete/PaymentCompleteView.Config+CollateralLoanLanding.swift
@@ -7,14 +7,68 @@
 
 extension PaymentCompleteView.Config {
     
-    static let collateralLoanLanding = Self(
+    static let collateralLoanLanding: Self = .init(
         statuses: .init(
-            completed: .default,
-            inflight: .default,
+            completed: .init(
+                content: .init(
+                    logo: .ic48Check,
+                    title: "Заявка на кредит успешно\nоформлена",
+                    subtitle: "Специалист банка свяжется с Вами в\nближайшее время"
+                ),
+                config: .init(
+                    amount: .init(
+                        textFont: .textH1Sb24322(),
+                        textColor: .backgroundBackground
+                    ),
+                    icon: .init(
+                        foregroundColor: .iconWhite,
+                        backgroundColor: .systemColorActive,
+                        innerSize: .init(width: 44, height: 44),
+                        outerSize: .init(width: 88, height: 88)
+                    ),
+                    logoHeight: 0,
+                    title: .init(
+                        textFont: .textH3Sb18240(),
+                        textColor: .textSecondary
+                    ),
+                    subtitle: .init(
+                        textFont: .textBodyMR14200(),
+                        textColor: .textPlaceholder
+                    )
+                )
+            ),
+            inflight: .init(
+                content: .init(
+                    logo: .ic48Clock,
+                    title: "Операция в обработке!",
+                    subtitle: nil
+                ),
+                config: .init(
+                    amount: .init(
+                        textFont: .textH1Sb24322(),
+                        textColor: .textSecondary
+                    ),
+                    icon: .init(
+                        foregroundColor: .iconWhite,
+                        backgroundColor: .systemColorWarning,
+                        innerSize: .init(width: 44, height: 44),
+                        outerSize: .init(width: 88, height: 88)
+                    ),
+                    logoHeight: 44,
+                    title: .init(
+                        textFont: .textH3Sb18240(),
+                        textColor: .textSecondary
+                    ),
+                    subtitle: .init(
+                        textFont: .textH3Sb18240(),
+                        textColor: .textSecondary
+                    )
+                )
+            ),
             rejected: .default,
             fraudCancelled: .default,
             fraudExpired: .default,
-            suspend: .default
+            suspend: .suspend()
         )
     )
 }
