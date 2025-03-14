ce73d1f47d51c0fecef9b7ec534f502a397dc067
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-03-14 08:25:00 +0300

    add splash text tests

--- Vortex/Vortex/CompositionRoot/RootViewModelFactory/RootViewModelFactory+Infra.swift
+++ Vortex/Vortex/CompositionRoot/RootViewModelFactory/RootViewModelFactory+Infra.swift
@@ -5,10 +5,14 @@
 //  Created by Igor Malyarov on 21.01.2025.
 //
 
+import Foundation
+
 extension RootViewModelFactory {
     
     struct Infra {
         
+        let calendar: Calendar
+        let currentDate: () -> Date
         let httpClient: HTTPClient
         let imageCache: ImageCache
         let logger: LoggerAgentProtocol
