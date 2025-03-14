8973842116af92751a56b420b774ada8bce64fe1
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-03-14 11:05:31 +0300

    add test for default splash settings

--- Vortex/VortexTests/CompositionRoot/RootViewModelFactory/Factories/SplashScreen/RootViewModelFactory+makeSplashScreenViewModelTests.swift
+++ Vortex/VortexTests/CompositionRoot/RootViewModelFactory/Factories/SplashScreen/RootViewModelFactory+makeSplashScreenViewModelTests.swift
@@ -5,6 +5,7 @@
 //  Created by Igor Malyarov on 14.03.2025.
 //
 
+import SplashScreenUI
 @testable import Vortex
 import XCTest
 
@@ -136,6 +137,16 @@ final class RootViewModelFactory_makeSplashScreenViewModelTests: RootViewModelFa
         XCTAssertNoDiff(splash.state.settings.text.value, "Добрый вечер,\n\(firstName)!")
     }
     
+    // MARK: - Settings
+    
+    func test_shouldDeliverDefaultSettings_onEmptyCache_onActiveFlag() throws {
+        
+        let sut = try makeSUT(calendar: .tokyo, hour: 20, minute: 45)
+        let splash = sut.makeSplashScreenViewModel(flag: .init(.active))
+        
+        XCTAssertNoDiff(splash.state.settings, defaultSettings())
+    }
+    
     // MARK: - Helpers Tests
     
     func test_makeTime() throws {
@@ -148,18 +159,6 @@ final class RootViewModelFactory_makeSplashScreenViewModelTests: RootViewModelFa
     }
     
     // MARK: - Helpers
-    
-    private func withClientInfo(
-        firstName: String,
-        customName: String? = nil
-    ) throws -> Model {
-        
-        let info = makeClientInfoData(firstName: firstName, customName: customName)
-        let localAgent = try LocalAgentStub(value: info)
-        let model: Model = .mockWithEmptyExcept(localAgent: localAgent)
-        
-        return model
-    }
     
     private func makeSUT(
         calendar: Calendar,
@@ -174,6 +173,105 @@ final class RootViewModelFactory_makeSplashScreenViewModelTests: RootViewModelFa
         let sut = makeSUT(calendar: calendar, currentDate: { date }, model: model, file: file, line: line).sut
         
         return sut
+    }
+    
+    private func withClientInfo(
+        firstName: String,
+        customName: String? = nil
+    ) throws -> Model {
+        
+        let info = makeClientInfoData(firstName: firstName, customName: customName)
+        let localAgent = try LocalAgentStub(value: info)
+        let model: Model = .mockWithEmptyExcept(localAgent: localAgent)
+        
+        return model
+    }
+    
+    private func defaultSettings() -> SplashScreenState.Settings {
+        
+        return .init(
+            image: .init("splash"),
+            bank: .init(
+                color: .init(hex: "FFFFFF"),
+                shadow: .init(color: .init(hex: "000000"), opacity: 0.25, radius: 64, x: 0, y: 4)
+            ),
+            name: .init(
+                color: .init(hex: "FFFFFF"),
+                shadow: .init(color: .init(hex: "000000"), opacity: 0.25, radius: 4, x: 0, y: 4)
+            ),
+            text: .init(
+                color: .init(hex: "FFFFFF"),
+                size: 24,
+                value: "Добрый вечер!",
+                shadow: .init(color: .init(hex: "000000"), opacity: 0.25, radius: 12, x: 0, y: 4)
+            ),
+            subtext: nil
+        )
+    }
+    
+    private func makeCodableSplashScreenStorage(
+        entries: [String: CodableSplashScreenStorage.Entry]
+    ) -> CodableSplashScreenStorage {
+        
+        return .init(entries: entries)
+    }
+    
+    private func makeEntry(
+        items: [CodableSplashScreenSettings],
+        serial: String = anyMessage()
+    ) -> CodableSplashScreenStorage.Entry {
+        
+        return .init(items: items, serial: serial)
+    }
+    
+    private func makeCodableSplashScreenSettings(
+        bank: CodableSplashScreenSettings.Logo? = nil,
+        name: CodableSplashScreenSettings.Logo? = nil,
+        text: CodableSplashScreenSettings.Text? = nil,
+        subtext: CodableSplashScreenSettings.Text? = nil,
+        imageData: CodableSplashScreenSettings.ImageData = .none,
+        link: String = anyMessage(),
+        period: String = anyMessage()
+    ) -> CodableSplashScreenSettings {
+        
+        return .init(
+            bank: bank ?? makeLogo(),
+            name: name ?? makeLogo(),
+            text: text ?? makeText(),
+            subtext: subtext,
+            imageData: imageData,
+            link: link,
+            period: period
+        )
+    }
+    
+    private func makeLogo(
+        color: String = anyMessage(),
+        shadow: CodableSplashScreenSettings.Shadow? = nil
+    ) -> CodableSplashScreenSettings.Logo {
+        
+        return .init(color: color, shadow: shadow ?? makeShadow())
+    }
+    
+    private func makeText(
+        color: String = anyMessage(),
+        size: CGFloat = .random(in: 1..<100),
+        value: String = anyMessage(),
+        shadow: CodableSplashScreenSettings.Shadow? = nil
+    ) -> CodableSplashScreenSettings.Text {
+        
+        return .init(color: color, size: size, value: value, shadow: shadow ?? makeShadow())
+    }
+    
+    private func makeShadow(
+        color: String = anyMessage(),
+        opacity: Double = .random(in: 1..<100),
+        radius: CGFloat = .random(in: 1..<100),
+        x: CGFloat = .random(in: 1..<100),
+        y: CGFloat = .random(in: 1..<100)
+    ) -> CodableSplashScreenSettings.Shadow {
+        
+        return .init(color: color, opacity: opacity, radius: radius, x: x, y: y)
     }
 }
 
