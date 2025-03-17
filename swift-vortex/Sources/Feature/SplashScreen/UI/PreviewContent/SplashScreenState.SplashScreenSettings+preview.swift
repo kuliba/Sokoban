17a388a5bdf466fa7ffb4e9a5a3d327dd67a995f
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-03-17 13:55:16 +0300

    add helper

--- swift-vortex/Sources/Feature/SplashScreen/UI/PreviewContent/SplashScreenState.SplashScreenSettings+preview.swift
+++ swift-vortex/Sources/Feature/SplashScreen/UI/PreviewContent/SplashScreenState.SplashScreenSettings+preview.swift
@@ -18,8 +18,46 @@ extension SplashScreenState.Settings {
 
 extension SplashScreenState.Settings.Shadow {
     
-    static let logo:    Self = .init(color: .black, opacity: 1, radius: 64, x: 0, y: 4)
+    static let logo:    Self = .init(color: .init(hex: "FF0000"), opacity: 0.5, radius: 10, x: 10, y: 0)
     static let text:    Self = .init(color: .black, opacity: 1, radius: 12, x: 0, y: 4)
     static let subtext: Self = .init(color: .black, opacity: 1, radius: 12, x: 0, y: 4)
-    static let footer:  Self = .init(color: .black, opacity: 1, radius: 4, x: 0, y: 4)
+    static let footer:  Self = .init(color: .black, opacity: 1, radius: 0, x: 20, y: 20)
+}
+
+import SwiftUI
+
+extension Color {
+    
+    init(hex: String) {
+        
+        let hex = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+            .trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        
+        var int: UInt64 = 0
+        guard Scanner(string: hex).scanHexInt64(&int) else {
+            self = Color.clear
+            return
+        }
+        
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit) -> Expand to 24-bit
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            self = Color.clear
+            return
+        }
+        
+        self.init(
+            .sRGB,
+            red: Double(r) / 255.0,
+            green: Double(g) / 255.0,
+            blue: Double(b) / 255.0,
+            opacity: Double(a) / 255.0
+        )
+    }
 }
