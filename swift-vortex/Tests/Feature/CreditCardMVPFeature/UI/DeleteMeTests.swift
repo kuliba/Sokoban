e042a3c8927ff5c532ed0889b71122794b88362d
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-03-18 17:54:54 +0300

    add files and folders scaffolding for `CreditCardMVPFeature`

---
+++ swift-vortex/Tests/Feature/CreditCardMVPFeature/UI/DeleteMeTests.swift
@@ -0,0 +1,18 @@
+//
+//  DeleteMeTests.swift
+//
+//
+//  Created by Igor Malyarov on 18.03.2025.
+//
+
+import CreditCardMVPUI
+import XCTest
+
+final class DeleteMeTests: XCTestCase {
+    
+    func test() {
+        
+        _ = DeleteMe()
+        XCTFail()
+    }
+}
