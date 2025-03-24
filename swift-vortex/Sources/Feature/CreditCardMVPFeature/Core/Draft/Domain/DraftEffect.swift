594b95f461acfdd06cdbfcf8a371b4448216a636
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-03-24 18:43:02 +0300

    add `ConsentEffect`

--- swift-vortex/Sources/Feature/CreditCardMVPFeature/Core/Draft/Domain/DraftEffect.swift
+++ swift-vortex/Sources/Feature/CreditCardMVPFeature/Core/Draft/Domain/DraftEffect.swift
@@ -1,17 +1,21 @@
 //
 //  DraftEffect.swift
-//  
+//
 //
 //  Created by Igor Malyarov on 23.03.2025.
 //
 
-/// Represents an effect triggered by processing a verification event.
+/// Represents an effect triggered by processing a draft event.
 ///
+/// - Parameter ConsentEffect: the type of effect produced by consent events.
 /// - Parameter VerificationEffect: the type of effect produced by verification events.
-public enum DraftEffect<VerificationEffect> {
+public enum DraftEffect<ConsentEffect, VerificationEffect> {
+    
+    /// A consent-related effect.
+    case consent(ConsentEffect)
     
     /// A verification-related effect.
     case verification(VerificationEffect)
 }
 
-extension DraftEffect: Equatable where VerificationEffect: Equatable {}
+extension DraftEffect: Equatable where ConsentEffect: Equatable, VerificationEffect: Equatable {}
