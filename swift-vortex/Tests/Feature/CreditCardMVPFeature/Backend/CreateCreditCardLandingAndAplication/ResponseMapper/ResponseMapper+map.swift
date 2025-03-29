3b2b374628add5a18a1b349c50825d8058715046
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-03-29 11:13:11 +0300

    add initial `ResponseMapper_mapCreateCreditCardLandingAndApplicationResponseTests`

---
+++ swift-vortex/Tests/Feature/CreditCardMVPFeature/Backend/CreateCreditCardLandingAndAplication/ResponseMapper/ResponseMapper+map.swift
@@ -0,0 +1,555 @@
+//
+//  ResponseMapper+mapCreateCreditCardLandingAndApplicationResponseTests.swift
+//
+//
+//  Created by Igor Malyarov on 29.03.2025.
+//
+
+extension ResponseMapper {
+    
+    struct CreateCreditCardLandingAndApplication: Equatable {
+        
+        let theme: String
+        let header: Header?
+    }
+}
+
+extension ResponseMapper.CreateCreditCardLandingAndApplication {
+    
+    struct Header: Equatable {
+        
+        let title: String
+        let subtitle: String
+    }
+}
+
+extension ResponseMapper {
+    
+    static func mapCreateCreditCardLandingAndApplicationResponse(
+        _ data: Data,
+        _ httpURLResponse: HTTPURLResponse
+    ) -> MappingResult<CreateCreditCardLandingAndApplication> {
+        
+        map(data, httpURLResponse, mapOrThrow: CreateCreditCardLandingAndApplication.init(data:))
+    }
+}
+
+private extension ResponseMapper.CreateCreditCardLandingAndApplication {
+    
+    init(data: ResponseMapper._DTO) throws {
+        
+        guard let theme = data.theme
+        else { throw ResponseFailure() }
+        
+        self.init(
+            theme: theme,
+            header: data._header
+        )
+    }
+    
+    struct ResponseFailure: Error {}
+}
+
+// MARK: - Mapping
+
+private extension ResponseMapper._DTO {
+    
+    var _header: ResponseMapper.CreateCreditCardLandingAndApplication.Header? {
+        
+        guard let title = header?.title,
+              let subtitle = header?.subtitle
+        else { return nil }
+        
+        return .init(title: title, subtitle: subtitle)
+    }
+}
+
+// MARK: - DTO
+
+private extension ResponseMapper {
+    
+    struct _DTO: Decodable {
+        
+        let theme: String?
+        let header: _Header?
+    }
+}
+
+extension ResponseMapper._DTO {
+    
+    struct _Header: Decodable {
+        
+        let title: String?
+        let subtitle: String?
+    }
+}
+
+import CreditCardMVPBackend
+import RemoteServices
+import XCTest
+
+final class ResponseMapper_mapCreateCreditCardLandingAndApplicationResponseTests: XCTestCase {
+    
+    func test_map_shouldDeliverInvalidFailure_onEmptyData() {
+        
+        let emptyData: Data = .empty
+        
+        XCTAssertNoDiff(
+            map(emptyData),
+            .failure(.invalid(statusCode: 200, data: emptyData))
+        )
+    }
+    
+    func test_map_shouldDeliverInvalidFailure_onInvalidData() {
+        
+        assert(json: .invalidData, delivers: .invalid(
+            statusCode: 200,
+            data: .invalidData
+        ))
+    }
+    
+    func test_map_shouldDeliverInvalidFailure_onEmptyJSON() {
+        
+        assert(json: .emptyJSON, delivers: .invalid(
+            statusCode: 200,
+            data: .emptyJSON
+        ))
+    }
+    
+    func test_map_shouldDeliverInvalidFailure_onEmptyDataResponse() {
+        
+        assert(json: .emptyDataResponse, delivers: .invalid(
+            statusCode: 200,
+            data: .emptyDataResponse
+        ))
+    }
+    
+    func test_map_shouldDeliverInvalidFailure_onNullServerResponse() {
+        
+        assert(json: .nullServerResponse, delivers: .invalid(
+            statusCode: 200,
+            data: .nullServerResponse
+        ))
+    }
+    
+    func test_map_shouldDeliverServerError_onServerError() {
+        
+        assert(json: .serverError, delivers: .server(
+            statusCode: 102,
+            errorMessage: "Возникла техническая ошибка"
+        ))
+    }
+    
+    func test_map_shouldDeliverInvalidFailure_onNonOkHTTPResponse() {
+        
+        for statusCode in [199, 201, 399, 400, 401, 404] {
+            
+            let json = String.validData()
+            let validData = Data(json.utf8)
+            let nonOkResponse = anyHTTPURLResponse(statusCode: statusCode)
+            
+            assert(
+                json: json,
+                httpURLResponse: nonOkResponse,
+                delivers: .invalid(statusCode: statusCode, data: validData)
+            )
+        }
+    }
+    
+    func test_map_shouldDeliverResponse_onValidData() throws {
+        
+        assert(json: .validData(), delivers: makeResponse())
+    }
+    
+    func test_map_shouldDeliverResponseWithTheme() throws {
+        
+        let theme = anyMessage()
+        
+        assert(
+            json: .validData(theme: theme),
+            delivers: makeResponse(theme: theme)
+        )
+    }
+    
+    func test_map_shouldDeliverResponseWithHeaderTitle() throws {
+        
+        let headerTitle = anyMessage()
+        
+        assert(
+            json: .validData(headerTitle: headerTitle),
+            delivers: makeResponse(headerTitle: headerTitle)
+        )
+    }
+    
+    func test_map_shouldDeliverResponseWithHeaderSubtitle() throws {
+        
+        let headerSubtitle = anyMessage()
+        
+        assert(
+            json: .validData(headerSubtitle: headerSubtitle),
+            delivers: makeResponse(headerSubtitle: headerSubtitle)
+        )
+    }
+    
+    func test_map_shouldDeliverResponseWithoutHeader_onMissingHeaderTitle() throws {
+        
+        assert(
+            json: .withMissingHeaderTitle,
+            delivers: makeResponse(header: nil)
+        )
+    }
+    
+    // MARK: - Helpers
+    
+    private typealias Response = ResponseMapper.CreateCreditCardLandingAndApplication
+    private typealias MappingResult = ResponseMapper.MappingResult<Response>
+    
+    private func map(
+        _ data: Data,
+        _ httpURLResponse: HTTPURLResponse = anyHTTPURLResponse()
+    ) -> MappingResult {
+        
+        ResponseMapper.mapCreateCreditCardLandingAndApplicationResponse(data, httpURLResponse)
+    }
+    
+    private func makeResponse(
+        theme: String = "DEFAULT",
+        header: Response.Header?
+    ) -> Response {
+        
+        return .init(
+            theme: theme,
+            header: header
+        )
+    }
+    
+    private func makeResponse(
+        theme: String = "DEFAULT",
+        headerTitle: String = "Кредитная карта",
+        headerSubtitle: String = "«Все включено»"
+    ) -> Response {
+        
+        return .init(
+            theme: theme,
+            header: .init(title: headerTitle, subtitle: headerSubtitle)
+        )
+    }
+    
+    private func assert(
+        json: String,
+        httpURLResponse: HTTPURLResponse = anyHTTPURLResponse(),
+        delivers response: Response,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        XCTAssertNoDiff(map(.init(json.utf8), httpURLResponse), .success(response), file: file, line: line)
+    }
+    
+    private func assert(
+        json: String,
+        httpURLResponse: HTTPURLResponse = anyHTTPURLResponse(),
+        delivers failure: ResponseMapper.MappingError,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        XCTAssertNoDiff(map(.init(json.utf8), httpURLResponse), .failure(failure), file: file, line: line)
+    }
+}
+
+private extension Data {
+    
+    static let empty: Data = .init()
+    static let invalidData: Data = String.invalidData.json
+    static let emptyJSON: Data = String.emptyJSON.json
+    static let emptyDataResponse: Data = String.emptyDataResponse.json
+    static let nullServerResponse: Data = String.nullServerResponse.json
+    static let serverError: Data = String.serverError.json
+}
+
+private extension String {
+    
+    var json: Data { .init(self.utf8) }
+    
+    static let invalidData = """
+{
+    "statusCode": 102,
+    "errorMessage": null,
+    "data": { "a": "junk" }
+}
+"""
+    
+    static let emptyJSON = "{}"
+    
+    static let emptyDataResponse = """
+{
+    "statusCode": 102,
+    "errorMessage": null,
+    "data": {}
+}
+"""
+    
+    static let nullServerResponse = """
+{
+    "statusCode": 0,
+    "errorMessage": null,
+    "data": null
+}
+"""
+    
+    static let serverError = """
+{
+    "statusCode": 102,
+    "errorMessage": "Возникла техническая ошибка",
+    "data": null
+}
+"""
+    
+    static func validData(
+        theme: String = "DEFAULT",
+        headerTitle: String = "Кредитная карта",
+        headerSubtitle: String = "«Все включено»"
+    ) -> String {
+        
+"""
+{
+  "statusCode": 0,
+  "errorMessage": null,
+  "data": {
+    "theme": "\(theme)",
+    "header": {
+      "title": "\(headerTitle)",
+      "subtitle": "\(headerSubtitle)"
+    },
+    "banner": {
+      "background": "dict/getProductCatalogImage?image=products/pages/order-credit-card/landing/images/digital_card_landing_bg.png",
+      "highlightedOfferConditions": [
+        "Вам одобрена сумма 97 000 ₽",
+        "Предложение действует до 30.04.2025"
+      ]
+    },
+    "offerConditions": {
+      "title": "Персональное предложение",
+      "list": [
+        {
+          "md5hash": "b6fa019f307d6a72951ab7268708aa15",
+          "title": "Процентная ставка",
+          "subTitle": "6,5 % годовых"
+        },
+        {
+          "md5hash": "b6fa019f307d6a72951ab7268708aa15",
+          "title": "Срок льготного периода",
+          "subTitle": "До 62 дней"
+        },
+        {
+          "md5hash": "b6fa019f307d6a72951ab7268708aa15",
+          "title": "Стоимость обслуживания",
+          "subTitle": "Бесплатно навсегда"
+        },
+        {
+          "md5hash": "b6fa019f307d6a72951ab7268708aa15",
+          "title": "Оформление",
+          "subTitle": "В отделении Банка"
+        }
+      ]
+    },
+    "frequentlyAskedQuestions": {
+      "title": "Часто задаваемые вопросы",
+      "list": [
+        {
+          "title": "Как повторно подключить подписку?",
+          "description": "тест"
+        },
+        {
+          "title": "Как начисляются проценты?",
+          "description": "тесттесттесттесттесттесттесттест"
+        },
+        {
+          "title": "Какие условия бесплатного обслуживания?",
+          "description": ""
+        }
+      ]
+    },
+    "consent": {
+      "terms": "https://www.forabank.ru/dkbo/dkbo.pdf",
+      "tariffs": "https://www.forabank.ru/tarify/",
+      "creditHistoryRequest": "https://www.forabank.ru/user-upload/dok-dbo-fl/coglasie-na-zapros-v-bki.pdf"
+    },
+    "application": {
+      "id": 123456789,
+      "status": "DRAFT"
+    },
+    "offer": {
+      "id": "123",
+      "gracePeriod": "36",
+      "tarifPlanRate": "69.99",
+      "offerPeriodValidity": "2025-03-31",
+      "offerLimitAmount": "10000.00",
+      "tarifPlanName": "ТП1",
+      "icon": "37baa2ff94fb468f65fa0ea4017bf44a"
+    }
+  }
+}
+"""
+    }
+    
+    static let withMissingHeaderTitle = """
+{
+  "statusCode": 0,
+  "errorMessage": null,
+  "data": {
+    "theme": "DEFAULT",
+    "header": {
+      "subtitle": "«Все включено»"
+    },
+    "banner": {
+      "background": "dict/getProductCatalogImage?image=products/pages/order-credit-card/landing/images/digital_card_landing_bg.png",
+      "highlightedOfferConditions": [
+        "Вам одобрена сумма 97 000 ₽",
+        "Предложение действует до 30.04.2025"
+      ]
+    },
+    "offerConditions": {
+      "title": "Персональное предложение",
+      "list": [
+        {
+          "md5hash": "b6fa019f307d6a72951ab7268708aa15",
+          "title": "Процентная ставка",
+          "subTitle": "6,5 % годовых"
+        },
+        {
+          "md5hash": "b6fa019f307d6a72951ab7268708aa15",
+          "title": "Срок льготного периода",
+          "subTitle": "До 62 дней"
+        },
+        {
+          "md5hash": "b6fa019f307d6a72951ab7268708aa15",
+          "title": "Стоимость обслуживания",
+          "subTitle": "Бесплатно навсегда"
+        },
+        {
+          "md5hash": "b6fa019f307d6a72951ab7268708aa15",
+          "title": "Оформление",
+          "subTitle": "В отделении Банка"
+        }
+      ]
+    },
+    "frequentlyAskedQuestions": {
+      "title": "Часто задаваемые вопросы",
+      "list": [
+        {
+          "title": "Как повторно подключить подписку?",
+          "description": "тест"
+        },
+        {
+          "title": "Как начисляются проценты?",
+          "description": "тесттесттесттесттесттесттесттест"
+        },
+        {
+          "title": "Какие условия бесплатного обслуживания?",
+          "description": ""
+        }
+      ]
+    },
+    "consent": {
+      "terms": "https://www.forabank.ru/dkbo/dkbo.pdf",
+      "tariffs": "https://www.forabank.ru/tarify/",
+      "creditHistoryRequest": "https://www.forabank.ru/user-upload/dok-dbo-fl/coglasie-na-zapros-v-bki.pdf"
+    },
+    "application": {
+      "id": 123456789,
+      "status": "DRAFT"
+    },
+    "offer": {
+      "id": "123",
+      "gracePeriod": "36",
+      "tarifPlanRate": "69.99",
+      "offerPeriodValidity": "2025-03-31",
+      "offerLimitAmount": "10000.00",
+      "tarifPlanName": "ТП1",
+      "icon": "37baa2ff94fb468f65fa0ea4017bf44a"
+    }
+  }
+}
+"""
+    
+    static let _source = """
+{
+  "statusCode": 0,
+  "errorMessage": null,
+  "data": {
+    "theme": "DEFAULT",
+    "header": {
+      "title": "Кредитная карта",
+      "subtitle": "«Все включено»"
+    },
+    "banner": {
+      "background": "dict/getProductCatalogImage?image=products/pages/order-credit-card/landing/images/digital_card_landing_bg.png",
+      "highlightedOfferConditions": [
+        "Вам одобрена сумма 97 000 ₽",
+        "Предложение действует до 30.04.2025"
+      ]
+    },
+    "offerConditions": {
+      "title": "Персональное предложение",
+      "list": [
+        {
+          "md5hash": "b6fa019f307d6a72951ab7268708aa15",
+          "title": "Процентная ставка",
+          "subTitle": "6,5 % годовых"
+        },
+        {
+          "md5hash": "b6fa019f307d6a72951ab7268708aa15",
+          "title": "Срок льготного периода",
+          "subTitle": "До 62 дней"
+        },
+        {
+          "md5hash": "b6fa019f307d6a72951ab7268708aa15",
+          "title": "Стоимость обслуживания",
+          "subTitle": "Бесплатно навсегда"
+        },
+        {
+          "md5hash": "b6fa019f307d6a72951ab7268708aa15",
+          "title": "Оформление",
+          "subTitle": "В отделении Банка"
+        }
+      ]
+    },
+    "frequentlyAskedQuestions": {
+      "title": "Часто задаваемые вопросы",
+      "list": [
+        {
+          "title": "Как повторно подключить подписку?",
+          "description": "тест"
+        },
+        {
+          "title": "Как начисляются проценты?",
+          "description": "тесттесттесттесттесттесттесттест"
+        },
+        {
+          "title": "Какие условия бесплатного обслуживания?",
+          "description": ""
+        }
+      ]
+    },
+    "consent": {
+      "terms": "https://www.forabank.ru/dkbo/dkbo.pdf",
+      "tariffs": "https://www.forabank.ru/tarify/",
+      "creditHistoryRequest": "https://www.forabank.ru/user-upload/dok-dbo-fl/coglasie-na-zapros-v-bki.pdf"
+    },
+    "application": {
+      "id": 123456789,
+      "status": "DRAFT"
+    },
+    "offer": {
+      "id": "123",
+      "gracePeriod": "36",
+      "tarifPlanRate": "69.99",
+      "offerPeriodValidity": "2025-03-31",
+      "offerLimitAmount": "10000.00",
+      "tarifPlanName": "ТП1",
+      "icon": "37baa2ff94fb468f65fa0ea4017bf44a"
+    }
+  }
+}
+"""
+}
