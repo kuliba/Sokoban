ada9283f7e153f3dc3362a975f67c4cc0ab4b5f2
Igor Malyarov <malyarov_i_a@forabank.ru>
2025-04-01 11:52:21 +0300

    use PreferenceKey to improve geometry reading

--- Vortex/Vortex/View/PaymentsTransfers/Templates/TemplatesListView.swift
+++ Vortex/Vortex/View/PaymentsTransfers/Templates/TemplatesListView.swift
@@ -29,7 +29,9 @@ struct TemplatesListView: View {
     
     var body: some View {
         
-        GeometryReader { geo in
+        ZStack {
+            
+            geometryReader()
             
             mainVStack()
                 .ignoresSafeArea(.container, edges: .bottom)
@@ -41,14 +43,46 @@ struct TemplatesListView: View {
                     dismiss: { viewModel.route.modal = nil },
                     bottomSheetContent: bottomSheetContent
                 )
-                .onAppear { height = geo.size.height }
-                .onChange(of: geo.size.height) { height = $0 }
         }
-        .overlay(content: spinnerView)
+    }
+}
+
+struct HeightPreferenceKey: PreferenceKey {
+    
+    static var defaultValue: CGFloat = 0
+    static func reduce(
+        value: inout CGFloat,
+        nextValue: () -> CGFloat
+    ) {
+        value = nextValue()
     }
 }
 
 private extension TemplatesListView {
+    
+    func geometryReader() -> some View {
+        
+        GeometryReader { geo in
+            
+            Color.clear
+                .preference(
+                    key: HeightPreferenceKey.self,
+                    value: geo.size.height
+                )
+        }
+        .onPreferenceChange(
+            HeightPreferenceKey.self,
+            perform: updateIfSignificant
+        )
+    }
+    
+    func updateIfSignificant(
+        _ newHeight: CGFloat
+    ) {
+        if abs(newHeight - height) > 1 {
+            height = newHeight
+        }
+    }
     
     var isLoading: Bool {
         
@@ -101,7 +135,7 @@ private extension TemplatesListView {
             
             switch viewModel.state {
             case .loading:
-                Color.clear
+                spinnerView()//Color.clear
                 
             case .normal, .select:
                 
