42b26b466651be3980e4db5b332b77c111e4d929
Valentin Ozerov <ozerov_v_v@forabank.ru>
2025-05-06 13:14:32 +0300

    Initial Commit

---
+++ Sokoban/SokobanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SokobanApp.swift
+//  Sokoban
+//
+//  Created by Valentin Ozerov on 06.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SokobanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
