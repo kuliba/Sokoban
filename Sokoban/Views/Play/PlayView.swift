dab8b9e014873c5dd5988e6db5ae0aa1d4aa9879
Valentin Ozerov <kuliba@gmail.com>
2025-05-07 18:40:40 +0300

    Add PlayView+Model

---
+++ Sokoban/Views/Play/PlayView.swift
@@ -0,0 +1,8 @@
+//
+//  PlayView.swift
+//  Sokoban
+//
+//  Created by Valentin Ozerov on 07.05.2025.
+//
+
+import Foundation
---
+++ Sokoban/Views/Play/PlayViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlayViewModel.swift
+//  Sokoban
+//
+//  Created by Valentin Ozerov on 07.05.2025.
+//
+
+import Foundation
